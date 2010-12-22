ae7cc0ccc98280f56b9a7705d47fb58b549a23c6
wangzheng <none@gmail.com>
2010-12-23 00:03:06 +0800

    delete unuse files

--- cache/class_path.cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'Router' => 'vendor/hf/lib/Router.php',
-  'Action' => 'vendor/hf/lib/Action.php',
-  'View' => 'vendor/hf/lib/View.php',
-  'ErrorHandler' => 'vendor/hf/lib/ErrorHandler.php',
-  'HomeAction' => 'app/Home/HomeAction.php',
-  'HomeScreen' => 'app/Home/HomeScreen.php',
-);
