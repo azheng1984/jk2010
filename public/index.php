a95ef80e97a138bc09d698cd48be4d96ec9fa215
wangzheng <none@gmail.com>
2010-12-31 15:28:47 +0800

    -

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ define('SITE_DIR', dirname(dirname(__FILE__)).'/');
 require SITE_DIR.'vendor/hf/lib/ClassLoader.php';
 ClassLoader::run();
 ClassLoader::import('hf');
+ClassLoader::import('self');
 
 $app = Router::run();
 Action::run($app);
