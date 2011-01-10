c9b44d32dd5e7f08a24065522b3b91e8fc205100
wangzheng <none@gmail.com>
2011-01-10 16:39:08 +0800

    -

---
+++ cache/ClassLoader.cache.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+  'HomeAction' => 'app/home',
+  'HomeScreen' => 'app/home',
+  'NotFoundScreen' => 'app/error/not_found',
+  'InternalServerErrorScreen' => 'app/error/internal_server_error',
+  'Router' => 'lib',
+  'ActionProcessor' => 'vendor/hf/lib/Processor',
+  'ViewProcessor' => 'vendor/hf/lib/Processor',
+  'Application' => 'vendor/hf/lib',
+  'ErrorHandler' => 'vendor/hf/lib',
+  'ApplicationException' => 'vendor/hf/lib/Exception',
+  'InternalServerErrorException' => 'vendor/hf/lib/Exception',
+  'NotFoundException' => 'lib/Exception',
+  'UnsupportedMediaTypeException' => 'vendor/hf/lib/Exception',
+  'MethodNotAllowedException' => 'vendor/hf/lib/Exception',
+);
