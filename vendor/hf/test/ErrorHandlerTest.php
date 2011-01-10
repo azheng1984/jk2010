b6baea200eaf6d4430f827c13c08507edd234930
azheng1984 <azheng1984@126.com>
2011-01-10 23:07:45 +0800

    -

--- vendor/hf/test/ErrorHandlerTest.php
+++ vendor/hf/test/ErrorHandlerTest.php
@@ -1,27 +1,36 @@
 <?php
 define('SITE_PATH', dirname(dirname(__FILE__)).'/');
 require SITE_PATH.'lib/ErrorHandler.php';
+require SITE_PATH.'lib/Exception/ApplicationException.php';
+require SITE_PATH.'lib/Exception/NotFoundException.php';
 
 class ErrorHandlerTest extends PHPUnit_Framework_TestCase
 {
-  protected function setUp()
+  public function testHandleException()
   {
-
+    $app = $this->getMock('Application', array('run'));
+    $app->expects($this->once())->method('run')->with($this->equalTo('error/internal_server_error'));
+    $handler = new ErrorHandler($app);
+    $exception = new Exception;
+    try {
+      $handler->handle($exception);
+    } catch (PHPUnit_Framework_Error $ex) {
+      $this->assertEquals(ErrorHandler::getException(), $exception);
+      return;
+    }
+    $this->fail();
   }
 
-  /**
-   * @expectedException Exception
-   */
-  public function testRender()
+  public function testHandleNotFoundException()
   {
-    throw new Exception;
-  }
-
-  /**
-   * @expectedException PHPUnit_Framework_Error
-   */
-  public function testGetException()
-  {
-    $a = $b;
+    $app = $this->getMock('Application', array('run'));
+    $app->expects($this->once())->method('run')->with($this->equalTo('error/not_found'));
+    $handler = new ErrorHandler($app);
+    $exception = new NotFoundException;
+    try {
+      $handler->handle($exception);
+    } catch (PHPUnit_Framework_Error $ex) {
+      $this->assertEquals(ErrorHandler::getException(), $exception);
+    }
   }
 }
