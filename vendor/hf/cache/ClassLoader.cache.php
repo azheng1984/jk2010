a7de7b8a81e2960b291a168964e94f34d87a39f8
azheng1984 <azheng1984@126.com>
2011-01-11 21:58:58 +0800

    -

---
+++ vendor/hf/cache/ClassLoader.cache.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+  'ActionProcessor' => 'lib/Processor',
+  'ViewProcessor' => 'lib/Processor',
+  'Application' => 'lib',
+  'ErrorHandler' => 'lib',
+  'ApplicationException' => 'lib/Exception',
+  'InternalServerErrorException' => 'lib/Exception',
+  'NotFoundException' => 'lib/Exception',
+  'UnsupportedMediaTypeException' => 'lib/Exception',
+  'MethodNotAllowedException' => 'lib/Exception',
+);
