9eefc3ae6cb874058c7abaefece528dc5f5a85b5
azheng1984 <azheng1984@126.com>
2011-01-13 13:53:39 +0800

    -

--- vendor/hf/tool/public/index.php
+++ vendor/hf/tool/public/index.php
@@ -1,1 +1,10 @@
 <?php
+define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
+define('HF_CACHE_PATH', ROOT_PATH.'cache/');
+require ROOT_PATH.'vendor/hf/lib/ClassLoader.php';
+$classLoader = new ClassLoader;
+$classLoader->run();
+$errorHandler = new ErrorHandler(new Application(new ViewProcessor));
+$errorHandler->run();
+$app = new Application(new ActionProcessor, new ViewProcessor);
+$app->run();
