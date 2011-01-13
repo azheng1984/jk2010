77e88f180d1b3df2a747de7faa938dc53b5e385b
azheng1984 <azheng1984@126.com>
2011-01-13 13:10:04 +0800

    -

--- vendor/hf/test/cache/ClassLoader.cache.php
+++ vendor/hf/test/cache/ClassLoader.cache.php
@@ -1,12 +1,28 @@
 <?php
-return array(
-  'ActionProcessor' => 'lib/Processor',
-  'ViewProcessor' => 'lib/Processor',
-  'Application' => 'lib',
-  'ErrorHandler' => 'lib',
-  'ApplicationException' => 'lib/Exception',
-  'InternalServerErrorException' => 'lib/Exception',
-  'NotFoundException' => 'lib/Exception',
-  'UnsupportedMediaTypeException' => 'lib/Exception',
-  'MethodNotAllowedException' => 'lib/Exception',
+return array (
+array (
+    'HomeAction' => 0,
+    'HomeScreen' => 0,
+    'NotFoundScreen' => 1,
+    'InternalServerErrorScreen' => 2,
+    'Router' => 3,
+    'ActionProcessor' => 4,
+    'ViewProcessor' => 4,
+    'Application' => 6,
+    'ErrorHandler' => 6,
+    'ApplicationException' => 5,
+    'InternalServerErrorException' => 5,
+    'NotFoundException' => 5,
+    'UnsupportedMediaTypeException' => 5,
+    'MethodNotAllowedException' => 5,
+),
+array (
+0 => '/home/wz/www/jk2010/app/home',
+1 => '/home/wz/www/jk2010/app/error/not_found',
+2 => '/home/wz/www/jk2010/app/error/internal_server_error',
+3 => '/home/wz/www/jk2010/lib',
+4 => '/home/wz/www/jk2010/vendor/hf/lib/Processor',
+5 => '/home/wz/www/jk2010/vendor/hf/lib/Exception',
+6 => '/home/wz/www/jk2010/vendor/hf/lib'
+),
 );
