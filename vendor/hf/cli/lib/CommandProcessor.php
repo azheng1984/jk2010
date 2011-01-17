02903123a460e3aeb705bc663f437601b1163b6f
azheng1984 <azheng1984@126.com>
2011-01-17 18:18:12 +0800

    -

---
+++ vendor/hf/cli/lib/CommandProcessor.php
@@ -0,0 +1,16 @@
+<?php
+class CommandProcessor {
+  private $argc;
+  private $argv;
+
+  public function __construct($argc, $argv) {
+    $this->argc = $argc;
+    $this->argv = $argv;
+  }
+
+  public function run($name) {
+    require_once CLI_TOOL_PATH."app/new/$name.php";
+    $command = new $name;
+    $command->run($this->argc, $this->argv);
+  }
+}
--- vendor/hf/tool/cli/hf.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
-define('HF_PATH', dirname(ROOT_PATH).'/');
-define('HF_CACHE_PATH', ROOT_PATH.'cache/');
-require HF_PATH.'lib/ClassLoader.php';
-$classLoader = new ClassLoader;
-$classLoader->run();
-
