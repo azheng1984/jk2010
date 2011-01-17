f999cfa61cadffc3daadf6bcc7a78dba532077e9
wangzheng <none@gmail.com>
2011-01-17 18:53:23 +0800

    -

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
 define('HF_CACHE_PATH', ROOT_PATH.'cache/vendor/hf/');
 define('HF_CONFIG_PATH', ROOT_PATH.'config/vendor/hf/');
 
-require ROOT_PATH.'vendor/hf/lib/ClassLoader.php';
+require ROOT_PATH.'vendor/hf/core/lib/ClassLoader.php';
 $classLoader = new ClassLoader;
 //$classLoader->run();
 $errorHandler = new ErrorHandler(new Application(new ViewProcessor));
