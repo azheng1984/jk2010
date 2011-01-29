ee8dfea317be16d414bcacb305ef392869e91d69
unknown <zjfcwangzheng@126.com>
2011-01-29 23:15:47 +0800

    -

--- vendor/hf/core/lib/Exception/ApplicationException.php
+++
@@ -1,8 +0,0 @@
-<?php
-abstract class ApplicationException extends Exception {
-  public function __construct($message, $code) {
-    parent::__construct($message);
-    $this->code = $code;
-    header("HTTP/1.1 $code");
-  }
-}
