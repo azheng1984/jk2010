aedcbf4d3d1ff45fe1e25075fcb61f43b4861f12
wangzheng <none@gmail.com>
2011-01-31 19:58:41 +0800

    -

--- vendor/hf/tool/cli/lib/Command/NewCommand.php
+++ vendor/hf/tool/cli/lib/Command/NewCommand.php
@@ -1,6 +1,6 @@
 <?php
 class NewCommand {
-  public function execute($context) {
+  public function execute($context, $arg1, $arg2 = null) {
     $color = $context->getOption('color'); //if null throw exception
   }
 }
