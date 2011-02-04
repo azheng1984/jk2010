42eb59721c4ce725e22a26edebacb040b13ca184
wangzheng <none@gmail.com>
2011-02-05 00:59:55 +0800

    -

--- vendor/hf/cli/lib/CommandParser.php
+++ vendor/hf/cli/lib/CommandParser.php
@@ -5,25 +5,28 @@ class CommandParser {
   private $arguments;
   private $argumentCount;
   private $currentIndex = 1;
-  private $commandClass;
   private $commandArguments = array();
 
   public function run() {
     $this->argumentCount = $_SERVER['argc'];
     $this->arguments = $_SERVER['argv'];
-    $this->setConfig(require HF_CONFIG_PATH.__CLASS__.'.config.php');
-    if (isset($this->config['class'])) {
-      $this->commandClass = new $this->config['class'];
-    }
+    $this->readConfig(require HF_CONFIG_PATH.__CLASS__.'.config.php');
     while ($this->currentIndex < $this->argumentCount) {
       $this->parse();
       ++$this->currentIndex;
     }
-    if ($this->commandClass == null) {
+    if ($this->config['class'] == null) {
       throw new Exception('Command not found');
     }
-    $function = array(new $this->commandClass, 'execute');
-    call_user_func_array($function, $this->commandArguments);
+    $class = $this->config['class'];
+    $isVariableLength = false;
+    if (in_array('variable_length', $this->config)) {
+      $isVariableLength = true;
+    }
+    $reflector = new ReflectionMethod($class, 'execute');
+    $length = count($this->commandArguments);
+    $this->verifyArguments($reflector, $length, $isVariableLength);
+    $reflector->invokeArgs(new $class, $this->commandArguments);
   }
 
   private function parse() {
@@ -32,7 +35,7 @@ class CommandParser {
       $this->buildOption($item);
       return;
     }
-    if ($this->commandClass == null) {
+    if (!isset($this->config['class'])) {
       $this->buildCommand($item);
       return;
     }
@@ -41,7 +44,7 @@ class CommandParser {
 
   private function buildOption($item) {
     $orignalKey = $item;
-    $value = true;
+    $value = null;
     $pieces = explode('=', $item, 2);
     if (count($pieces) == 2) {
       $orignalKey = $pieces[0];
@@ -56,11 +59,9 @@ class CommandParser {
       $this->expand($this->config['option'][$name]['expansion']);
       return;
     }
-    if ($value === true && isset($this->config['option']['default'])) {
-      $value = $this->config['option']['default'];
-    }
     if (isset($this->config['option'][$name]['class'])) {
-      $value = new $this->config['option'][$name]['class']($value);
+      $class = $this->config['option'][$name]['class'];
+      $value = $this->buildOptionObject($class, $value);
     }
     $_ENV['context']->addOption($name, $value);
   }
@@ -72,21 +73,30 @@ class CommandParser {
     return substr($orignalKey, 2);
   }
 
+  private function buildOptionObject($class, $argument) {
+    $reflector = new ReflectionClass($class);
+    $constructor = $reflector->getConstructor();
+    if ($constructor == null && $argument !== null) {
+      throw new Exception;
+    }
+    if ($constructor != null) {
+      $this->verifyArguments($constructor, $argument === null ? 0 : 1, false);
+    }
+    return $argument === null ? new $class : new $class($argument);
+  }
+
   private function buildCommand($item) {
     if (!isset($this->config['command'][$item])) {
       throw new Exception("Command '$item' not found");
     }
     $config = $this->config['command'][$item];
     if (!is_array($config)) {
-      $this->commandClass = $config;
-      $this->setConfig(array('option' => array()));
-      return;
+      $config = (array('class' => $config, 'option' => array()));
     }
-    $this->commandClass = $this->config['class'];
-    $this->setConfig($config);
+    $this->readConfig($config);
   }
 
-  private function setConfig($value) {
+  private function readConfig($value) {
     $this->config = $value;
     foreach ($this->config['option'] as $key => $value) {
       if (!isset($value['short'])) {
@@ -107,6 +117,21 @@ class CommandParser {
     array_splice($this->arguments, $this->currentIndex, 1, $arguments);
     $this->argumentCount = count($this->arguments);
     --$this->currentIndex;
+  }
+
+  private function verifyArguments($reflector, $length, $isVariableLength) {
+    foreach ($reflector->getParameters() as $parameter) {
+      if ($parameter->isOptional() && $length == 0) {
+        break;
+      }
+      --$length;
+    }
+    if ($length < 0) {
+      throw new Exception;
+    }
+    if ($length > 0 && $isVariableLength == false) {
+      throw new Exception;
+    }
   }
 
   private function startsWith($haystack, $needle){
