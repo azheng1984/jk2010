ce2788a401ff5b1ba9780be29d6ead06a24cbf65
unknown <zjfcwangzheng@126.com>
2011-02-09 17:36:57 +0800

    -

--- vendor/hf/cli/lib/OptionParser.php
+++ vendor/hf/cli/lib/OptionParser.php
@@ -1,9 +1,14 @@
 <?php
 class OptionParser {
+  private $reader;
   private $config;
-  private $shorts;
+  private $isCommandFound;
+  private $shorts = array();
 
-  public function __construct($config) {
+  public function __construct($reader, $config, $isCommandFound) {
+    $this->reader = $reader;
+    $this->config = $config;
+    $this->isCommandFound = $isCommandFound;
     foreach ($config as $key => $value) {
       if (!isset($value['short'])) {
         continue;
@@ -17,15 +22,14 @@ class OptionParser {
         $this->shorts[$item] = $key;
       }
     }
-    $this->config = $config;
   }
 
-  private function run($reader) {
-    $item = $reader ->getItem();
-    $name = $this->getOptionName($item);
+  public function run() {
+    $item = $this->reader->getItem();
+    $name = $this->getName($this->reader->getItem());
     if (is_array($name)) {
-      $$reader->expand($name);
-      return false;
+      $this->reader->expand($name);
+      return;
     }
     if (!isset($this->config[$name])
      && !in_array($name, $this->config, true)) {
@@ -33,27 +37,27 @@ class OptionParser {
     }
     if (isset($this->config[$name]['expansion'])) {
       $this->reader->expand($this->config[$name]['expansion']);
-      return false;
+      return;
     }
     $value = null;
     if (isset($this->config[$name]['class'])) {
-      $isInfiniteLength = false;
-      if (in_array('infinite_length', $this->config[$name])) {
-        $isInfiniteLength = true;
+      $isInfinite = false;
+      if (in_array('infinite_argument', $this->config[$name])) {
+        $isInfinite = true;
       }
       $class = $this->config[$name]['class'];
-      $value = $this->buildOptionObject($class, $isInfiniteLength);
+      $value = $this->buildObject($class, $isInfinite);
     }
     $_ENV['context']->addOption($name, $value);
   }
 
-  private function getOptionName($item) {
+  private function getName($item) {
     if (strpos($item, '--') === 0) {
       return substr($item, 2);
     }
     $shorts = substr($item, 1);
     if (strlen($shorts) == 1) {
-      return $this->getOptionFullName($shorts);
+      return $this->getFullName($shorts);
     }
     $options = array();
     foreach (str_split($shorts) as $item) {
@@ -62,14 +66,14 @@ class OptionParser {
     return $options;
   }
 
-  private function getOptionFullName($shortName) {
+  private function getFullName($shortName) {
     if (!isset($this->shorts[$shortName])) {
       throw new Exception("Option '$shortName' not allowed");
     }
     return $this->shorts[$shortName];
   }
 
-  private function buildOptionObject($class, $isInfinite) {
+  private function buildObject($class, $isInfinite) {
     $reflector = new ReflectionClass($class);
     $constructor = $reflector->getConstructor();
     $maximumArgumentLength = 0;
@@ -79,40 +83,40 @@ class OptionParser {
     if ($isInfinite) {
       $maximumArgumentLength = null;
     }
-    $arguments = $this->readOptionArguments($maximumArgumentLength);
+    $arguments = $this->getArguments($maximumArgumentLength);
     if ($constructor == null && count($arguments) !== 0) {
       throw new Exception("Option argument length not matched");
     }
     if ($constructor != null) {
       $length = count($arguments);
       $verifier = new ArgumentVerifier;
-      $$verifier->run($constructor, $length, $isInfinite);
+      $verifier->run($constructor, $length, $isInfinite);
     }
     return $reflector->newInstanceArgs($arguments);
   }
 
-  private function readOptionArguments($maximumLength) {
+  private function getArguments($maximumLength) {
     $arguments = array();
-    $currentIndex = $this->commandParser->getCurrentIndex();
-    while ($currentIndex < $this->inputArgumentLength) {
-      $item = $this->inputArgumentLength[++$currentIndex];
+    while (($item = $this->reader->getItem()) !== null) {
       if (strpos($item, '-') === 0 && $item != '-') {
+        $this->reader->move(-1);
         break;
       }
       $arguments[] = $item;
+      $this->reader->move();
     }
-    $count = count($arguments);
+    $amount = count($arguments);
     if ($maximumLength !== null
-     && $count > $maximumLength
-     && $this->currentIndex == $this->inputArgumentLength) {
-      $this->arguments = array_slice($arguments, $maximumLength);
-      $arguments = array_slice($arguments, 0, $maximumLength);
-    }
-    if ($maximumLength !== null
-     && $count == $maximumLength + 1
-     && !isset($this->config['class'])) {
-      $this->commandParser->setCurrentIndex($currentIndex - 1);
+     && $amount == $maximumLength + 1
+     && !$this->isCommandFound) {
       array_pop($arguments);
+      $this->reader->move(-1);
+    }
+    if ($maximumLength !== null
+     && $amount > $maximumLength
+     && $this->reader->getItem() === null) {
+      $arguments = array_slice($arguments, 0, $maximumLength);
+      $this->reader->move($maximumLength - $amount);
     }
     return $arguments;
   }
