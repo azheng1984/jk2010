683642057cd5c5c78d61f8c546b4c426cdef85ce
wangzheng <none@gmail.com>
2011-02-09 22:05:37 +0800

    -

--- vendor/hf/cli/lib/CommandContext.php
+++ vendor/hf/cli/lib/CommandContext.php
@@ -1,11 +1,19 @@
 <?php
 class CommandContext {
+  private $options = array();
+
   public function getOption($name, $isNullable = false) {
-    return true;
+    if (!isset($this->options[$name]) && !$isNullable) {
+      throw new Exception;
+    }
+    if (!isset($this->options[$name])) {
+      return null;
+    }
+    return $this->options[$name];
   }
-  
+
   public function addOption($name, $value) {
-    
+    $this->options[$name] = $value;
   }
 
   public function read() {
