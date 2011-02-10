b9feeff5d5e172dc793127ac277bfc3e908d1338
wangzheng <none@gmail.com>
2011-02-10 12:40:32 +0800

    -

---
+++ config/vendor/hf/Processor/ActionProcessor.config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+  'home' => array('class' => 'HomeAction', 'method' => array ('GET')),
+  'category' => array('class' => 'CategoryAction', 'method'=> array('GET')),
+  'document', //ignore action
+);
---
+++ config/vendor/hf/Processor/ViewProcessor.coinfig.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'category' => array('screen' => 'CategoryScreen'),
+  'home' => array('screen' => 'HomeScreen'),
+  'document' => array('screen'), //ignore view when show in screen
+  'error/internal_server_error' => array('screen' => 'InternalServerErrorScreen'),
+  'error/not_found' => array('screen' => 'NotFoundScreen'),
+);
