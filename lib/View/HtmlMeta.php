4ae382d76264e249d3f983882a99f4753b2e868f
wangzheng <none@gmail.com>
2011-02-13 11:56:32 +0800

    -

--- lib/View/HtmlMeta.php
+++ lib/View/HtmlMeta.php
@@ -2,19 +2,20 @@
 class HtmlMeta {
   private $metas;
 
-  public function __construct($title, $description, $keywords) {
-  	$this->addMeta('title', $title);
+  public function __construct($description, $keywords) {
   	$this->addMeta('description', $description);
   	$this->addMeta('keywords', $keywords);
   }
 
   public function render() {
+    echo '    <meta http-equiv="Content-Type"', 
+         ' content="text/html; charset=UTF-8"/>', "\n";
     foreach ($this->metas as $item) {
       echo '    <meta name="', $item['name'],
            '" content="', $item['content'], '" />', "\n";
     }
   }
-  
+
   private function addMeta($name, $content) {
     $this->metas[] = array ('name' => $name, 'content' => $content);
   }
--- lib/View/ScreenWrapper.php
+++ lib/View/ScreenWrapper.php
@@ -3,9 +3,12 @@ class ScreenWrapper {
   private $content;
   private $meta;
 
-  public function __construct($content, $meta) {
+  public function __construct($content, $title, $meta) {
     $this->content = $content;
     $this->meta = $meta;
+    $this->title = $title;
+    $this->header = new ScreenHeader;
+    $this->footer = new ScreenFooter;
   }
 
   public function render() {
@@ -14,9 +17,12 @@ class ScreenWrapper {
          '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN">', "\n",
          "  <head>\n";
     $this->meta->render();
+    echo "    <title>$this->title</title>\n";
     echo '  </head>', "\n",
          '  <body>', "\n";
+    $this->header->render();
     $this->content->renderContent();
+    $this->footer->render();
     echo "\n", '  </body>', "\n";
     echo '</html>';
   }
