8ec63f97726f564804ea54e6dd93a824aee039b7
wangzheng <none@gmail.com>
2011-02-14 13:50:00 +0800

    -

--- database/definition/table/image.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `image` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  `data` varchar(127) NOT NULL,
-  `document_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
