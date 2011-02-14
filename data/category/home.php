b333c569dcda039377b48763aae627448c6cb825
wangzheng <none@gmail.com>
2011-02-14 17:47:35 +0800

    -

---
+++ data/category/home.php
@@ -0,0 +1,24 @@
+<?php
+return array (
+  'update_time' => '',
+  'hot' => array (
+    array (
+      'title' => '',
+      'url' => '',
+      'image_url' => '',
+      'description' => '',
+      'source_id' => '',
+      'time' => '',
+      'place' => '',
+      'people' => '',
+    ),
+  ),
+  'recent' => array (
+    array (
+      'title' => '',
+      'url' => '',
+      'source_id' => '',
+    ),
+  ),
+  'document_list_url' => '',
+);
