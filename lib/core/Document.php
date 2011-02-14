69c35d98428522d24b57ae6a9e3322163686d39e
wangzheng <none@gmail.com>
2011-02-14 13:20:50 +0800

    -

--- lib/core/Document.php
+++ lib/core/Document.php
@@ -12,11 +12,10 @@ class Document {
   private $down;
   private $source_name;
   private $source_url;
-  private $related_elements;
   private $related_elements_cache; //可以分布在不同的数据库，通过缓存解决性能问题
   //可以通过本地服务器批处理生成缓存，然后上传到服务器上，在本地过期，服务器端不需要维持 sentence 到 document 的关系，
  //但是本地最好维护，因为可以在修改/删除数据时更新缓存
-  private $category_page_number;
+  private $list_page;
 
   public function __construct($id, $title, $time, $place,
                               $people, $up, $down, $source, $url) {
--- vendor/hf/web/lib/ErrorHandler.php
+++ vendor/hf/web/lib/ErrorHandler.php
@@ -1,19 +1,19 @@
 <?php
 class ErrorHandler {
   private $app;
-  
+
   public function __construct($app) {
     $this->app = $app;
   }
-  
+
   public function run() {
     set_exception_handler(array ($this, 'handle'));
   }
-  
+
   public function stop() {
     restore_exception_handler();
   }
-  
+
   public function handle($exception) {
     if (!headers_sent()) {
       $_ENV['exception'] = $exception;
@@ -22,13 +22,14 @@ class ErrorHandler {
     }
     trigger_error($exception, E_USER_ERROR);
   }
-  
+
   private function reload() {
     $status = '500';
     if (self::$exception instanceof ApplicationException) {
       $status = substr(self::$exception->getCode(), 0, 3);
     }
-    $configPath = HF_CONFIG_PATH . 'web' . DIRECTORY_SEPARATOR . __CLASS__ . '.config.php';
+    $configPath = HF_CONFIG_PATH.'web'
+                 .DIRECTORY_SEPARATOR.__CLASS__.'.config.php';
     $config = require $configPath;
     if (isset($config[$status])) {
       $this->app->run($config[$status]);
