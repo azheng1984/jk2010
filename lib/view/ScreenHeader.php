d00495261e57671a01b849db53b91679a5665dc0
azheng1984 <azheng1984@126.com>
2011-02-17 23:30:47 +0800

    -

--- lib/view/ScreenHeader.php
+++ lib/view/ScreenHeader.php
@@ -1,7 +1,7 @@
 <?php
 class ScreenHeader {
   public function render() {
-    echo '    <div id="logo"><a href="/">甲壳 - 发现热点，驱动潮流！</a></div>', "\n";
+    echo '    <div id="logo"><a href="/">甲壳</a></div>', "\n";
     $currentCategory = isset($_GET['category']) ? $_GET['category'] : 'home';
     echo '<div id="category">';
     $this->renderCategoryList($currentCategory);
