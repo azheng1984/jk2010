1b7f887cce8e7dfbe3837307e3d7a36f632f70c2
unknown <zjfcwangzheng@126.com>
2011-02-19 00:36:29 +0800

    -

---
+++ vendor/hf/cli/lib/CommandErrorHandler.php
@@ -0,0 +1,18 @@
+<?php
+class CommandErrorHandler {
+  public function run() {
+    set_exception_handler(array ($this, 'handle'));
+  }
+
+  public function stop() {
+    restore_exception_handler();
+  }
+
+  public function handle($exception) {
+    $message = $exception;
+    if ($exception instanceof ICommandMessage) {
+      $message = $exception->getMessage();
+    }
+    fwrite(STDERR, $message);
+  }
+}
