8165d434aa381996c678a4de0790d6f0f2f6655e
wangzheng <none@gmail.com>
2011-02-18 09:51:01 +0800

    -

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
 define('HF_CACHE_PATH', ROOT_PATH.'cache/vendor/hf/');
 define('HF_CONFIG_PATH', ROOT_PATH.'config/vendor/hf/');
 define('DATA_PATH', ROOT_PATH.'data/');
-require ROOT_PATH.'data/define.php';
+require DATA_PATH.'define.php';
 require ROOT_PATH.'vendor/hf/class_loader/lib/ClassLoader.php';
 $classLoader = new ClassLoader;
 $classLoader->run();
