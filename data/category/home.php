151a7dd4f17eb992d4eb8003e24e2d7ee702ca54
wangzheng <none@gmail.com>
2011-02-18 08:55:24 +0800

    -

--- data/category/home.php
+++ data/category/home.php
@@ -22,23 +22,3 @@ return array (
     ),
   ),
 );
-
-/*
-id
-url_name
-title
-description
-source_id
-image_url_prefix
-time
-place
-people
-"1","ballmer_confirmed_wp7_will_support_multi_tasking","鲍尔默确认WP7将支持多任务","微软CEO史蒂夫·鲍尔默(Steve Ballmer)在昨天开幕的2011年移动世界大会(MWC 2011)上发表主题演讲。他表示，Windows Phone 7(以下简称&quot;WP7&quot;)手机操作系统的下一个更新将添加第三方应用的多任务处理能力；Twitter将被整合到联系人列表(People Hub)中；手机版IE 9也即将发布。","0","1-1","2011-02-14","西班牙, 巴塞罗","史蒂夫·鲍尔默(Steve Ballmer)";"2","windows_phone_number_of_applications_exceeded_8000_store_software_models","Windows Phone应用商店软件数量突破8000款","截至上周末，微软Windows Phone Marketplace应用商店中的应用数量已突破8000款。","","0","2011-02-14";"3","google_vice_president_hinted_that_nokia_will_support_the_wp7","谷歌副总裁暗示诺基亚将支持WP7","谷歌副总裁维克·宫多拉(Vic Gundotra)周二晚暗示，诺基亚周五可能宣布支持微软Windows Phone 7(以下简&quot;WP7&quot;)平台。","0","","2011-02-09","","维克·宫多拉(Vic Gundotra)"
-
-id, title, category, url_name, description, image_url_prefix
-"2","Windows Phone应用商店软件数量突破8000款","tech/1-1","windows_phone_number_of_applications_exceeded_8000_store_software_models","Marketplace拥有5000名以上的开发人员。";"3","谷歌副总裁暗示诺基亚将支持WP7","tech/1-1","google_vice_president_hinted_that_nokia_will_support_the_wp7","诺基亚本周五将发布公司的新战略，有传言称，届时诺基亚可能宣布与谷歌Android或微软WP7联姻的决定。"
-"1","鲍尔默确认WP7将支持多任务","tech/1-1","ballmer_confirmed_wp7_will_support_multi_tasking","鲍尔默：“今年，WP7将整合Twitter。如果你喜欢Facebook的整合，你就会喜欢Twitter的整合。”";"2","Windows Phone应用商店软件数量突破8000款","tech/1-1","windows_phone_number_of_applications_exceeded_8000_store_software_models","然而，苹果应用商店App Store的应用数量已于去年10月超过30万款；谷歌Android Market也已超过10万款。"
-"3","谷歌副总裁暗示诺基亚将支持WP7","tech/1-1","google_vice_president_hinted_that_nokia_will_support_the_wp7","业内人士称，从宫多拉发表的Twitter信息看，似乎在暗示诺基亚已经选择了微软WP7平台。";"1","鲍尔默确认WP7将支持多任务","tech/1-1","ballmer_confirmed_wp7_will_support_multi_tasking","鲍尔默：“Marketplace是应用商店。最后是Games Hub，与Xbox Live整合。”","1-2"
-
-
-*/
