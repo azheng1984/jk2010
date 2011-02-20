6357167f8fdf1a241e5602a89c40414e1ff21af7
wangzheng <none@gmail.com>
2011-02-20 16:59:30 +0800

    -

--- vendor/hf/web/lib/Application.php
+++ vendor/hf/web/lib/Application.php
@@ -1,38 +1,31 @@
 <?php
 class Application {
   private $processors;
-  private static $isProcessed = false;
-  private static $cache = array();
+  private $cache;
 
-  public function __construct($processor/*, ...*/) {
-    $this->processors = func_get_args();
+  public function __construct($processors) {
+    $this->processors = $processors;
   }
 
   public function run($path = null) {
     if ($path === null) {
       $path = $_SERVER['REQUEST_URI'];
     }
-    foreach ($this->processors as $processor) {
-      $this->process($processor, $path);
+    if ($this->cache === null) {
+      $this->cache = require HF_CACHE_PATH.'web'
+                            .DIRECTORY_SEPARATOR.__CLASS__.'.cache.php';
     }
-    if (!self::$isProcessed) {
-      throw new NotFoundException("Path '$path' not processed");
+    if (!isset($this->cache[$path]) && !in_array($path, $this->cache)) {
+      throw new NotFoundException("Path '$path' not found");
     }
-  }
-
-  private function process($processor, $path) {
-    $class = get_class($processor);
-    if (!isset(self::$cache[$class])) {
-      self::$cache[$class] = require $processor->getCachePath();
+    foreach ($this->processors as $name => $processor) {
+      $cache = null;
+      if (isset($this->cache[$path][$name])) {
+        $cache = $this->cache[$path][$name];
+      }
+      if ($cache !== null || in_array($name, $this->cache[$path])) {
+        $processor->run($cache);
+      }
     }
-    if (isset(self::$cache[$class][$path])) {
-      $processor->run(self::$cache[$class][$path]);
-      self::$isProcessed = true;
-    }
-  }
-
-  public static function reset() {
-    self::$cache = array();
-    self::$isProcessed = false;
   }
 }
