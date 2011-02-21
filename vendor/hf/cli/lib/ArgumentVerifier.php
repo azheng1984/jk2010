31c3b95a4fae09ba420192c54326ea0c555ccc9e
azheng1984 <azheng1984@126.com>
2011-02-21 16:02:16 +0800

    -

---
+++ vendor/hf/cli/lib/ArgumentVerifier.php
@@ -0,0 +1,18 @@
+<?php
+class ArgumentVerifier {
+  public function verify($reflector, $length, $isInfinite) {
+    foreach ($reflector->getParameters() as $parameter) {
+      if ($parameter->isOptional() && $length === 0) {
+        break;
+      }
+      --$length;
+    }
+    if ($length < 0) {
+      return false;
+    }
+    if ($length > 0 && $isInfinite === false) {
+      return false;
+    }
+    return true;
+  }
+}
