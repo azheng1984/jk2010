545e047c3a4e14d11e6e45b3a4db451842200ea0
wangzheng <none@gmail.com>
2011-02-22 13:11:13 +0800

    -

--- vendor/hf/tool/cli/app/NewCommand.php
+++
@@ -1,9 +0,0 @@
-<?php
-class NewCommand {
-  public function execute($type, $name) {
-    $config = array();
-    $processor = $config[$type];
-    $processor->run($name);
-    $color = $_ENV['context']->getOption('color');
-  }
-}
