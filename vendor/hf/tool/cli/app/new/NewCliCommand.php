88036c77021a1e9e2c8a0a6b5adc3f2b45e5b059
wangzheng <none@gmail.com>
2011-02-23 23:01:06 +0800

    -

--- vendor/hf/tool/cli/app/new/NewCliCommand.php
+++ vendor/hf/tool/cli/app/new/NewCliCommand.php
@@ -3,4 +3,17 @@ class NewCliCommand {
   public function execute() {
     
   }
+
+  public function getOptionConfig() {
+    return array (
+      'preview' => array (
+        'short' => 'p',
+        'description' => '',
+      ),
+      'quite' => array (
+        'short' => 'q',
+        'description' => '',
+      ),
+    );
+  }
 }
