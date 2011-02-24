887c56bebf051edb1b74410f6efe51ee3a19f70f
azheng1984 <azheng1984@126.com>
2011-02-24 23:58:25 +0800

    -

--- vendor/hf/tool/cli/app/new/NewCliCommand.php
+++ vendor/hf/tool/cli/app/new/NewCliCommand.php
@@ -1,6 +1,7 @@
 <?php
 class NewCliCommand {
   public function execute() {
-    
+    $config = require HF_PATH.'tool/core/config/scaffold/cli_scaffold.config.php';
+    echo var_export($config);
   }
 }
