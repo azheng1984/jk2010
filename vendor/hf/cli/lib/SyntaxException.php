72a9df81cfdc217bec40d0a680652f964327db29
azheng1984 <azheng1984@126.com>
2011-02-28 21:23:48 +0800

    -

--- vendor/hf/cli/lib/SyntaxException.php
+++ vendor/hf/cli/lib/SyntaxException.php
@@ -1,6 +1,17 @@
 <?php
 class SyntaxException extends Exception {
+  private $config;
+
+  public function __construct($message = null, $config = null) {
+    parent::__construct($message);
+    $this->config = $config;
+  }
+
   public function __toString() {
-    return parent::__toString();
+    return $this->message."\n".$this->getUsage();
+  }
+
+  public function getUsage() {
+    return print_r($this->config, true);
   }
 }
