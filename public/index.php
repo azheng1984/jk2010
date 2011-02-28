426f89992f4c0951b14966cb97b9ac63d57c6fb3
wangzheng <none@gmail.com>
2011-02-28 22:58:39 +0800

    -

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
-define('CACHE_PATH', ROOT_PATH.'cache/');
 define('CONFIG_PATH', ROOT_PATH.'config/');
+define('CACHE_PATH', ROOT_PATH.'cache/');
 define('DATA_PATH', ROOT_PATH.'data/');
 require DATA_PATH.'define.php';
 require ROOT_PATH.'vendor/hf/class_loader/lib/ClassLoader.php';
