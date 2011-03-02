bba4bde170fb96b0a173de0cfcee66352d02459c
azheng1984 <azheng1984@126.com>
2011-03-02 09:59:15 +0800

    -

--- vendor/hf/web/lib/action/MethodNotAllowedException.php
+++ vendor/hf/web/lib/action/MethodNotAllowedException.php
@@ -1,6 +1,6 @@
 <?php
 class MethodNotAllowedException extends ApplicationException {
-  public function __construct($methods, $message = '') {
+  public function __construct($methods, $message = null) {
     parent::__construct($message, '405 Method Not Allowed');
     header('Method: '.implode(';', $methods));
   }
