723edf6726f022093eee00cd7eeea47ebaad00e9
azheng1984 <azheng1984@126.com>
2011-03-03 17:44:40 +0800

    -

--- vendor/hf/tool/web/config/make.config.php
+++ vendor/hf/tool/web/config/make.config.php
@@ -4,6 +4,6 @@ return array(
   'processor' => array('action', 'view'),
   'class_path' => array(
     '.' => array('app'),
-    '/home/wz/www/jk2010/vendor/hf/web/lib',
+    '../../web/lib',
   ),
 );
