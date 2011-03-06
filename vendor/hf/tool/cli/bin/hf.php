6ee4418ee575d84b3726c51450404d989ac0b7c8
azheng1984 <azheng1984@126.com>
2011-03-06 23:06:49 +0800

    -

--- vendor/hf/tool/cli/bin/hf.php
+++ vendor/hf/tool/cli/bin/hf.php
@@ -2,9 +2,9 @@
 <?php
 ini_set('display_errors', 0);
 define('ROOT_PATH', dirname(dirname(__FILE__)).'/');
-define('HF_PATH', dirname(dirname(ROOT_PATH)).'/');
 define('CACHE_PATH', ROOT_PATH.'cache/');
 define('CONFIG_PATH', ROOT_PATH.'config/');
+define('HF_PATH', dirname(dirname(ROOT_PATH)).'/');
 require HF_PATH.'class_loader/lib/ClassLoader.php';
 $classLoader = new ClassLoader;
 $classLoader->run();
