65b6712c80fcc42bb776863c561cf82bfa4149bf
azheng1984 <azheng1984@126.com>
2011-03-11 10:33:02 +0800

    -

--- config/make.config.php
+++ config/make.config.php
@@ -3,7 +3,7 @@ return array(
   'application' => array('action', 'view'),
   'class_loader' => array(
     'app',
-    'vendor/hf/web' => array('lib'),
-    '/home/wz/www/jk2010/lib',
+    'lib',
+    'vendor/hf' => array('class_loader/lib', 'web/lib'),
   ),
 );
