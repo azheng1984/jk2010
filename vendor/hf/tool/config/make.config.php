a8782c66ed99a2240d23c4a0300698b9c3db40e2
azheng1984 <azheng1984@126.com>
2011-03-14 16:16:54 +0800

    -

--- vendor/hf/tool/config/make.config.php
+++ vendor/hf/tool/config/make.config.php
@@ -1,8 +1,4 @@
 <?php
 return array(
-  'class_loader' => array(
-    '/home/wz/www/jk2010/vendor/hf/cli/lib',
-    'app',
-    'lib',
-  ),
+  'class_loader' => array('/home/wz/www/jk2010/vendor/hf/cli/lib', 'app', 'lib'),
 );
