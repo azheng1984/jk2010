903796f0ad71ced3d665c5b7e6013d2276e80f62
wangzheng <none@gmail.com>
2011-03-14 20:10:19 +0800

    -

--- vendor/hf/tool/app/HelpCommand.php
+++ vendor/hf/tool/app/HelpCommand.php
@@ -1,6 +1,6 @@
 <?php
 class HelpCommand {
-  public function execute($target = 'main') {
-    echo 'helpx!';
+  public function execute($target = null) {
+    echo 'Hyperframework version 0.1'.PHP_EOL;
   }
 }
