e7078a5e3ffe3fc3a7dfb00579945b32f2458935
wangzheng <none@gmail.com>
2011-03-15 17:05:13 +0800

    -

--- vendor/hf/cli/lib/CommandCollection.php
+++ vendor/hf/cli/lib/CommandCollection.php
@@ -2,7 +2,7 @@
 class CommandCollection {
   public function render($config) {
     if (isset($config['description'])) {
-      echo $config['description'].PHP_EOL;
+      echo $config['description'].PHP_EOL.PHP_EOL;
     }
     if (isset($config['option'])) {
       $this->renderOption($config['option']);
@@ -18,45 +18,71 @@ class CommandCollection {
         }
       }
       if (count($indexes) !== 0) {
-        echo PHP_EOL.'[collection]'.PHP_EOL;
+        echo '[collection]'.PHP_EOL;
         foreach ($indexes as $name => $item) {
-          echo "  ".$name;
+          echo $name.PHP_EOL;
           if (isset($item['description'])) {
-            echo PHP_EOL.'    '.$item['description'].PHP_EOL;
+            echo '  '.$item['description'].PHP_EOL.PHP_EOL;
           }
-          echo PHP_EOL;
         }
       }
       if (count($commands) !== 0) {
-        echo PHP_EOL.'[command]'.PHP_EOL;
+        echo '[command]'.PHP_EOL;
         foreach ($commands as $name => $item) {
           echo $name;
-          if (isset($item['description'])) {
-            echo PHP_EOL.'  '.$item['description'].PHP_EOL;
-          }
-          if (is_array($item) && isset($item['option'])) {
-            $this->renderOption($item['option']);
+          if (is_array($item) && isset($item['class'])) {
+            $this->renderArgument($item['class']);
+          } else {
+            $this->renderArgument($item);
           }
           echo PHP_EOL;
+          if (is_array($item)) {
+            if (isset($item['description'])) {
+              echo '  '.$item['description'].PHP_EOL;
+            }
+            if (isset($item['option'])) {
+              $this->renderOption($item['option']);
+            }
+          }
         }
+      }
+    } else {
+      if (is_array($config) && isset($config['class'])) {
+        $this->renderArgument($config['class']);
+      } else {
+        $this->renderArgument($config);
       }
     }
   }
 
   private function renderOption($config) {
-    echo PHP_EOL.'  [option]'.PHP_EOL;
+    echo '  [option]'.PHP_EOL;
     foreach ($config as $name => $item) {
       echo "  --".$name;
       if (isset($item['short'])) {
         if (is_array($item['short'])) {
           $item['short'] = implode(', -', $item['short']);
         }
-        echo ', -'.$item['short'];
+        echo ', -'.$item['short'].PHP_EOL;
         if (isset($item['description'])) {
-          echo PHP_EOL.'    '.$item['description'].PHP_EOL;
+          echo '    '.$item['description'].PHP_EOL;
         }
+        echo PHP_EOL;
       }
-      echo ''.PHP_EOL;
+    }
+  }
+
+  private function renderArgument($class) {
+    $reflector = new ReflectionMethod($class, 'execute');
+    $args = $reflector->getParameters();
+    if (count($args) !== 0) {
+      $count = 0;
+      echo '(';
+      foreach ($args as $item) {
+        $item->isOptional();
+        echo $item->getName();
+      }
+      echo ')';
     }
   }
 }
