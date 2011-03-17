cb9f0a7f589c9288aa8a7dcd9dc246eab28c6038
wangzheng <none@gmail.com>
2011-03-17 22:51:12 +0800

    -

--- vendor/hf/tool/app/MakeCommand.php
+++ vendor/hf/tool/app/MakeCommand.php
@@ -3,7 +3,14 @@ class MakeCommand {
   private $config;
 
   public function execute() {
-    $this->config = require 'config/make.config.php';
+    $configPath = (
+      getcwd().DIRECTORY_SEPARATOR.
+      'config'.DIRECTORY_SEPARATOR.'make.config.php'
+    );
+    if (!file_exists($configPath)) {
+      throw new CommandException("can't find the make file");
+    }
+    $this->config = require $configPath;
     if (isset($this->config['class_loader']) || in_array('class_loader', $this->config)) {
       $config = isset($this->config['class_loader']) ? $this->config['class_loader'] : null;
       $builder = new ClassLoaderCacheBuilder($config);
