967e1c555c89ad5f9392903066aa2ff925674143
azheng1984 <azheng1984@126.com>
2011-03-21 10:11:00 +0800

    -

--- config/build.config.php
+++ config/build.config.php
@@ -1,9 +1,5 @@
 <?php
 return array(
   'Application' => array('Action', 'View' => array('Screen', 'Image')),
-  'ClassLoader' => array(
-    'ClassRecognizer' => array(
-      'app', 'lib', HF_PATH.'web'.DIRECTORY_SEPARATOR.'lib'
-    ),
-  ),
+  'ClassLoader' => array('app', 'lib', HF_PATH.'web'.DIRECTORY_SEPARATOR.'lib'),
 );
