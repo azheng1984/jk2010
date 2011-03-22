2a990084c5f74023ebef23ac5eb2406a89003295
azheng1984 <azheng1984@126.com>
2011-03-23 01:08:53 +0800

    -

--- vendor/hyperframework/tool/lib/class_loader/ClassLoaderCache.php
+++ vendor/hyperframework/tool/lib/class_loader/ClassLoaderCache.php
@@ -14,42 +14,30 @@ class ClassLoaderCache {
     if (isset($this->cache[0][$class])) {
       throw new Exception("Conflict Class '$class'");
     }
-    $cache[0][$class] = $this->getFolderIndex($rootPath, $relativePath);
+    $cache[0][$class] = $this->getIndex($rootPath, $relativePath);
   }
 
-  private function getFolderIndex($rootPath, $relativePath) {
+  private function getIndex($rootPath, $relativePath) {
     if ($rootPath === null && $relativePath === null) {
       return true;
     }
-    $rootPathIndex = $this->getRootPathIndex($rootPath);
+    $rootPathIndex = $this->getFolderIndex(rootPath, array($rootPath));
     if ($relativePath === null) {
       return $rootPathIndex;
     }
-    return $this->getRelativePathIndex(
-      $rootPath, $relativePath, $rootPathIndex
+    $fullPath = $rootPath.DIRECTORY_SEPARATOR.$relativePath;
+    return $this->getFolderIndex(
+      $fullPath, array($relativePath, $rootPathIndex)
     );
   }
 
-  private function getRootPathIndex($rootPath) {
-    if (isset($this->$folders[$rootPath])) {
-      return $this->$folders[$rootPath];
+  private function getFolderIndex($path, $cache) {
+    if (isset($this->$folders[$path])) {
+      return $this->$folders[$path];
     }
-    $rootPathIndex = count($this->cache[1]);
-    $this->cache[1][$rootPathIndex] = array($rootPath);
-    $this->folders[$rootPath] = $rootPathIndex;
-    return $rootPathIndex;
-  }
-
-  private function getRelativePathIndex(
-    $rootPath,$relativePath, $rootPathIndex
-  ) {
-    $fullPath = $rootPath.DIRECTORY_SEPARATOR.$relativePath;
-    if (isset($this->$folders[$fullPath])) {
-      return $this->$folders[$fullPath];
-    }
-    $relativePathIndex = count($this->cache[1]);
-    $this->cache[1][$relativePathIndex] = array($relativePath, $rootPathIndex);
-    $this->folders[$fullPath] = $relativePathIndex;
-    return $relativePathIndex;
+    $index = count($this->cache[1]);
+    $this->cache[1][$index] = $cache;
+    $this->folders[$path] = $index;
+    return $index;
   }
 }
