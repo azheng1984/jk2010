571c597b7c0c8db8096e0c9bd303c5156a758054
wangzheng <none@gmail.com>
2011-03-24 23:02:19 +0800

    -

--- vendor/hyperframework/tool/app/NewCommand.php
+++ vendor/hyperframework/tool/app/NewCommand.php
@@ -1,36 +1,53 @@
 <?php
 class NewCommand {
   public function execute($type, $hyperframeworkPath = HYPERFRAMEWORK_PATH) {
-    $_ENV['hyperframework_path'] = $hyperframeworkPath;
+    $this->setPath($hyperframeworkPath);
     if (count(scandir(getcwd())) !== 2) {
       throw new CommandException('directory must empty');
     }
     $config = require CONFIG_PATH.'new/'.$type.'.config.php';
     foreach ($config as $path => $content) {
       if (is_int($path)) {
-        $path = $content;
-        $content = null;
+        list($path, $content) = array($content, null);
       }
       if (substr($path, -1) === '/') {
-        $this->generateDirectory($path);
+        $this->generateDirectory($path, $content);
         continue;
       }
       $this->generateFile($path, $content);
     }
   }
 
-  private function generateFile($path, $content) {
-    $this->createDirectory(dirname($path));
-    $data = null;
-    if (is_array($content)) {
-      $data = implode(PHP_EOL, $content);
+  private function setPath($hyperframeworkPath) {
+    $classLoaderPathPrefix = 'HYPERFRAMEWORK_PATH';
+    if (strpos(HYPERFRAMEWORK_PATH, getcwd()) === 0) {
+      $classLoaderPathPrefix = 'ROOT_PATH.'.$classLoaderPathPrefix;
+      $hyperframeworkPath = str_replace(getcwd(), '', $hyperframeworkPath);
     }
-    file_put_contents($path, $data);
+    $_ENV['new'] = array('hyperframework_path' => $hyperframeworkPath);
+    $_ENV['new']['class_loader_prefix'] = $classLoaderPathPrefix;
   }
 
-  private function generateDirectory($path) {
+  private function generateFile($path, $content) {
+    $this->generateDirectory(dirname($path), null);
+    $mode = 0644;
+    $output = null;
+    if ($content !== null) {
+      $mode = array_shift($content);
+      $output = implode(PHP_EOL, $content);
+    }
+    file_put_contents($path, $output);
+    chmod($path, $mode);
+  }
+
+  private function generateDirectory($path, $mode) {
+    if ($mode === null) {
+      $mode = 0755;
+    }
     if (!is_dir($path)) {
-      mkdir($path, 0777, true); //todo:code dirs are readable only for php
+      $mask = umask(0);
+      mkdir($path, $mode, true);
+      umask($mask);
     }
   }
 }
