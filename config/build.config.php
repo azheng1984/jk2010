612ba28697380f8cb52cd523057457386b1ee497
wangzheng <none@gmail.com>
2011-03-25 19:50:42 +0800

    -

--- config/build.config.php
+++ config/build.config.php
@@ -1,7 +1,5 @@
 <?php
 return array(
   'Application' => array('Action', 'View' => array('Screen', 'Image')),
-  'ClassLoader' => array(
-    'app', 'lib', HYPERFRAMEWORK_PATH.'web'.DIRECTORY_SEPARATOR.'lib'
-  ),
+  'ClassLoader' => array('app', 'lib', HYPERFRAMEWORK_PATH.'web/lib'),
 );
