7cf6ba5a1b177eb931df3c7bf6e5064b3448f5a4
wangzheng <none@gmail.com>
2011-03-29 22:03:09 +0800

    -

--- vendor/hyperframework/tool/app/NewCommand.php
+++ vendor/hyperframework/tool/app/NewCommand.php
@@ -9,16 +9,17 @@ class NewCommand {
       throw new CommandException('directory must empty');
     }
     $this->setEnvironment($hyperframeworkPath);
+    $generator = new FileGenerator();
     $config = require $configPath;
     foreach ($config as $path => $content) {
       if (is_int($path)) {
         list($path, $content) = array($content, null);
       }
       if (substr($path, -1) === '/') {
-        $this->generateDirectory($path, $content);
+        $generator->generateDirectory($path, $content);
         continue;
       }
-      $this->generateFile($path, $content);
+      $generator->generateFile($path, $content);
     }
   }
 
@@ -29,32 +30,5 @@ class NewCommand {
       $hyperframeworkPath = str_replace($_SERVER['PWD'], '', $hyperframeworkPath);
     }
     $_ENV['hyperframework_path'] = var_export($hyperframeworkPath, true);
-  }
-
-  private function generateFile($path, $content) {
-    $this->generateDirectory(dirname($path));
-    $mode = null;
-    if (isset($content[0]) && is_int($content[0])) {
-      $mode = array_shift($content);
-    }
-    $output = null;
-    if ($content !== null) {
-      $output = implode(PHP_EOL, $content);
-    }
-    file_put_contents($path, $output);
-    if ($mode !== null) {
-      chmod($path, $mode);
-    }
-  }
-
-  private function generateDirectory($path, $mode = null) {
-    if ($mode === null) {
-      $mode = 0755;
-    }
-    if (!is_dir($path)) {
-      $mask = umask(0);
-      mkdir($path, $mode, true);
-      umask($mask);
-    }
   }
 }
