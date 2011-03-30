867603aa9ab63d2ecf72f1a36ad26ac9dbf8b0e5
azheng1984 <azheng1984@126.com>
2011-03-30 11:22:35 +0800

    -

--- vendor/hyperframework/tool/app/NewCommand.php
+++ vendor/hyperframework/tool/app/NewCommand.php
@@ -3,7 +3,7 @@ class NewCommand {
   public function execute($type, $hyperframeworkPath = HYPERFRAMEWORK_PATH) {
     $configPath = CONFIG_PATH.'new/'.$type.'.config.php';
     if (!file_exists($configPath)) {
-      throw new CommandException("Application type '$type' is invalide");
+      throw new CommandException("Application type '$type' is invalid");
     }
     if (count(scandir($_SERVER['PWD'])) !== 2) {
       throw new CommandException('directory must empty');
