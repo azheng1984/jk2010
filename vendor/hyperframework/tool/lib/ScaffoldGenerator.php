e08332801f9352363f60fdc3261cfeb32f42ab2b
wangzheng <none@gmail.com>
2011-03-30 10:24:36 +0800

    -

--- vendor/hyperframework/tool/lib/ScaffoldGenerator.php
+++ vendor/hyperframework/tool/lib/ScaffoldGenerator.php
@@ -1,6 +1,19 @@
 <?php
-class FileSystemGenerator {
-  public function generateFile($path, $content) {
+class ScaffoldGenerator {
+  public function generate($config) {
+    foreach ($config as $path => $content) {
+      if (is_int($path)) {
+        list($path, $content) = array($content, null);
+      }
+      if (substr($path, -1) === '/') {
+        $this->generateDirectory($path, $content);
+        continue;
+      }
+      $this->generateFile($path, $content);
+    }
+  }
+
+  private function generateFile($path, $content) {
     $this->generateDirectory(dirname($path));
     $mode = null;
     if (isset($content[0]) && is_int($content[0])) {
@@ -16,7 +29,7 @@ class FileSystemGenerator {
     }
   }
 
-  public function generateDirectory($path, $mode = null) {
+  private function generateDirectory($path, $mode = null) {
     if ($mode === null) {
       $mode = 0755;
     }
