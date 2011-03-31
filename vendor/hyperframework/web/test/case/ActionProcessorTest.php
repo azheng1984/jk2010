4c6dbf7b85a2d94972c16067c47b5749a1eeed4e
wangzheng <none@gmail.com>
2011-03-31 22:47:04 +0800

    -

--- vendor/hyperframework/web/test/case/ActionProcessorTest.php
+++ vendor/hyperframework/web/test/case/ActionProcessorTest.php
@@ -2,11 +2,12 @@
 class ActionProcessorTest extends PHPUnit_Framework_TestCase {
   public function testRun() {
     $_SERVER['REQUEST_METHOD'] = 'GET';
+    $_ENV['callback'] = array();
     $processor = new ActionProcessor;
     $processor->run(array(
       'class' => 'TestAction', 'method' => array('GET')
     ));
-    $this->assertEquals('TestAction->GET', $_ENV['callback']);
+    $this->assertEquals('TestAction->GET', $_ENV['callback'][0]);
   }
 
   /**
