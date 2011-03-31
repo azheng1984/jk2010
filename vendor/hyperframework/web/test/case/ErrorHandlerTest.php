706cac434618725a7798e159d55451e9de1af884
wangzheng <none@gmail.com>
2011-03-31 23:39:40 +0800

    -

--- vendor/hyperframework/web/test/case/ErrorHandlerTest.php
+++ vendor/hyperframework/web/test/case/ErrorHandlerTest.php
@@ -1,18 +1,21 @@
 <?php
 class ErrorHandlerTest extends PHPUnit_Framework_TestCase {
+  /**
+   * @expectedException PHPUnit_Framework_Error
+   */
   public function testReload() {
     $app = $this->getMock('Application', array('run'));
     $app->expects($this->once())->method('run')->with(
       $this->equalTo('/error/internal_server_error')
     );
-    $handler = new ErrorHandler($app);
     $exception = new Exception;
+    $handler = new ErrorHandler($app);
+    $isCatched = false;
     try {
       $handler->handle($exception);
     } catch (PHPUnit_Framework_Error $error) {
       $this->assertEquals($_ENV['exception'], $exception);
-      return;
+      throw $error;
     }
-    $this->fail();
   }
 }
