a0c459c88eb66c847786e85365fd5427d00cbf3a
azheng1984 <azheng1984@126.com>
2011-04-01 11:39:31 +0800

    -

--- vendor/hyperframework/web/test/case/ViewProcessorTest.php
+++ vendor/hyperframework/web/test/case/ViewProcessorTest.php
@@ -1,9 +1,12 @@
 <?php
 class ViewProcessorTest extends PHPUnit_Framework_TestCase {
-  public function testRun() {
+  protected function setUp() {
     $_ENV['callback'] = array();
-    $processor = new ViewProcessor;
-    $processor->run(array('Screen' => 'TestScreen'));
+  }
+
+  public function testRenderView() {
+    $this->process();
+    $this->assertEquals(1, count($_ENV['callback']));
     $this->assertEquals('TestScreen->render', $_ENV['callback'][0]);
   }
 
@@ -11,8 +14,17 @@ class ViewProcessorTest extends PHPUnit_Framework_TestCase {
    * @expectedException UnsupportedMediaTypeException
    */
   public function testMethodNotAllowed() {
-    $processor = new ViewProcessor;
     $_ENV['media_type'] = 'Handheld';
+    try {
+      $this->process();
+    } catch (UnsupportedMediaTypeException $exception) {
+      $this->assertEquals(0, count($_ENV['callback']));
+      throw $exception;
+    }
+  }
+
+  private function process() {
+    $processor = new ViewProcessor;
     $processor->run(array('Screen' => 'TestScreen'));
   }
 }
