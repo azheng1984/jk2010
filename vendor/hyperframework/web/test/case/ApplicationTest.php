cdcaf92ac3874d471839a1472f4ddd7225203607
azheng1984 <azheng1984@126.com>
2011-04-01 11:06:45 +0800

    -

--- vendor/hyperframework/web/test/case/ApplicationTest.php
+++ vendor/hyperframework/web/test/case/ApplicationTest.php
@@ -7,13 +7,14 @@ class ApplicationTest extends PHPUnit_Framework_TestCase {
     self::$app = new Application;
   }
 
+  protected function setUp() {
+    $_ENV['callback'] = array();
+  }
+
   public function testPathWithParameter() {
     $_SERVER['REQUEST_URI'] = '/?key=value';
-    $_ENV['callback'] = array();
     self::$app->run();
-    $this->assertEquals(2, count($_ENV['callback']));
-    $this->assertEquals('TestAction->GET', $_ENV['callback'][0]);
-    $this->assertEquals('TestScreen->render', $_ENV['callback'][1]);
+    $this->verifyCallback();
   }
 
   /**
@@ -22,11 +23,24 @@ class ApplicationTest extends PHPUnit_Framework_TestCase {
    */
   public function testPathNotFound() {
     $_SERVER['REQUEST_URI'] = '/inexistent_path';
-    self::$app->run();
+    try {
+      self::$app->run();
+    } catch (NotFoundException $exception) {
+      $this->assertEquals(0, count($_ENV['callback']));
+      throw $exception;
+    }
   }
 
   public function testRewritePath() {
     $_SERVER['REQUEST_URI'] = '/inexistent_path';
+    $_ENV['callback'] = array();
     self::$app->run('/');
+    $this->verifyCallback();
+  }
+
+  private function verifyCallback() {
+    $this->assertEquals(2, count($_ENV['callback']));
+    $this->assertEquals('TestAction->GET', $_ENV['callback'][0]);
+    $this->assertEquals('TestScreen->render', $_ENV['callback'][1]);
   }
 }
