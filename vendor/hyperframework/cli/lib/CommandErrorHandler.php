a9fb76d688903e9b31ac05b0afa500dcebb16086
azheng1984 <azheng1984@126.com>
2011-04-02 17:04:33 +0800

    -

--- vendor/hyperframework/cli/lib/CommandErrorHandler.php
+++ vendor/hyperframework/cli/lib/CommandErrorHandler.php
@@ -4,6 +4,10 @@ class CommandErrorHandler {
     set_exception_handler(array($this, 'handle'));
   }
 
+  public function stop() {
+    restore_exception_handler();
+  }
+
   public function handle($exception) {
     fwrite(STDERR, $exception);
     exit($exception->getCode());
