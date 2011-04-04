0debcf714c44c556b5edb9deb768c21ad7fbd51e
wangzheng <none@gmail.com>
2011-04-04 19:57:23 +0800

    -

---
+++ vendor/hyperframework/web/lib/ExceptionHandler.php
@@ -0,0 +1,34 @@
+<?php
+class ExceptionHandler {
+  private $app;
+
+  public function __construct($app) {
+    $this->app = $app;
+  }
+
+  public function run() {
+    set_exception_handler(array($this, 'handle'));
+  }
+
+  public function stop() {
+    restore_exception_handler();
+  }
+
+  public function handle($exception) {
+    if (!headers_sent()) {
+      $this->reload($exception);
+    }
+    trigger_error($exception, E_USER_ERROR);
+  }
+
+  private function reload($exception) {
+    if (!$exception instanceof ApplicationException) {
+      $exception = new InternalServerErrorException;
+    }
+    $config = require CONFIG_PATH.'error_handler.config.php';
+    $statusCode = $exception->getCode();
+    if (isset($config[$statusCode])) {
+      $this->app->run($config[$statusCode]);
+    }
+  }
+}
