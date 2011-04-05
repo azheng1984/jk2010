085694854c31802056b0a76d5c9d8ba5d8f020b5
azheng1984 <azheng1984@126.com>
2011-04-05 10:54:50 +0800

    -

--- vendor/hyperframework/web/lib/action/ActionProcessor.php
+++ vendor/hyperframework/web/lib/action/ActionProcessor.php
@@ -2,7 +2,7 @@
 class ActionProcessor {
   public function run($cache) {
     $method = $_SERVER['REQUEST_METHOD'];
-    if (!in_array($method, $cache['method'], true)) {
+    if (in_array($method, $cache['method'], true) === false) {
       throw new MethodNotAllowedException($cache['method']);
     }
     $action = new $cache['class'];
--- vendor/hyperframework/web/lib/view/ViewProcessor.php
+++ vendor/hyperframework/web/lib/view/ViewProcessor.php
@@ -1,9 +1,11 @@
 <?php
 class ViewProcessor {
   public function run($cache) {
-    $type = isset($_SERVER['REQUEST_MEDIA_TYPE']) ?
-      $_SERVER['REQUEST_MEDIA_TYPE'] : $cache[0];
-    if (!isset($cache[$type])) {
+    if (isset($_SERVER['REQUEST_MEDIA_TYPE']) === false) {
+      $_SERVER['REQUEST_MEDIA_TYPE'] = key($cache);
+    }
+    $type = $_SERVER['REQUEST_MEDIA_TYPE'];
+    if (isset($cache[$type]) === false) {
       throw new UnsupportedMediaTypeException;
     }
     $view = new $cache[$type];
