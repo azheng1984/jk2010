914d83bd83d068713918be8097e709544ed77e8a
wangzheng <none@gmail.com>
2011-04-06 17:20:01 +0800

    -

--- vendor/hyperframework/cli/test/case/explorer/OptionExplorerTest.php
+++ vendor/hyperframework/cli/test/case/explorer/OptionExplorerTest.php
@@ -1,34 +1,30 @@
 <?php
-class OptionExplorerTest extends CliTestCase {
-  protected function tearDown() {
-    ExplorerContext::reset();
-  }
-
+class OptionExplorerTest extends ExplorerTestCase {
   public function testRenderHead() {
-    $this->expectOutput(
-      '--test-option',
-      '  test-option-description'
-    );
     ExplorerContext::getExplorer('Option')->render('test-option', array(
       'description' => 'test-option-description',
     ));
+    $this->assertOutput(
+      '--test-option',
+      '  test-option-description'
+    );
   }
 
   public function testRenderShortList() {
-    $this->expectOutput(
-      '--test-option, -first-short-option, -second-short-option'
-    );
     ExplorerContext::getExplorer('Option')->render('test-option', array(
       'short' => array('first-short-option', 'second-short-option'),
     ));
+    $this->assertOutput(
+      '--test-option, -first-short-option, -second-short-option'
+    );
   }
 
   public function testRenderShort() {
-    $this->expectOutput(
-      '--test-option, -short-option'
-    );
     ExplorerContext::getExplorer('Option')->render('test-option', array(
       'short' => 'short-option',
     ));
+    $this->assertOutput(
+      '--test-option, -short-option'
+    );
   }
 }
