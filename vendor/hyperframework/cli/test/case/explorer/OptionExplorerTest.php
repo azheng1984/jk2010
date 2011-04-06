e0eaa95a292c8f6de57941691371bff98832b32c
azheng1984 <azheng1984@126.com>
2011-04-06 17:27:31 +0800

    -

--- vendor/hyperframework/cli/test/case/explorer/OptionExplorerTest.php
+++ vendor/hyperframework/cli/test/case/explorer/OptionExplorerTest.php
@@ -1,9 +1,9 @@
 <?php
 class OptionExplorerTest extends ExplorerTestCase {
   public function testRenderHead() {
-    ExplorerContext::getExplorer('Option')->render('test-option', array(
-      'description' => 'test-option-description',
-    ));
+    ExplorerContext::getExplorer('Option')->render('test-option',
+      array('description' => 'test-option-description')
+    );
     $this->assertOutput(
       '--test-option',
       '  test-option-description'
@@ -11,18 +11,18 @@ class OptionExplorerTest extends ExplorerTestCase {
   }
 
   public function testRenderShortList() {
-    ExplorerContext::getExplorer('Option')->render('test-option', array(
-      'short' => array('first-short-option', 'second-short-option'),
-    ));
+    ExplorerContext::getExplorer('Option')->render('test-option',
+      array('short' => array('first-short-option', 'second-short-option'))
+    );
     $this->assertOutput(
       '--test-option, -first-short-option, -second-short-option'
     );
   }
 
   public function testRenderShort() {
-    ExplorerContext::getExplorer('Option')->render('test-option', array(
-      'short' => 'short-option',
-    ));
+    ExplorerContext::getExplorer('Option')->render('test-option',
+      array('short' => 'short-option')
+    );
     $this->assertOutput(
       '--test-option, -short-option'
     );
