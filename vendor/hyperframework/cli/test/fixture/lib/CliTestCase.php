6496d3e7d28c0af934ec64af9e3f0e1e6b10b5fd
wangzheng <none@gmail.com>
2011-04-07 13:19:46 +0800

    -

--- vendor/hyperframework/cli/test/fixture/lib/CliTestCase.php
+++ vendor/hyperframework/cli/test/fixture/lib/CliTestCase.php
@@ -1,12 +1,12 @@
 <?php
 abstract class CliTestCase extends PHPUnit_Framework_TestCase {
-  protected function setInputArguments() {
+  protected function setInputArguments(/*...*/) {
     $arguments = func_get_args();
     $_SERVER['argc'] = array_unshift($arguments, 'index.php');
     $_SERVER['argv'] = $arguments;
   }
 
-  protected function assertOutput() {
+  protected function assertOutput(/*...*/) {
     $output = null;
     $lines = func_get_args();
     if (count($lines) !== 0) {
