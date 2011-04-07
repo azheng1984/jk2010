da7242191a095e64ca43cf0e6efe58881ed35bf6
wangzheng <none@gmail.com>
2011-04-07 10:07:13 +0800

    -

--- vendor/hyperframework/cli/test/case/application/CommandApplicationTest.php
+++ vendor/hyperframework/cli/test/case/application/CommandApplicationTest.php
@@ -81,8 +81,7 @@ class CommandApplicationTest extends CliTestCase {
     file_put_contents(
       self::$configPath, '<?php return '.var_export($config, true).';'
     );
-    $_SERVER['argc'] = array_unshift($arguments, 'index.php');
-    $_SERVER['argv'] = $arguments;
+    $this->setArguments($arguments);
     $app = new CommandApplication;
     $app->run();
   }
