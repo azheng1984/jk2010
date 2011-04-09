5f64a81ca811afdf7501b43dc57678777dee714d
wangzheng <none@gmail.com>
2011-04-09 21:41:48 +0800

    -

--- vendor/hyperframework/tool/test/fixture/config/build.config.php
+++ vendor/hyperframework/tool/test/fixture/config/build.config.php
@@ -1,9 +1,9 @@
 <?php return array(
-  'ClassLoader' => array(
+'ClassLoader' => array(
     'app',
     'lib',
-    HYPERFRAMEWORK_PATH.'cli/lib',
     HYPERFRAMEWORK_PATH.'tool/lib',
+    HYPERFRAMEWORK_PATH.'cli/lib',
     HYPERFRAMEWORK_PATH.'tool/app',
   )
 );
