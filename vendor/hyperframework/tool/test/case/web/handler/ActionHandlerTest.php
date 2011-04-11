53500edee3fe7f1d486e8d6dacfb72fbd32f3cf2
wangzheng <none@gmail.com>
2011-04-11 19:23:08 +0800

    -

--- vendor/hyperframework/tool/test/case/web/handler/ActionHandlerTest.php
+++ vendor/hyperframework/tool/test/case/web/handler/ActionHandlerTest.php
@@ -1,6 +1,26 @@
 <?php
 class ActionHandlerTest extends PHPUnit_Framework_TestCase {
-  public function test() {
-    
+  public function testNotAction() {
+    $this->assertNull($this->handle('Test.php'));
+  }
+
+  public function testPublicLowerCaseMethod() {
+    $fullPath = ROOT_PATH.'app/TestPublicLowerCaseMethodAction.php';
+    $this->setExpectedException(
+      'Exception', "Invalid action method 'get' in $fullPath"
+    );
+    $this->handle($fullPath);
+  }
+
+  public function testUpperCaseMethod() {
+    $this->assertSame(
+      array('class' => 'TestAction', 'method' => array('GET')),
+      $this->handle(ROOT_PATH.'app/TestAction.php')
+    );
+  }
+
+  private function handle($fullPath) {
+    $handler = new ActionHandler;
+    return $handler->handle(basename($fullPath), $fullPath);
   }
 }
