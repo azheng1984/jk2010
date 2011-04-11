c295974fe8005925c1035ff142aaf75938f9e08c
azheng1984 <azheng1984@126.com>
2011-04-11 12:01:42 +0800

    -

--- vendor/hyperframework/tool/test/case/web/ApplicationBuilderTest.php
+++ vendor/hyperframework/tool/test/case/web/ApplicationBuilderTest.php
@@ -1,10 +1,9 @@
 <?php
 class ApplicationBuilderTest extends PHPUnit_Framework_TestCase {
   public function testBuild() {
-    $_SERVER['PWD'] = TEST_PATH.'fixture';
     $builder  = new ApplicationBuilder;
     $this->assertTrue(
-      $builder->build('Action') instanceof ApplicationCache
+      $builder->build('View') instanceof ApplicationCache
     );
   }
 }
