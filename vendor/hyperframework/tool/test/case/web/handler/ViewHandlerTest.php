2ffeda5f96bd9fe58d425183736f51842511a960
azheng1984 <azheng1984@126.com>
2011-04-11 21:22:20 +0800

    -

--- vendor/hyperframework/tool/test/case/web/handler/ViewHandlerTest.php
+++ vendor/hyperframework/tool/test/case/web/handler/ViewHandlerTest.php
@@ -1,20 +1,35 @@
 <?php
 class ViewHandlerTest extends PHPUnit_Framework_TestCase {
   public function testNotView() {
-    $handler = new ViewHandler(array('Screen'));
+    $this->assertNull($this->handle('Test', null));
   }
 
   public function testInvalidType() {
-    
+    $this->setExpectedException('Exception', "View type 'test' is invalid");
+    $this->assertNull($this->handle(null, null, 'test'));
   }
 
   public function testNoRenderingMethod() {
-    
+    $fullPath = ROOT_PATH.'app/TestNoRenderingMethodScreen.php';
+    $this->setExpectedException(
+      'Exception', "Rendering method of view not found in '$fullPath'"
+    );
+    $this->handle('TestNoRenderingMethodScreen', $fullPath);
   }
 
   public function testPrivateRenderingMethod() {
+    $fullPath = ROOT_PATH.'app/TestPrivateRenderingMethodScreen.php';
+    $this->setExpectedException(
+      'Exception', "Rendering method of view not public in '$fullPath'"
+    );
+    $this->handle('TestPrivateRenderingMethodScreen', $fullPath);
   }
 
-  public function testReturnCache() {
+  public function testCache() {
+  }
+
+  private function handle($class, $fullPath, $type = 'Screen') {
+    $handler = new ViewHandler($type);
+    return $handler->handle($class, $fullPath);
   }
 }
