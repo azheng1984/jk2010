6eaad6a52045c3ac7e73dc2fa0754589467ef1b8
wangzheng <none@gmail.com>
2011-04-11 16:30:58 +0800

    -

--- vendor/hyperframework/tool/test/case/web/handler/ApplicationHandlerTest.php
+++ vendor/hyperframework/tool/test/case/web/handler/ApplicationHandlerTest.php
@@ -1,6 +1,13 @@
 <?php
 class ApplicationHandlerTest extends PHPUnit_Framework_TestCase {
-  public function test() {
-    
+  public function testHandle() {
+    $handlers = array('Action' => new ActionHandler);
+    $cache = new ApplicationCache($handlers);
+    $handler = new ApplicationHandler($handlers, $cache);
+    $handler->handle(
+      ROOT_PATH.'app'.DIRECTORY_SEPARATOR.'TestAction.php', 'app'
+    );
+    $result = $cache->export();
+    $this->assertTrue(count($result[1]) > 0);
   }
 }
