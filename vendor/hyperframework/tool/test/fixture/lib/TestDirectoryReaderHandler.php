4cc4776d535c1d5b588553bd659345532fd6ad6c
azheng1984 <azheng1984@126.com>
2011-04-11 15:57:13 +0800

    -

--- vendor/hyperframework/tool/test/fixture/lib/TestDirectoryReaderHandler.php
+++ vendor/hyperframework/tool/test/fixture/lib/TestDirectoryReaderHandler.php
@@ -1,10 +1,10 @@
 <?php
 class TestDirectoryReaderHandler {
-  public function handle($fileName, $relativeFolder, $rootFolder) {
+  public function handle($fullPath, $relativeFolder, $rootFolder) {
     $GLOBALS['TEST_CALLBACK_TRACE'][] = array(
       'name' => __CLASS__.'->'.__FUNCTION__,
       'argument' => array (
-        'full_path' => $fileName,
+        'full_path' => $fullPath,
         'relative_folder' => $relativeFolder,
         'root_folder' => $rootFolder
       ),
