53f3641918482bc0bf5c9a6e678a08a4040137ea
azheng1984 <azheng1984@126.com>
2011-04-12 20:39:58 +0800

    -

--- vendor/hyperframework/tool/test/case/CacheExporterTest.php
+++ vendor/hyperframework/tool/test/case/CacheExporterTest.php
@@ -1,17 +1,17 @@
 <?php
 class CacheExporterTest extends FileGenerationTestCase {
   private static $cacheFolder;
-  private static $testCachePath;
+  private static $cacheFile;
 
   public static function setUpBeforeClass() {
     parent::setUpBeforeClass();
     self::$cacheFolder = 'cache'.DIRECTORY_SEPARATOR;
-    self::$testCachePath = self::$cacheFolder.'test.cache.php';
+    self::$cacheFile = self::$cacheFolder.'test.cache.php';
   }
 
   protected function tearDown() {
-    if (is_file(self::$testCachePath)) {
-      unlink(self::$testCachePath);
+    if (is_file(self::$cacheFile)) {
+      unlink(self::$cacheFile);
     }
     if (is_dir(self::$cacheFolder)) {
       rmdir(self::$cacheFolder);
@@ -28,6 +28,6 @@ class CacheExporterTest extends FileGenerationTestCase {
     $exporter = new CacheExporter;
     $exporter->export(new TestCache);
     $cacheVerifier = new TestCacheVerifier;
-    $cacheVerifier->verify($this, self::$testCachePath);
+    $cacheVerifier->verify($this, self::$cacheFile);
   }
 }
