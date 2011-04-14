1271b42112139f15e0187b33752b63f987342c84
azheng1984 <azheng1984@126.com>
2011-04-14 11:28:52 +0800

    -

--- vendor/hyperframework/tool/test/fixture/lib/FileGenerationTestCase.php
+++ vendor/hyperframework/tool/test/fixture/lib/FileGenerationTestCase.php
@@ -1,6 +1,9 @@
 <?php
 class FileGenerationTestCase extends PHPUnit_Framework_TestCase {
+  private static $currentWorkingDirectory;
+
   public static function setUpBeforeClass() {
+    self::$currentWorkingDirectory = getcwd();
     $_SERVER['OLD_PWD'] = $_SERVER['PWD'];
     $_SERVER['PWD'] = ROOT_PATH.'tmp';
     mkdir($_SERVER['PWD']);
@@ -8,7 +11,7 @@ class FileGenerationTestCase extends PHPUnit_Framework_TestCase {
   }
 
   public static function tearDownAfterClass() {
-    chdir(TEST_PATH);
+    chdir(self::$currentWorkingDirectory);
     rmdir($_SERVER['PWD']);
     $_SERVER['PWD'] = $_SERVER['OLD_PWD'];
   }
