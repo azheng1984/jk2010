3582abd24104771bee42448c268ab2af8d40913c
azheng1984 <azheng1984@126.com>
2011-05-05 21:48:03 +0800

    -

--- vendor/wj/app/category/edit/CategoryEditScreen.php
+++ vendor/wj/app/category/edit/CategoryEditScreen.php
@@ -1,16 +1,6 @@
 <?php
 class CategoryEditScreen extends Screen {
   public function renderContent() {
-    echo '<a href="/">首页</a> ';
-    foreach ($_GET['category'] as $cateory) {
-      echo ' &gt; <b>'.$cateory['name'].'</b> ';
-    }
-    echo '<br />';
-    $isLeaf = true;
-    $last = end($_GET['category']);
-    foreach (Db::execute('select * from global_category where parent_id='.$last['id']) as $row) {
-      $isLeaf = false;
-      echo '<a href="'.urlencode($row['name']).'/">'.$row['name'].'</a> [<a href="'.urlencode($row['name']).'.edit/">编辑</a>]';
-    }
+    echo 'edit';
   }
 }
--- vendor/wj/app/product_list/ProductListScreen.php
+++ vendor/wj/app/product_list/ProductListScreen.php
@@ -1,7 +1,6 @@
 <?php
 class ProductListScreen extends Screen {
   public function renderContent() {
-    echo '[<a href="edit">编辑</a>]';
-    echo '<a href="'.urlencode('惠普P2200笔记本电脑').'/">惠普P2200笔记本电脑</a>';
+    echo 'edit';
   }
 }
