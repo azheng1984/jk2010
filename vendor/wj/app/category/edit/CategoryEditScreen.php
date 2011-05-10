195693ea8ae5129444cb449ae92513a0a87f59e5
wangzheng <none@gmail.com>
2011-05-10 23:56:18 +0800

    -

--- vendor/wj/app/category/edit/CategoryEditScreen.php
+++ vendor/wj/app/category/edit/CategoryEditScreen.php
@@ -7,7 +7,8 @@ class CategoryEditScreen extends Screen {
     echo '<form method="POST" action=".">';
     echo '<input type="hidden" name="_method" value="PUT">';
     echo '<input type="hidden" name="type" value="category">';
-    echo '<input id= name="name" type="text" value="'.$currentCategory['name'].'" />';
+    echo '<input name="id" type="hidden" value="'.$currentCategory['id'].'" />';
+    echo '<input name="name" type="text" value="'.$currentCategory['name'].'" />';
     echo '<input type="submit" />';
     echo '</form>';
   }
