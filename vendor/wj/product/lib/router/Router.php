dab7cc3ff48913b4ad2be12e99a4923013b0ed58
wangzheng <none@gmail.com>
2011-05-19 17:32:07 +0800

    -

--- vendor/wj/product/lib/router/Router.php
+++ vendor/wj/product/lib/router/Router.php
@@ -1,15 +1,55 @@
 <?php
 class Router {
+  private $path = 'category';
+
   public function execute() {
-    $pathBuilder = new PathBuilder;
     $sections = explode('?', $_SERVER['REQUEST_URI'], 2);
     if ($sections[0] === '/') {
       return '/home';
     }
-    $path = $pathBuilder->execute(explode('/', $sections[0]));
-    if (isset($_GET['_path'])) {
-      $path = $this->rewritePath($path);
+    $GLOBALS['target_data'] = array();
+    foreach (explode('/', $sections[0]) as $section) {
+      if ($this->path === '/category') {
+        $this->parseCategoryPath($section);
+        continue;
+      }
+      if ($this->path === '/product_list') {
+        $this->parseProductPath($section);
+        continue;
+      }
+      $this->parsePropertyPath($section);
     }
-    return $path;
+    return $this->path;
+  }
+
+  private function parseCategoryPath($section) {
+    $parent = null;
+    if (isset($GLOBALS['target_data']['category'])) {
+      $parent = $GLOBALS['target_data']['category'];
+    }
+    $category = Category::getData($section, $parent);
+    $this->setTarget('category', $category);
+    if ($category['table_prefix'] !== null) {
+      $this->path = '/product_list';
+    }
+  }
+
+  private function parseProductPath($section) {
+    $product = Product::getData($section, $GLOBALS['target']['category']);
+    $this->setTarget('product', $product);
+    $this->path = '/product';
+  }
+
+  private function parsePropertyPath($section) {
+    $property = Property::getData($section, $GLOBALS['target']['product']);
+    $this->setTarget('property', $property);
+    $this->path = 'property';
+  }
+
+  private function setTarget($key, $value) {
+    if ($value === null) {
+      throw new NotFoundException;
+    }
+    $GLOBALS['target_data'][$key] = $value;
   }
 }
