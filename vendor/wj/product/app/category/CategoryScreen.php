c1760aaf18fc01abcad00a985581d75d6688cd5e
wangzheng <none@gmail.com>
2011-05-22 17:00:59 +0800

    -

--- vendor/wj/product/app/category/CategoryScreen.php
+++ vendor/wj/product/app/category/CategoryScreen.php
@@ -3,8 +3,7 @@ class CategoryScreen extends Screen {
   public function renderContent() {
     $breadcrumb = new Breadcrumb;
     $breadcrumb->render();
-    $category = $GLOBALS['category'];
-    echo '[<a href="'.$category->getEditLink().'">编辑</a> | <a href="'.$category->getNewLink().'">新建分类</a>]';
+    echo '[<a href="'.$GLOBALS['category']->getEditLink().'">编辑</a> | <a href="'.$GLOBALS['category']->getNewLink().'">新建分类</a>]';
     echo '<br />';
     foreach (Category::getList($GLOBALS['category']) as $row) {
       echo '<div><a href="'.urlencode($row['name']).'/">'.$row['name'].'</a><input type="button" value="删除" /></div>';
