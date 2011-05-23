2a0ab1eb52afe78f4665ad25563fe6238c394535
azheng1984 <azheng1984@126.com>
2011-05-23 12:01:44 +0800

    -

--- vendor/wj/product/app/category/CategoryScreen.php
+++ vendor/wj/product/app/category/CategoryScreen.php
@@ -3,10 +3,8 @@ class CategoryScreen extends Screen {
   public function renderContent() {
     $breadcrumb = new Breadcrumb;
     $breadcrumb->render();
-    echo '[<a href="'.$GLOBALS['category']->getEditLink().'">编辑</a> | <a href="'.$GLOBALS['category']->getNewLink().'">新建分类</a>]';
-    echo '<br />';
     foreach (Category::getList($GLOBALS['category']) as $row) {
-      echo '<div><a href="'.urlencode($row['name']).'/">'.$row['name'].'</a><input type="button" value="删除" /></div>';
+      echo '<div><a href="'.urlencode($row['name']).'/">'.$row['name'].'</a></div>';
     }
   }
 }
--- vendor/wj/product/app/home/HomeScreen.php
+++ vendor/wj/product/app/home/HomeScreen.php
@@ -1,9 +1,8 @@
 <?php
 class HomeScreen extends Screen {
   public function renderContent() {
-    echo '<div>[<a href="?view=new_category">新建分类</a>]</div>';
     foreach (Category::getList() as $row) {
-      echo '<div><a href="/'.urlencode($row['name']).'/">'.$row['name'].'</a> <input type="button" value="删除" /></div>';
+      echo '<div><a href="/'.urlencode($row['name']).'/">'.$row['name'].'</a></div>';
     }
   }
 }
