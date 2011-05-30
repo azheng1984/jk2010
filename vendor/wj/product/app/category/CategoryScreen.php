08504cd8e83bd8b9782308cb88a9e4e1d179e0d6
azheng1984 <azheng1984@126.com>
2011-05-30 21:30:34 +0800

    -

--- vendor/wj/product/app/category/CategoryScreen.php
+++ vendor/wj/product/app/category/CategoryScreen.php
@@ -3,6 +3,7 @@ class CategoryScreen extends Screen {
   public function renderContent() {
     $breadcrumb = new Breadcrumb;
     $breadcrumb->render();
+    echo ' <h1>'.$GLOBALS['category']->getName().'</h1>';
     foreach (Category::getList($GLOBALS['category']) as $row) {
       echo '<div><a href="'.urlencode($row['name']).'/">'.$row['name'].'</a></div>';
     }
