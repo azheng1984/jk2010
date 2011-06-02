abb1ec3ee5f00914d5a0cfaef775139190369cbc
unknown <zjfcwangzheng@126.com>
2011-06-02 15:06:09 +0800

    -

--- config/build.config.php
+++ config/build.config.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-  'Application' => array('Action', 'View' => array('Screen', 'Image')),
   'ClassLoader' => array('app', 'lib', HYPERFRAMEWORK_PATH.'web/lib'),
+  'Application' => array('Action', 'View' => array('Screen', 'Image')),
 );
