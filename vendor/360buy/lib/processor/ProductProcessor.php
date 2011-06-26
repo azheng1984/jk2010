fc5660d5e370eed06717bb379efa27dc0a50494c
wangzheng <none@gmail.com>
2011-06-26 22:45:21 +0800

    -

--- vendor/360buy/lib/processor/ProductProcessor.php
+++ vendor/360buy/lib/processor/ProductProcessor.php
@@ -1,6 +1,13 @@
 <?php
 class ProductListProcessor {
   public function execute($arguments) {
-    
+    $product = new Product;
+    $client = new WebClient;
+    $result = $client->get(
+      'www.360buy.com', '/product/'.$arguments['path'].'.html'
+    );
+    $product->insert(
+      $arguments['id'], $arguments['category_id'], $result['content']
+    );
   }
 }
