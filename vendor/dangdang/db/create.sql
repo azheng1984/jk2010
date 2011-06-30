40e74170c7adb942930d35f1fc4acc7a7154f27e
wangzheng <none@gmail.com>
2011-06-30 21:36:41 +0800

    -

---
+++ vendor/dangdang/db/create.sql
@@ -0,0 +1,64 @@
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `price` decimal(9,2) DEFAULT NULL,
+  `list_price` decimal(9,2) DEFAULT NULL,
+  `promotion_price` decimal(9,2) DEFAULT NULL,
+  `html` blob NOT NULL,
+  `version` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `product_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `property_value_id` int(11) DEFAULT NULL,
+  `page` int(11) NOT NULL,
+  `path` varchar(128) DEFAULT NULL,
+  `html` blob NOT NULL,
+  `version` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `path` (`category_id`,`property_value_id`,`page`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `property_key` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `key` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key` (`category_id`,`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `property_value` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_id` int(11) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `alias` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `value` (`key_id`,`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `arguments` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `task_fail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `arguments` text,
+  `result` blob,
+  `time` datetime DEFAULT NULL,
+  `retry_count` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
