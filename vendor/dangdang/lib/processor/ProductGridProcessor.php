99b87baefefe3aacd94462ec95f61464ba263f92
azheng1984 <azheng1984@126.com>
2011-07-03 23:03:30 +0800

    -

--- vendor/dangdang/lib/processor/ProductGridProcessor.php
+++ vendor/dangdang/lib/processor/ProductGridProcessor.php
@@ -1,5 +1,5 @@
 <?php
-class ProductListProcessor {
+class ProductGridProcessor {
   private $html;
   private $name;
   private $page;
@@ -7,15 +7,12 @@ class ProductListProcessor {
 
   public function execute($arguments) {
     $result = WebClient::get(
-      'www.360buy.com', '/products/'.$arguments['path'].'.html'
+      'category.dangdang.com', 'list?cat='.$arguments['category_id']
     );
     $this->page = $arguments['page'];
-    $this->html = $result['content'];
-    if ($this->html === false) {
+      if (($this->html = $result['content']) === false) {
       return $result;
     }
-    $this->name = $arguments['name'];
-    $this->parseBreadcrumb($arguments);
     $this->saveContent($arguments);
     if ($this->page === 1) {
       $this->parsePropertyList();
@@ -24,70 +21,47 @@ class ProductListProcessor {
     $this->parseNextPage();
   }
 
-  private function saveContent($arguments) {
+  private function saveContent($arguments, $url) {
     DbProductList::insert(
-      $this->categoryId, null, $arguments['path'], $this->page, $this->html
+      $this->categoryId, null, $url, $this->page, $this->html
     );
   }
 
   private function parsePropertyList() {
-    $matches = array();
-    preg_match(
-      '{<div id="select" [\s|\S]*<!--select end -->}', $this->html, $matches
-    );
-    if (count($matches) > 0) {
-      $section = iconv('gbk', 'utf-8', $matches[0]);
-      preg_match_all(
-        '{<dl.*?</dl>}', $section, $matches
-      );
-      foreach ($matches[0] as $item) {
-        preg_match_all(
-          "{<dt>(.*?)：</dt>}", $item, $matches
-        );
-        $key = $matches[1][0];
-        if ($key === '价格') {
-          continue;
-        }
-        preg_match_all(
-          "{<a.*?href='(.*?).html'.*?>(.*?)</a>}", $item, $matches
-        );
-        $valueLinkList = $matches[1];
-        $valueList = $matches[2];
-        $valueAmount = count($valueList);
-        $keyId = DbProperty::getOrNewKeyId($this->categoryId, $key);
-        for ($index = 0; $index < $valueAmount; ++$index) {
-          $value = $valueList[$index];
-          if ($value === '全部' || $value === '其它') {
-            continue;
+    $pattern = '{<div class="condition_panel" id="condition_panel">'
+      .'[\s|\S]*<div class="bottom_panel">}';
+    if (preg_match($pattern, $this->html, $match) === 1) {
+      $section = iconv('gbk', 'utf-8', $match[0]);
+      foreach (explode('<div class="tip">', $section) as $item) {
+        if (preg_match(
+          '{<div class="conditions panel_fold">.*?}', $item, $match
+        ) === 1) {
+          if (preg_match(
+            '{\s+<span>(.*?)</span>}', $item, $match
+          ) === 1) {
+            $key = $match[1];
+            if ($key === '价格') {
+              continue;
+            }
+            $keyId = DbProperty::getOrNewKeyId($this->categoryId, $key);
+            $pattern = '{<a href="http://category.dangdang.com/list?att=(.*?)&cat='.$this->categoryId.'" title="(.*?)">}';
+            preg_match_all($pattern, $item, $matches, PREG_SET_ORDER);
+            foreach ($matches as $attribute) {
+              $value = $attribute[2];
+              if ($value === '全部' || $value === '其它') {
+                continue;
+              }
+              $valueId = DbProperty::getOrNewValueId($keyId, $value);
+              DbTask::add('PropertyProductList', array(
+                'attribute_id' => $attribute[1],
+                'category_id' => $this->categoryId,
+                'value_id' => $valueId,
+                'page' => 1
+              ));
+            }
           }
-          $valueId = DbProperty::getOrNewValueId($keyId, $valueList[$index]);
-          DbTask::add('PropertyProductList', array(
-            'path' => $valueLinkList[$index],
-            'category_id' => $this->categoryId,
-            'value_id' => $valueId,
-            'page' => 1
-          ));
         }
       }
-    }
-  }
-
-  private function parseBreadcrumb($arguments) {
-    if (isset($arguments['category_id'])) {
-      $this->categoryId = $arguments['category_id'];
-      return;
-    }
-    $matches = array();
-    preg_match_all(
-      '{&gt;&nbsp;<a .*?www.360buy.com.*?">(.*?)</a>}', $this->html, $matches
-    );
-    $this->categoryId = $arguments['root_category_id'];
-    $amount = count($matches[1]);
-    for ($index = 1; $index < $amount; ++$index) {
-      $categoryName = iconv('gbk', 'utf-8', $matches[1][$index]);
-      $this->categoryId = DbCategory::getOrNewId(
-        $categoryName, $this->categoryId
-      );
     }
   }
 
