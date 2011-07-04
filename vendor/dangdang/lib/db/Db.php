b1aa7f976c6de6bfab8ba8d205bf235e007d5700
wangzheng <none@gmail.com>
2011-07-04 21:56:57 +0800

    -

--- vendor/dangdang/lib/db/Db.php
+++ vendor/dangdang/lib/db/Db.php
@@ -10,7 +10,7 @@ class Db {
     return self::execute($sql, $parameters)->fetch(PDO::FETCH_ASSOC);
   }
 
-  public function getAll($sql, $parameters = array()) {
+  public static function getAll($sql, $parameters = array()) {
     return self::execute($sql, $parameters)->fetchAll(PDO::FETCH_ASSOC);
   }
 
@@ -38,7 +38,7 @@ class Db {
   private static function getConnection() {
     if (self::$connection === null) {
       self::$connection = new PDO(
-        "mysql:host=localhost;dbname=dangdang",
+        "mysql:host=localhost;dbname=source_dangdang",
         "root",
         "a841107!",
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
