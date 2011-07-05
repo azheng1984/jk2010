fbaf9fc619859fe9360170817f1c53ce7a9ce23f
unknown <zjfcwangzheng@126.com>
2011-07-05 16:33:02 +0800

    -a

---
+++ vendor/wj_spider/lib/command/TaskRetryDumpCommand.php
@@ -0,0 +1,5 @@
+<?php
+class TaskRetryDumpCommand {
+  public function execute() {
+  }
+}
---
+++ vendor/wj_spider/lib/db/DbTaskRetry.php
@@ -0,0 +1,34 @@
+<?php
+class DbTaskRetry {
+  public static function insert($task, $result) {
+    if ($task['is_retry']) {
+      $sql = 'insert into retry_task(task_id, type, arguments, result, `time`)'
+        .' values(?, ?, ?, now())';
+      Db::executeNonQuery($sql, array($type, $arguments, $result));
+    }
+  }
+
+  public static function get($id) {
+    
+  }
+
+  public static function getAll() {
+    
+  }
+
+  public static function retry($id = null) {
+    $sql = 'select * from retry_task';
+    foreach (Db::getAll($sql) as $task) {
+      $sql = "insert into task(type, arguments) values(?, ?)";
+      Db::executeNonQuery($sql, array($task['type'], $task['arguments']));
+    }
+  }
+
+  private static function retryAll() {
+    $sql = 'select * from retry_task';
+    foreach (Db::getAll($sql) as $task) {
+      $sql = "insert into task(type, arguments) values(?, ?)";
+      Db::executeNonQuery($sql, array($task['type'], $task['arguments']));
+    }
+  }
+}
