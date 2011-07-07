b4da0f561db515ce27b68dd503f690018722203b
unknown <zjfcwangzheng@126.com>
2011-07-07 18:13:53 +0800

    -a

--- vendor/wj_spider/lib/db/DbCategory.php
+++ vendor/wj_spider/lib/db/DbCategory.php
@@ -5,8 +5,10 @@ class DbCategory {
       .Db::getFilter('parent_id', $parentId)." and `name`=?";
     $row = Db::getRow($sql, array($parentId, $name));
     if ($row === false) {
-      Db::executeNonQuery("insert into category(parent_id, `name`)"
-        ." values(?, ?)", array($parentId, $name));
+      Db::executeNonQuery(
+        "insert into category(parent_id, `name`) values(?, ?)",
+        array($parentId, $name)
+      );
       return Db::getLastInsertId();
     }
     return $row['id'];
