8ce20a56c4e31daa801ee5fabeccbc92b329f97b
wangzheng <none@gmail.com>
2011-07-11 13:55:02 +0800

    -a

---
+++ vendor/wj_spider/lib/command/RetryTaskCommand.php
@@ -0,0 +1,20 @@
+<?php
+class RetryTaskCommand {
+  public function execute($id = null) {
+    if ($id !== null) {
+      $task = DbTaskRetry::get($id);
+      $this->moveTask($task);
+      return;
+    }
+    foreach (DbTaskRetry::getAll() as $task) {
+      $this->moveTask($task);
+    }
+  }
+
+  private function moveTask($task) {
+    DbTask::reinsert(
+      $task['id'], $task['type'], $task['arguments']
+    );
+    DbTaskRetry::delete($task['id']);
+  }
+}
---
+++ vendor/wj_spider/lib/command/ShowTaskCommand.php
@@ -0,0 +1,10 @@
+<?php
+class ShowTaskCommand {
+  private $isExportToFile;
+
+  public function __construct($options) {
+  }
+
+  public function execute($id) {
+  }
+}
