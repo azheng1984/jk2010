05d69e3dc2d2bf80478c34257f920b36285e303b
wangzheng <none@gmail.com>
2011-07-12 20:30:08 +0800

    -

--- vendor/dangdang/public/index.php
+++ vendor/dangdang/public/index.php
@@ -4,7 +4,6 @@ define('ROOT_PATH', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 define('CACHE_PATH', ROOT_PATH.'cache'.DIRECTORY_SEPARATOR);
 define('CONFIG_PATH', ROOT_PATH.'config'.DIRECTORY_SEPARATOR);
 
-define('DB_NAME', 'dangdang');
 define('NO_IMAGE_MD5', '074f08d6ad97e753c7d99553c7fa530a');
 
 require ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'env.php';require HYPERFRAMEWORK_PATH.'class_loader'.DIRECTORY_SEPARATOR
