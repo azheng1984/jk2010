258ffd30d8822cc4a7ed35357d24d1ffca920fb9
wangzheng <none@gmail.com>
2011-07-12 23:04:59 +0800

    -

--- vendor/wj_spider/lib/db/DbProperty.php
+++ vendor/wj_spider/lib/db/DbProperty.php
@@ -4,16 +4,15 @@ class DbProperty {
     $sql = 'select * from property_key where category_id = ? and `key` = ?';
     $row = Db::getRow($sql, array($categoryId, $name));
     if ($row === false) {
-      Db::executeNonQuery('insert into property_key(`key`, category_id)'
-        .' values(?, ?)', array($name, $categoryId));
+      $sql = 'insert into property_key(`key`, category_id) values(?, ?)';
+      Db::executeNonQuery($sql, array($name, $categoryId));
       return Db::getLastInsertId();
     }
     return $row['id'];
   }
 
   public static function getOrNewValueId($keyId, $name) {
-    $sql = 'select * from property_value'
-      .' where key_id = ? and `value` = ?';
+    $sql = 'select * from property_value where key_id = ? and `value` = ?';
     $row = Db::getRow($sql, array($keyId, $name));
     if ($row === false) {
       $sql = 'insert into property_value(key_id, `value`) values(?, ?)';
