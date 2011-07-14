b49c14d1528037d6be06690af93816715d7dae8b
wangzheng <none@gmail.com>
2011-07-14 10:57:35 +0800

    -a

--- vendor/wj_spider/lib/db/DbTaskRecord.php
+++ vendor/wj_spider/lib/db/DbTaskRecord.php
@@ -12,7 +12,7 @@ class DbTaskRecord {
 
   public static function insert($taskId, $result) {
     $sql = 'INSERT INTO task_record(task_id, result, `time`)'
-      .' VALUES(?, ?, now())';
+      .' VALUES(?, ?, NOW())';
     Db::executeNonQuery($sql, array($taskId, var_export($result, true)));
   }
 }
