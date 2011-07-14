d5e09ad605d6e0c7f232b10d0b6e2331110dd470
unknown <zjfcwangzheng@126.com>
2011-07-14 09:22:10 +0800

    -a

--- vendor/dangdang/public/index.php
+++ vendor/dangdang/public/index.php
@@ -7,7 +7,8 @@ $GLOBALS['no_image_md5'] = array(
   'aff02bb2aaa921e4671522b94f0061a5' => true,
   '074f08d6ad97e753c7d99553c7fa530a' => true,
 );
-require ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'env.php';require HYPERFRAMEWORK_PATH.'class_loader'.DIRECTORY_SEPARATOR
+require ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'env.php';
+require HYPERFRAMEWORK_PATH.'class_loader'.DIRECTORY_SEPARATOR
   .'lib'.DIRECTORY_SEPARATOR.'ClassLoader.php';
 $classLoader = new ClassLoader;
 $classLoader->run();
