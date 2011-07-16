4593a5d25e65a9aa8ee257d3a0c82450eae9a75d
wangzheng <none@gmail.com>
2011-07-16 15:48:14 +0800

    -

--- vendor/hyperframework/web/lib/action/MethodNotAllowedException.php
+++ vendor/hyperframework/web/lib/action/MethodNotAllowedException.php
@@ -2,6 +2,6 @@
 class MethodNotAllowedException extends ApplicationException {
   public function __construct($methods, $message = null) {
     parent::__construct($message, '405 Method Not Allowed');
-    header('Method: '.implode(';', $methods));
+    header('Allow: '.implode(', ', $methods));
   }
 }
