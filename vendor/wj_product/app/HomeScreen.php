77025e56bc1fbe05762eccd89db020104bf7224f
wangzheng <none@gmail.com>
2011-07-17 22:42:44 +0800

    -

--- vendor/wj_product/app/HomeScreen.php
+++ vendor/wj_product/app/HomeScreen.php
@@ -1,6 +1,7 @@
 <?php
-class HomeScreen {
-  public function render() {
-    echo 'Home!';
+class HomeScreen extends Screen {
+  public function renderContent() {
+    echo '<a href="数码/">数码</a>';
+    echo ' <a href="日用化妆/">日用化妆</a>';
   }
 }
--- vendor/wj_product/lib/db/DbProduct.php
+++ vendor/wj_product/lib/db/DbProduct.php
@@ -1,6 +1,14 @@
 <?php
 class DbProduct {
-  public static function get($id) {
-    return Db::getRow('SELECT * FROM global_product_index WHERE id = ?', $id);
+  public static function getIndex($id) {
+    return Db::getRow(
+      'SELECT * FROM global_product_index WHERE product_id = ?', $id
+    );
+  }
+
+  public static function get($tablePrefix, $id) {
+    return Db::getRow(
+      'SELECT * FROM '.$tablePrefix.'_product WHERE id = ?', $id
+    );
   }
 }
