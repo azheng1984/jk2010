15d419ea2a0639c2f57e111a27eccae284dcc772
wangzheng <none@gmail.com>
2011-07-18 16:14:00 +0800

    -a

--- vendor/wj_product/lib/view/Breadcrumb.php
+++ vendor/wj_product/lib/view/Breadcrumb.php
@@ -1,6 +1,27 @@
 <?php
 class Breadcrumb {
-  public function render() {
+  public function render($categories) {
+    array_pop($categories);
+    echo '<div id="breadcrumb">';
     echo '<a href="/">首页</a> ';
+    $distance = count($categories);
+    foreach ($categories as $category) {
+      echo '&gt; <a href="'.$this->getPath($distance).'">'
+        .$category['name'].'</a> ';
+      --$distance;
+    }
+    echo '</div>';
+  }
+
+  private function getPath($distance) {
+    $path = '';
+    while ($distance > 0) {
+      if ($path !== '') {
+        $path .= '/';
+      }
+      $path .= '..';
+      --$distance;
+    }
+    return $path;
   }
 }
