8a3789610a555b7ffb642f7549b1e9dce60d47d7
unknown <zjfcwangzheng@126.com>
2011-07-20 17:53:10 +0800

    -a

--- vendor/wj_product/app/product/ProductScreen.php
+++ vendor/wj_product/app/product/ProductScreen.php
@@ -24,8 +24,9 @@ class ProductScreen extends Screen {
     $breadcrumb->render($categories, $this->product);
     echo '<div id="product">';
     echo '<h1>'.$this->product['name'].'</h1>';
+    echo '<div id="action"><a href="/">比较</a> <a href="/">关注 </a> <a href="/">分享</a></div>';
     echo '<div id="property_list">';
-    echo '<img title="'.$this->product['name'].'" class="product_image" src="/x.jpg" />';
+    echo '<img title="'.$this->product['name'].'" class="product_image" src="/x_large.jpg" />';
     echo '<div class="brand">品牌: <a href="/">DELL</a></div>';
     echo '</div>';
     echo '<div id="merchant_list">';
@@ -33,7 +34,20 @@ class ProductScreen extends Screen {
     echo '<tbody><tr><td><a href="/">京东商城</a></td><td>全国</td><td>全国</td><td>￥10.00</td><td>免运费</td><td>￥10.00</td></tr>';
     echo '<tr><td><a href="/">卓越亚马逊</a></td><td>北京 广州 上海</td><td>全国</td><td>￥9.00</td><td>￥5.00</td><td>￥14.00</td></tr>';
     echo '</tbody></table>';
+    $this->renderProductList('同品牌');
+    $this->renderProductList('同类热销');
+    $this->renderProductList('最终购买');
     echo '</div>';
     echo '</div>';
+  }
+
+  private function renderProductList($name) {
+    echo '<div class="featured_name">'.$name.'</div>';
+    echo '<ul class="featured_product_list">';
+    foreach (DbProduct::getList($this->category['table_prefix']) as $item) {
+      echo '<li class="item"><div class="image"><a href="/'.$item['id'].'"><img title="'.$item['name'].'" alt="'.$item['name'].'" src="/x.jpg" /></a></div><h2><a href="/'.$item['id'].'">'
+        .$item['name'].'</a></h2><div class="price_block">￥<span class="price">10000.00</span>~<span class="price">12299.00</span> 7个商城</div></li>';
+    }
+    echo '</ul>';
   }
 }
