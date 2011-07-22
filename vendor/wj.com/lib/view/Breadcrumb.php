757703b20c475c3f07ce809d3e80d666fd941bb5
wangzheng <none@gmail.com>
2011-07-22 15:08:55 +0800

    -a

---
+++ vendor/wj.com/lib/view/Breadcrumb.php
@@ -0,0 +1,47 @@
+<?php
+class Breadcrumb {
+  public function render($categories, $product = null) {
+    $current = $product;
+    if ($product === null) {
+      $current = array_pop($categories);
+    }
+    echo '<div id="breadcrumb">';
+    echo '<a href="/">首页</a> ';
+    if ($product === null) {
+      $this->renderRelativeLink($categories);
+    } else {
+      $this->renderFullLink($categories);
+    }
+    echo '&gt; <h1>'.$current['name'].'</h1>';
+    echo '</div>';
+  }
+
+  private function renderRelativeLink($categories) {
+    $distance = count($categories);
+    foreach ($categories as $category) {
+      echo '&gt; <a href="'.$this->getPath($distance).'">'
+        .$category['name'].'</a> ';
+      --$distance;
+    }
+  }
+
+  private function renderFullLink($categories) {
+    $path = '/';
+    foreach ($categories as $category) {
+      $path .= urlencode($category['name']).'/';
+      echo '&gt; <a href="'.$path.'">'.$category['name'].'</a> ';
+    }
+  }
+
+  private function getPath($distance) {
+    $path = '';
+    while ($distance > 0) {
+      if ($path !== '') {
+        $path .= '/';
+      }
+      $path .= '..';
+      --$distance;
+    }
+    return $path;
+  }
+}
