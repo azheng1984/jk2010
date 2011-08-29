be63c615f0c581d8287c684e4c0c99d3ce510dce
wangzheng <none@gmail.com>
2011-08-29 12:20:02 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -2,11 +2,11 @@
 class HomeScreen extends Screen {
   public function renderContent() {
     $categories = DbCategory::getList();
-    echo '<ul id="category_list">';
+    echo '<div id="product_search"><ul id="category_list" class="home_category_list">';
     foreach ($categories as $category) {
       echo '<li><a href="'.urlencode($category['name']).'/">'
         .$category['name'].'</a></li>';
     }
-    echo '</ul>';
+    echo '</ul></div>';
   }
 }
