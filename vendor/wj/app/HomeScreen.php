65a04eb8b96c69cd6748a828fe5d73efb4fccc9f
azheng1984 <azheng1984@126.com>
2011-09-15 16:27:54 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -2,17 +2,18 @@
 class HomeScreen extends Screen {
   public function renderContent() {
     $categories = DbCategory::getList();
-    echo '<div id="product_search"><ul id="category_list" class="home_category_list">';
-    foreach ($categories as $category) {
-      echo '<li><a href="'.urlencode($category['name']).'/">'
-        .$category['name'].'</a>';
-      $children = DbCategory::getList($category['id']);
-      if (count($children) !== 0) {
-        echo '<div class="children">';
-        echo '<div>'.implode('</div><div>', $this->getChildLinks($category, $children)).'</div>';
-        echo '</div>';
+    echo '<div id="home"><ul id="category_list" class="home_category_list">';
+    for($i = 1; $i < 30; ++$i) {
+      foreach ($categories as $category) {
+        echo '<li><h2><a href="'.urlencode($category['name']).'/">'
+          .$category['name'].'</a></h2>';
+        $children = DbCategory::getList($category['id']);
+        if (count($children) !== 0) {
+          echo '<div class="children">';
+          echo implode(' ', $this->getChildLinks($category, $children)).' ... </div>';
+        }
+        echo '</li>';
       }
-      echo '</li>';
     }
     echo '</ul></div>';
   }
@@ -20,8 +21,10 @@ class HomeScreen extends Screen {
   private function getChildLinks($category, $children) {
     $result = array();
     $parentLink = urlencode($category['name']).'/';
-    foreach ($children as $child) {
-      $result[] = '<a href="'.$parentLink.urlencode($child['name']).'/">'.$child['name'].'</a>';
+    for($i = 1; $i < 30; ++$i) {
+      foreach ($children as $child) {
+        $result[] = '<a href="'.$parentLink.urlencode($child['name']).'/">'.$child['name'].'</a>';
+      }
     }
     return $result;
   }
