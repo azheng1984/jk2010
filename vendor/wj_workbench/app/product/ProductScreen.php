c8a01bb8619e5d536dd7b204b19b5bf5821b58fe
wangzheng <none@gmail.com>
2011-09-26 00:17:53 +0800

    -

--- vendor/wj_workbench/app/product/ProductScreen.php
+++ vendor/wj_workbench/app/product/ProductScreen.php
@@ -17,6 +17,12 @@ class ProductScreen extends Screen {
     echo '<title>货比万家</title>';
     echo '<link type="text/css" href="/css/home.css" charset="utf-8"',
       ' media="screen" rel="stylesheet" />';
+    echo '<link type="text/css" href="/css/category_list.css" charset="utf-8"',
+      ' media="screen" rel="stylesheet" />';
+    echo '<link type="text/css" href="/css/product_list.css" charset="utf-8"',
+      ' media="screen" rel="stylesheet" />';
+    echo '<link type="text/css" href="/css/product.css" charset="utf-8"',
+      ' media="screen" rel="stylesheet" />';
   }
 
   protected function renderBodyContent() {
@@ -28,13 +34,14 @@ class ProductScreen extends Screen {
     }
     $breadcrumb = new Breadcrumb($categories, $this->product);
     $breadcrumb->render();
-    echo '<div id="product"><div class="title">';
-    echo '<div id="action"><div class="right"><a href="/" class="box">对比</a> <a href="/">关注 <span class="note">12134</span></a> <a href="/">分享</a></div></div>';
+    echo '<div id="title_wrapper">';
     echo '<h1>'.$this->product['name'].'</h1>';
-    echo '</div>';
-    echo '<div id="property_list">';
+    echo '<div id="action"><div class="right"><a href="/" class="box">对比</a> <a href="/">关注 <span class="note">12134</span></a> <a href="/">分享</a></div></div>';
+    echo '</div><div id="product">';
+    echo '<div id="property_list_wrapper">';
     echo '<div class="product_image_box"><img title="'.$this->product['name'].'" class="product_image" src="/x.jpg" /></div>';
     $categoryPath = $this->getCategoryPath($categories);
+    echo '<div id="property_list">';
     echo '<div>品牌: <a href="..">诺基亚(Nokia)</a></strong></div>';
     echo '<div>型号: G470AH-ITH</div>';
     foreach (explode(',', $this->product['property_value_list']) as $id) {
@@ -42,23 +49,26 @@ class ProductScreen extends Screen {
       echo '<div>'.$result['key'].': <a rel="nofollow" href="'.$categoryPath.'?'.urlencode($result['key']).'='.urlencode($result['value']).'">'.$result['value'].'</a></div>';
     }
     echo '</div>';
-    echo '<div class="sort">排序: <span class="selected">购买率</span> <a href="/">价格</a></div>';
+    echo '</div>';
+    echo '<div id="right_wrapper">';
+    echo '<div id="sort_wrapper"><div class="sort">排序: <span class="selected">销量</span> <a href="/">价格</a></div></div>';
     echo '<div id="merchant_list">';
     echo '<table>';
     echo '<tbody><tr><td class="logo"><a href="/"  target="_blank"><img alt="京东商城" title="京东商城" class="merchant_logo" src="/360buy.com.2.gif" /> </a></td>';
     echo '<td><a href="/" target="_blank"><span class="merchant_name">京东商城</span></a></td>';
     echo '<td class="price_column"><span class="price_block">&yen;</span><span class="price">10.23</span></td>';
-    echo '<td class="rate"> 购买率: <span class="number">97</span>% <span class="key"></span></td>';
+    echo '<td class="rate"></td>';
     echo '</tr>';
     echo '<tr><td class="logo"><a href="/" target="_blank"><img alt="新蛋" title="新蛋" class="merchant_logo" src="/newegg.com.cn.2.gif" /></a></td>';
     echo '<td><a href="/" target="_blank"><span class="merchant_name">新蛋网</span></a></td>';
     echo '<td class="price_column"><span class="price_block">&yen;</span><span class="price">14</span></a></td>';
-    echo '<td class="rate"> <span class="block">购买率: <span class="number">3</span>%</span> <span class="key"></span></td>';
+    echo '<td class="rate"></td></td>';
     echo '</tr>';
     echo '</tbody></table>';
     $this->renderProductList('笔记本电脑推荐');
     $this->renderAds();
     echo '</div>';
+    echo '</div>';//right wrapper
     echo '</div>';
   }
 
