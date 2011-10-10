ee7130f16957a61e09b6694b44ae8f8fd34c7b22
azheng1984 <azheng1984@126.com>
2011-10-10 13:11:46 +0800

    -

--- vendor/wj_workbench/lib/view/AdSenseScreen.php
+++ vendor/wj_workbench/lib/view/AdSenseScreen.php
@@ -34,4 +34,22 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
 <?php
   }
+
+  public static function renderBigBlock() {
+    if ($_SERVER['HTTP_HOST'] !== 'huobiwanjia.com') {
+      echo 'Google 提供的广告';
+      return;
+    } ?>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6400920337804269";
+/* wj_336x280_test */
+google_ad_slot = "8883448359";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+  <?php }
 }
