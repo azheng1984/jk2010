5eda987d711990026e9bb9029c1c5a802fe035bf
azheng1984 <azheng1984@126.com>
2011-10-15 18:46:50 +0800

    -

--- vendor/wj_workbench/app/product_list/ProductListScreen.php
+++ vendor/wj_workbench/app/product_list/ProductListScreen.php
@@ -67,13 +67,13 @@ class ProductListScreen extends Screen {
     echo '<div id="sort">排序: <span>销量</span> <a rel="nofollow" href="/">新品</a> <a href="/" rel="nofollow">降价</a> <a rel="nofollow" href="/">价格</a></div>';
     echo '<div id="total">找到 '.$result['total'].' 个产品</div>';
     echo '</div>';
-    echo '<div id="product_list_wrapper"><ul id="product_list">';
+    echo '<div id="product_list_wrapper"><ol id="product_list">';
     foreach ($items as $item) {
       $name = $item['brand'].' '.$item['model'].' '.$this->category['name'];
       echo '<li><div class="image"><a target="_blank" href="/'.$item['id'].'"><img title="'.$name.'" alt="'.$name.'" src="http://img.workbench.wj.com/'.$item['id'].'.jpg" /></a></div><div class="title"><a target="_blank" href="/'.$item['id'].'">'
         .$name.'</a></div><div class="data"><div>&yen;<span class="price">'.$item['lowest_price'].'</span> ~ <span class="price">1234567890</span></div> <div>7 个商城</div></div></li>';
     }
-    echo '</ul></div>';
+    echo '</ol></div>';
     $this->renderPagination($result['total']);
     echo '<div id="bottom_ads_wrapper"><div id="bottom_ads">';
     AdSenseScreen::render(true);
