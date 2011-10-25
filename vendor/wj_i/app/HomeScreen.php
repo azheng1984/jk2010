c35f29749cbd7132bc642132b257b92145a49e27
wangzheng <none@gmail.com>
2011-10-25 12:55:51 +0800

    -

--- vendor/wj_i/app/HomeScreen.php
+++ vendor/wj_i/app/HomeScreen.php
@@ -14,7 +14,7 @@ class HomeScreen extends Screen {
     echo '<li><a href="http://activity.wj.com">动态</a></li>';
     echo '<li><a href="http://tracking.wj.com">关注</a></li>';
     echo '<li><a href="http://share.wj.com">分享</a></li>';
-    echo '<li><a href="http://feedback.wj.com">留言</a></li>';
+    echo '<li><a href="http://message.wj.com">留言</a></li>';
     echo '<li><a href="http://advertisement.wj.com">广告</a></li>';
     echo '<li><a href="http://integration.wj.com">数据整合</a></li>';
     echo '<li><a href="http://analytics.wj.com">访客分析</a></li>';
