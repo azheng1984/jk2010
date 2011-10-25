b1e3df7f0215e1256ac0bf684d0ecc8d1997136b
azheng1984 <azheng1984@126.com>
2011-10-25 23:01:38 +0800

    -

--- vendor/wj_builder/config/build.config.php
+++ vendor/wj_builder/config/build.config.php
@@ -1,4 +1,6 @@
 <?php
 return array(
-  'ClassLoader' => array('app', 'lib', '../db', HYPERFRAMEWORK_PATH.'cli/lib'),
+  'ClassLoader' => array(
+    'app', 'lib', '/home/wz/www/jk2010/vendor/db', HYPERFRAMEWORK_PATH.'cli/lib'
+  ),
 );
