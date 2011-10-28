fe4a0ac6bb9c6b8b896862055c85c888ccbc13a5
azheng1984 <azheng1984@126.com>
2011-10-28 22:09:55 +0800

    -

--- vendor/wj_spider/common/lib/database/DbImage.php
+++ vendor/wj_spider/common/lib/database/DbImage.php
@@ -1,5 +1,7 @@
 <?php
 class DbImage {
+  private static $connectionList = array();
+
   public static function createTable($tablePrefix) {
     if (!file_exists($tablePrefix.'_image.sqlite')) {
       DbConnection::connect(
@@ -13,17 +15,27 @@ class DbImage {
   }
 
   public static function deleteImage($tablePrefix, $productId) {
-    DbConnection::connect(
-        'image', new PDO('sqlite:'.$tablePrefix.'_image.sqlite')
-    );
-    $sql = 'DELETE FROM image WHERE ';
+    self::connect($tablePrefix);
+    $sql = 'DELETE FROM image WHERE product_id = ?';
+    Db::execute($sql, $productId);
+    DbConnection::connect('default');
   }
 
-  public static function insertImage($tablePrefix, $productId) {
-    
+  public static function replaceImage($tablePrefix, $productId, $image) {
+    self::connect($tablePrefix);
+    $sql = 'REPLACE INTO image SET product_id = ?, image = ?';
+    Db::execute($sql, $productId, $image);
+    DbConnection::connect('default');
   }
 
-  public static function updateImage($tablePrefix, $productId) {
-    
+  private static function connect($tablePrefix) {
+    if (!isset(self::$connectionList[$tablePrefix])) {
+      DbConnection::connect(
+          $tablePrefix.'_image', new PDO('sqlite:'.$tablePrefix.'_image.sqlite')
+      );
+      self::$connectionList[$tablePrefix] = true;
+      return;
+    }
+    DbConnection::connect($tablePrefix.'_image');
   }
 }
