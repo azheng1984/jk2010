d351cd5d2745f8191311d09e03e41ba0499a4d4d
wangzheng <none@gmail.com>
2011-10-28 22:53:29 +0800

    -

--- vendor/wj_spider/common/lib/database/DbProduct.php
+++ vendor/wj_spider/common/lib/database/DbProduct.php
@@ -7,9 +7,10 @@ class DbProduct {
     );
   }
 
-  public static function getEtag($tablePrefix, $id) {
+  public static function getUpdateInfo($tablePrefix, $id) {
     return Db::getColumn(
-      'SELECT etag FROM '.$tablePrefix.'_product WHERE id = ?', $id
+      'SELECT content_md5, image_md5, image_last_modified FROM '
+      .$tablePrefix.'_product WHERE id = ?', $id
     );
   }
 
@@ -22,11 +23,14 @@ class DbProduct {
     $title,
     $propertyList,
     $description,
-    $etag
+    $contentMd5,
+    $imageMd5,
+    $imageLastModified
   ) {
-    $sql = 'INSERT INTO '.$tablePrefix.'_product(id, category_id, title, property_list,'
-      .' description, etag, lowest_price, highest_price, index_time)'
-      .' VALUES(?, ?, ?, ?, ?, ?, ?, ?, NOW())';
+    $sql = 'INSERT INTO '.$tablePrefix.'_product(id, category_id, title,'
+      .' property_list, description, content_md5, image_md5,'
+      .' image_last_modified, lowest_price, highest_price, index_time)'
+      .' VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())';
     Db::execute(
       $sql,
       $id,
@@ -34,7 +38,9 @@ class DbProduct {
       $title,
       $propertyList,
       $description,
-      $etag,
+      $contentMd5,
+      $imageMd5,
+      $imageLastModified,
       $lowestPrice,
       $highestPrice
     );
@@ -71,11 +77,14 @@ class DbProduct {
         `title` varchar(511) DEFAULT NULL,
         `property_list` text,
         `description` text NOT NULL,
-        `etag` varchar(45) DEFAULT NULL,
+        `content_md5` varchar(45) DEFAULT NULL,
+        `image_md5` varchar(45) DEFAULT NULL,
+        `image_last_modified` datetime DEFAULT NULL,
         `lowest_price` decimal(9,2) DEFAULT NULL,
         `highest_price` decimal(9,2) DEFAULT NULL,
         `index_time` datetime DEFAULT NULL,
-        PRIMARY KEY (`id`)
+        PRIMARY KEY (`id`),
+        KEY `category_id` (`category_id`) USING BTREE
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
       Db::execute($sql);
     }
