7efbc1a10f98c074ba46c71423655ba46a8a2940
wangzheng <none@gmail.com>
2011-10-28 21:55:02 +0800

    -

--- vendor/wj_spider/common/lib/database/DbImage.php
+++ vendor/wj_spider/common/lib/database/DbImage.php
@@ -1,14 +1,29 @@
 <?php
 class DbImage {
   public static function createTable($tablePrefix) {
-    if (file_exists($tablePrefix.'_image.sqlite')) {
+    if (!file_exists($tablePrefix.'_image.sqlite')) {
       DbConnection::connect(
-        'image', new PDO('sqlite:'.$tablePrefix.'_image.sqlite')
+        $tablePrefix.'_image', new PDO('sqlite:'.$tablePrefix.'_image.sqlite')
       );
       $sql = 'CREATE  TABLE "main"."image" '
         .'("product_id" INTEGER PRIMARY KEY  NOT NULL , "image" BLOB NOT NULL )';
       Db::execute($sql);
       DbConnection::connect('defalut');
     }
+  }
+
+  public static function deleteImage($tablePrefix, $productId) {
+    DbConnection::connect(
+        'image', new PDO('sqlite:'.$tablePrefix.'_image.sqlite')
+    );
+    $sql = 'DELETE FROM image WHERE ';
+  }
+
+  public static function insertImage($tablePrefix, $productId) {
+    
+  }
+
+  public static function updateImage($tablePrefix, $productId) {
+    
   }
 }
