44e444bbf951b00e1247e9df77cc659c9b6f2eb8
wangzheng <none@gmail.com>
2011-10-29 17:59:51 +0800

    -

--- vendor/wj_spider/common/lib/database/DbProductProperty.php
+++ vendor/wj_spider/common/lib/database/DbProductProperty.php
@@ -15,6 +15,25 @@ class DbProductProperty {
   }
 
   public static function createTable($tablePrefix) {
-    
+    $table = Db::getColumn(
+      'SHOW TABLES LIKE ?', $tablePrefix.'_product_property'
+    );
+    if ($table === false) {
+      $sql = "CREATE TABLE `".$tablePrefix."_product_property` (
+        `id` int(11) NOT NULL,
+        `merchant_product_id` int(11) DEFAULT NULL,
+        `property_value_id` int(11) DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `product_id-property_value_id` (`merchant_product_id`,`property_value_id`)
+      ) ENGINE=MyISAM DEFAULT CHARSET=latin1";
+      Db::execute($sql);
+    }
+  }
+
+  public static function getListByMerchantProductId(
+    $tablePrefix, $merchantProductId
+  ) {
+    Db::getAll('SELECT property_value_id FROM '.$tablePrefix.'_product_property'
+      .' WHERE merchant_product_id = ?', $merchantProductId);
   }
 }
--- vendor/wj_spider/jingdong/lib/processor/www/ProductProcessor.php
+++ vendor/wj_spider/jingdong/lib/processor/www/ProductProcessor.php
@@ -1,5 +1,9 @@
 <?php
 class ProductProcessor {
+  private $tablePrefix;
+  private $html;
+  private $merchantProductId;
+
   public function execute($arguments) {
     $result = WebClient::get(
       'www.360buy.com', '/product/'.$arguments['id'].'.html'
@@ -7,9 +11,13 @@ class ProductProcessor {
     if ($result['content'] === false) {
       return $result;
     }
-    DbProduct::insert(
-      $arguments['id'], $arguments['category_id'], $result['content']
-    );
+    $this->html = $result;
+    $this->tablePrefix = $arguments['table_prefix'];
+    $this->merchantProductId = $arguments['id'];
+    $this->parseTitle();
+    $this->parseDescription();
+    $this->parseProperties();
+    $this->save();
     $matches = array();
     preg_match(
       '{jqzoom.*? src="http://(.*?)/(\S+)"}', $result['content'], $matches
@@ -24,5 +32,87 @@ class ProductProcessor {
       'path' => $matches[2],
     ));
     DbTask::insert('Price', array('id' => $arguments['id']));
+  }
+
+  private function save() {
+    $contentInfo = DbProduct::getContentInfo(
+      $this->tablePrefix, $this->merchantProductId
+    );
+    $md5 = $this->getContentMd5();
+    if ($contentInfo === false) {
+      $this->insertContent($md5);
+      return;
+    }
+    $this->updateContent($contentInfo['id'], $md5);
+  }
+
+  private function parseProperties() {
+    preg_match(
+      '{class="Ptable">(.*?)</table>}',
+      $this->html,
+      $matches
+    );
+    if (isset($matches[1])) {
+      $specTable = iconv('GBK', 'utf-8', $matches[1]);
+      $sections = explode('<tr><th class="tdTitle" colspan="2">', $specTable);
+      foreach ($sections as $section) {
+        if ($section === '') {
+          continue;
+        }
+        preg_match(
+          '{^(.*?)</th><tr>(.*)}',
+          $section,
+          $matches
+        );
+        preg_match_all(
+          '{<tr><td class="tdTitle">(.*?)</td><td>(.*?)</td></tr>}',
+          $matches['2'],
+          $matches,
+          PREG_SET_ORDER
+        );
+        foreach ($matches as $match) {
+          $key = $match[1];
+          $value = $match[2];
+          $keyId = DbProperty::getOrNewKeyId($this->tablePrefix, $key);
+          $valueId = DbProperty::getOrNewValueId(
+            $this->tablePrefix, $keyId, $value
+          );
+          DbProductProperty::replace(
+            $this->tablePrefix, $this->merchantProductId, $valueId
+          );
+        }
+      }
+    }
+  }
+
+  private function parseDescription() {
+    echo 'parseDescription';
+    exit;
+  }
+
+  private function parseTitle() {
+    echo 'parseTitle';
+    exit;
+  }
+
+  private function insertContent($md5) {
+    $id = DbProduct::insert($this->tablePrefix, $this->merchantProductId,
+      $this->categoryId, $this->title, $this->description, $md5
+    );
+    DbProductUpdate::insert($this->tablePrefix, $id, 'CONTENT');
+  }
+
+  private function updateContent($id, $md5) {
+    DbProduct::updateContent(
+      $this->tablePrefix, $id, $this->categoryId, $this->title,
+      $this->description, $md5
+    );
+    DbProductUpdate::insert($this->tablePrefix, $id, 'CONTENT');
+  }
+
+  private function getContentMd5() {
+    $propertyList = var_export(DbProductProperty::getListByMerchantProductId(
+      $this->tablePrefix, $this->merchantProductId), true);
+    $md5 = md5($propertyList.$this->categoryId.$this->title.$this->description);
   }
 }
