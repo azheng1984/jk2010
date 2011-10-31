5f0bcaefdc0bc8c6bb0abedaa41a1b32fe713ce2
azheng1984 <azheng1984@126.com>
2011-10-31 15:14:06 +0800

    -

--- vendor/wj_spider/common/lib/database/DbProductUpdate.php
+++ vendor/wj_spider/common/lib/database/DbProductUpdate.php
@@ -14,7 +14,7 @@ class DbProductUpdate {
     ) {
       $sql = "CREATE TABLE `".$tablePrefix."_product_update` (
         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-        `product_id` int(11) unsigned NOT NULL,
+        `product_id` bigint(20) unsigned NOT NULL,
         `type` enum('NEW', 'PRICE','CONTENT','IMAGE', 'SALE_INDEX') NOT NULL,
         PRIMARY KEY (`id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
