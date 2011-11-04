de93b810d6f1d89d1c40dee76ac276a03172bba4
wangzheng <none@gmail.com>
2011-11-04 18:45:16 +0800

    -

---
+++ vendor/wj_builder/config/reset_db.sql
@@ -0,0 +1,4 @@
+delete from wj_web.category where id != 0;
+delete from wj_web.product where id != 0;
+delete from wj_web.property_key where id != 0;
+delete from wj_web.property_value where id != 0;
