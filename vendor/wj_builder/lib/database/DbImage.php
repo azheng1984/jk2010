330dc9b585d688851758d6df5489484b4a63b930
wangzheng <none@gmail.com>
2011-11-04 23:33:36 +0800

    -

--- vendor/wj_builder/lib/database/DbImage.php
+++ vendor/wj_builder/lib/database/DbImage.php
@@ -1,2 +1,55 @@
 <?php
-//use sqlite
+class DbImage {
+  private static $isConnected;
+  private static $isWebConnected;
+
+  public static function get($id) {
+    self::connect();
+    $sql = 'SELECT image FROM image WHERE product_id = ?';
+    Db::execute($sql, $id);
+    DbConnection::connect('default');
+  }
+
+  public static function updateWebImage($webProductId, $image) {
+    self::connectWebDb();
+    $sql = 'UPDATE image SET image = ? WHERE product_id = ?';
+    Db::execute($sql, $image, $webProductId);
+    DbConnection::connect('default');
+  }
+
+  public static function insertWebImage($webProductId, $image) {
+    self::connectWebDb();
+    $sql = 'INSERT INTO image(product_id, image) VALUES(?, ?)';
+    Db::execute($sql, $webProductId, $image);
+    DbConnection::connect('default');
+  }
+
+  public static function deleteWebImage($webProductId) {
+    self::connectWebDb();
+    $sql = 'DELETE FROM image WHERE product_id = ?';
+    Db::execute($sql, $webProductId);
+    DbConnection::connect('default');
+  }
+
+  private static function connectWebDb() {
+    if (!self::$isWebConnected) {
+      DbConnection::connect(
+        'web_image', new PDO('sqlite:'.IMAGE_PATH.'image.sqlite')
+      );
+      self::$isWebConnected = true;
+      return;
+    }
+    DbConnection::connect('web_image');
+  }
+
+  private static function connect() {
+    if (!self::$isConnected) {
+      DbConnection::connect(
+        'image', new PDO('sqlite:'.IMAGE_PATH.'food_image.sqlite')
+      );
+      self::$isConnected = true;
+      return;
+    }
+    DbConnection::connect('image');
+  }
+}
--- vendor/wj_builder/public/index.php
+++ vendor/wj_builder/public/index.php
@@ -4,6 +4,8 @@ define('ROOT_PATH', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 define('CACHE_PATH', ROOT_PATH.'cache'.DIRECTORY_SEPARATOR);
 define('CONFIG_PATH', ROOT_PATH.'config'.DIRECTORY_SEPARATOR);
 define('HYPERFRAMEWORK_PATH', '/home/wz/www/jk2010/vendor/hyperframework/');
+define('WEB_IMAGE_PATH', '/home/wz/wj_web/image/');
+define ('IMAGE_PATH', '/home/wz/spider/image/jingdong/');
 require HYPERFRAMEWORK_PATH.'class_loader'.DIRECTORY_SEPARATOR
   .'lib'.DIRECTORY_SEPARATOR.'ClassLoader.php';
 $classLoader = new ClassLoader;
