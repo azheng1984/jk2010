0d9633573d771672aae83c2691ed1665478fdd6a
azheng1984 <azheng1984@126.com>
2011-11-04 17:12:48 +0800

    -

--- vendor/wj_builder/lib/database/DbProduct.php
+++ vendor/wj_builder/lib/database/DbProduct.php
@@ -1,8 +1,75 @@
 <?php
 class DbProduct {
-  public static function getAll($source) {
-    Db::execute('USE '.$source);
-    $sql = 'SELECT * FROM product WHERE status != "NOT_MODIFIED"';
-    return Db::getAll($sql);
+  public static function insertIntoLive(
+    $lowestPriceX100,
+    $highestPriceX100,
+    $cutPriceX100,
+    $merchantId,
+    $url,
+    $imageDbIndex,
+    $categoryId,
+    $title,
+    $properties,
+    $description
+  ) {
+    $sql = 'INSERT INTO `wj_live`.`product` (
+      `lowest_price_x_100`,
+      `highest_price_x_100`,
+      `cut_price_x_100`,
+      `merchant_id`,
+      `url`,
+      `image_db_index`,
+      `category_id`,
+      `title`,
+      `properties`,
+      `description`
+    ) VALUES(?,?,?,?,?,?,?,?,?,?)';
+    Db::execute($sql,
+      $lowestPriceX100,
+      $highestPriceX100,
+      $cutPriceX100,
+      $merchantId,
+      $url,
+      $imageDbIndex,
+      $categoryId,
+      $title,
+      $properties,
+      $description
+    );
+  }
+
+  public static function insertIntoSearch(
+    $id,
+    $lowestPriceX100,
+    $cutPriceX100,
+    $saleRank,
+    $categoryId,
+    $propertyIdList,
+    $title,
+    $properties,
+    $description
+  ) {
+    $sql = 'INSERT INTO `wj_search`.`product` (
+      `id`,
+      `lowest_price_x_100`,
+      `cut_price_x_100`,
+      `sale_rank`,
+      `category_id`,
+      `property_id_list`,
+      `title`,
+      `description`,
+      `properties`
+    ) VALUES(?,?,?,?,?,?,?,?,?)';
+    Db::execute($sql,
+      $id,
+      $lowestPriceX100,
+      $cutPriceX100,
+      $saleRank,
+      $categoryId,
+      $propertyIdList,
+      $title,
+      $properties,
+      $description
+    );
   }
 }
--- vendor/wj_builder/lib/updater/ProductNewUpdater.php
+++ vendor/wj_builder/lib/updater/ProductNewUpdater.php
@@ -1,6 +1,75 @@
 <?php
 class ProductNewUpdater {
   public function execute($item) {
-    print_r($item);
+    $product = DbProduct::get($item['id']);
+    $this->updateCategory($product['category_id']);
+    $product['properties'] = $this->updateProperties(
+      $product['merchant_product_id'], $product['category_id']
+    );
+    $this->updateWebDb($product);
+    //$this->updateSearchDb($product);
+    exit;
+  }
+
+  private function updateCategory($id) {
+    $target = DbCategory::getWeb($id);
+    if ($target === false) {
+      $product = DbCategory::get($id);
+      DbCategory::insertIntoWeb($id, $product['name']);
+    }
+  }
+
+  private function updateProperties($merchantProductId, $categoryId) {
+    $properties = array();
+    foreach (DbProperty::getList($merchantProductId) as $product) {
+      $value = DbProperty::getValue($product['property_value_id']);
+      $key = DbProperty::getKey($value['key_id']);
+        $webKey = DbProperty::getWebKey($categoryId, $key['key']);
+      if ($webKey === false) {
+        $webKey = DbProperty::insertIntoWebKey($categoryId, $key['key']);
+      }
+      $webValue = DbProperty::getWebValue($categoryId, $value['value']);
+      if ($webValue === false) {
+        $webValue = DbProperty::insertIntoWebKey($categoryId, $value['value']);
+      }
+      $properties[] = array('key' => $webKey, 'value' => $webValue);
+    }
+  }
+
+  private function updateWebDb($product) {
+    $lowestPriceX100 = $product['lowest_product'] * 100;
+    $highestPriceX100 = null;
+    if ($product['highest_product'] !== null) {
+      $highestPriceX100 = $product['highest_product'] * 100;
+    }
+    $cutPriceX100 = 0;
+    $merchantId = 1;
+    $url = 'product/'.$product['merchant_product_id'].'.html';
+    $imageDbIndex = 0;
+    $categoryId = $product['category_id'];
+    $title = $product['title'];
+    $propertyList = '';
+    foreach ($product['properties'] as $item) {
+      $key = $item['key'];
+      $value = $item['value'];
+      $propertyList[] = $key['key'].':'.$value['value'];
+    }
+    $properties = implode(', ', $propertyList);
+    $description = $product['description'];
+    DbProduct::insertIntoWeb(
+      $lowestPriceX100, $highestPriceX100, $cutPriceX100, $merchantId, $url,
+      $imageDbIndex, $categoryId, $title, $properties, $description
+    );
+  }
+
+  private function updateSearchDb($product) {
+    DbProduct::insertIntoSearch(
+      $id, $lowestPriceX100, $cutPriceX100, $saleRank, $categoryId,
+      $propertyIdList, $title, $properties, $description
+    );
+    Segmentation::execute($product['title']);
+    Segmentation::execute($product['description']);
+    Segmentation::execute($product['properties']);
+    Segmentation::execute($product['categories']);
   }
 }
