0bb9d82b839d49b40b01441649348de175a82ef2
wangzheng <none@gmail.com>
2011-11-04 18:48:37 +0800

    -

--- vendor/wj_builder/lib/database/DbProductUpdate.php
+++ vendor/wj_builder/lib/database/DbProductUpdate.php
@@ -1,14 +1,14 @@
 <?php
 class DbProductUpdate {
   public static function get($tablePrefix) {
-    Db::execute('use jingdong');
-    return Db::getRow('SELECT * FROM '.$tablePrefix.'_product_update LIMIT 1');
+    return Db::getRow(
+      'SELECT * FROM jingdong.'.$tablePrefix.'_product_update LIMIT 1'
+    );
   }
 
   public static function delete($tablePrefix, $id) {
-    Db::execute('use jingdong');
     Db::execute(
-      'DELETE FROM '.$tablePrefix.'_product_update WHERE id = ?', $id
+      'DELETE FROM jingdong.'.$tablePrefix.'_product_update WHERE id = ?', $id
     );
   }
 }
