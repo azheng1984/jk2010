12e41733ca8fb2ebc40080e7401b749369f9c097
wangzheng <none@gmail.com>
2011-11-06 21:40:29 +0800

    -

--- vendor/wj/lib/url/Router.php
+++ vendor/wj/lib/url/Router.php
@@ -1,20 +1,17 @@
 <?php
 class Router {
   public function execute() {
-    if ($_SERVER['SERVER_NAME'] === 'www.wj.com') {
-      header('Location: http://wj.com'.$_SERVER['REQUEST_URI']);
+    if ($_SERVER['SERVER_NAME'] === 'www.'.DOMAIN_NAME) {
+      header('Location: http://'.DOMAIN_NAME.$_SERVER['REQUEST_URI']);
       return '/redirect';
     }
     if ($_SERVER['REQUEST_URI'] === '/') {
       return '/';
     }
-    $tmps = explode('?', $_SERVER['REQUEST_URI'], 2);
-    $sections = explode('/', $tmps[0]);
-    if (count($sections) === 2 && $sections[1] !== '') {
-      $_GET['product_id'] = $sections[1];
-      return '/product';
+    list($path, $parameters) = explode('?', $_SERVER['REQUEST_URI'], 2);
+    if ($path !== '/') {
+      throw new NotFoundException;
     }
-    $_GET['categories'] = $sections;
     return '/search';
   }
 }
