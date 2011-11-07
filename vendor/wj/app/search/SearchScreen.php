39c658122347e77d39fdef8dbfefc24ccbb79175
azheng1984 <azheng1984@126.com>
2011-11-07 23:33:15 +0800

    -

--- vendor/wj/app/search/SearchScreen.php
+++ vendor/wj/app/search/SearchScreen.php
@@ -57,8 +57,8 @@ class SearchScreen extends Screen {
     }
     foreach ($items as $item) {
       $name = $item['title'];
-      echo '<li><div class="image"><a target="_blank" href="/'.$item['id'].'"><img alt="'.$name.'" src="http://img.workbench.wj.com/'.$item['id'].'.jpg" /></a></div><div class="title"><a target="_blank" href="/'.$item['id'].'">'
-        .$name.'</a></div><div class="data"><div>&yen;<span class="price">'.$item['lowest_price_x_100'].'</span> ~ <span class="price">1234567890</span></div> <div>京东商城</div></div></li>';
+      echo '<li><div class="image"><a target="_blank" href="/'.$item['id'].'"><img alt="'.$name.'" src="http://img.wj.com/'.$item['id'].'.jpg" /></a></div><div class="title"><a target="_blank" href="/'.$item['id'].'">'
+        .$name.'</a></div><div class="data"><div>&yen;<span class="price">'.($item['lowest_price_x_100']/100).'</span> ~ <span class="price">1234567890</span></div> <div>京东商城</div></div></li>';
     }
     echo '</ol>';
     $this->renderPagination($result['total_found']);
