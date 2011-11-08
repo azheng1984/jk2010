ed176f6a264cdce3a28ff3421c3ef5e52fb9d2d4
azheng1984 <azheng1984@126.com>
2011-11-08 14:47:41 +0800

    -

--- vendor/wj/lib/database/DbProperty.php
+++ vendor/wj/lib/database/DbProperty.php
@@ -5,23 +5,18 @@ class DbProperty {
     return Db::getRow($sql, $valueId);
   }
 
-  public static function getList($tablePrefix) {
-    $sql = 'SELECT * FROM '.$tablePrefix.'_property_key';
-    $results = Db::getAll($sql);
-    foreach ($results as &$key) {
-      $sql = 'SELECT * FROM '.$tablePrefix.'_property_value WHERE key_id = ? order by rank';
-      $key['values'] = Db::getAll($sql, $key['id']);
-    }
-    return $results;
+  public static function getByKeyId($keyId) {
+    $sql = 'SELECT * FROM wj_web.property_key  WHERE id = ?';
+    return Db::getRow($sql, $keyId);
   }
 
-  public static function getKeyByName($tablePrefix, $name) {
-    $sql = 'SELECT * FROM '.$tablePrefix.'_property_key WHERE `key` = ?';
-    return Db::getRow($sql, $name);
+  public static function getKeyByName($categoryId, $name) {
+    $sql = 'SELECT * FROM wj_web.property_key WHERE category_id=? AND `key` = ?';
+    return Db::getRow($sql, $categoryId, $name);
   }
 
-  public static function getValueByKeyIdAndName($tablePrefix, $keyId, $name) {
-    $sql = 'SELECT * FROM '.$tablePrefix.'_property_value WHERE `key_id` = ? AND `value` = ?';
-    return Db::getRow($sql, $keyId, $name);
+  public static function getValueIdList($keyId) {
+    $sql = 'SELECT id FROM wj_web.property_value  WHERE `key_id` = ?';
+    return Db::getAll($sql, $keyId);
   }
 }
