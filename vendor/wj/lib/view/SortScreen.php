e4fd5e3861db7de0b1ad7cab533edda87121a9d3
wangzheng <none@gmail.com>
2011-11-29 14:10:27 +0800

    -

--- vendor/wj/lib/view/SortScreen.php
+++ vendor/wj/lib/view/SortScreen.php
@@ -3,8 +3,8 @@ class SortScreen {
   public static function render($total) {
     echo '<h2>',
     '<div id="sort">排序: <span>销量</span>',
-    ' <a rel="nofollow" href="?sort=新品">新品</a>',
     ' <a rel="nofollow" href="?sort=价格">价格</a>',
+    ' <a rel="nofollow" href="?sort=上架时间">上架时间</a>',
     ' <a rel="nofollow" href="?sort=折扣">折扣</a>',
     '</div>',
     '<div id="total">搜索到 ', $total, ' 个产品</div>',
