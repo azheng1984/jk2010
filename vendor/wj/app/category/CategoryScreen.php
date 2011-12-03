f603826a8da1160365b8d5502589fedb206d3e28
azheng1984 <azheng1984@126.com>
2011-12-03 22:02:34 +0800

    -

---
+++ vendor/wj/app/category/CategoryScreen.php
@@ -0,0 +1,39 @@
+<?php
+class CategoryScreen extends Screen {
+  protected function renderHeadContent() {
+    echo '<title>货比万家</title>';
+    $this->renderCssLink('category');
+  }
+
+  protected function renderBodyContent() {
+    echo '<div id="h1_wrapper"><h1><img src="/home.png"> <img src="/bread_arrow.png"> 鞋子</h1></div>';
+    echo '<div id="list_wrapper">';
+    $char = 65;
+    echo '<h2><img src="/tag.png">属性</h2>';
+    echo '<table>';
+    for ($i = 0; $i < 6; ++$i) {
+      echo '<tr>';
+      for ($j = 0; $j < 5; ++$j) {
+        echo '<td><a class="categoty" href="鞋子/">鞋子</a></td>';
+      }
+      echo '</tr>';
+    }
+    echo '</table>';
+    echo '<a href="javascript:void(0)">更多 &raquo;</a>';
+    echo '</div>';
+    echo '<div id="list_wrapper">';
+    $char = 65;
+    echo '<h2>搜索</h2>';
+    echo '<table>';
+    for ($i = 0; $i < 6; ++$i) {
+      echo '<tr>';
+      for ($j = 0; $j < 5; ++$j) {
+        echo '<td><a class="categoty" href="鞋子/">鞋子</a></td>';
+      }
+      echo '</tr>';
+    }
+    echo '</table>';
+    echo '<a href="javascript:void(0)">更多 &raquo;</a>';
+    echo '</div>';
+  }
+}
