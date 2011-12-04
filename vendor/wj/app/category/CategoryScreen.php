f571956672a3b3ed6288c37e452f843b769072f2
wangzheng <none@gmail.com>
2011-12-04 18:41:38 +0800

    -

--- vendor/wj/app/category/CategoryScreen.php
+++ vendor/wj/app/category/CategoryScreen.php
@@ -6,34 +6,34 @@ class CategoryScreen extends Screen {
   }
 
   protected function renderBodyContent() {
-    echo '<div id="h1_wrapper"><h1><img src="/home.png"> <img src="/bread_arrow.png"> <a href="..">分类</a> <img src="/bread_arrow.png"> 鞋子</h1></div>';
-    echo '<div id="list_wrapper">';
+    echo '<div id="h1_wrapper"><h1><a href="/"><img src="/home.png"></a> <img src="/bread_arrow.png"> <a href="..">分类</a> <img src="/bread_arrow.png"> 鞋子</h1></div>';
+    echo '<div id="list_wrapper" class="tag">';
     $char = 65;
-    echo '<h2><img src="/tag.png">属性</h2>';
+    echo '<h2 class="tag_icon">属性</h2>';
     echo '<table>';
     for ($i = 0; $i < 6; ++$i) {
       echo '<tr>';
       for ($j = 0; $j < 5; ++$j) {
-        echo '<td><a class="categoty" href="鞋子/">鞋子</a></td>';
+        echo '<td><a class="categoty" href="品牌/">品牌</a></td>';
       }
       echo '</tr>';
     }
     echo '</table>';
-    echo '<a href="javascript:void(0)">更多 &raquo;</a>';
+    echo '<div class="more"><strong><a href="javascript:void(0)">更多 &raquo;</a></strong></div>';
     echo '</div>';
-    echo '<div id="list_wrapper">';
+    echo '<div id="list_wrapper" class="search">';
     $char = 65;
     echo '<h2>搜索</h2>';
     echo '<table>';
     for ($i = 0; $i < 6; ++$i) {
       echo '<tr>';
       for ($j = 0; $j < 5; ++$j) {
-        echo '<td><a class="categoty" href="鞋子/">鞋子</a><span>123</span></td>';
+        echo '<td><a class="categoty" href="/儿童+胶囊/">儿童 胶囊</a> <span>123</span></td>';
       }
       echo '</tr>';
     }
     echo '</table>';
-    echo '<a href="javascript:void(0)">更多 &raquo;</a>';
+    echo '<div class="more"><strong><a href="javascript:void(0)">更多 &raquo;</a></strong></div>';
     echo '</div>';
   }
 }
