ae57465aa8f2ca669c60a87271c7194ab10212e2
azheng1984 <azheng1984@126.com>
2011-12-04 18:41:38 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -8,11 +8,19 @@ class HomeScreen extends Screen {
   protected function renderBodyContent() {
     echo '<div id="h1_wrapper"><div></div><h1>在 11421 家网上商城，1508 万个产品中搜索：</h1></div>';
     echo '<ol>';
-    for ($i = 0; $i < 28; ++$i) {
-      echo '<li><a href="javascript:void(0)"><img alt="鞋子" src="/product.jpg" /></a><a class="categoty" href="javascript:void(0)">鞋子</a><div><a href="javascript:void(0)">耐克</a> <a href="javascript:void(0)">回力</a> <a href="javascript:void(0)">...</a></div></li>';
+    for ($i = 0; $i < 8; ++$i) {
+      echo '<li class="l',$i,'">',
+      '<a href="javascript:void(0)" class="a_img"><img alt="鞋子" src="/product.jpg" /></a>',
+      '<div class="keywords"><div><a class="categoty" href="javascript:void(0)">鞋子</a></div>',
+      '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
+      '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
+      '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
+      '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div> <div><a href="javascript:void(0)">...</a></div></div>',
+
+      '</li>';
     }
     echo '</ol>';
-    echo '<div id="more"><a href="/+i/">更多 &raquo;</a></div>';
+    echo '<div id="more"><strong><a href="/+i/">更多 &raquo;</a></strong></div>';
     echo '<script>document.getElementById("search_input").focus()</script>';
   }
 }
