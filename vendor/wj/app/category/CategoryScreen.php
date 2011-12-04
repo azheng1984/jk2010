04b093c754f8738edc51db9ab32f6e9dc830c3db
wangzheng <none@gmail.com>
2011-12-04 08:35:27 +0800

    -

--- vendor/wj/app/category/CategoryScreen.php
+++ vendor/wj/app/category/CategoryScreen.php
@@ -6,7 +6,7 @@ class CategoryScreen extends Screen {
   }
 
   protected function renderBodyContent() {
-    echo '<div id="h1_wrapper"><h1><img src="/home.png"> <img src="/bread_arrow.png"> 鞋子</h1></div>';
+    echo '<div id="h1_wrapper"><h1><img src="/home.png"> <img src="/bread_arrow.png"> <a href="..">分类</a> <img src="/bread_arrow.png"> 鞋子</h1></div>';
     echo '<div id="list_wrapper">';
     $char = 65;
     echo '<h2><img src="/tag.png">属性</h2>';
@@ -28,7 +28,7 @@ class CategoryScreen extends Screen {
     for ($i = 0; $i < 6; ++$i) {
       echo '<tr>';
       for ($j = 0; $j < 5; ++$j) {
-        echo '<td><a class="categoty" href="鞋子/">鞋子</a></td>';
+        echo '<td><a class="categoty" href="鞋子/">鞋子</a><span>123</span></td>';
       }
       echo '</tr>';
     }
