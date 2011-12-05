60d1b58d5f7b066e377162e80cc13db566cf6240
wangzheng <none@gmail.com>
2011-12-05 16:43:23 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -8,15 +8,15 @@ class HomeScreen extends Screen {
   protected function renderBodyContent() {
     echo '<div id="h1_wrapper"><div></div><h1>在 11421 家网上商城，1508 万个产品中搜索：</h1></div>';
     echo '<ol>';
-    for ($i = 0; $i < 8; ++$i) {
+    for ($i = 0; $i < 7; ++$i) {
       echo '<li class="l',$i,'">',
       '<a href="javascript:void(0)" class="a_img"><img alt="鞋子" src="/product.jpg" /></a>',
-      '<div class="keywords"><div><a class="categoty" href="javascript:void(0)">鞋子</a></div>',
+      '<div class="keywords"><div class="category"><a href="javascript:void(0)">鞋子</a></div>',
       '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
       '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
       '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div>',
       '<div><a href="javascript:void(0)">耐克</a></div> <div><a href="javascript:void(0)">回力</a></div> <div><a href="javascript:void(0)">...</a></div></div>',
-
+      
       '</li>';
     }
     echo '</ol>';
