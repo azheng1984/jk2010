23fea9fcbc3ce89846f50fdaeaf085c7a792aaa0
azheng1984 <azheng1984@126.com>
2011-12-06 11:45:00 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -7,7 +7,7 @@ class HomeScreen extends Screen {
 
   protected function renderBodyContent() {
     echo '<div id="h1_wrapper"><div></div><h1>在 11421 家网上商城，1508 万个产品中搜索：</h1></div>';
-    echo '<ol>';
+    echo '<ul>';
     for ($i = 0; $i < 7; ++$i) {
       echo '<li class="l',$i,'">',
       '<a href="javascript:void(0)" class="a_img"><img alt="鞋子" src="/product.jpg" /></a>',
@@ -19,7 +19,7 @@ class HomeScreen extends Screen {
       
       '</li>';
     }
-    echo '</ol>';
+    echo '</ul>';
     echo '<div id="more"><strong><a href="/+i/">更多 &raquo;</a></strong></div>';
     echo '<script>document.getElementById("search_input").focus()</script>';
   }
