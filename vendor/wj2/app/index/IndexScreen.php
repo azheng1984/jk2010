850563626d479ef6a6bb2c011d5497b84c177acc
azheng1984 <azheng1984@126.com>
2011-12-06 21:14:45 +0800

    -

---
+++ vendor/wj2/app/index/IndexScreen.php
@@ -0,0 +1,20 @@
+<?php
+class IndexScreen extends Screen {
+	/* (non-PHPdoc)
+ * @see Screen::renderHeadContent()
+ */
+  protected function renderHeadContent() {
+    // TODO Auto-generated method stub
+    
+  }
+
+	/* (non-PHPdoc)
+ * @see Screen::renderBodyContent()
+ */
+  protected function renderBodyContent() {
+    // TODO Auto-generated method stub
+    
+  }
+
+
+}
