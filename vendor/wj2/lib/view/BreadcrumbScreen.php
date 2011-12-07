595e5eaf41ec2ae05281df921b3d00c8219f1008
wangzheng <none@gmail.com>
2011-12-07 12:10:05 +0800

    -

--- vendor/wj2/lib/view/BreadcrumbScreen.php
+++ vendor/wj2/lib/view/BreadcrumbScreen.php
@@ -2,7 +2,7 @@
 class BreadcrumbScreen {
   public static function render() {
     $query = htmlentities($GLOBALS['URI']['QUERY'], ENT_QUOTES, 'utf-8');
-    echo '<div id="h1_wrapper"><h1>';
+    echo '<div id="breadcrumb"><h1>';
     if (isset($GLOBALS['URI']['CATEGORY'])) {
       echo ' <a href="..">', $query, '</a> <img src="/bread_arrow.png" /> 分类: '.$GLOBALS['URI']['CATEGORY']['name'].'';
     } else {
