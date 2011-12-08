62f04fb4c5404418c81c363fbf8b68e4936d6bb3
azheng1984 <azheng1984@126.com>
2011-12-08 15:41:20 +0800

    -

---
+++ vendor/wj2/public/js/search.js
@@ -0,0 +1,55 @@
+function bindEvent() {
+  $('.value_list li').hover(
+      function() {
+        $(this).attr('class', 'current');
+        $('.value_list .current .delete').show();
+      },function() {
+        $('.value_list .current .delete').hide();
+        $(this).attr('class', '');
+    }
+  );
+}
+$(function() {
+  $uri = window.location.pathname + '?';
+  if (window.location.hash != '') {
+    $uri += 'anchor=' + window.location.hash.replace('#', '') + '&';
+  }
+  $uri += 'media=json';
+  $.get($uri, function(data) {
+    $('#filter').html(data);
+    $('#key_list .key').mouseup(function() {
+      $uri2 = window.location.pathname + '?anchor=' + $(this).text() + '&media=json';
+      $(this).attr('id', 'target');
+      $.get($uri2, function(data) {
+        $('#target').after(data).attr('id', '').css('background', "url('/img/minus.png') 0 1px no-repeat");
+        bindEvent();
+      });
+    });
+    bindEvent();
+  });
+  var isHover = false;
+  $('#result ol li').hover(function() {
+    if (isHover == true) {
+      return;
+    }
+    isHover = true;
+    $(this).append('<div id="hover"><div class="toolbar"><a href="javascript:void(0)">同款</a> <a href="javascript:void(0)">关注</a></div><div class="tag"><a href="javascript:void(0)">分类: 手机</a></div>'
+    + '</div>');
+  }, function() {
+    $('#hover').remove();
+    isHover = false;
+  });
+  var suggestion = false;
+  $('#search_input').click(function() {
+    if (suggestion == false) {
+      $('#header').append('<div id="suggestion"><ul><li>缓释胶囊 <div>234</div></li><li>缓释胶囊 <div>234</div></li><li>缓释胶囊 <div>234</div></li></ul></div>');
+    }
+    suggestion = true;
+  });
+  $('#search_input').focusout(function() {
+    if (suggestion) {
+      $('#suggestion').remove();
+      suggestion = false;
+    }
+  });
+});
