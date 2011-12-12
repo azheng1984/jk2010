9efde9f4ffe26d2b46911297a927335dd5847d47
wangzheng <none@gmail.com>
2011-12-12 12:45:40 +0800

    -

--- vendor/wj2/public/js/search.js
+++ vendor/wj2/public/js/search.js
@@ -52,7 +52,7 @@ $(function() {
       return;
     }
     isHover = true;
-    $(this).append('<div id="hover"><div class="toolbar"><a href="javascript:void(0)">同款</a> <a href="javascript:void(0)">关注</a></div><div class="tag"><a href="javascript:void(0)">分类: 手机</a></div>'
+    $(this).append('<div id="hover"><div class="toolbar"><a href="javascript:void(0)"><span> </span>同款</a> <a href="javascript:void(0)"><span class="heart"> </span>关注</a></div><div class="tag"><a href="javascript:void(0)">分类: 手机</a></div>'
     + '</div>');
   }, function() {
     $('#hover').remove();
