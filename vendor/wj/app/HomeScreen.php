4899dadae9696d905b26a26f7029499b41d304fb
wangzheng <none@gmail.com>
2011-12-13 22:24:14 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -1,6 +1,50 @@
 <?php
-class HomeScreen {
-  public function render() {
-    echo 'hello';
+class HomeScreen extends Screen {
+  protected function renderHeadContent() {
+    echo '<title>货比万家</title>';
+    $this->renderCssLink('home');
+  }
+
+  protected function renderBodyContent() {
+    echo '<div id="home">';
+    $this->renderSlogon();
+    $this->renderQueryList();
+    $this->renderFooter();
+    $this->renderFocusJs();
+    echo '</div>';
+  }
+
+  private function renderSlogon() {
+    echo '<div id="slogon">',
+      '<span></span>',
+      '<h1>11421个网上商城，1508万产品，搜索：</h1>',
+      '</div>';
+  }
+
+  private function renderQueryList() {
+    echo '<table>';
+    for ($i = 0; $i < 12; ++$i) {
+      echo '<tr>';
+      for ($j = 0; $j < 5; ++$j) {
+        for ($j = 0; $j < 5; ++$j) {
+          $uri = urlencode('儿童');
+          echo '<td><a href="/', $uri, '/">儿童 胶囊</a> <span>123</span></td>';
+        }
+      }
+      echo '</tr>';
+    }
+    echo '</table>';
+  }
+
+  private function renderFooter() {
+    echo '<div class="footer">',
+     '<a href="/+i/">更多 &raquo;</a>',
+     '<div>品质保证</div>',
+     '<div class="merchant">品牌商城</div>',
+     '</div>';
+  }
+
+  private function renderFocusJs() {
+    echo '<script>document.getElementById("search_input").focus()</script>';
   }
 }
