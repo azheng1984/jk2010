c6dbc352acb81abe489f7de4ee221e5847323256
azheng1984 <azheng1984@126.com>
2011-12-14 21:00:59 +0800

    -

---
+++ vendor/wj/lib/search/CategoryIndexSearch.php
@@ -0,0 +1,6 @@
+<?php
+class CategoryIndexSearch {
+  public static function search() {
+    
+  }
+}
---
+++ vendor/wj/lib/search/ValueIndexSearch.php
@@ -0,0 +1,6 @@
+<?php
+class ValueIndexSearch {
+  public static function search() {
+    
+  }
+}
