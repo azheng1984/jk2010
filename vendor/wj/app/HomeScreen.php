86979d516f9dcc8c63c6c2bde79934f9442685ee
azheng1984 <azheng1984@126.com>
2011-12-17 14:35:19 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -9,6 +9,7 @@ class HomeScreen extends Screen {
     echo '<div id="home">';
     $this->renderSlogon();
     $this->renderMerchantList();
+    $this->renderCategoryList();
     $this->renderFooter();
     $this->renderFocusJs();
     echo '</div>';
@@ -17,7 +18,7 @@ class HomeScreen extends Screen {
   private function renderSlogon() {
     echo '<div id="slogon">',
       '<span class="arrow"></span>',
-      '<h1>11421个网上商城，1508万产品，搜索：</h1>';
+      '<h1>11421个网上商店，1508万产品，搜索：</h1>';
     $this->renderQueryList();
     echo '</div>';
   }
@@ -38,23 +39,31 @@ class HomeScreen extends Screen {
 
   private function renderMerchantList() {
     echo '<table>';
-    for ($i = 0; $i < 15; ++$i) {
+    for ($i = 0; $i < 12; ++$i) {
       echo '<tr>';
-      for ($j = 0; $j < 5; ++$j) {
-        for ($j = 0; $j < 5; ++$j) {
+      for ($j = 0; $j < 4; ++$j) {
           $uri = urlencode('儿童');
-          echo '<td><a target="_blank" href="http://www.360buy.com/" rel="nofollow">京东商城</a></td>';
-        }
+          echo '<td><a target="_blank" href="http://www.360buy.com/?source=huobiwanjia" rel="nofollow">京东商城</a></td>';
       }
       echo '</tr>';
     }
     echo '</table>';
   }
+  private function renderCategoryList() {
+    $uri = urlencode('儿童');
+    echo ' <ul id="category">';
+    echo '<li><span>所有商店</span></li>';
+    echo '<li><a href="?type=综合">百货</a></li>';
+    echo '<li><a href="?type=综合">数码家电</a></li>';
+    echo '<li><a href="?type=综合">服装鞋帽</a></li>';
+    echo '<li><a href="?type=综合">奢侈品</a></li>';
+    echo '</ul>';
+  }
 
   private function renderFooter() {
     echo '<div class="footer">',
-     '<a href="/+i/">更多 &raquo;</a>',
-     '<span><em>品质保证</em> 100%</span><span class="left"><em>品牌商城</em> 100%</span>',
+     '<a href="javascript:void(0)">更多 &raquo;</a>',
+     '<span><em>品质保证</em> 100%</span><span class="left"><em>公司经营</em> 100%</span>',
      '</div>';
   }
 
