b05fa1b59673f67166a822cc69462a8dc8890bcd
wangzheng <none@gmail.com>
2011-12-17 22:12:28 +0800

    -

--- vendor/wj/lib/view/search/SortScreen.php
+++ vendor/wj/lib/view/search/SortScreen.php
@@ -53,7 +53,7 @@ class SortScreen {
   }
 
   private static function renderPriceLimit() {
-    echo '<form action=""><label for="price_from">范围:</label> ',
+    echo '<form action="/"><label for="price_from">范围:</label> ',
       '<input id="price_from" name="price_from" type="text" value="" />-',
       '<input name="price_to" type="text" value="" />',
       ' <button type="submit"></button><a href=".">确定</a></form>';
