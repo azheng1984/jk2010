172f02b6630c5f82636c696be9ee3bfab66f273c
wangzheng <none@gmail.com>
2011-12-20 23:45:01 +0800

    -

--- vendor/wj/lib/view/search/PaginationScreen.php
+++ vendor/wj/lib/view/search/PaginationScreen.php
@@ -1,78 +1,61 @@
 <?php
 class PaginationScreen {
-  public static function render($prefix, $total, $specialForPageOne = null) {
-    if ($total <= 20) {
+  private static function getTotalPage($total) {
+    if ($total % 16 === 0) {
+      return $total / 16;
+    }
+    return ($total + 16 - $total % 16) / 16;
+  }
+
+  private static function getFirstPage($currentPage, $totalPage) {
+    $result = 1;
+    if ($currentPage > 5) {
+      $result = $currentPage - 4;
+    }
+    if (($result + 9) > $totalPage) {
+      $result = $totalPage - 9;
+    }
+    if ($result < 1) {
+      $result = 1;
+    }
+    return $result;
+  }
+
+  public static function render($prefix, $total) {
+    $postfix = '';
+    if ($_SERVER['QUERY_STRING'] !== '') {
+      $postfix = '?'.$_SERVER['QUERY_STRING'];
+    }
+    if ($total <= 16) {
       return;
     }
     $currentPage = 1;
-    if (isset($_GET['page']) && is_numeric($_GET['page'])) {
-      $currentPage = $_GET['page'];
+    if (isset($GLOBALS['URI']['PAGE'])) {
+      $currentPage = $GLOBALS['URI']['PAGE'];
     }
-    define('ITEM_PER_PAGE', 20);
-    //把总数补足成 ITEM_PER_PAGE 的倍数
-    if ($total % ITEM_PER_PAGE != 0) {
-      $total += ITEM_PER_PAGE - $total % ITEM_PER_PAGE;
-    }
-    
-    $totalPage = $total / ITEM_PER_PAGE;
-    
-    //保证有一页存在
-    if ($totalPage == 0) {
-      $totalPage = 1;
-    }
-    
-    //为隐藏分页作准备
-    $totalPageItem = 10; //最多显示的页面条目数
-    $startPageItem = 1;
-    if ($currentPage > 5) {
-      $startPageItem = $currentPage - 4;
-      if (($currentPage + 5) > $totalPage) {
-        $startPageItem = $startPageItem - (5 - ($totalPage - $currentPage));
-      }
-    }
-
+    $totalPage = self::getTotalPage($total);
+    $firstPage = self::getFirstPage($currentPage, $totalPage);
     $result = '<div id="pagination"> ';
-    //Previous
     if ($currentPage != 1) {
-      if ($specialForPageOne !== null && $currentPage == 2) {
-        $result .= '<a rel="nofollow" href="' . $specialForPageOne . '">&laquo; 上一页</a>';
-      } else {
-        $prevPage = $currentPage - 1;
-        $result .= '<a rel="nofollow" href="';
-        if ($prevPage != 1) {
-          $result .= '' . $prevPage;
-        }
-        $result .= '">&laquo; 上一页</a>';
+      $previousPage = $currentPage - 1;
+      $path = $previousPage === 1 ? '.' : $previousPage;
+      $result .= '<a rel="nofollow" href="'.$path.$postfix. '">&laquo; 上一页</a>';
+    }
+    $lastPage = $firstPage + 9;
+    if ($lastPage > $totalPage) {
+      $lastPage = $totalPage;
+    }
+    for ($index = $firstPage; $index <= $lastPage; ++$index) {
+      if ($index == $currentPage) {
+        $result .= ' <span>' . $index . '</span>';
+        continue;
       }
+      $path = $index === 1 ? '.' : $index;
+      $result .= ' <a rel="nofollow" href="'.$path.$postfix.'">'.$index.'</a>';
     }
-
-    if ($startPageItem != 1) {
-      $result .= ' &hellip; ';
-    }
-
-    $count = $startPageItem;
-    for (; $count <= $totalPage && $totalPageItem > 0; ++$count) {
-      if ($count == $currentPage) {
-        $result .= ' <span>' . $count . '</span>';
-      } else {
-        if ($specialForPageOne !== null && $count == 1) {
-          $result .= ' <a rel="nofollow" href="' . $specialForPageOne . '">1</a>';
-        } else {
-          $result .= ' <a rel="nofollow" href="';
-          if ($count != 1) {
-            $result .= $count;
-          }
-          $result .= '">' . $count . '</a>';
-        }
-      }
-      --$totalPageItem;
-    }
-    if ($count != $totalPage + 1) {
-      $result .= ' &hellip; ';
-    }
-    //Next
-    if ($currentPage != $totalPage && $totalPage != 0) {
-      $result .= ' <a rel="nofollow" href="' . ($currentPage + 1) . '">下一页 &raquo;</a>';
+    if ($currentPage != $totalPage) {
+      $result .= ' <a rel="nofollow" href="'
+        .($currentPage + 1).$postfix.'">下一页 &raquo;</a>';
     }
     echo $result, '</div>';
   }
