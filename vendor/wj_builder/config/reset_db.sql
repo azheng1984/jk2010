7f569d4b4741f0ddd96d2bb18cd4b26ec2d6998a
wangzheng <none@gmail.com>
2011-12-23 17:39:32 +0800

    -

--- vendor/wj_builder/config/reset_db.sql
+++ vendor/wj_builder/config/reset_db.sql
@@ -1,6 +1,10 @@
+delete from wj_builder.key_mva_index where category_id != 0;
+delete from wj_builder.`spider_product-web_product` where id != 0;
+delete from wj_search.product where id != 0;
+delete from wj_search.category where id != 0;
+delete from wj_search.property_key where id != 0;
+delete from wj_search.property_value where id != 0;
 delete from wj_web.category where id != 0;
-delete from wj_search.category_key_count where category_id != 0;
 delete from wj_web.product where id != 0;
 delete from wj_web.property_key where id != 0;
 delete from wj_web.property_value where id != 0;
-delete from wj_search.product where id != 0;
