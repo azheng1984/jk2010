d782334916e9c08a10ea549ed0f48fcdd536b453
azheng1984 <azheng1984@126.com>
2011-12-23 14:02:28 +0800

    -

--- vendor/wj_builder/lib/builder/ProductImageBuilder.php
+++ vendor/wj_builder/lib/builder/ProductImageBuilder.php
@@ -1,5 +1,12 @@
 <?php
 class ProductImageBuilder {
   public function execute($item) {
+    $spiderProductWebProduct =
+      DbBuilderSpiderProductWebProduct::getBySpiderProductId(
+        $item['product_id']
+      );
+    if ($spiderProductWebProduct === false) {
+      
+    }
   }
 }
