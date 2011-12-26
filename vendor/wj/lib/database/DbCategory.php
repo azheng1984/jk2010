81a1ec82f79299aa516564145321ec3fdf5f8eb9
azheng1984 <azheng1984@126.com>
2011-12-26 18:12:28 +0800

    -

--- vendor/wj/lib/database/DbCategory.php
+++ vendor/wj/lib/database/DbCategory.php
@@ -4,8 +4,22 @@ class DbCategory {
     return Db::getRow('SELECT * FROM wj_web.category WHERE id = ?', $id);
   }
 
-  public static function getList() {
-    return Db::getAll('SELECT * FROM wj_web.category');
+  public static function getList($alphabetIndex, $page) {
+    $sql = 'SELECT * FROM wj_web.category';
+    if ($alphabetIndex !== null) {
+      $sql .= " WHERE alphabet_index = $alphabetIndex";
+    }
+    $start = ($page - 1) * 60;
+    $sql .= ' LIMIT '.$start.',60';
+    return Db::getAll($sql);
+  }
+
+  public static function count($alphabetIndex) {
+    $sql = 'SELECT count(*) FROM wj_web.category';
+    if ($alphabetIndex !== null) {
+      $sql .= " WHERE alphabet_index = $alphabetIndex";
+    }
+    return Db::getColumn($sql);
   }
 
   public static function getByName($name) {
