0e34fbf32decd1e6e466b7651099514abfad08f9
wangzheng <none@gmail.com>
2011-12-27 14:53:12 +0800

    -

--- vendor/wj_builder/config/command_application.config.php
+++ vendor/wj_builder/config/command_application.config.php
@@ -3,5 +3,6 @@ return array(
   'description' => 'Add your own description here',
   'sub' => array(
     'build' => 'BuildCommand',
+    'build_query' => 'BuildQueryCommand',
   ),
 );
--- vendor/wj_builder/lib/database/web/DbWebValue.php
+++ vendor/wj_builder/lib/database/web/DbWebValue.php
@@ -8,6 +8,12 @@ class DbWebValue {
     );
   }
 
+  public static function getList($keyId) {
+    return Db::getAll(
+      'SELECT * FROM `wj_web`.`property_value` WHERE key_id = ?', $keyId
+    );
+  }
+
   public static function insert($keyId, $name) {
     Db::execute(
       'INSERT INTO `wj_web`.`property_value`(`key_id`, `name`)'
