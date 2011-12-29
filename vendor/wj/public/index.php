4d66decd377a7df309633139ca00b5b1fb04422f
wangzheng <none@gmail.com>
2011-12-29 23:34:52 +0800

    -

--- vendor/wj/public/index.php
+++ vendor/wj/public/index.php
@@ -2,7 +2,7 @@
 define('ROOT_PATH', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 define('CACHE_PATH', ROOT_PATH.'cache'.DIRECTORY_SEPARATOR);
 define('CONFIG_PATH', ROOT_PATH.'config'.DIRECTORY_SEPARATOR);
-define('HYPERFRAMEWORK_PATH', '/home/wz/www/jk2010/vendor/hyperframework/');
+require CONFIG_PATH.'env.config.php';
 require HYPERFRAMEWORK_PATH.'class_loader'.DIRECTORY_SEPARATOR
   .'lib'.DIRECTORY_SEPARATOR.'ClassLoader.php';
 $CLASS_LOADER = new ClassLoader;
@@ -10,4 +10,5 @@ $CLASS_LOADER->run();
 $APP = new Application;
 $EXCEPTION_HANDLER = new ExceptionHandler($APP);
 $EXCEPTION_HANDLER->run();
-$APP->run();
+$ROUTER = new Router;
+$APP->run($ROUTER->execute());
