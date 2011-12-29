5c56e520279b2dd4091e61a1ffb12956983de20d
azheng1984 <azheng1984@126.com>
2011-12-29 17:04:55 +0800

    -

--- vendor/wj/lib/database/DbPropertyValue.php
+++ vendor/wj/lib/database/DbPropertyValue.php
@@ -1,17 +1,24 @@
 <?php
 class DbPropertyValue {
   public static function get($id) {
-    $sql = 'SELECT * FROM wj_web.property_value  WHERE id = ?';
+    $sql = 'SELECT * FROM property_value  WHERE id = ?';
     return Db::getRow($sql, $id);
   }
 
-
   public static function getByName($keyId, $name) {
-    $sql = 'SELECT * FROM wj_web.property_value WHERE key_id=? AND `name` = ?';
+    $sql = 'SELECT * FROM property_value WHERE key_id=? AND `name` = ?';
     return Db::getRow($sql, $keyId, $name);
   }
 
-  public static function getList() {
-    
+  public static function getList(
+    $keyId, $alphabetIndex, $page, $amount = 60
+  ) {
+    $sql = 'SELECT * FROM property_key WHERE key_id = ?';
+    if ($alphabetIndex !== null) {
+      $sql .= " AND alphabet_index = $alphabetIndex";
+    }
+    $start = ($page - 1) * 60;
+    $sql .= ' ORDER BY `rank` LIMIT '.$start.','.$amount;
+    return Db::getAll($sql, $keyId);
   }
 }
--- vendor/wj/lib/database/DbQuery.php
+++ vendor/wj/lib/database/DbQuery.php
@@ -1,11 +1,18 @@
 <?php
-
 class DbQuery {
-  public function getList() {
-    return array();
-  }
-
   public function get($id) {
     return Db::getRow('SELECT * FROM query WHERE id = ?', $id);
+  }
+
+  public static function getList(
+    $categoryId, $alphabetIndex, $page, $amount = 60
+  ) {
+    $sql = 'SELECT * FROM query WHERE category_id = ?';
+    if ($alphabetIndex !== null) {
+      $sql .= " AND alphabet_index = $alphabetIndex";
+    }
+    $start = ($page - 1) * 60;
+    $sql .= ' ORDER BY `amount` LIMIT '.$start.','.$amount;
+    return Db::getAll($sql, $categoryId);
   }
 }
