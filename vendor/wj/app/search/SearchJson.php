777318e8fd290ba7c852348cab5e9ef8cd8a3f25
wangzheng <none@gmail.com>
2012-01-02 12:50:22 +0800

    -

---
+++ vendor/wj/app/search/SearchJson.php
@@ -0,0 +1,4 @@
+<?php
+class SearchJson {
+  public function render() {}
+}
--- vendor/wj/config/build.config.php
+++ vendor/wj/config/build.config.php
@@ -2,4 +2,5 @@
 return array(
   'ClassLoader' => array('app', 'lib', HYPERFRAMEWORK_PATH.'web/lib', '/home/wz/www/jk2010/vendor/db'),
   'Application' => array('Action', 'View' => array('Screen', 'Json')),
+  'Asset' => '+'
 );
