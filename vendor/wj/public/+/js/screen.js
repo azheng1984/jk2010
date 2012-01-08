4823d6b88659d63353d1aac60ebd28f49f8fd548
azheng1984 <azheng1984@126.com>
2012-01-08 16:00:08 +0800

    -

--- vendor/wj/public/+/js/screen.js
+++ vendor/wj/public/+/js/screen.js
@@ -1,12 +1,17 @@
-//window.location='/' + encodeURIComponent(search_input.value).replace(/%20/g, '+') + '/'; return false;
 $(function() {
   if(window.location.pathname === '/') {
     $('#header input').focus();
   }
+  $('#header form').bind('submit', function() {
+    $query = encodeURIComponent($('#header input').attr('value'))
+      .replace(/%20/g, '+');
+    window.location = '/' + $query + '/';
+    return false;
+  });
   var suggestion = false;
   $('#header input').click(function() {
     if (suggestion == false) {
-      $('#header').append('<div id="suggestion"><ul><li>缓释胶囊 <div>234</div></li><li>缓释胶囊 <div>234</div></li><li>缓释胶囊 <div>234</div></li></ul></div>');
+      $('#header').append('<div id="suggestion"><ul><li><span class="query">缓释胶囊</span><span class="product_amount">234</span></li><li><span class="query">缓释胶囊</span><span class="product_amount">234</span></li></ul></div>');
     }
     suggestion = true;
   });
