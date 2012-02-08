0a0d8180203b23d50045b7232bdb42c2930a79c2
unknown <zjfcwangzheng@126.com>
2012-02-08 13:54:32 +0800

    -

---
+++ vendor/wj/lib/view/ErrorScreen.php
@@ -0,0 +1,10 @@
+<?php
+class ErrorScreen extends Screen {
+  protected abstract function renderMessage();
+
+  public function renderHeadContent() {
+  }
+
+  public function renderHtmlBodyContent() {
+  }
+}
