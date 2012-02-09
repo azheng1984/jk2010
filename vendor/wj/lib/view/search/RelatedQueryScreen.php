84e8a6787aa0c47ec97672bd6a8898c605cf68e6
wangzheng <none@gmail.com>
2012-02-09 10:52:25 +0800

    -

--- vendor/wj/lib/view/search/RelatedQueryScreen.php
+++ vendor/wj/lib/view/search/RelatedQueryScreen.php
@@ -5,15 +5,15 @@ class RelatedQueryScreen {
     if ($query === false || $query['related_list'] === null) {
       return;
     }
-    $buffer = '';
+    $buffer = array();
     foreach (explode(',', $query['related_list']) as $item) {
-      $buffer .= '<li><a href="/'.urlencode($item).'/">'
-        .$item.'</a></li>';
+      $buffer[] = '<td><a href="/'.urlencode($item).'/">'
+        .$item.'</a></td>';
     }
-    if ($buffer === '') {
+    if (count($buffer) === 0) {
       return;
     }
     //TODO: 用 table 代替
-    echo '<div id="related"><h2>相关搜索:</h2><ul>', $buffer, '</ul></div>';
+    echo '<div id="related"><h2>相关搜索:</h2><table><tr>', implode('', $buffer), '</tr><tr>', implode('', array_reverse($buffer)), '</tr></table></div>';
   }
 }
