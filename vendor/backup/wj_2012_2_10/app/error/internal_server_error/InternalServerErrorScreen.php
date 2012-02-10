3c39c6d414f6368e29577d791e8c20dd504b6a46
azheng1984 <azheng1984@126.com>
2012-02-10 14:51:21 +0800

    -

---
+++ vendor/backup/wj_2012_2_10/app/error/internal_server_error/InternalServerErrorScreen.php
@@ -0,0 +1,10 @@
+<?php
+class InternalServerErrorScreen extends ErrorScreen {
+  protected function getMessage() {
+    return '出错了(500 Internal Server Error)';
+  }
+
+  protected function getCode() {
+    return 500;
+  }
+}
--- vendor/wj/app/error/not_found/NotFoundScreen.php
+++ vendor/wj/app/error/not_found/NotFoundScreen.php
@@ -1,10 +1,6 @@
 <?php
-class NotFoundScreen extends ErrorScreen {
-  protected function getMessage() {
-    echo '页面没找到(404 Not Found)';
-  }
-
-  protected function getCode() {
-    return 404;
+class NotFoundScreen {
+  public function render() {
+    echo '404 Not Found';
   }
 }
