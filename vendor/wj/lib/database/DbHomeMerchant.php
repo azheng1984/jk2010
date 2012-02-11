12d68da5c0d15140d7bf3ac990a252db6f4570ab
azheng1984 <azheng1984@126.com>
2012-02-11 21:50:53 +0800

    -

--- vendor/wj/lib/database/DbHomeMerchant.php
+++ vendor/wj/lib/database/DbHomeMerchant.php
@@ -2,7 +2,7 @@
 class DbHomeMerchant {
   public static function getList($typeId, $start = 0) {
     $sqlPrefix = 'SELECT merchant.name,merchant_home.path,merchant_home.uri '
-      .'FROM merchant_home LEFT JOIN merchant ON '
+      .'FROM merchant_home INNER JOIN merchant ON '
       .'merchant.id = merchant_home.merchant_id';
     $sqlSuffix = ' ORDER BY merchant_home.id LIMIT '.$start.', 25';
     if ($typeId !== null) {
