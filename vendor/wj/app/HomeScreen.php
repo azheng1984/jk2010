51049ecb338325b1a01dcdd9fef52c919177414c
wangzheng <none@gmail.com>
2012-02-11 11:53:43 +0800

    -

--- vendor/wj/app/HomeScreen.php
+++ vendor/wj/app/HomeScreen.php
@@ -3,12 +3,14 @@ class HomeScreen extends Screen {
   private $config;
   private $merchantList;
   private $merchantIndexName;
+  private $merchantIndex;
 
   public function __construct() {
     $this->config = require CONFIG_PATH.'home.config.php';
     if ($GLOBALS['PATH_SECTION_LIST'][1] === '') {
       $this->merchantList = $this->config['merchant_list'];
       $this->merchantIndexName = null;
+      $this->merchantIndex = null;
       return;
     }
     $this->merchantIndexName = $GLOBALS['PATH_SECTION_LIST'][1];
@@ -17,14 +19,21 @@ class HomeScreen extends Screen {
     )) {
       throw new NotFoundException;
     }
+    $this->merchantIndex
+      = $this->config['merchant_index_list'][$this->merchantIndexName];
   }
 
   protected function renderHtmlHeadContent() {
     echo '<title>货比万家</title>',
     '<meta name="description" content="货比万家购物搜索引擎，',
     '商品信息100%来自公司经营（B2C）的正规商店-网上购物，货比万家！"/>';
+    $this->addCssLink('home');
+    $this->addJsLink('home');
   }
 
   protected function renderHtmlBodyContent() {
+    if ($this->merchantIndexName !== null) {
+      $this->addJs('merchant_amount='.$this->merchantIndex[2].';');
+    }
   }
 }
