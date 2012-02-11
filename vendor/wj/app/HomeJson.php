f91141c16d2d1aad7a70e241446c8094c5a8cffe
azheng1984 <azheng1984@126.com>
2012-02-11 18:47:01 +0800

    -

--- vendor/wj/app/HomeJson.php
+++ vendor/wj/app/HomeJson.php
@@ -1,6 +1,33 @@
 <?php
-class HomeJson {
-  public function render() {
-    echo 'Welcome!';
+class HomeJson extends Json {
+  private $page;
+  private $merchantIndex;
+
+  public function __construct() {
+    $config = require CONFIG_PATH.'home.config.php';
+    if (!isset($_GET['page']) || !is_numeric($_GET['page'])
+        || $_GET['page'] < 2) {
+      throw new NotFoundException;
+    }
+    $this->page = $_GET['page'];
+    if ($GLOBALS['PATH_SECTION_LIST'][1] === '') {
+      $this->merchantIndex = null;
+      return;
+    }
+    $path = $GLOBALS['PATH_SECTION_LIST'][1];
+    if (!isset($config['merchant_index_list'][$path])) {
+      throw new NotFoundException;
+    }
+    $this->merchantIndex = $config['merchant_index_list'][$path];
+  }
+
+  protected function renderJson() {
+    $start = ($this->page - 1) * 25;
+    $merchantTypeId = null;
+    if ($this->merchantIndex !== null) {
+      $merchantTypeId = $this->merchantIndex[0];
+    }
+    $list = DbHomeMerchant::getList($merchantTypeId, $start);
+    echo '[]';
   }
 }
--- vendor/wj/lib/database/DbHomeMerchant.php
+++ vendor/wj/lib/database/DbHomeMerchant.php
@@ -1,10 +1,13 @@
 <?php
 class DbHomeMerchant {
-  public static function getList($typeId) {
-    $sql = 'SELECT merchant.name,merchant_home.path,merchant_home.uri '
+  public static function getList($typeId, $start = 0) {
+    $sqlPrefix = 'SELECT merchant.name,merchant_home.path,merchant_home.uri '
       .'FROM merchant_home LEFT JOIN merchant ON '
-      .'merchant.id = merchant_home.merchant_id WHERE '
-      .'type_id = ? ORDER BY merchant_home.id LIMIT 0, 25';
-    return Db::getAll($sql, $typeId);
+      .'merchant.id = merchant_home.merchant_id WHERE ';
+    $sqlSuffix = ' ORDER BY merchant_home.id LIMIT '.$start.', 25';
+    if ($typeId !== null) {
+      return Db::getAll($sqlPrefix.'type_id = ?'.$sqlSuffix, $typeId);
+    }
+    return Db::getAll($sqlPrefix.'type_id IS NULL'.$sqlSuffix);
   }
 }
