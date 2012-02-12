180d3a0cdf3f0e247374ee3b9607b5e1e76bd5f2
wangzheng <none@gmail.com>
2012-02-12 18:16:06 +0800

    -

--- vendor/wj/lib/database/DbCategory.php
+++ vendor/wj/lib/database/DbCategory.php
@@ -5,4 +5,11 @@ class DbCategory {
       'SELECT * FROM category WHERE `name` = ?', $name
     );
   }
+
+  public static function getList($page) {
+    $start = ($page - 1) * 100;
+    $sql = 'SELECT * FROM category ORDER BY `product_amount` LIMIT '
+      .$start.',100';
+    return Db::getAll($sql);
+  }
 }
