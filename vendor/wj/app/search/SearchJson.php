5e99ba91c69ec2db4f138f73ecda1772731a78f3
wangzheng <none@gmail.com>
2012-02-12 14:13:12 +0800

    -

---
+++ vendor/wj/app/search/SearchJson.php
@@ -0,0 +1,10 @@
+<?php
+class SearchJson extends Json {
+  public function __construct() {
+
+  }
+
+  protected function renderJson() {
+
+  }
+}
---
+++ vendor/wj/app/search/SearchScreen.php
@@ -0,0 +1,14 @@
+<?php
+class SearchScreen extends Screen {
+  public function __construct() {
+
+  }
+
+  protected function renderHtmlHeadContent() {
+
+  }
+
+  protected function renderHtmlBodyContent() {
+
+  }
+}
