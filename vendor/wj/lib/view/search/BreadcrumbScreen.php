78004ee1d60272a39914f2c5323fbdc6c6a61568
wangzheng <none@gmail.com>
2012-02-13 22:08:16 +0800

    -

--- vendor/wj/lib/view/search/BreadcrumbScreen.php
+++ vendor/wj/lib/view/search/BreadcrumbScreen.php
@@ -1,6 +1,33 @@
 <?php
 class BreadcrumbScreen {
   public static function render() {
-    
+    $list = array(htmlentities($GLOBALS['QUERY'], ENT_NOQUOTES, 'UTF-8'));
+    $pathList = array();
+    if (isset($GLOBALS['CATEGORY'])) {
+      $list[] = '分类:'.htmlentities($GLOBALS['CATEGORY']['name'], ENT_NOQUOTES, 'UTF-8');
+      $pathList[] = '..';
+    }
+    if (isset($GLOBALS['PROPERTY_LIST'])) {
+      $propertyList = array();
+      foreach ($GLOBALS['PROPERTY_LIST'] as $property) {
+        $item = htmlentities($property['KEY']['name'], ENT_NOQUOTES, 'UTF-8').':';
+        $valueList = array();
+        foreach ($property['VALUE_LIST'] as $value) {
+          $valueList[] = '<span class="value">'
+            .htmlentities($value['name'], ENT_NOQUOTES, 'UTF-8').'</span>';
+        }
+        $item .= implode(' ', $valueList);
+        $propertyList[] = $item;
+      }
+      $list[] = implode(' ', $propertyList);
+      $pathList[] = '..';
+    }
+    echo '<h1>';
+    $lastIndex = count($list) - 1;
+    for ($index = 0; $index < $lastIndex; ++$index) {
+      echo '<a href="'.implode('/', $pathList).'">', $list[$index], '</a> ';
+      array_pop($pathList);
+    }
+    echo $list[$lastIndex], '</h1>';
   }
 }
