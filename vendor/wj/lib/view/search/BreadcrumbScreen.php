58a17fc3b4aba316be0cd4c04ed70eaa0e501b9a
azheng1984 <azheng1984@126.com>
2012-02-13 22:32:57 +0800

    -

--- vendor/wj/lib/view/search/BreadcrumbScreen.php
+++ vendor/wj/lib/view/search/BreadcrumbScreen.php
@@ -4,22 +4,25 @@ class BreadcrumbScreen {
     $list = array(htmlentities($GLOBALS['QUERY'], ENT_NOQUOTES, 'UTF-8'));
     $pathList = array();
     if (isset($GLOBALS['CATEGORY'])) {
-      $list[] = '分类:'.htmlentities($GLOBALS['CATEGORY']['name'], ENT_NOQUOTES, 'UTF-8');
+      $list[] = '分类:'.htmlentities(
+        $GLOBALS['CATEGORY']['name'], ENT_NOQUOTES, 'UTF-8'
+      );
       $pathList[] = '..';
     }
     if (isset($GLOBALS['PROPERTY_LIST'])) {
-      $propertyList = array();
+      $propertySectionList = array();
       foreach ($GLOBALS['PROPERTY_LIST'] as $property) {
-        $item = htmlentities($property['KEY']['name'], ENT_NOQUOTES, 'UTF-8').':';
-        $valueList = array();
+        $propertySection =
+          htmlentities($property['KEY']['name'], ENT_NOQUOTES, 'UTF-8').':';
+        $valueSectionList = array();
         foreach ($property['VALUE_LIST'] as $value) {
-          $valueList[] = '<span class="value">'
+          $valueSectionList[] = '<span class="value">'
             .htmlentities($value['name'], ENT_NOQUOTES, 'UTF-8').'</span>';
         }
-        $item .= implode(' ', $valueList);
-        $propertyList[] = $item;
+        $propertySection .= implode(' ', $valueSectionList);
+        $propertySectionList[] = $propertySection;
       }
-      $list[] = implode(' ', $propertyList);
+      $list[] = implode(' ', $propertySectionList);
       $pathList[] = '..';
     }
     echo '<h1>';
