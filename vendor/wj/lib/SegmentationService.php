2e598df747700ee35782fb540dfc75113e9a4dcd
azheng1984 <azheng1984@126.com>
2012-02-14 11:13:51 +0800

    -

---
+++ vendor/wj/lib/SegmentationService.php
@@ -0,0 +1,19 @@
+<?php
+class SegmentationService {
+  public static function execute($input, $retry = 2) {
+    $handler = fsockopen("127.0.0.1", 8080, $errno, $errstr, 10);
+    if ($handler === false && $retry === 0) {
+      return $input;
+    }
+    if ($handler === false) {
+      return self::execute($input, --$retry);
+    }
+    fwrite($handler, $input);
+    $output = '';
+    while (!feof($handler)) {
+      $output .= fgets($handler, 256);
+    }
+    fclose($handler);
+    return $output;
+  }
+}
