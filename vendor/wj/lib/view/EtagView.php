1f5213b75efe749a03343a19169e2df76a15184a
azheng1984 <azheng1984@126.com>
2012-02-14 23:06:50 +0800

    -

--- vendor/wj/lib/view/EtagView.php
+++ vendor/wj/lib/view/EtagView.php
@@ -5,12 +5,16 @@ abstract class EtagView {
   public function render() {
     ob_start();
     $this->renderContent();
+    $content = ob_get_contents();
+    if ($content === '') {
+      return;
+    }
     $etag = md5(ob_get_contents());
     if (isset($_SERVER['HTTP_IF_NONE_MATCH'])
-      && $_SERVER['HTTP_IF_NONE_MATCH'] === $etag) {
-        header('HTTP/1.1 304 Not Modified');
-        ob_end_clean();
-        return;
+        && $_SERVER['HTTP_IF_NONE_MATCH'] === $etag) {
+      header('HTTP/1.1 304 Not Modified');
+      ob_end_clean();
+      return;
     }
     header('Etag: '.$etag);
     ob_end_flush();
--- vendor/wj/lib/view/Screen.php
+++ vendor/wj/lib/view/Screen.php
@@ -3,6 +3,7 @@ abstract class Screen extends EtagView {
   private $jsList = array();
   private $cssList = array();
   private $js = '';
+  private $stop = false;
 
   abstract protected function renderHtmlHeadContent();
   abstract protected function renderHtmlBodyContent();
@@ -19,12 +20,18 @@ abstract class Screen extends EtagView {
     $this->jsList[] = $name;
   }
 
+  protected function stop() {
+    $stop = true;
+  }
+
   protected function renderContent() {
     header('Cache-Control: private, max-age=0');
-    echo '<!DOCTYPE html><html>';
-    $this->renderHtmlHead();
-    $this->renderHtmlBody();
-    echo '</html>';
+    if ($this->stop === false) {
+      echo '<!DOCTYPE html><html>';
+      $this->renderHtmlHead();
+      $this->renderHtmlBody();
+      echo '</html>';
+    }
   }
 
   private function renderCssLinkList() {
