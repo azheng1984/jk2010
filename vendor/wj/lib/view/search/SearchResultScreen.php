727468143ac555c2e8ece61c54e75a2edb4159ff
wangzheng <none@gmail.com>
2012-02-14 16:54:58 +0800

    -

--- vendor/wj/lib/view/search/SearchResultScreen.php
+++ vendor/wj/lib/view/search/SearchResultScreen.php
@@ -1,13 +1,13 @@
 <?php
 class SearchResultScreen {
   public static function render($searchScreen) {
-    ProductSearch::search();
     echo '<div id="search"><div id="result">';
     SearchSortScreen::render();
     self::renderTotalFound();
     SearchProductListScreen::render($searchScreen);
-    $total = 1600;
-    PaginationScreen::render($GLOBALS['PAGE'], $total);
+    PaginationScreen::render(
+      $GLOBALS['PAGE'], $GLOBALS['SEARCH_RESULT']['total_found']
+    );
     echo '</div></div>';
   }
 
