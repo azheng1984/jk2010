892a0486d3063b3fc9253d91b278b86edf10c0f0
azheng1984 <azheng1984@126.com>
2012-02-15 11:11:59 +0800

    -

--- vendor/wj/app/search/SearchScreen.php
+++ vendor/wj/app/search/SearchScreen.php
@@ -17,12 +17,11 @@ class SearchScreen extends Screen {
     if (isset($GLOBALS['PROPERTY_LIST'])) {
       $title .= urldecode($GLOBALS['PATH_SECTION_LIST'][3]).'/';
     }
-
-    $title = htmlentities($title, ENT_NOQUOTES, 'UTF-8').'价格、折扣、销量排行';
+    echo htmlentities($title, ENT_NOQUOTES, 'UTF-8'), '价格、折扣、销量排行';
     if ($GLOBALS['PAGE'] > 1) {
-      $title .= '('.$GLOBALS['PAGE'].')';
+      echo '(', $GLOBALS['PAGE'], ')';
     }
-    echo '<title>', $title, '-货比万家</title>';
+    echo '-货比万家</title>';
     $this->addCssLink('search');
     $this->addJsLink('search');
   }
