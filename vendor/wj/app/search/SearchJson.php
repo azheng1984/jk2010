b65ac9ec91d3bc2afed376970ab2db15b112ebc5
azheng1984 <azheng1984@126.com>
2012-02-15 19:06:44 +0800

    -

--- vendor/wj/app/search/SearchJson.php
+++ vendor/wj/app/search/SearchJson.php
@@ -1,12 +1,52 @@
 <?php
 class SearchJson extends Json {
+  private $list = array();
+
   public function __construct() {
     if ($GLOBALS['PAGE'] > 5) {
       throw new NotFoundException;
     }
+    if (isset($GLOBALS['CATEGORY']) === false) {
+      $this->buildCategoryList();
+      return;
+    }
+    if (isset($_GET['key']) === false) {
+      $this->buildKeyList();
+      return;
+    }
+    if (isset($GLOBALS['CATEGORY']['id']) === false) {
+      throw new NotFoundException;
+    }
+    $GLOBALS['KEY'] =
+      DbPropertyKey::getByName($GLOBALS['CATEGORY']['id'], $_GET['key']);
+    if ($GLOBALS['KEY'] === false) {
+      return;
+    }
+    $this->buildValueList();
+  }
+
+  private function buildCategoryList() {
+    $result = CategorySearchService::search();
+    if ($result === false || isset($result['matches']) === false) {
+      return;
+    }
+  }
+
+  private function buildKeyList() {
+    $result = KeySearchService::search();
+    if ($result === false || isset($result['matches']) === false) {
+      return;
+    }
+  }
+
+  private function buildValueList() {
+    $result = ValueSearchService::search();
+    if ($result === false || isset($result['matches']) === false) {
+      return;
+    }
   }
 
   protected function renderJson() {
-    
+    echo '[]';
   }
 }
