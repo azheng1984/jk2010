85bf7326583d675182a6eff499365be1712c3b8a
wangzheng <none@gmail.com>
2012-02-16 17:22:37 +0800

    -

--- vendor/wj/lib/database/DbMerchant.php
+++ vendor/wj/lib/database/DbMerchant.php
@@ -1,6 +1,7 @@
 <?php
 class DbMerchant {
-  public static function get() {
-    
+  public static function get($id) {
+    $sql = 'SELECT * FROM merchant WHERE id = ?';
+    return Db::getRow($sql, $id);
   }
 }
--- vendor/wj/lib/view/search/SearchProductListScreen.php
+++ vendor/wj/lib/view/search/SearchProductListScreen.php
@@ -1,6 +1,7 @@
 <?php
 class SearchProductListScreen {
   public static function render($searchScreen) {
+    return;
     $metaList = array();
     $index = 0;
     $hasCategory = isset($GLOBALS['CATEGORY']);
@@ -12,12 +13,17 @@ class SearchProductListScreen {
       echo '<div class="image"><a href="" target="_blank" rel="nofollow">',
         '<img alt="'.$product['title'].'" src="http://img.dev.huobiwanjia.com/',
         $product['id'].'.jpg"/></a></div>';
-      echo '<h3>', $product['title'], '</h3>';
+      echo '<h3><a href="" target="_blank" rel="nofollow">',
+        $product['title'], '</a></h3>';
+      echo '<div class="price">&yen;<span>',
+        $product['lowest_price_x_100']/100,'</span></div>';
+      echo '<p>', $product['description'], '&hellip;</p>';
       echo '<div class="merchant">', $merchant['name'], '</div>';
       echo '</li>';
       $metaList[] = self::getMeta($product, $hasCategory);
     }
     echo '<ol>';
+    self::renderMetaList($metaList);
   }
 
   private static function getMeta($product, $hasCategory) {
@@ -33,5 +39,15 @@ class SearchProductListScreen {
       $meta[2] = $product['brand_name'];
     }
     return $meta;
+  }
+
+  private static function excerpt($text, $keywordList) { //TODO
+  }
+
+  private static function highlight($text, $keywordList) { //TODO
+  }
+
+  private static function renderMetaList() { //TODO
+     
   }
 }
