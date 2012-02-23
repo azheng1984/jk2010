43db32d69484f0f797b0414b212bc22b0882937f
azheng1984 <azheng1984@126.com>
2012-02-23 12:41:41 +0800

    -

--- vendor/wj/lib/view/search/SearchRelatedQueryScreen.php
+++ vendor/wj/lib/view/search/SearchRelatedQueryScreen.php
@@ -5,24 +5,24 @@ class SearchRelatedQueryScreen {
       return;
     }
     $relatedList = explode(',', $GLOBALS['QUERY']['related_list']);
-    $maxColumn = 4;
+    $columnAmount = 4;
     foreach ($relatedList as $query) {
       if (mb_strlen($query, 'UTF-8') > 15) {
-        $maxColumn = 2;
+        $columnAmount = 2;
         break;
       }
     }
     $index = 0;
     echo '<div id="related"><h2>相关搜索:</h2><table><tr>';
     foreach ($relatedList as $query) {
-      if ($index % $maxColumn === 0 && $index !== 0) {
+      if ($index % $columnAmount === 0 && $index !== 0) {
         echo '</tr><tr>';
       }
       echo '<td><a href="/', urlencode($query), '/">', $query, '</a></td>';
       ++$index;
     }
-    if ($index % $maxColumn !== 0 && $index > $maxColumn) {
-      $colspan = $maxColumn - $index % $maxColumn;
+    if ($index % $columnAmount !== 0 && $index > $columnAmount) {
+      $colspan = $columnAmount - $index % $columnAmount;
       $colspanAttribute = $colspan === 1 ? '' : ' colspan="'.$colspan.'"';
       echo '<td', $colspanAttribute, '></td>';
     }
