37e828e48aaab4318b677c9a06e354714713eb84
azheng1984 <azheng1984@126.com>
2012-02-26 12:07:02 +0800

    -

--- vendor/wj/lib/view/search/SearchBreadcrumbScreen.php
+++ vendor/wj/lib/view/search/SearchBreadcrumbScreen.php
@@ -1,27 +1,33 @@
 <?php
 class SearchBreadcrumbScreen {
   public static function render() {
+    $list = array(
+        htmlentities($GLOBALS['QUERY']['name'], ENT_NOQUOTES, 'UTF-8')
+    );
     $recognitionSection = '';
     if (isset($GLOBALS['IS_RECOGNITION'])) {
       $recognitionSection = ' <span class="recognition">同款</span>';
+      if (isset($GLOBALS['CATEGORY']) === false) {
+        $list[0] = '<span class="query">'.$list[0].'</span>';
+      }
     }
-    $list = array(
-      htmlentities($GLOBALS['QUERY']['name'], ENT_NOQUOTES, 'UTF-8')
-    );
     if (isset($GLOBALS['CATEGORY']) === false) {
       echo '<div id="breadcrumb"><h1>', $list[0], $recognitionSection,
         '</h1></div>';
       return;
     }
-    $list[] = '分类: <span class="category">'.htmlentities(
+    $list[] = '分类: '.htmlentities(
       $GLOBALS['CATEGORY']['name'], ENT_NOQUOTES, 'UTF-8'
-    ).'</span>';
+    );
+    if (isset($GLOBALS['PROPERTY_LIST']) === false) {
+      $list[1] = '<span class="category">'.$list[1].'</span>';
+    }
     $pathList = array('..');
     if (isset($GLOBALS['PROPERTY_LIST'])) {
       $propertySectionList = array();
       foreach ($GLOBALS['PROPERTY_LIST'] as $property) {
-        $propertySection = '<span class="property">'
-          .htmlentities($property['key']['name'], ENT_NOQUOTES, 'UTF-8').': ';
+        $propertySection = '<span class="property"><span>'
+          .htmlentities($property['key']['name'], ENT_NOQUOTES, 'UTF-8').':</span>';
         $valueSectionList = array();
         foreach ($property['value_list'] as $value) {
           $valueSectionList[] = '<span class="value">'
