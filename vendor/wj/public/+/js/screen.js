ff0b6f0b569588413a0041a7950f9a9888277e3c
wangzheng <none@gmail.com>
2012-02-28 23:38:07 +0800

    -

--- vendor/wj/public/+/js/screen.js
+++ vendor/wj/public/+/js/screen.js
@@ -1,10 +1,223 @@
-function suggest(data) {
+var suggestionCache = {};
+var currentAjaxQuery = null;
+var currentQuery = null;
+var suggestionTimeoutId = null;
+var hoverQuery = null;
+var stopHiddenSuggestion = false;
+
+function suggest(query, data) {
+  if (typeof(suggestionCache[currentAjaxQuery]) == 'undefined') {
+    suggestionCache[currentAjaxQuery] = [query, data];
+  }
+  if (currentQuery == currentAjaxQuery) {
+    renderSuggestion(query, data);
+  }
+  if (currentQuery != currentAjaxQuery && currentQuery != null) {
+    getSuggestion();
+    return;
+  }
+  currentAjaxQuery = null;
+  suggestionTimeoutId = null;
+}
+
+function renderSuggestion(query, data) {
+  if (typeof(data) == 'undefined') {
+    $('#suggestion').remove();
+    return;
+  }
+  var html = '<div id="suggestion"><ul>';
+  $.each(data, function(index, value) {
+    html += '<li><a href="/' + encodeURIComponent(index)
+      + '/"><span class="query">' + highlight(index, query)
+      + '</span><span class="product_amount">' + value
+      + '</span></a></li>';
+  });
+  html += '</ul></div>';
+  $('#suggestion').remove();
+  $('#header').append(html);
+  $('#suggestion li a').hover(
+    function(){
+      hoverQuery = $(this).find('.query').val();
+      $('#suggestion li.hover').removeClass('hover');
+      $(this).parent().addClass('hover');
+      stopHiddenSuggestion = true;
+    },
+    function(){
+      $('#suggestion li.hover').removeClass('hover');
+      hoverQuery = null;
+      stopHiddenSuggestion = false;
+    }
+  );
+}
+
+function highlight(query, keywordList) {
+  return query;
+}
+
+function initializeQueryForm() {
+  $('#header input').attr('autocomplete', 'off');
+  $('#header form').bind('submit', function() {
+    query = encodeURIComponent($.trim($('#header input').attr('value')))
+      .replace(/%20/g, '+') + '/';
+    if (query == '%2B/') {
+      query = '';
+    }
+    window.location = '/' + query;
+    return false;
+  });
+  initializeQuerySuggestion();
+}
+
+function up() {
+  var target = null;
+  var previous = null;
+  $('#suggestion li').each(function() {
+    if (target != null) {
+      return;
+    }
+    var current = $(this);
+    if (current.hasClass('hover')) {
+      current.removeClass('hover');
+      if (previous == null) {
+        target = false;
+        return;
+      }
+      target = previous;
+    }
+    previous = current;
+  });
+  if (target == null) {
+    target = previous;
+  }
+  if (target != false) {
+    target.addClass('hover');
+    hoverQuery = target.find('.query').text();
+    $('#header input').val(hoverQuery);
+    return;
+  }
+  hoverQuery = null;
+  $('#header input').val(currentQuery);
+}
+
+function down() {
+  var current = null;
+  $('#suggestion li').each(function() {
+    if (current != null) {
+      return;
+    }
+    if ($(this).hasClass('hover')) {
+      current = $(this);
+      current.removeClass('hover');
+    }
+  });
+  var target = null;
+  if (current == null) {
+    target = $('#suggestion li').first();
+  }
+  if (target == null && current != null) {
+    target = current.next();
+  }
+  if (target != null && target.length != 0) {
+    target.addClass('hover');
+    hoverQuery = target.find('.query').text();
+    $('#header input').val(hoverQuery);
+    return;
+  }
+  hoverQuery = null;
+  $('#header input').val(currentQuery);
+}
+
+function initializeQuerySuggestion() {
+  $('#header input').keydown(function(event){
+    if ($('#suggestion').length == 0) {
+      return;
+    }
+    if (event.which == 27) {
+      $('#suggestion').remove();
+      return;
+    }
+    if (event.which == 38) {
+      up();
+      return false;
+    }
+    if (event.which == 40) {
+      down();
+      return false;
+    }
+  });
+  $('#header input').keyup(function(evnet) {
+    //query = $('#header input').val().trim();
+    $('#header').append('dfasdf <br/>');
+    return;
+    if (query == currentQuery || query == hoverQuery) {
+      return;
+    }
+    currentQuery = query;
+    if (suggestionTimeoutId == false) {
+      return;
+    }
+    if (suggestionTimeoutId != null) {
+      clearTimeout(suggestionTimeoutId);
+      suggestionTimeoutId = null;
+    }
+    suggestionTimeoutId = setTimeout(getSuggestion, 600);
+  });
+  $('#header input').focusin(function() {
+    currentQuery = $('#header input').val().trim();
+  });
+  $('#header input').focusout(function() {
+    currentQuery = null;
+    if (stopHiddenSuggestion != false) {
+      return;
+    }
+    $('#suggestion').remove();
+  });
+}
+
+function getSuggestion() {
+  $('#header').append('getSuggestion');
+  suggestionTimeoutId = false;
+  currentAjaxQuery = currentQuery;
+  if (currentAjaxQuery == '') {
+    suggest(currentAjaxQuery);
+    return;
+  }
+  if (typeof(suggestionCache[currentAjaxQuery]) == 'undefined') {
+    var uri = 'http://q.dev.huobiwanjia.com/'
+      + encodeURIComponent(currentAjaxQuery);
+    $.ajax({
+      url:uri,
+      cache:true,
+      dataType:'jsonp',
+      jsonp:false
+    });
+    return;
+  }
+  suggest(
+    suggestionCache[currentAjaxQuery][0], suggestionCache[currentAjaxQuery][1]
+  );
+}
+
+$(function() {
+  initializeQueryForm();
+});
+
+
+
+
+
+/*
+
+
+
+
+function suggest(query, data) {
   var text = '<div id="suggestion"><ul>';
   if (typeof(data) == 'undefined') {
     return;
   }
   $.each(data, function(index, value){
-    text += '<li><span class="query">' + index + '</span><span class="product_amount">' + value + '</span></li>';
+    text += '<li><a href="/' + encodeURIComponent(index) + '/"><span class="query">' + index + '</span><span class="product_amount">' + value + '</span></a></li>';
   });
   text += '</ul></div>';
   $('#header').append(text);
@@ -66,3 +279,4 @@ $(function() {
     }
   });
 });
+*/
--- vendor/wj_query_suggestion/lib/QuerySearch.php
+++ vendor/wj_query_suggestion/lib/QuerySearch.php
@@ -1,19 +1,10 @@
 <?php
 class QuerySearch {
-  public static function search() {
-    $query = urldecode(substr($_SERVER['REQUEST_URI'], '1'));
-    if ($query === '') {
-      return array();
-    }
-    $query = SegmentationService::execute($query);
+  public static function search($query) {
     $sphinx = new SphinxClient;
     $sphinx->setServer("localhost", 9312);
     $sphinx->setMaxQueryTime(1000);
     $sphinx->SetSortMode(SPH_SORT_ATTR_DESC, 'product_amount');
-    $result = $sphinx->query($query, 'wj_query');
-    if ($result === false) {
-      $result = array('total_found' => 0, 'matches' => array());
-    }
-    return $result;
+    return $sphinx->query($query, 'wj_query');
   }
 }
