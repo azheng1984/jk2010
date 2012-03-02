852376818071ce03efc4ad092b5f76d3155b592a
azheng1984 <azheng1984@126.com>
2012-03-02 18:25:25 +0800

    -

--- vendor/wj/public/+/js/home.js
+++ vendor/wj/public/+/js/home.js
@@ -2,3 +2,14 @@ $(function() {
   $('#header input').focus();
 });
 //TODO:merchant list enhancement
+
+$(function() {
+  $('#merchant td a').each(function() {
+    $(this).bind('mousedown', function(button) {
+      if (button.which == 1) {
+        alert($(this).attr('href'));
+        //商家 url + session，通过  jsonp 的方式发送到 tracking 服务器 click.huobiwanjia.com
+      }
+    });
+  });
+});
