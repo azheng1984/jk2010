f4ea732f8decf813963eb3db443bd80ec53f95ab
wangzheng <none@gmail.com>
2012-03-22 12:36:25 +0800

    -

--- vendor/wj/public/+/js/home.js
+++ vendor/wj/public/+/js/home.js
@@ -5,32 +5,11 @@ $(function() {
   $('#header input').focus();
 });
 /*
- * activity slideshow
+ * slideshow
  *****************************/
 
 /* merchant list
  *****************************/
-$(function() {
-  var more = $('#more');
-  if (more.length === 0) {
-    return;
-  }
-  more.remove();
-  return;
-  $('#list_wrapper').after('<span id="more"><span>更多商店</span></span>');
-  $('#more').hover(function() {
-    $(this).css('background-position', '-247px -106px')
-      .css('cursor', 'pointer');
-    $('#more span').css('color', '#111').css('border-color', '#111');
-  }, function() {
-    $(this).css('background-position', '-77px -106px')
-      .css('cursor', 'default');
-    $('#more span').css('color', '#555').css('border-color', '#555');
-  });
-  $('#more').mouseup(function() {
-    alert('load');
-  });
-});
 
 /* merchant click tracking
  *****************************/
