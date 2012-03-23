5e8fc7df5dd8bae17fb0a4a222d431072b2b10e3
wangzheng <none@gmail.com>
2012-03-23 22:47:09 +0800

    -

---
+++ vendor/backup/wj_2012_3_23/public/+/js/screen.js
@@ -0,0 +1,23 @@
+var huobiwanjia = {};
+/* search suggestion
+ *****************************/
+$(function() {
+  $('#header input').attr('autocomplete', 'off');
+});
+
+/* tracking function
+ *****************************/
+huobiwanjia.trackPageview = function() {
+  $.ajax({
+    url:'http://tracking.' + document.domain + '/',
+    cache:true,
+    dataType:'jsonp',
+    jsonp:false
+  });
+};
+
+/* page tracking
+ *****************************/
+$(function() {
+  //huobiwanjia.trackPageview();
+});
