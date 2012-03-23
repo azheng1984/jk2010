4c6eb801d88a949c71d2f0c76f60b921ce392268
wangzheng <none@gmail.com>
2012-03-23 10:54:26 +0800

    -

--- vendor/wj/public/+/js/home.js
+++ vendor/wj/public/+/js/home.js
@@ -9,12 +9,44 @@ $(function() {
  * slide list
  *****************************/
 $(function() {
+  var index = 0;
   $('#slide_list a').each(function() {
+    var id = 'slide-' + index;
+    $(this).replaceWith('<span id="' + id + '" class="item"></span>');
+    var item = $('#' + id);
+    item.hover(function() {
+      $(this).addClass('hover');
+    }, function() {
+      $(this).removeClass('hover');
+    });
+    item.click(function() {
+      alert('ajax');
+    });
+    ++index;
   });
 });
 
 /* merchant list
  *****************************/
+$(function() {
+  var index = 0;
+  $('#merchant_list a').each(function() {
+    var id = 'merchant-' + index;
+    $(this).replaceWith('<span id="' + id + '" class="item">'+$(this).html()+'</span>');
+    $('#' + id).hover(function() {
+      $(this).addClass('hover');
+    }, function() {
+      $(this).removeClass('hover');
+    });
+    ++index;
+  });
+  $('#down').replaceWith('<span id="down" class="full"></a>');
+  $('#down').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+});
 
 /* merchant click tracking
  *****************************/
