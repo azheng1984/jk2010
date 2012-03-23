eeba129412702fe0e6887cd7cd16fe4539fabd7f
azheng1984 <azheng1984@126.com>
2012-03-23 22:47:09 +0800

    -

---
+++ vendor/backup/wj_2012_3_23/public/+/js/search.js
@@ -0,0 +1,15 @@
+huobiwanjia.search = {};
+/* price range
+ *****************************/
+
+/* tag list
+ *****************************/
+
+/* product link list
+ *****************************/
+
+/* product property highlight
+ *****************************/
+
+/* product click tracking
+ *****************************/
