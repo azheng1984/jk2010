9f7699454840d1868adc9b9177baf230112289fc
azheng1984 <azheng1984@126.com>
2012-03-30 13:24:06 +0800

    -

--- vendor/wj/public/+/js/search.js
+++ vendor/wj/public/+/js/search.js
@@ -155,7 +155,7 @@ huobiwanjia.search.enhanceKey = function(key) {
         var item = data[index];
         html += '<li class="value"><a href="'
           + huobiwanjia.search.getTagHref(keyName, item[0]) +'">'
-          + '<span> ' + item[0] + '</span>' + '</a></li>';
+          + '<span> ' + item[0] + '</span> ' + item[1] + '</a></li>';
       }
       html += '</ol>';
       current.after(html);
@@ -186,7 +186,7 @@ huobiwanjia.search.enhanceMoreValue = function(key, keyName, page) {
           var item = data[index];
           html += '<li class="value"><a href="'
             + huobiwanjia.search.getTagHref(keyName, item[0]) +'">'
-            + '<span> ' + item[0] + '</span>' + '</a></li>';
+            + '<span> ' + item[0] + '</span> ' + item[1] + '</a></li>';
         }
         key.next().append(html);
         //key.next().children('.hidden').fadeIn('fast');
@@ -281,138 +281,6 @@ huobiwanjia.search.getTagHref = function(keyName, valueName) {
   return result;
 };
 
-/*
-$(function() {
-  $.getJSON(window.location.pathname + '?media=json', function(data) {
-    var totalFound = data.shift();
-    var type  = window.location.pathname.split('/').length === 3
-      ? 'category' : 'property';
-    if (type === 'category') {
-      var html = '';
-      for (var index = 0; index < data.length; ++index) {
-        html += '<li class="value"><a href="'
-          + encodeURIComponent(data[index][0]) + '/"><span>' + data[index][0] + '</span> ' + data[index][1] + '</a></li>';
-      }
-      $('#result_wrapper').after('<div id="tag"><h2>分类:</h2><ol>' + html + '</ol><div class="more"><span>更多</span></div></div>');
-      enhanceCategoryList();
-      return;
-    }
-    var html = '';
-    for (var index = 0; index < data.length; ++index) {
-      html += '<li><span class="key"><span>' + data[index][0] + '</span></span></li>';
-    }
-    $('#result_wrapper').after('<div id="tag"><h2>属性:</h2><ol>' + html + '</ol><div class="more property"><span>更多属性</span></div></div>');
-    $('#tag .key').click(function() {
-      if ($(this).attr('class') === 'key open') {
-        $(this).attr('class', 'key');
-        $(this).parent().children('ol').hide();
-        $(this).parent().children('div').hide();
-        return;
-      }
-      var target = $(this).attr('class', 'key open');
-      if ($(this).next('ol').length > 0) {
-        $(this).nextUntil('li').show();
-        return;
-      }
-      //TODO: 如果有属性选定，先判断 is_multiple 值，再决定是否发起请求
-      var keyName = $(this).text();
-      $uri2 = window.location.pathname + '?key=' + keyName + '&media=json';
-      target.after('<div id="load">正在加载...</div>');
-      $.getJSON($uri2, function(data) {
-        $('#load').fadeOut('fast', function() {
-          $(this).remove();
-          var html = '';
-          for (var index = 1; index < data.length; ++index) {
-            var item = data[index];
-            //var tmp = getPropertyHref(keyName, item[0], true);
-            var href = '/';//tmp[0];
-            if (window.location.pathname.split('/').length > 4) {
-              href = '../' + href;
-            }
-            if (true) {
-              html += '<li style="display:none"><span class="value"><a href="' + href + '">' + '<span>' + item[0] + '</span> ' + item[1] + '</a></li>';
-            } else {
-              html += '<li><span class="value selected"><a href="' + href + '">' + '<span>' + item[0] + '</span> ' + '</a></li>';
-            }
-          }
-          target.after('<ol>' + html + '</ol><div class="more"><span>更多</span></div>');
-          target.next().children(':hidden').fadeIn('slow');
-          target = null;
-        });
-      });
-    });
-  });
-});
-var categoryLoadCount = 1;
-function enhanceCategoryList() {
-  $('#tag .more').click(function() {
-    $(this).replaceWith('<div id="load">正在加载…</div>');
-    ++categoryLoadCount;
-    $.getJSON(window.location.pathname + '?media=json', function(data){
-      var html = '';
-      for (var index = 1; index < data.length; ++index) {
-        html += '<li class="value" style="display:none"><a href="'
-          + encodeURIComponent(data[index][0]) + '/"><span>' + data[index][0] + '</span> ' + data[index][1] + '</a></li>';
-      }
-      $('#load').fadeOut('fast', function(){
-        $(this).remove();
-        if (categoryLoadCount < 5) {
-          $('#tag ol').append(html).after('<div class="more"><span>更多</span></div>');
-          $('#tag li:hidden').fadeIn('slow');
-          enhanceCategoryList();
-          return;
-        }
-        $('#tag li').last().after(html + '<li>…</li>');
-        $('#tag li:hidden').fadeIn('slow');
-      });
-    });
-  });
-}
-var propertyList = {};
-var level = null;
-function getTagHref(keyName, valueName, removeFlag) {
-  if (typeof propertyList[keyName] === 'undefined') {
-    var href = propertyListPath;
-    if (level === 2) {
-      href += '&';
-    }
-    result = href + encodeURIComponent(keyName) + '=' + encodeURIComponent(valueName) + '/' + queryString;
-    if ((typeof removeFlag) !== 'undefined') {
-      result = [result, false];
-    }
-    return result;
-  }
-  var isRemove = false;
-  sectionList = [];
-  for (var propertyName in propertyList) {
-    var valueList = propertyList[propertyName];
-    var valuePath = encodeURIComponent(valueName);
-    if (keyName === propertyName && $.inArray(valuePath, valueList) !== -1) {
-      isRemove = true;
-      valueList = $.grep(valueList, function(value) {
-        return value != valuePath;
-      });
-    } else {
-      valueList = valueList.slice(0);
-      valueList.push(valuePath);
-    }
-    if (valueList.length !== 0) {
-      sectionList.push(
-        encodeURIComponent(propertyName) + '=' + valueList.join('&')
-      );
-    }
-  }
-  result = '';
-  if (sectionList.length !== 0) {
-    result = sectionList.join('&') + '/';
-  }
-  result = result + queryString;
-  if ((typeof removeFlag) !== 'undefined') {
-    result = [result, isRemove];
-  }
-  return result;
-}
-*/
 /* product link list
  *****************************/
 
