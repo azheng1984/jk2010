d4e6b2dd4dabfda8d402f586e8d980525225d3eb
wangzheng <none@gmail.com>
2012-04-21 14:35:43 +0800

    -

--- vendor/wj/spider/common/lib/Lock.php
+++ vendor/wj/spider/common/lib/Lock.php
@@ -2,24 +2,18 @@
 class Lock {
   public static function execute() {
     $lockList = DbLock::getAll();
-    self::check();
-  }
-
-  private static function check() {
-    if (function_exists('posix_getpid') === false) {
-      return null;
+    $processId = null;
+    if (function_exists('posix_getpid') === true) {
+      $processId = posix_getpid();
     }
-    posix_getpid();
-  }
-
-  private static function check($lockList) {
-    if (processId === null && count($lockList) !== 0) {
+    if (count($lockList) !== 0 && $processId === null) {
       throw new Exception('lock fail');
     }
-  }
-
-  private static function checkByPs($processIdList) {
-    foreach ($processIdList as $item) {
+    DbLock::insert($processId);
+    if ($processId === null) {
+      return;
+    }
+    foreach ($lockList as $item) {
       $output = shell_exec("ps -p {$item['process_id']}");
       if (strstr($output, $item['process_id']) !== false) {
         throw new Exception('lock fail');
