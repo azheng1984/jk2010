6e0a4227baddd16080a9dc6f84965d29100487d0
azheng <azheng1984@126.com>
2012-05-13 21:52:40 +0800

    -

--- vendor/wj/spider/config/merchant/dangdang/env.default.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS['no_image_md5'] = array(
-  'aff02bb2aaa921e4671522b94f0061a5' => true,
-  '074f08d6ad97e753c7d99553c7fa530a' => true,
-);
-define('HYPERFRAMEWORK_PATH', '/home/wz/www/jk2010/vendor/hyperframework/');
-define ('IMAGE_PATH', '/home/wz/spider/image/dangdang/');
-define('DB_NAME', 'dangdang');
--- vendor/wj/spider/config/merchant/jingdong/database.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-  'dsn' => 'mysql:host=localhost;dbname=jingdong',
-  'username' => 'root',
-  'password' => 'a841107!',
-);
