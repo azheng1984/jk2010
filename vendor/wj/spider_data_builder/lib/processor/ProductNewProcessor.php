ff02903eda8ce6b0bd1d1837d170fb04af7321ca
azheng <azheng1984@126.com>
2012-05-21 13:36:02 +0800

    -

--- vendor/wj/spider_data_builder/lib/processor/ProductNewProcessor.php
+++ vendor/wj/spider_data_builder/lib/processor/ProductNewProcessor.php
@@ -1,93 +1,127 @@
 <?php
 class ProductNewProcessor {
   private $wjProductId;
-  private $categoryId;
   private $category;
 
   public function execute($productId, $category, $keyMapper, $valueMapper) {
     $this->category = $category;
+    DbConnection::connect('spider');
     $propertyValueIdList = Db::getAll(
       'SELECT property_value_id, is_updated FROM '
         .'`electronic-product-property_value` WHERE product_id = ?',
       $productId
     );
-    $propertyList = array();
+    $propertyKeyList = array();
+    $propertyValueList = array();
     foreach ($propertyValueIdList as $item) {
-      
+      $value = $valueMapper[$item['value_id']];
       if ($item['is_updated'] === '0') {
-        Db::execute('UPDATE property_key SET product_amount = product_amount - 1 WHERE id = ?', $item['']);
-        //TODO decrease builder key product amount
+        DbConnection::connect('builder');
+        Db::execute(
+          'UPDATE property_key'
+            .' SET product_amount = product_amount - 1 WHERE id = ?',
+          $value['wj_id']
+        );
         continue;
       }
       if ($item['is_new'] === '1') {
-        //TODO: increase builder key product amount
+        Db::execute(
+          'UPDATE property_key'
+            .' SET product_amount = product_amount + 1 WHERE id = ?',
+          $value['wj_id']
+        );
       }
-      $value = $valueMapper[$item['value_id']];
       $key = $keyMapper[$value['key_id']];
-      $propertyValueId = $item['id'];
       if (isset($propertyList[$key['name']]) === false) {
-        $propertyList[$key['name']] = array();
+        $propertyValueList[$key['name']] = array();
+        $propertyKeyList[$key['name']] = $key;
       }
-      $propertyList[$key['name']][] = $value['name'];
+      $propertyValueList[$key['name']][] = $value;
     }
-    $webPropertyList = array();
-    foreach ($propertyList as $keyName => $valueNameList) {
-      $webPropertyList[] = $keyName.implode("\n\t", $valueNameList);
-    }
-    $webPropertyListValue = implode("\n", $webPropertyList);
+    DbConnection::connect('spider');
     $product = Db::getRow(
-      'SELECT category_id, merchant_product_id, title,'
+      'SELECT merchant_product_id, title,'
         .' image_md5, price_from_x_100, price_to_x_100, list_price_x_100,'
         .' sale_rank, index_time FROM `product` WHERE id = ?',
       $productId
-    );//spider
-    $this->syncWeb($product, $webPropertyListValue, $propertyList);
-    $this->syncSearch($product, $webPropertyList);
+    );
+    $this->syncWeb($product, $propertyKeyList, $propertyValueList);
+    $this->syncSearch($product, $propertyKeyList, $propertyValueList);
   }
 
-  private function syncWeb($product, $webPropertyListValue, $propertyList) {
+  private function syncWeb($product, $keyList, $valueList) {
+    $mvaPropertyList = array();
+    $textPropertyList = array();
+    foreach ($valueList as $keyName => $propertyValueList) {
+      $webPropertyValueList = array();
+      foreach ($propertyValueList as $value) {
+        $webPropertyValueList[$value['index']] = $value;
+      }
+      if (count($webPropertyValueList) > 1) {
+        ksort($webPropertyValueList);
+      }
+      if ($keyList[$keyName]['mva_index'] !== null) {
+        $mvaPropertyList[$keyList[$keyName]['index']] =
+          $keyName.implode("\n\t", $webPropertyValueList);
+        continue;
+      }
+      $textPropertyList[$keyList[$keyName]['index']] =
+        $keyName.implode("\n\t", $webPropertyValueList);
+    }
+    ksort($mvaPropertyList);
+    ksort($textPropertyList);
+    $product['property_list'] = implode("\n", $mvaPropertyList);
+    if (count($textPropertyList) !== 0) {
+      $product['property_list'] .= "\n".implode("\n", $textPropertyList);
+    }
     DbConnection::connect('builder');
     Db::insert('`wj_product-merchant_product`', array(
       'merchant_id' => 1,
       'merchant_product_id' => $product['merchant_product_id']
     ));
     $this->wjProductId = Db::getLastInsertId();
-    DbConnection::connect('jingdong');
+    $product['id'] = $this->wjProductId;
+    $product['category_id'] = $this->category['wj_id'];
     $product['merchant_uri_argument_list'] = $product['merchant_product_id'];
     unset($product['merchant_product_id']);
     unset($product['sale_rank']);
     unset($product['index_time']);
-    if ($webPropertyListValue !== '') {
-      $product['property_list'] = $webPropertyListValue;
-    }
     $product['category_name'] = $this->category['name'];
     $product['image_path'] = '/';
+    DbConnection::connect('web');
     Db::insert('product', $product);
-    DbConnection::connect('jingdong');
   }
 
-  private function syncSearch($product, $category, $keyMapper, $valueMapper) {
+  private function syncSearch($product, $keyList, $valueList) {
     $row = array();
     $row['id'] = $this->wjProductId;
-    $row['category_id'] = $this->categoryId;
+    $row['category_id'] = $this->category['wj_id'];
     $row['price_from_x_100'] = $product['price_from_x_100'];
     $row['sale_rank'] = $product['sale_rank'];
     $row['index_timestamp'] = strtotime($product['index_time']);
-    if ($row['list_price_x_10'] !== null && $row['price_to_x_10'] === null) {
+    if ($row['list_price_x_10'] !== null && $row['price_to_x_10'] !== null) {
       $row['discount_x_10'] = ceil(
         $row['price_x_10'] / $row['list_price_x_10'] * 100
       );
     }
-    $row['keyword_list'] = SegmentationService::execute();
-    $row['key_id_list'] = array();
-    //TODO set key_id_list
+    $row['keyword_list'] = implode(' ', array_unique(
+      explode(' ', SegmentationService::execute())
+    ));
+    $keyIdList = array();
     $propertyList = array();
-    foreach ($propertyList as $property) {
-      $key = $property['key'];
-      $value = $property['value'];
-      $row['value_id_list_'] = array();
-      //TODO set value_id_list
+    foreach ($keyList as $keyName => $key) {
+      if ($key['mva_index'] === null) {
+        continue;
+      }
+      $keyIdList[] = $key['wj_id'];
+      $valueIdList = array();
+      foreach ($valueList[$key['name']] as $value) {
+        $valueIdList = $value['wj_id'];
+      }
+      $row['value_id_list_'.$key['mva_index']] = implode(',', $valueIdList);
     }
-    //TODO insert into search db
+    $row['key_id_list'] = implode(',', $keyIdList);
+    DbConnection::connect('search');
+    Db::insert('product', $row);
   }
 }
