38c296ab00451d3723428414fe9c8f2296b1d721
wangzheng <none@gmail.com>
2012-05-22 15:48:25 +0800

    -

--- vendor/wj/spider_data_builder/app/BuildCommand.php
+++ vendor/wj/spider_data_builder/app/BuildCommand.php
@@ -8,53 +8,58 @@ class BuildCommand {
   private $isMvaIndexListModified = false;
 
   public function execute() {
-    //sync meta
     DbConnection::connect('spider');
     $categoryList = Db::getAll('SELECT * FROM category');
     foreach ($categoryList as $category) {
-      $this->syncByCategory($category);
+      $this->category = $category;
+      $this->syncByCategory();
+      $this->syncProductByCategory();
     }
-    //sync product
-    $this->syncProductByCategory($category);
   }
 
-  private function syncByCategory($category) {
-    $this->category = $category;
+  private function syncByCategory() {
     $this->mvaIndexList = null;
     $this->mvaIndexSearchStartPoint = 0;
     $this->isMvaIndexListModified = false;
     DbConnection::connect('builder');
     $isNew = false;
     $categoryId = Db::bind(
-      'category', array('name' => $category['name']), null, $isNew
+      'category', array('name' => $this->category['name']), null, $isNew
     );
     $this->category['wj_id'] = $categoryId;
     if ($isNew) {
       DbConnection::connect('web');
       Db::insert('category', array(
-        'id' => $categoryId, 'name' => $category['name'])
+        'id' => $categoryId, 'name' => $this->category['name'])
       );
     }
     DbConnection::connect('spider');
     $keyList = Db::getAll(
-      'SELECT * FROM property_key WHERE category_id = ?', $category['id']
+      'SELECT * FROM property_key WHERE category_id = ?', $this->category['id']
     );
     $keyMapper = array();
     $valueMapper = array();
-    $wjKeyId = null;
     foreach ($keyList as $key) {
       $keyMapper[$key['id']] = $key;
-      if ($key['is_new'] === '1') {
-        $mvaIndex = null;
-        if (count($keyMapper) < 101) {
-          $mvaIndex = $this->getMvaIndex($categoryId);
-        }
-        DbConnection::connect('builer');
-        $isNew = false;
-        $wjKeyId = Db::bind('property_key', array(
+      $isNew = false;
+      DbConnection::connect('builder');
+      $wjKeyId = Db::bind('property_key', array(
           'category_id' => $key['category_id'], 'name' => $key['name']
-        ), array('mva_index' => $mvaIndex), $isNew);
+      ), $isNew);
+      $keyMapper[$key['id']]['wj_id'] = $wjKeyId;
+      if ($key['is_new'] === '1') {
+        DbConnection::connect('builer');
         if ($isNew) {
+          $mvaIndex = null;
+          if (count($keyMapper) < 100) {
+            $mvaIndex = $this->getMvaIndex($categoryId);
+          }
+          DbConnection::connect('spider');
+          if ($mvaIndex !== null) {
+            Db::update('property_key', array(
+              'mva_index' => $mvaIndex
+            ), 'id = ?', $wjKeyId);
+          }
           DbConnection::connect('web');
           Db::insert('property_key', array(
             'id' => $wjKeyId, 'category_id' => $key['category_id'],
@@ -66,28 +71,12 @@ class BuildCommand {
       $valueList = Db::getAll(
         'SELECT * FROM property_value WHERE key_id = ?', $key['id']
       );
-      if ($wjKeyId === null) {
-        DbConnection::connect('builder');
-        $wjKeyId = Db::getColumn(
-          'SELECT id FROM property_key WHERE category_id = ? AND name = ?',
-          $categoryId, $key['name']
-        );
-      }
-      $keyMapper[$key['id']]['wj_id'] = $wjKeyId;
       foreach ($valueList as $value) {
         $valueMapper[$value['id']] = $value;
-        $wjValueId = null;
-        if ($value['is_new']) {
-          DbConnection::connect('web');
-          $wjValueId = Db::bind('property_value', array(
-            'key_id' => $wjKeyId, 'name' => $value['name']
-          ));
-        } else {
-          DbConnection::connect('web');
-          $wjValueId = Db::bind('property_value', array(
-            'key_id' => $wjKeyId, 'name' => $value['name']
-          ));
-        }
+        DbConnection::connect('web');
+        $wjValueId = Db::bind('property_value', array(
+          'key_id' => $wjKeyId, 'name' => $value['name']
+        ));
         $value['wj_id'] = $wjValueId;
       }
     }
@@ -96,7 +85,7 @@ class BuildCommand {
       Db::update('property_key', array(
         'mva_index_list',
         implode(',', $this->mvaIndexList).';'.$this->mvaIndexSearchStartPoint)
-      );
+      , 'id = ?', $wjKeyId);
     }
     $this->keyMapper = $keyMapper;
     $this->valueMapper = $valueMapper;
@@ -117,7 +106,7 @@ class BuildCommand {
     }
     for ($index = $this->mvaIndexSearchStartPoint; $index < 100; ++$index) {
       if (in_array($index, $this->mvaIndexList) === false) {
-        $mvaIndexList[] = $index;
+        $this->mvaIndexList[] = $index;
         $this->isMvaIndexListModified = true;
         $this->mvaIndexSearchStartPoint = $index + 1;
         return $index;
@@ -125,15 +114,18 @@ class BuildCommand {
     }
   }
 
-  private function syncProductByCategory($category) {
+  private function syncProductByCategory() {
     $logList = Db::getAll(
-      'SELECT * FROM log WHERE category_id = ?', $category['id']
+      'SELECT * FROM log WHERE category_id = ?', $this->category['id']
     );
     foreach ($logList as $log) {
       $class = 'Product'.ucfirst(strtolower($log['type'])).'Processor';
       $processor = new $class;
       $processor->execute(
-        $log['product_id'], $category, $this->keyMapper, $this->valueMapper
+        $log['product_id'],
+        $this->category,
+        $this->keyMapper,
+        $this->valueMapper
       );
       Db::delete('log', 'id = ?', $log['id']);
     }
