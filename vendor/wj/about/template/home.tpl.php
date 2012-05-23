b79f29ed0fa38ea34277a3425c1bd842e73ae941
azheng <azheng1984@126.com>
2012-05-23 21:52:39 +0800

    -

--- vendor/wj/about/template/home.tpl.php
+++ vendor/wj/about/template/home.tpl.php
@@ -1,6 +1,6 @@
 “一页说明”
 <ul>
-  <li>货比万家 + 开源项目（项目管理）</li>
+  <li>货比万家（shopping(buyer) + merchant + publisher） + 开源项目（项目管理）</li>
   <li>官方博客 &amp; 微博（link）</li>
   <li>联系我们(tel, address)</li>
 </ul>
