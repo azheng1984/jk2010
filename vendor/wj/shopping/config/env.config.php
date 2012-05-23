6a8f98390f6c36eb24bffc7f3bfda47c11cbc9dc
wangzheng <none@gmail.com>
2012-05-23 13:54:46 +0800

    -

--- vendor/wj/shopping/config/env.config.php
+++ vendor/wj/shopping/config/env.config.php
@@ -1,5 +1,3 @@
 <?php
-define('HYPERFRAMEWORK_PATH', '/home/wz/www/jk2010/vendor/hyperframework/');
-define('DB_PATH', '/home/wz/www/jk2010/vendor/db');
-define('DOMAIN_NAME', 'dev.huobiwanjia.com');
-define('IMAGE_DOMAIN_NAME', 'img.dev.huobiwanjia.com');
+define('HYPERFRAMEWORK_PATH', '/home/azheng/wj/vendor/hyperframework/');
+$GLOBALS['DOMAIN_PREFIX'] = 'dev.';
