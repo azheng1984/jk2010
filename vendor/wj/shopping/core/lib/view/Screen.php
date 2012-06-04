41c802564aef7e55e4de0ef3f978f20c6adb6807
azheng <azheng1984@126.com>
2012-06-04 10:52:15 +0800

    -

--- vendor/wj/shopping/core/lib/view/Screen.php
+++ vendor/wj/shopping/core/lib/view/Screen.php
@@ -84,8 +84,8 @@ abstract class Screen extends EtagView {
   }
 
   private function renderBanner() {
-    echo '<div id="banner"><span>100%公司经营</span>',
-      ' <span class="left">100%正规商店</span></div>';
+    echo '<div id="banner">',
+      '<span class="left">100% 公司经营 正规商店</span></div>';
   }
 
   private function renderLogo() {
