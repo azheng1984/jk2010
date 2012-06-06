b15c0ed87baff1ea489530ab850c9251840947d5
azheng <azheng1984@126.com>
2012-06-06 15:46:01 +0800

    -

---
+++ vendor/wj/api/merchant/callback/app/Command.php
@@ -0,0 +1,6 @@
+<?php
+class Command {
+  public function execute() {
+    echo 'Welcome!'.PHP_EOL;
+  }
+}
---
+++ vendor/wj/api/publisher/open/app/error/internal_server_error/InternalServerErrorScreen.php
@@ -0,0 +1,6 @@
+<?php
+class InternalServerErrorScreen {
+  public function render() {
+    echo '500 Internal Server Error';
+  }
+}
