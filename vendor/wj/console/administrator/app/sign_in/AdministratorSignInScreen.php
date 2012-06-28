21c6758b4044f64890a5fdcce24da75b612a3687
wangzheng <none@gmail.com>
2012-06-28 16:48:20 +0800

    -

---
+++ vendor/wj/console/administrator/app/sign_in/AdministratorSignInScreen.php
@@ -0,0 +1,3 @@
+<?php
+class AdministratorSignInScreen extends ConsoleSignInScreen {
+}
--- vendor/wj/console/administrator/app/sign_up/SignUpScreen.php
+++
@@ -1,8 +0,0 @@
-<?php
-class SignUpScreen {
-  public function render() {
-    header('Content-Type:text/html; charset=UTF-8');
-    echo '<form method="POST"><div>用户名<input type="text"/></div>';
-    echo '<div>密码<input type="password"/></div><div><input type="submit"/></div></form>';
-  }
-}
