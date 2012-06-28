ddb2526335792bb3ffb32d9f88c3f611d5bde36f
azheng <azheng1984@126.com>
2012-06-28 16:53:58 +0800

    -

--- vendor/wj/console/administrator/app/log_out/AdministratorLogOutScreen.php
+++ vendor/wj/console/administrator/app/log_out/AdministratorLogOutScreen.php
@@ -2,13 +2,12 @@
 class AdministratorLogOutScreen extends Screen {
   public function __construct() {
     setcookie('user_session_id', '');
+    $this->stop();
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: /sign_in');
   }
 
-  protected function renderHtmlHeadContent() {
-    // TODO Auto-generated method stub
-  }
+  protected function renderHtmlHeadContent() {}
 
-  protected function renderHtmlBodyContent() {
-    echo 'log out <a href="/sign_in">sign in</a>';
-  }
+  protected function renderHtmlBodyContent() {}
 }
