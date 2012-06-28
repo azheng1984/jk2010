45197dbb1a41fd3b04f19c7bf273da93e769f1cb
wangzheng <none@gmail.com>
2012-06-28 18:45:25 +0800

    -

--- vendor/wj/console/common/ConsoleScreen.php
+++ vendor/wj/console/common/ConsoleScreen.php
@@ -14,6 +14,17 @@ class ConsoleScreen extends Screen {
   }
 
   protected function renderHtmlBodyContent() {
-    echo '<a href="/log_out">log out</a>';
+    echo '<h1>管理员</h1><div id="toolbar">';
+    echo '<span>yywz@126.com | </span>';
+    echo '<a href="/sign_out">退出</a>';
+    echo '</div>';
+    echo '<ul>',
+      '<li><b style="background:red;color:#fff;padding:2px 5px">首页</b></li>',
+      '<li><a href="/publisher">广告发布商</a></li>',
+      '<li><a href="/merchant">商家<a></li>',
+      '<li><a href="/administrator">管理员</a>（只有 root 用户才有）</li>',
+      '<li><a href="/notification">通知</a></li>',
+      '<li><a href="/account">帐号设置</a></li>';
+    echo '</ul>';
   }
 }
