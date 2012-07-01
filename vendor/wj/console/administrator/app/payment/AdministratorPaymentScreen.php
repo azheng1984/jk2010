8a810d9b9b47b865ecac3b5ccfc77d3bab38798d
wangzheng <none@gmail.com>
2012-07-01 21:13:12 +0800

    -

--- vendor/wj/console/administrator/app/payment/AdministratorPaymentScreen.php
+++ vendor/wj/console/administrator/app/payment/AdministratorPaymentScreen.php
@@ -1,6 +1,6 @@
 <?php
 class AdministratorPaymentScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />结算【收款】【付款】';
+    echo '<hr />【收款】<br /><input type="submit" value="已收款" /><br />【付款】<br /><input type="submit" value="已付款" />';
   }
 }
--- vendor/wj/console/merchant/app/payment/MerchantPaymentScreen.php
+++ vendor/wj/console/merchant/app/payment/MerchantPaymentScreen.php
@@ -4,8 +4,8 @@ class MerchantPaymentScreen extends MerchantScreen {
     echo '<hr /><ul>';
     echo '<li>【未付款】</li>';
     echo 'level 1 <li></li>';
-    echo 'level 2 <li></li>';
-    echo '<li>【正在付款】</li>';
+    echo 'level 2 <li><input type="submit" value="付款" /></li>';
+    echo '<li>【正在付款】<br />对帐单<br /><input type="submit" value="取消" /></li>';
     echo '<li>【已付款】</li>';
     echo '</ul>';
   }
