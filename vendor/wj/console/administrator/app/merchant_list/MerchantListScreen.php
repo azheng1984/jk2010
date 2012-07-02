8e9d5153c07b260017d61ad2181a6684522888e8
wangzheng <none@gmail.com>
2012-07-02 22:02:18 +0800

    -

--- vendor/wj/console/administrator/app/merchant_list/MerchantListScreen.php
+++ vendor/wj/console/administrator/app/merchant_list/MerchantListScreen.php
@@ -1,7 +1,7 @@
 <?php
 class MerchantListScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />[有效 | 无效 | 新注册]<div><input /><input type="submit" value="搜索" /></div>';
+    echo '<hr />[活跃 | 等待激活 | 不活跃 | 停用]<div><input /><input type="submit" value="搜索" /></div>';
     echo '图标 名称 今日导出流量（IP） 活跃订单佣金 未支付佣金';
     echo '（通过黄色背景表示已经帐号被暂停）';
   }
