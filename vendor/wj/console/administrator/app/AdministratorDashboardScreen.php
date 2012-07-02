4fd8e9f0d42bc14a39247e6dd8889b0f017acfec
azheng <azheng1984@126.com>
2012-07-02 17:48:48 +0800

    -

--- vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
+++ vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
@@ -1,7 +1,7 @@
 <?php
 class AdministratorDashboardScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />（自然流量 & 广告发布商 & 总计）今日导入流量 今日导出流量 活跃订单佣金 未支付佣金';
+    echo '<hr />今日导入流量 今日导出流量 活跃订单佣金 未支付佣金';
     echo '<br />广告发布商总数 商家总数 商品总数 促销活动总数';
   }
 }
--- vendor/wj/console/administrator/app/publisher_list/PublisherListScreen.php
+++ vendor/wj/console/administrator/app/publisher_list/PublisherListScreen.php
@@ -1,8 +1,8 @@
 <?php
 class PublisherListScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr /><div><input /><input type="submit" value="搜索" /></div>';
-    echo '图标 名称 今日导入流量（IP） 活跃订单佣金 未支付佣金';
+    echo '<hr />[有效 | 无效 | 新注册]<div><input /><input type="submit" value="搜索" /></div>';
+    echo 'level 1: 图标 名称 今日导入流量（IP） 活跃订单佣金 未支付佣金';
     echo '（通过黄色背景表示已经帐号被暂停）';
   }
 }
