ed6832612fc9879d634bdc9ea85367edf32cffc8
wangzheng <none@gmail.com>
2012-07-02 23:26:12 +0800

    -

--- vendor/wj/console/administrator/app/administrator_list/AdministratorListScreen.php
+++ vendor/wj/console/administrator/app/administrator_list/AdministratorListScreen.php
@@ -1,6 +1,6 @@
 <?php
 class AdministratorListScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />[活跃 | 等待激活 | 不活跃 | 停用]（只有 root 可以查看 无效/新注册 管理员）<br />姓名 头像 邮箱 电话 IM（类似产品列表，在单元格内放所有信息）';
+    echo '<hr />[活跃 | 不活跃 | 未激活 | 已停用]（只有 root 可以查看 未激活/已停用）<br />姓名 头像 邮箱 电话 IM（类似产品列表，在单元格内放所有信息）';
   }
 }
--- vendor/wj/console/administrator/app/publisher_list/PublisherListScreen.php
+++ vendor/wj/console/administrator/app/publisher_list/PublisherListScreen.php
@@ -1,7 +1,7 @@
 <?php
 class PublisherListScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />[活跃 | 等待激活 | 不活跃 | 停用]<div><input /><input type="submit" value="搜索" /></div>';
+    echo '<hr />[活跃 | 不活跃 | 未激活 | 已停用]<div><input /><input type="submit" value="搜索" /></div>';
     echo 'level 1: 图标 名称 今日导入流量（IP） 活跃订单佣金 未支付佣金';
     echo '（通过黄色背景表示已经帐号被暂停）';
   }
