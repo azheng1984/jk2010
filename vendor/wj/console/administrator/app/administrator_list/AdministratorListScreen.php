7165e2c7730f4342dc28402acd068ca285a505bc
azheng <azheng1984@126.com>
2012-07-02 22:02:18 +0800

    -

--- vendor/wj/console/administrator/app/administrator_list/AdministratorListScreen.php
+++ vendor/wj/console/administrator/app/administrator_list/AdministratorListScreen.php
@@ -1,6 +1,6 @@
 <?php
 class AdministratorListScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />[有效 | 无效 | 新注册]（只有 root 可以查看 无效/新注册 管理员）<br />姓名 头像 邮箱 电话 IM（类似产品列表，在单元格内放所有信息）';
+    echo '<hr />[活跃 | 等待激活 | 不活跃 | 停用]（只有 root 可以查看 无效/新注册 管理员）<br />姓名 头像 邮箱 电话 IM（类似产品列表，在单元格内放所有信息）';
   }
 }
