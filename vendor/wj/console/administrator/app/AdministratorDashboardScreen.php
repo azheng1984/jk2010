812cb3e45ed123a696d082c827e3b1cfc2ef8b19
azheng <azheng1984@126.com>
2012-07-02 23:26:12 +0800

    -

--- vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
+++ vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
@@ -1,7 +1,9 @@
 <?php
 class AdministratorDashboardScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr />今日导入流量 今日导出流量 活跃订单佣金 未支付佣金';
-    echo '<br />广告发布商总数 商家总数 商品总数 促销活动总数';
+    echo '<hr />[今天]<br />导入流量 导出流量 活跃订单总量 活跃订单总金额 活跃订单佣金 收入<br />';
+    echo '[总计]<br />活跃订单总量 活跃订单总金额 活跃订单总佣金<br />';
+    echo '未支付佣金 正在支付佣金（只在存在的时候显示）';
+    echo '<br />活跃广告发布商总数 活跃商家总数 商品总数 促销活动总数';
   }
 }
