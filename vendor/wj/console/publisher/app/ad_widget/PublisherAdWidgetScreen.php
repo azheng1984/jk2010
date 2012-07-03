8b8c5b09835146ba6ad6596d79d48689e04ee5f9
wangzheng <none@gmail.com>
2012-07-04 00:59:51 +0800

    -

--- vendor/wj/console/publisher/app/ad_widget/PublisherAdWidgetScreen.php
+++ vendor/wj/console/publisher/app/ad_widget/PublisherAdWidgetScreen.php
@@ -1,6 +1,6 @@
 <?php
 class PublisherAdWidgetScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<hr />商家列表（包括首页），商品搜索（交互式文档）';
+    echo '<hr />商家列表（包括首页），商品搜索 - 交互式文档（url 约定结构），图标/图片 资源库';
   }
 }
