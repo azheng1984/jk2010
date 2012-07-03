3d1337dcce7bc1ed58097516f933e9705ee7894a
wangzheng <none@gmail.com>
2012-07-03 19:03:44 +0800

    -

--- vendor/wj/console/publisher/app/ad_widget/PublisherAdWidgetScreen.php
+++ vendor/wj/console/publisher/app/ad_widget/PublisherAdWidgetScreen.php
@@ -1,6 +1,6 @@
 <?php
 class PublisherAdWidgetScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<hr />首页（各种尺寸和格式图标，包括 favicon），商品集合链接（目标 url 格式，publisher 可以自定义搜索框），单一商品链接（同款）（交互式文档）';
+    echo '<hr />商家列表（包括首页），商品搜索（交互式文档）';
   }
 }
