9ac53de7940473521f7abc13861006a08fbe549a
azheng <azheng1984@126.com>
2012-07-06 23:22:04 +0800

    -

--- vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
+++ vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
@@ -3,7 +3,7 @@ class AdministratorReportScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
     echo '<hr /><ul>';
     echo '<li>单位：日/月/年 | 下单日期区间（日/月/年 日历选择） | 视角: 商家/广告发布商</li>';
-    echo '<li>level 1(summary) 时间 | 单位订单总数 | 单位总订单金额 | 单位总佣金</li>';
+    echo '<li>level 1(summary) 时间 | [完成订单 | 活跃订单]总数 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
     echo '<li>level 1.5 (merchant) 商家 | 流量 | [完成订单 | 活跃订单]数量 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
     echo '<li>level 1.5 level 2 (publisher) 广告发布商 | 流量 | [完成订单 | 活跃订单]总数 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
     echo '<li>level 2(order) 订单编号 | 订单支付金额 | 状态 | 佣金</li>';
--- vendor/wj/console/publisher/app/PublisherDashboardScreen.php
+++ vendor/wj/console/publisher/app/PublisherDashboardScreen.php
@@ -2,6 +2,6 @@
 class PublisherDashboardScreen extends PublisherScreen {
   protected function renderPublisherContent() {
     echo '<hr />【今日】<br />流量 | 新增活跃订单佣金 | 新增收入<br />';
-    echo '【总计】<br />活跃订单佣金 | 未支付收入 | 正在付款（只在存在的时候显示）<br />';
+    echo '<br />【总计】<br />活跃订单佣金 | 未支付收入 | 正在付款（只在存在的时候显示）<br />';
   }
 }
