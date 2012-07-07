fc56d8bd11cf849a6a24d03ebdcf0b7c3c21c082
wangzheng <none@gmail.com>
2012-07-07 23:13:21 +0800

    -

--- vendor/wj/console/publisher/app/account/PublisherAccountScreen.php
+++ vendor/wj/console/publisher/app/account/PublisherAccountScreen.php
@@ -1,6 +1,6 @@
 <?php
 class PublisherAccountScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<hr />联系人（姓名 邮箱 电话） 密码 收款帐户（开户行，户名，帐号） 网站（名称，图标，域名）';
+    echo '联系人（姓名 邮箱 电话） 密码 收款帐户（开户行，户名，帐号） 网站（名称，图标，域名）';
   }
 }
--- vendor/wj/console/publisher/lib/PublisherScreen.php
+++ vendor/wj/console/publisher/lib/PublisherScreen.php
@@ -1,38 +1,38 @@
 <?php
 abstract class PublisherScreen extends ConsoleScreen {
   protected function renderConsoleContent() {
-    echo '<a href="/"><h1>货比万家 - 广告发布商</h1></a><div id="toolbar">';
+    echo '<a href="/"><h1> 广告发布商</h1></a><div id="toolbar">';
     echo '<span>root | </span>';
     echo '<a href="/sign_out">退出</a>';
     echo '</div>';
     echo '<ul id="navigator">';
     if ($_SERVER['REQUEST_URI'] === '/') {
-      echo '<li><span>首页</span></li>';
+      echo '<li class="selected"><span>首页</span></li>';
     } else {
       echo '<li><a href="/">首页</a></li>';
     }
     if ($_SERVER['REQUEST_URI'] === '/report') {
-      echo '<li><span>效果报表</span></li>';
+      echo '<li class="selected"><span>效果报表</span></li>';
     } else {
       echo '<li><a href="/report">效果报告</a></li>';
     }
     if ($_SERVER['REQUEST_URI'] === '/payment') {
-      echo '<li><span>结算</span></li>';
+      echo '<li class="selected"><span>结算</span></li>';
     } else {
       echo '<li><a href="/payment">结算</a></li>';
     }
     if ($_SERVER['REQUEST_URI'] === '/io') {
-      echo '<li><span>数据接口</span></li>';
+      echo '<li class="selected"><span>数据接口</span></li>';
     } else {
       echo '<li><a href="/io">数据接口</a></li>';
     }
     if ($_SERVER['REQUEST_URI'] === '/ad_widget') {
-      echo '<li><span>广告控件</span></li>';
+      echo '<li class="selected"><span>广告控件</span></li>';
     } else {
       echo '<li><a href="/ad_widget">广告控件</a></li>';
     }
     if ($_SERVER['REQUEST_URI'] === '/account') {
-      echo '<li><span>帐户设置</span></li>';
+      echo '<li class="selected"><span>帐户设置</span></li>';
     } else {
       echo '<li><a href="/account">帐户设置</a></li>';
     }
