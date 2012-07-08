c85f7e2d74189f0656f31ddbb88449e6a6cdaa02
wangzheng <none@gmail.com>
2012-07-08 21:06:46 +0800

    -

--- vendor/wj/console/publisher/app/PublisherDashboardScreen.php
+++ vendor/wj/console/publisher/app/PublisherDashboardScreen.php
@@ -1,7 +1,9 @@
 <?php
 class PublisherDashboardScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box"><div class="title">今日</div>流量 | 新增活跃订单佣金 | 新增收入</div>';
-    echo '<div class="box"><div class="title">总计</div>活跃订单佣金 | 未支付收入 | 正在付款（只在存在的时候显示）</div>';
+    echo '<div class="box"><div class="title">今日</div><div class="box-content"><div class="block">流量<br /><span class="big">2322233</span></div><div class="block">新增活跃订单佣金<br />￥<span class="big">23232.23</span></div><div class="block">新增收入<br />￥<span class="big">23232.23</span></div></div>';
+    echo '</div>';
+    echo '<div class="box"><div class="title">总计</div><div class="box-content"><div class="block">活跃订单佣金<br />￥<span class="big">23232.23</span></div><div class="block">未支付收入<br />￥<span class="big">23232.23</span></div><div class="block">正在付款<br />￥<span class="big">23232.23</span></div></div>';
+    echo '</div>';
   }
 }
