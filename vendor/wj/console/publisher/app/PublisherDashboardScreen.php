d22909ba56148fcac94a1cf047a948352bfbf0b1
azheng <azheng1984@126.com>
2012-07-08 15:16:45 +0800

    -

--- vendor/wj/console/publisher/app/PublisherDashboardScreen.php
+++ vendor/wj/console/publisher/app/PublisherDashboardScreen.php
@@ -1,7 +1,7 @@
 <?php
 class PublisherDashboardScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<br />【今日】<br />流量 | 新增活跃订单佣金 | 新增收入<br />';
-    echo '<br />【总计】<br />活跃订单佣金 | 未支付收入 | 正在付款（只在存在的时候显示）<br />';
+    echo '<div class="box"><div class="title">今日</div>流量 | 新增活跃订单佣金 | 新增收入</div>';
+    echo '<div class="box"><div class="title">总计</div>活跃订单佣金 | 未支付收入 | 正在付款（只在存在的时候显示）</div>';
   }
 }
