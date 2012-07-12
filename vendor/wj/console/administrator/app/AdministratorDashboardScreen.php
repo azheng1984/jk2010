d2d58188eca4cd5134fa9601d1fb0c085e7a1462
wangzheng <none@gmail.com>
2012-07-12 21:38:46 +0800

    -

--- vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
+++ vendor/wj/console/administrator/app/AdministratorDashboardScreen.php
@@ -1,12 +1,12 @@
 <?php
 class AdministratorDashboardScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<br />【今日】<br />导入流量 | 导出流量 | 订单数量 | 订单支付金额 | 活跃订单佣金 | 完成订单佣金<br />';
+    echo '<br />【今日】<br />导入流量 | 导出流量 | 订单数量 | 订单支付金额 | 活跃订单佣金 | 收款<br />';
     echo '<br />【总计】<br />活跃订单佣金 未付款 未收款<br />';
     echo '活跃广告发布商 活跃商家 商品 促销活动';
   }
 
   protected function getTitle() {
-    return '商家';
+    return '管理员';
   }
 }
