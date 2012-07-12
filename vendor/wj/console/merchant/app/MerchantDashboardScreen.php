a38300363a948b893f14b19d794cec948d45f679
wangzheng <none@gmail.com>
2012-07-12 14:58:54 +0800

    -

--- vendor/wj/console/merchant/app/MerchantDashboardScreen.php
+++ vendor/wj/console/merchant/app/MerchantDashboardScreen.php
@@ -1,8 +1,7 @@
 <?php
 class MerchantDashboardScreen extends MerchantScreen {
   protected function renderMerchantContent() {
-    echo '<hr />【今日】<br />流量<br /> level 1 新增订单 | 完成订单<br />';
-    echo 'level 2 数量 金额 佣金<br />';
+    echo '<br />【今日】<br />流量 订单数量 订单支付金额 订单佣金<br />';
     echo '<br />【总计】<br />';
     echo '活跃订单佣金 未支付佣金 正在支付佣金（只在存在的时候显示）<br />商品总数 促销活动总数';
   }
