9f8f58769f0f559c535d768b79a2b02133753432
azheng <azheng1984@126.com>
2012-07-12 13:57:03 +0800

    -

--- vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
+++ vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
@@ -1,13 +1,9 @@
 <?php
 class AdministratorReportScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
-    echo '<hr /><ul>';
-    echo '<li>单位：日/月/年 | 下单日期区间（日/月/年 日历选择） | 视角: 商家/广告发布商 | 商家/广告发布商: []（选定后包括 商家/广告发布商 的所有功能）</li>';
-    echo '<li>level 1(summary) 时间 | [完成订单 | 活跃订单]总数 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
-    echo '<li>level 1.5 (merchant) 商家 | 流量 | [完成订单 | 活跃订单]数量 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
-    echo '<li>level 1.5 level 2 (publisher) 广告发布商 | 流量 | [完成订单 | 活跃订单]总数 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
-    echo '<li>level 2(order) 订单编号 | 订单支付金额 | 状态 | 佣金</li>';
-    echo '<li>level 3(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金</li>';
+    echo '<ul>';
+    echo '<li>分组 时间：日/月/年 | 时间区间（日/月/年 日历选择）</li>';
+    echo '<li>时间 | 导入流量 | 导出流量 | 订单数量 | 订单支付金额 | 活跃订单佣金 | 导入 CPC | 导出 CPC | 完成订单佣金</li>';
     echo '</ul>';
   }
 
--- vendor/wj/console/merchant/app/report/MerchantReportScreen.php
+++ vendor/wj/console/merchant/app/report/MerchantReportScreen.php
@@ -2,10 +2,8 @@
 class MerchantReportScreen extends MerchantScreen {
   protected function renderMerchantContent() {
     echo '<hr /><ul>';
-    echo '<li>单位：日/月/年 | 下单日期区间（日/月/年 日历选择） | 流量导出类型（首页/促销/商品）</li>';
-    echo '<li>level 1(summary) 时间 | 流量 | [完成订单 | 活跃订单]总数 | [完成订单 | 活跃订单]金额 | [完成订单 | 活跃订单]佣金</li>';
-    echo '<li>level 2(order) 订单编号 | 订单支付金额 | 状态 | 佣金</li>';
-    echo '<li>level 3(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金</li>';
+    echo '<li>时间区间（日/月/年 日历选择） <br />分组: [时间：日/月/年] <br /> </li>';
+    echo '<li>时间 | 流量 | 订单数量 | 订单支付金额 | 活跃订单佣金 | CPC | 已结算佣金</li>';
     echo '</ul>';
   }
 
