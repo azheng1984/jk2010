cf1b6c4b634a57d1be7669c3e719fc8aff8fb45a
azheng <azheng1984@126.com>
2012-07-13 16:59:52 +0800

    -

--- vendor/wj/console/publisher/app/active_order/PublisherActiveOrderScreen.php
+++ vendor/wj/console/publisher/app/active_order/PublisherActiveOrderScreen.php
@@ -1,9 +1,9 @@
 <?php
 class PublisherActiveOrderScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box">level 1(order) 网站 | 自定义渠道 | 商家 | 订单编号 | 订单支付金额 | 佣金<br />
-level 2(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金</div>';
-    echo '<div class="box">总计：订单数量 订单支付金额 活跃订单佣金</div>';
+    echo '<div class="box">level 1(order) 网站 | 渠道 | 商家 | 订单编号 | 订单支付金额 | 佣金<br />
+level 2(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金<br />';
+    echo '总计：数量 支付金额 佣金</div>';
   }
 
   protected function getTitle() {
