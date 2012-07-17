5ae865752a69d5f8e5b8d0f4904d16f312b6e21e
azheng <azheng1984@126.com>
2012-07-17 17:48:57 +0800

    -

--- vendor/wj/console/publisher/app/PublisherDashboardScreen.php
+++ vendor/wj/console/publisher/app/PublisherDashboardScreen.php
@@ -4,7 +4,7 @@ class PublisherDashboardScreen extends PublisherScreen {
     echo '<div class="box"><div class="title">今日</div><div class="box-content"><div class="block first_block">流量<br /><span class="big">2322233</span></div><div class="block">订单数量<br /><span class="big">233322</span></div><div class="block">订单支付金额<br />¥<span class="big">23232.23</span></div>';
     echo '<div class="block">活跃订单佣金<br />¥<span class="big">23232.23</span></div></div>';
     echo '</div>';
-    echo '<div class="box"><div class="title">总计</div><div class="box-content"><div class="block first_block">活跃订单佣金<br />¥<span class="big">23232.23</span></div><div class="block">未支付收入<br />¥<span class="big">23232.23</span></div><div class="block">正在付款<br />¥<span class="big">23232.23</span></div></div>';
+    echo '<div class="box"><div class="title">总计</div><div class="box-content"><div class="block first_block">未支付收入<br />¥<span class="big">23232.23</span></div><div class="block">正在付款<br />¥<span class="big">23232.23</span></div><div class="block">活跃订单佣金<br />¥<span class="big">23232.23</span></div></div>';
     echo '</div>';
   }
 
