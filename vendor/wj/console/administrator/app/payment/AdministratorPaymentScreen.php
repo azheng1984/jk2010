82948b4d606f3356082204a764d76fd9e4a4ee40
wangzheng <none@gmail.com>
2012-07-20 15:33:52 +0800

    -

--- vendor/wj/console/administrator/app/payment/AdministratorPaymentScreen.php
+++ vendor/wj/console/administrator/app/payment/AdministratorPaymentScreen.php
@@ -4,12 +4,14 @@ class AdministratorPaymentScreen extends AdministratorScreen {
     echo '（只有 “财务” 才可以确认收款和付款，30天过期时间，过期需要重新申请 对帐/付款）';
     echo '<br />【收款】';
     echo '<br />level 1(merchant) 商家 佣金 <input type="submit" value="已收款" /><br />';
+    echo 'level 1.1 广告发布商 佣金<br />';
     echo 'level 2(order) 订单编号 | 订单支付金额 | 佣金<br />';
     echo 'level 3(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金<br />';
     echo '历史记录 <input type="submit" value="撤销" />';
     echo '<br /><br />【付款】';
     echo '<br />level 1(publisher) 广告发布商 佣金 <input type="submit" value="已付款" /><br />';
-    echo 'level 2(order) 订单编号 | 订单支付金额 | 佣金<br />';
+    echo 'level 1.1 商家 佣金<br />';
+    echo 'level 2(order) 渠道 | 跟踪编号 | 订单编号 | 支付金额 | 佣金<br />';
     echo 'level 3(order detail) 商品编号 | 商品名称 | 单价 | 数量 | 总价 | 佣金<br />';
     echo '历史记录 <input type="submit" value="撤销" />';
     
