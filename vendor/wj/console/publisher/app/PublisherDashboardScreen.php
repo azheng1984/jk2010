267eca8545b5443badd71d8dc301e6ea5f04c7d6
wangzheng <none@gmail.com>
2012-07-25 16:29:29 +0800

    -

--- vendor/wj/console/publisher/app/PublisherDashboardScreen.php
+++ vendor/wj/console/publisher/app/PublisherDashboardScreen.php
@@ -1,10 +1,49 @@
 <?php
 class PublisherDashboardScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box"><div class="title">今天</div><div class="box-content"><div class="block first_block">流量<br /><span class="big">2322233</span></div><div class="block">订单数量<br /><span class="big">233322</span></div><div class="block">订单支付金额<br />¥<span class="big">23232.23</span></div>';
-    echo '<div class="block">活跃订单佣金<br />¥<span class="big">23232.23</span></div></div>';
+    $today = Db::getRow(
+      'SELECT traffic, order_amount, order_payment, active_order_commission'
+        .' FROM performance_report'
+        .' WHERE user_id = ? AND `date` = ?', 1, date('Y-m-d')
+    );
+    if ($today === false) {
+      $today = array(
+        'traffic'=> 0,
+        'order_amount' => 0,
+        'order_payment' => 0,
+        'active_order_commission' => 0
+      );
+    }
+    $activeOrderCommission = Db::getColumn(
+      'SELECT commission FROM active_order_report WHERE user_id = ?', 1
+    );
+    if ($activeOrderCommission === false) {
+      $activeOrderCommission = 0;
+    }
+    $unpaidCommission = Db::getColumn(
+      'SELECT commission FROM payment'
+        ." WHERE user_id = ? AND status = 'UNPAID'", 1
+    );
+    if ($unpaidCommission === false) {
+      $unpaidCommission = 0;
+    }
+    $processingCommission = Db::getColumn(
+      'SELECT commission FROM payment'
+        ." WHERE user_id = ? AND status = 'PROCESSING'", 1
+    );
+    if ($processingCommission === false) {
+      $processingCommission = 0;
+    }
+    echo '<div class="box"><div class="title">今天</div><div class="box-content">',
+      '<div class="block first_block">流量<br /><span class="big">', $today['traffic'], '</span></div>',
+      '<div class="block">订单数量<br /><span class="big">', $today['order_amount'], '</span></div>',
+      '<div class="block">订单支付金额<br />¥<span class="big">', number_format($today['order_payment'], 2), '</span></div>';
+    echo '<div class="block">活跃订单佣金<br />¥<span class="big">', number_format($today['active_order_commission'], 2), '</span></div></div>';
     echo '</div>';
-    echo '<div class="box"><div class="title">总计</div><div class="box-content"><div class="block first_block">未支付收入<br />¥<span class="big">23232.23</span></div><div class="block">正在付款<br />¥<span class="big">23232.23</span></div><div class="block">活跃订单佣金<br />¥<span class="big">23232.23</span></div></div>';
+    echo '<div class="box"><div class="title">总计</div><div class="box-content">',
+      '<div class="block first_block">未支付收入<br />¥<span class="big">', number_format($unpaidCommission, 2), '</span></div>',
+      '<div class="block">正在付款<br />¥<span class="big">', number_format($processingCommission, 2), '</span></div>',
+      '<div class="block">活跃订单佣金<br />¥<span class="big">', number_format($activeOrderCommission, 2), '</span></div></div>';
     echo '</div>';
   }
 
--- vendor/wj/console/publisher/app/report/PublisherReportScreen.php
+++ vendor/wj/console/publisher/app/report/PublisherReportScreen.php
@@ -1,9 +1,30 @@
 <?php
 class PublisherReportScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box">';
-    echo '<div><br />过滤：时间区间: <input type="text" />-<input type="text" /> | 渠道</div>';
-    echo '<div>分组: [时间: <b>日</b> <a href="javascript:void(0)">月</a> <a href="javascript:void(0)">年</a>] | 渠道</div>';
+    $channelList = Db::getAll('SELECT id, name FROM channel WHERE user_id = ?', 1);
+    echo '<div class="box">',
+      '<div><br />过滤：',
+      '<form method="GET">时间区间: ';
+    if (isset($_GET['group_by'])) {
+      echo '<input name="group_by" type="hidden" value="', $_GET['group_by'], '" />';
+    }
+    if (isset($_GET['channel_id'])) {
+      echo '<input name="channel_id" type="hidden" value="', $_GET['channel_id'], '" />';
+    }
+    echo '<input name="start_date" type="text" value="', date('Y-n-1'), '" />',
+      '-<input name="end_date" type="text" value="', date('Y-n-j'), '" />',
+      '</div><input type="submit" value="确定" /></form><div>渠道</div>';
+    echo '<ul>';
+    foreach ($channelList as $channel) {
+      if (isset($_GET['channel_id']) && $_GET['channel_id'] === $channel['id']) {
+        echo '<b>', $channel['name'], '</b> <a href="?">X</a>';
+        continue;
+      }
+      echo '<li><a href="?channel_id=', $channel['id'], '">',
+        $channel['name'], '</a></li>';
+    }
+    echo '</ul>';
+    echo '<div>分组: [时间: <a href="?group_by=day">日</a> <a href="?group_by=month">月</a> <a href="?group_by=year">年</a>] | <a href="?group_by=channel">渠道</a></div>';
     echo '<br /><table><tr><th>时间/渠道</th><th>流量</th><th>订单数量</th><th>订单支付金额</th><th>活跃订单佣金</th><th>CPC</th><th>完成订单佣金</th></tr><tr><td colspan="7">空</td></tr></table>';
     echo '</div>';
   }
