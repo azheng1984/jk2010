fee2ead2f1c9f5e3eb5fac76ece7e16874027e7c
azheng <azheng1984@126.com>
2012-07-31 21:48:41 +0800

    -

--- vendor/wj/console/publisher/app/account/PublisherAccountScreen.php
+++ vendor/wj/console/publisher/app/account/PublisherAccountScreen.php
@@ -3,7 +3,7 @@ class PublisherAccountScreen extends PublisherScreen {
   protected function renderPublisherContent() {
     echo '<div class="box"><div class="title">联系人</div>（姓名 邮箱 电话（可以是多个，包括手机））</div>';
     echo '<div class="box"><div class="title">密码</div><input /></div>';
-    echo '<div class="box"><div class="title">收款帐户</div>（开户行，户名，帐号）</div>';
+    echo '<div class="box"><div class="title">收款帐户</div>（开户行，户名，帐号）,（自动付款，代扣税费（历史配置会保存，类似订单收款地址））</div>';
     echo '<div class="box"><div class="title">网站</div>（名称，图标，域名，域名 <=> 渠道 映射（类似 adsense 网址渠道））</div>';
   }
 
--- vendor/wj/console/publisher/app/payment/PublisherPaymentScreen.php
+++ vendor/wj/console/publisher/app/payment/PublisherPaymentScreen.php
@@ -7,12 +7,27 @@ class PublisherPaymentScreen extends PublisherScreen {
     }
     echo '<div class="box"><div class="title">未付款</div>账户余额: ￥';
     echo $row['commission'];
-    echo ' <input type="submit" value="付款" /> 设置（自动付款，代扣税费（在付款时的选项会被自动保存，避免重复操作））';
+    echo ' <input type="submit" value="付款" />';
     if ($row['commission'] !== '0') {
+      $detailList = Db::getAll('SELECT * FROM payment_detail WHERE user_id = ?');//console
+      echo '<ul>';
+      foreach ($detailList as $detail) {
+        $merchantName = Db::getColumn('SELECT name FROM profile WHERE user_id = ?', $detail['merchant_user_id']);
+        echo '<li>', $merchantName, ' | ', $detail['commission'], '</li>';
+        $orderList = Db::getAll('SELECT * FROM complete_order WHERE payment_detail_id = ?');
+        foreach ($orderList as $order) {
+          echo $order['time'], ' - ',
+            $order['publisher_channel_id'], ' - ',
+            $order['merchant_order_id'], ' - ',
+            $order['payment_amount'], ' - ',
+            $order['commission'];
+        }
+      }
+      echo '</ul>';
     }
     echo '<br />level 1 商家 佣金</div>';
     echo '<div class="box"><div class="title">正在付款</div>付款总额: ￥xx <br />level 1 商家 佣金<br />
-level 2.1(order) 时间 | 渠道 | 跟踪编号 | 商家 | 订单编号 | 支付金额 | 佣金<br />
+level 2.1(order) 时间 | 渠道 | 跟踪编号 | 订单编号 | 支付金额 | 佣金<br />
 level 2.2(order detail) 商品编号 | 名称 | 单价 | 数量 | 总价 | 佣金
 <br />总计：数量 支付金额 佣金</div>';
     echo '<div class="box"><div class="title">已付款</div>';
