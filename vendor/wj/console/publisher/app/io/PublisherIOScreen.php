42229aa48bdf80b45397ac7850654a6c594887d5
azheng <azheng1984@126.com>
2012-08-12 16:20:20 +0800

    -

--- vendor/wj/console/publisher/app/io/PublisherIOScreen.php
+++ vendor/wj/console/publisher/app/io/PublisherIOScreen.php
@@ -1,10 +1,9 @@
 <?php
 class PublisherIOScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box"><div class="title">状态</div>（最后一次更新）</div>';
-    echo '<div class="box">API: 效果报告，结算（地址 & 交互式文档）</div>';
-    echo '<div class="box">回调: 活跃订单更新（文档）</div>';
     echo '<div class="box"><div class="title">网站</div>（名称，图标，域名，域名 <=> 渠道 映射（类似 adsense 网址渠道））</div>';
+    echo '<div class="box">API: 效果报告，结算（地址 & 交互式文档 & 当前状态）</div>';
+    echo '<div class="box">回调: 活跃订单更新（文档）</div>';
   }
 
   protected function getTitle() {
