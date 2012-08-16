fe1c0405dac2fbf4794ec316e362397779412f18
wangzheng <none@gmail.com>
2012-08-16 16:40:34 +0800

    -

--- vendor/wj/console/publisher/app/advertisement/PublisherAdvertisementScreen.php
+++ vendor/wj/console/publisher/app/advertisement/PublisherAdvertisementScreen.php
@@ -1,7 +1,7 @@
 <?php
 class PublisherAdvertisementScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box"><div class="title">购物助手</div>站内/浏览器插件</div>';
+    echo '<div class="box"><div class="title">购物助手</div>应用整合/浏览器插件</div>';
     echo '<div class="box"><div class="title">自定义链接</div>商标和内容显示政策/url 约定结构</div>';
   }
 
