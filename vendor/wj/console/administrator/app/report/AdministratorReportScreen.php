15af8a77a5a2be36e0f35b179542e395544fd047
wangzheng <none@gmail.com>
2012-08-27 14:08:13 +0800

    -

--- vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
+++ vendor/wj/console/administrator/app/report/AdministratorReportScreen.php
@@ -3,7 +3,7 @@ class AdministratorReportScreen extends AdministratorScreen {
   protected function renderAdministratorContent() {
     echo '<div class="box">';
     echo '<div>分组 时间：日/月/年 | 时间区间（日/月/年 日历选择）</div>';
-    echo '<div>时间 | 导入流量 | 导出流量 | 订单数量 | 订单支付金额 | 活跃订单佣金 | 导入 CPC | 导出 CPC | 完成订单佣金</div>';
+    echo '<div>时间 | 导入流量 | 导出流量 | 订单数量 | 订单支付金额 | 订单佣金 | 导入 CPC | 导出 CPC</div>';
     echo '</div>';
   }
 
