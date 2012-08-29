4e2b94b821eeada995f659f3cd270d303f7d8018
wangzheng <none@gmail.com>
2012-08-29 13:51:05 +0800

    -

--- vendor/wj/console/publisher/app/advertisement/PublisherAdvertisementScreen.php
+++ vendor/wj/console/publisher/app/advertisement/PublisherAdvertisementScreen.php
@@ -1,7 +1,7 @@
 <?php
 class PublisherAdvertisementScreen extends PublisherScreen {
   protected function renderPublisherContent() {
-    echo '<div class="box"><div class="title">内容插件</div>使用文档/sdk</div>';
+    echo '<div class="box"><div class="title">网页插件</div>使用文档/sdk</div>';
     echo '<div class="box"><div class="title">浏览器插件</div>使用文档 & 定制插件生成器</div>';
     echo '<div class="box"><div class="title">自定义链接</div>icon（桌面链接）/logo 和内容显示政策/url 约定结构/widget 生成器</div>';
   }
