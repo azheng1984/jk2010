df99bd28ff3783d04b2cc949b5417cef5efbc2ec
azheng <azheng1984@126.com>
2012-09-05 13:45:57 +0800

    -

--- vendor/wj/console/publisher/app/performance_report/PublisherPerformanceReportScreen.php
+++ vendor/wj/console/publisher/app/performance_report/PublisherPerformanceReportScreen.php
@@ -1,9 +1,36 @@
 <?php
 class PublisherPerformanceReportScreen extends PublisherScreen {
+  public function __construct() {
+    if (isset($_COOKIE['session_id']) === false) {
+      header('HTTP/1.1 302 Found');
+      header('Location: /');
+      $this->stop();
+    }
+  }
+
   protected function renderHtmlHeadContent() {
+    echo '<title>广告发布商 / 效果报告 - 货比万家</title>';
   }
 
   protected function renderHtmlBodyContent() {
-    echo '<title>广告发布商 / 效果报告 - 货比万家</title>';
+    echo '<h1><a href="/">广告发布商</a></h1>';
+    echo '<div id="toolbar">用户名 | <a href="sign_out">退出</a></div>';
+    PublisherNavigationScreen::render('performance_report');
+    $this->renderFilter();
+    $this->renderResult();
+    $this->renderTatal();
+  }
+
+  private function renderFilter() {
+    
+  }
+
+  private function renderResult() {
+    echo '<table>';
+    echo '</table>';
+  }
+
+  private function renderTotal() {
+    
   }
 }
