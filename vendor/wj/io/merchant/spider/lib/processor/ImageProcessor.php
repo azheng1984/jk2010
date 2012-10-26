20724c15de791ba8d1c275cee1336b2db13ed530
wangzheng <none@gmail.com>
2012-10-26 11:39:46 +0800

    -

---
+++ vendor/wj/io/merchant/spider/lib/processor/ImageProcessor.php
@@ -0,0 +1,59 @@
+<?php
+class ImageProcessor {
+  public function execute($path) {
+    $headers = array();
+    if ($imageLastModified !== null) {
+      $headers = array('If-Modified-Since: '.$imageLastModified);
+    }
+    $result = WebClient::get($domain, '/'.$path, $headers, null, true);
+    if ($result['http_code'] === 304) {
+      return;
+    }
+    $lastModified = $this->getLastModified($result['header']);
+    $md5 = md5($result['content']);
+    if ($md5 === $imageMd5) {
+      return;
+    }
+    $this->save(
+      $tablePrefix, $productId, $result['content'], $md5, $imageMd5 !== null
+    );
+    Db::update(
+      '`'.$tablePrefix.'-product`',
+      array('image_md5' => $md5, 'image_last_modified' => $lastModified),
+      'id = ?',
+      $productId
+    );
+    if ($imageMd5 !== null) {
+      Db::insert(
+        '`'.$tablePrefix.'-log`',
+        array(
+          'type' => 'IMAGE',
+          'product_id' => $productId,
+          'category_id' => $categoryId
+        )
+      );
+    }
+  }
+
+  private function getLastModified($header) {
+    preg_match('/Last-Modified: (.*?)\r\n/', $header, $matches);
+    if (count($matches) === 2) {
+      return $matches[1];
+    }
+  }
+
+  private function save($tablePrefix, $productId, $content, $md5, $isNew) {
+    $isEmpty = isset($GLOBALS['no_image_md5'][$md5]);
+    if ($isEmpty && $isNew) {
+      ImageDb::delete($tablePrefix, $productId);
+    }
+    if ($isEmpty) {
+      return;
+    }
+    if ($isNew) {
+      ImageDb::insert($tablePrefix, $productId, $content);
+      return;
+    }
+    ImageDb::update($tablePrefix, $productId, $content);
+  }
+}
