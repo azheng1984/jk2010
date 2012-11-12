3ab68556abe0e9d0e5de84bd9e3f17c11180c3ce
wangzheng <none@gmail.com>
2012-11-12 16:49:01 +0800

    -

--- vendor/wj/io/merchant/product_update_manager/app/RunCommand.php
+++ vendor/wj/io/merchant/product_update_manager/app/RunCommand.php
@@ -1,7 +1,10 @@
 <?php
 class RunCommand {
+  private $versionInfo;
+
   public function execute() {
     Lock::execute();
+    $GLOBALS['VERSION'] = $this->getVersion();
     for (;;) {
       $task = $this->getNextTask();
       DbConnection::connect($task['merchant_name']);
@@ -12,16 +15,45 @@ class RunCommand {
       ShoppingCommandFile::finalize($categoryId);
       ShoppingImageFolder::finalize($categoryId);
       ShoppingRemoteTask::add($task);
+      DbConnection::connect('default');
       $this->removeTask($task['id']);
       if ($task['is_last'] === '1') {
-        //TODO:更新整体版本完成度
+        $this->updateVersion();
       }
     }
   }
 
   private function getNextTask() {
+    return Db::getRow('SELECT * FROM task ORDER BY id LIMIT 1');
   }
 
   private function removeTask($id) {
+    Db::delete('task', $id);
+  }
+
+  private function updateVersion($merchantName) {
+    $this->versionInfo['merchant'][$merchantName] = true;
+    $merchantList = array(); //TODO
+    $isUpdate = true;
+    foreach ($merchantList as $merchant) {
+      if (isset($version[$merchant]) === false) {
+        $isUpdate = false;
+        break;
+      }
+    }
+    if ($isUpdate) {
+      $this->versionInfo['version'] = ++$this->versionInfo['version'];
+      $this->versionInfo['version']['merchant'] = array();
+      ++$GLOBALS['VERSION'];
+    }
+    file_put_contents(
+      DATA_PATH.'version.php',
+      '<?php return '.var_export($this->versionInfo, true).';'
+    );
+  }
+
+  private function getVersion() {
+    $this->versionInfo = require DATA_PATH.'version.php';
+    return $this->versionInfo['version'];
   }
 }
