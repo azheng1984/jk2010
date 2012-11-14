e0c3d8e7dc53b08e207c770f588243fc87dddb54
azheng <azheng1984@126.com>
2012-11-14 15:02:05 +0800

    -

--- vendor/wj/shopping/update_manager/lib/SyncRemoteFile.php
+++ vendor/wj/shopping/update_manager/lib/SyncRemoteFile.php
@@ -1,11 +1,9 @@
 <?php
 class SyncRemoteFile {
   public static function sync($task) {
-    $conn = ftp_connect("") or $x = 2;//throw new Exception;
-    ftp_login($conn,"admin","ert456");
-    
-    echo ftp_get($conn,"target.txt","source.txt",FTP_ASCII);
-    
-    ftp_close($conn);
+    $ftp = ftp_connect("");
+    ftp_login($ftp,"admin","ert456");
+    echo ftp_get($ftp, "target.txt", "source.txt", FTP_ASCII);
+    ftp_close($ftp);
   }
 }
