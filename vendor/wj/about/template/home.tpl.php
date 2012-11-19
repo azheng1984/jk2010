440e4564a1a5c0e4e359662bf6c508af5a398e08
wangzheng <none@gmail.com>
2012-11-19 15:20:42 +0800

    -

--- vendor/wj/about/template/home.tpl.php
+++
@@ -1,6 +0,0 @@
-“导航式说明”
-<ul>
-  <li>货比万家（shopping(buyer) + merchant + publisher） + 开源项目（项目管理）</li>
-  <li>官方博客 &amp; 微博（link）</li>
-  <li>联系我们(tel, address)</li>
-</ul>
--- vendor/wj/click/app/Action.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Action {
-  public function GET() {
-    
-  }
-}
