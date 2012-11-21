cbbc37cd13b82600e3c69995eadb138247ddc269
azheng <azheng1984@126.com>
2012-11-21 16:22:32 +0800

    -

--- vendor/db/DbConnection.php
+++ vendor/db/DbConnection.php
@@ -1,42 +1,55 @@
 <?php
 class DbConnection {
-  private static $name = 'default';
+  private static $current = null;
   private static $pool = array();
   private static $factory;
   private static $list;
 
-  public static function connect($name, $pdo = null) {
-    self::$list[] = self::$name;
-    self::$name = $name;
-    if ($name !== null && $pdo !== null) {
+  public static function connect(
+    $name = 'default', $pdo = null, $isPersistent = true
+  ) {
+    if (self::$current !== null) {
+      self::$list[] = self::$current;
+    }
+    if ($pdo === null) {
+      $pdo = self::get($name, $isPersistent);
+    }
+    self::$current = $pdo;
+    if ($isPersistent && $name !== null) {
       self::$pool[$name] = $pdo;
     }
   }
 
   public static function close() {
     if (count(self::$list) > 0) {
-      self::$name = array_pop(self::$list);
+      self::$current = array_pop(self::$list);
       return;
     }
-    self::$name = 'default';
+    self::$current = null;
+  }
+
+  public static function getCurrent() {
+    return self::$current;
   }
 
   public static function reset() {
+    unset(self::$current);
     unset(self::$factory);
     unset(self::$pool);
     unset(self::$list);
   }
 
-  public static function get($name = null) {
-    if ($name === null) {
-      $name = self::$name;
-    }
+  private static function get($name, $isPersistent) {
+    $pdo = null;
     if (isset(self::$pool[$name]) === false) {
       $connection = self::getFactory()->get($name);
       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      self::$pool[$name] = $connection;
+      $pdo = $connection;
     }
-    return self::$pool[$name];
+    if ($isPersistent) {
+      self::$pool[$name] = $pdo;
+    }
+    return $pdo;
   }
 
   private static function getFactory() {
