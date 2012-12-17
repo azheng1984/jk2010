693f74af042233296c3796456314004bb9afba5a
wangzheng <none@gmail.com>
2012-12-17 15:49:41 +0800

    -

--- vendor/wj/shopping/portal/app/HomeScreen.php
+++ vendor/wj/shopping/portal/app/HomeScreen.php
@@ -3,7 +3,7 @@ class HomeScreen {
   public function render() {
     echo '<div><a href="/">货比万家</a></div>';
     echo '<form><input name="q" /><input type="submit" value="搜索" /></form>';
-    echo '<div>referrer/share</div>';
+    echo '<div>referrer</div>';
     echo '<h1>xxx 个网上商店，xxx 万商品，搜索：</h1>';
     echo '<ul>';
     echo '<li><a href="/aaa/">aaa</a> [12]</li>';
