6420b3e21dedb9d73d88e4fb3fc21f1a1f91d5c0
azheng <azheng1984@126.com>
2012-12-21 20:37:39 +0800

    -

--- vendor/wj/shopping/portal/lib/view/PaginationScreen.php
+++ vendor/wj/shopping/portal/lib/view/PaginationScreen.php
@@ -14,24 +14,28 @@ class PaginationScreen {
     if ($page !== 1) {
       $previousPage = $page - 1;
       $path = $previousPage === 1 ? '.' : $previousPage;
-      echo '<a href="', $prefix, $path, $postfix, '"', $rel, '>« 上一页</a>';
+      echo '<a class="prev" href="', $prefix, $path, $postfix, '"', $rel, '>‹ 上一页</a> ';
     }
     $lastPage = $firstPage + 9;
     if ($lastPage > $totalPage) {
       $lastPage = $totalPage;
     }
     for ($index = $firstPage; $index <= $lastPage; ++$index) {
+      $class = '';
+      if ($index === $firstPage) {
+        $class = ' class="first" ';
+      }
       if ($index === $page) {
-        echo ' <span>', $index, '</span>';
+        echo '<span'.$class.'>', $index, '</span>';
         continue;
       }
       $path = $index === 1 ? '.' : $index;
-      echo ' <a href="', $prefix, $path, $postfix, '"', $rel, '>',
+      echo '<a href="', $prefix, $path, $postfix, '"', $rel, $class , '>',
         $index, '</a>';
     }
     if ($page !== $totalPage) {
-      echo ' <a href="', $prefix, ($page + 1),
-        $postfix, '"', $rel, '>下一页 »</a>';
+      echo ' <a class="next" href="', $prefix, ($page + 1),
+        $postfix, '"', $rel, '>下一页 ›</a>';
     }
     echo '</div>';
   }
