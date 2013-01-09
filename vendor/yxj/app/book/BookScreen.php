d85fada109380fa188b9a1a68d2d9a73c270bb78
wangzheng <none@gmail.com>
2013-01-09 23:12:45 +0800

    -

--- vendor/yxj/app/book/BookScreen.php
+++ vendor/yxj/app/book/BookScreen.php
@@ -8,13 +8,21 @@ class BookScreen extends Screen {
     $book = Db::getRow('SELECT * FROM book WHERE id = 1');
     echo '<h1>', $book['name'], '</h1>';
     NavigationScreen::render();
-    $pageIdList = explode("\n", str_replace(' ', '', $book['page_id_list']));
-    $pageList = Db::getAll('SELECT * FROM page WHERE id IN ('.implode(',', $pageIdList).')');
-    echo '<ul>';
-    foreach ($pageList as $page) {
-      echo '<li><a href="/book/1/page/', $page['id'], '">',Db::getColumn('SELECT content FROM line WHERE id = '.$page['name_line_id']).'</a></li>';
-    }
-    echo '</ul>';
+//     $pageIdList = explode("\n", $book['page_id_list']);
+//     $pageList = Db::getAll('SELECT * FROM page WHERE id IN ('.implode(',', $pageIdList).')');
+//     echo '<ol>';
+//     $index = 0;
+//     foreach ($pageList as $page) {
+//       if ($pageIdList[$index][0] === ' ') {
+//         echo '<ul>';
+//       }
+//       echo '<li><a href="/book/1/page/', $page['id'], '">',Db::getColumn('SELECT content FROM line WHERE id = '.$page['name_line_id']).'</a></li>';
+//       if ($pageIdList[$index][0] === ' ') {
+//         echo '</ul>';
+//       }
+//       $index++;
+//     }
+    echo '</ol>';
     echo '<p>广告</p>';
   }
 }
