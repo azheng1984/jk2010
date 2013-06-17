dd98f8ebaab383e1612604fb7fd851aab0008a55
wangzheng <none@gmail.com>
2013-06-17 22:29:35 +0800

    -

--- vendor/hyperframework/web/lib/ExceptionHandler.php
+++ vendor/hyperframework/web/lib/ExceptionHandler.php
@@ -1,58 +1,60 @@
 <?php
+namespace Hyperframework\Web;
+
 class ExceptionHandler {
-  private $configPath;
-  private $appClass;
-  private $exception;
+    private $configPath;
+    private $appClass;
+    private $exception;
 
-  public function __construct(
-    $configPath = CONFIG_PATH, $appClass = 'Application'
-  ) {
-    $this->configPath = $configPath;
-    $this->appClass = $appClass;
-  }
-
-  public function run() {
-    set_exception_handler(array($this, 'handle'));
-  }
-
-  public function getException() {
-    return $this->exception;
-  }
-
-  public function handle($exception) {
-    if (headers_sent()) {
-      trigger_error($exception, E_USER_ERROR);
+    public function __construct(
+        $configPath = CONFIG_PATH, $appClass = 'Application'
+    ) {
+        $this->configPath = $configPath;
+        $this->appClass = $appClass;
     }
-    $this->exception = $exception;
-    $this->reload($exception);
-  }
 
-  public function stop() {
-    restore_exception_handler();
-  }
+    public function run() {
+        set_exception_handler(array($this, 'handle'));
+    }
 
-  private function reload($exception) {
-    if ($exception instanceof ApplicationException === false) {
-      $exception = new InternalServerErrorException;
+    public function getException() {
+        return $this->exception;
     }
-    $config = require $this->configPath.'error_handler.config.php';
-    $exception->header();
-    $statusCode = $exception->getCode();
-    $path = null;
-    if (isset($config[$statusCode])) {
-      $path = $config[$statusCode];
+
+    public function handle($exception) {
+        if (headers_sent()) {
+            trigger_error($exception, E_USER_ERROR);
+        }
+        $this->exception = $exception;
+        $this->reload($exception);
     }
-    if ($path === null && isset($config['default'])) {
-      $path = $config['default'];
+
+    public function stop() {
+        restore_exception_handler();
     }
-    if ($path !== null) {
-      try {
-        $app = new $this->appClass;
-        $app->run($path);
-      } catch (UnsupportedMediaTypeException $exception) {
-      } catch (\Exception $exception) {
-        trigger_error($exception, E_USER_ERROR);
-      }
+
+    private function reload($exception) {
+        if ($exception instanceof ApplicationException === false) {
+            $exception = new InternalServerErrorException;
+        }
+        $config = require $this->configPath . 'error_handler.config.php';
+        $exception->header();
+        $statusCode = $exception->getCode();
+        $path = null;
+        if (isset($config[$statusCode])) {
+            $path = $config[$statusCode];
+        }
+        if ($path === null && isset($config['default'])) {
+            $path = $config['default'];
+        }
+        if ($path !== null) {
+            try {
+                $app = new $this->appClass;
+                $app->run($path);
+            } catch (UnsupportedMediaTypeException $exception) {
+            } catch (\Exception $exception) {
+                trigger_error($exception, E_USER_ERROR);
+            }
+        }
     }
-  }
 }
