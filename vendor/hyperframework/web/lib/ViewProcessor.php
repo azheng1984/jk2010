949e58d032fb93ac35ecbae4f1fae1d545a8b80d
wangzheng <wangzheng@daoxila.com>
2013-06-17 20:07:32 +0800

    -

--- vendor/hyperframework/web/lib/ViewProcessor.php
+++ vendor/hyperframework/web/lib/ViewProcessor.php
@@ -1,14 +1,16 @@
 <?php
+namespace Hyperframework\Web;
+
 class ViewProcessor {
-  public function run($cache) {
-    if (isset($_SERVER['REQUEST_MEDIA_TYPE']) === false) {
-      $_SERVER['REQUEST_MEDIA_TYPE'] = key($cache);
+    public function run($cache) {
+        if (isset($_SERVER['REQUEST_MEDIA_TYPE']) === false) {
+            $_SERVER['REQUEST_MEDIA_TYPE'] = key($cache);
+        }
+        $type = $_SERVER['REQUEST_MEDIA_TYPE'];
+        if (isset($cache[$type]) === false) {
+            throw new UnsupportedMediaTypeException;
+        }
+        $view = new $cache[$type];
+        $view->render();
     }
-    $type = $_SERVER['REQUEST_MEDIA_TYPE'];
-    if (isset($cache[$type]) === false) {
-      throw new UnsupportedMediaTypeException;
-    }
-    $view = new $cache[$type];
-    $view->render();
-  }
 }
