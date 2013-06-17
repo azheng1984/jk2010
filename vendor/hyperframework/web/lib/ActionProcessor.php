a517d01ebff4652491d84f2c5f184ae8cb788820
wangzheng <wangzheng@daoxila.com>
2013-06-17 16:36:10 +0800

    -

---
+++ vendor/hyperframework/web/lib/ActionProcessor.php
@@ -0,0 +1,41 @@
+<?php
+namespace Hyperframework::Web;
+
+class ActionProcessor {
+    public function run($cache) {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method === 'HEAD' && isset($cache['method']['HEAD']) === false) {
+            $method = 'GET';
+        }
+        if ($cache === null) {
+            $this->checkImplicitAction($method);
+            return;
+        }
+        $methodList = $cache['method'];
+        if (isset($methodList[$method])) {
+            $action = new $cache['class'];
+            $action->$method();
+            return;
+        }
+        if (isset($cache['get_not_allowed'])) {
+            $this->throwMethodNotAllowedException($methodList);
+        }
+        if ($method !== 'GET') {
+            $methodList['HEAD'] = 1;
+            $methodList['GET'] = 1;
+            $this->throwMethodNotAllowedException($methodList);
+        }
+    }
+
+    private function checkImplicitAction($method) {
+        if ($method !== 'GET') {
+            $this->throwMethodNotAllowedException(array('HEAD', 'GET'));
+        }
+    }
+
+    private function throwMethodNotAllowedException($methodList) {
+        throw new MethodNotAllowedException(
+            implode(', ', array_keys($methodList))
+        );
+    }
+}
---
+++ vendor/hyperframework/web/lib/UnsupportedMediaTypeException.php
@@ -0,0 +1,6 @@
+<?php
+class UnsupportedMediaTypeException extends ApplicationException {
+  public function __construct($message = null, $previous = null) {
+    parent::__construct($message, '415 Unsupported Media Type', $previous);
+  }
+}
