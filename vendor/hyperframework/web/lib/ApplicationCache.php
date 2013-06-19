3e526129891fc2147d51d5c8e70ad6f2aa785e72
azheng <azheng@azheng-laptop.(none)>
2013-06-19 23:06:19 +0800

    -

---
+++ vendor/hyperframework/web/lib/ApplicationCache.php
@@ -0,0 +1,22 @@
+<?php
+namespace Hyperframework\Web;
+
+class ApplicationCache {
+    private static $cache;
+
+    public static function read($path = null) {
+        if ($path === null) {
+            $segmentList = explode('?', $_SERVER['REQUEST_URI'], 2);
+            $path = $segmentList[0];
+        }
+        if (static::$cache === null) {
+            static::$cache = require CACHE_PATH . 'application.cache.php';
+        }
+        if (isset(static::$cache[$path]) === false) {
+            throw new NotFoundException(
+                'Application path \'' . $path . '\' not found'
+            );
+        }
+        return static::$cache[$path];
+    }
+}
