ec2719f5cb46349173ee69c87ff23d13c490f3a2
wangzheng <none@gmail.com>
2013-07-17 21:43:33 +0800

    -

--- vendor/hyperframework/web/lib/Application.php
+++ vendor/hyperframework/web/lib/Application.php
@@ -2,10 +2,16 @@
 namespace Hyperframework\Web;
 
 class Application {
+    private static $info;
     private $isViewEnabled = true;
     private $actionResult;
 
-    public function run($info) {
+    public static function initialize($info) {
+        static::$info = $info;
+    }
+
+    public function run($path = null) {
+        $info = $this->getInfo($path);
         $this->executeAction($info);
         $this->renderView($info);
     }
@@ -22,6 +28,22 @@ class Application {
         return $this->actionResult;
     }
 
+    protected function getInfo($path) {
+        if ($path === null) {
+            $segments = explode('?', $_SERVER['REQUEST_URI'], 2);
+            $path = $segments[0];
+        }
+        if (static::$info === null) {
+            static::$info = require CACHE_PATH . 'appication_info.cache.php';
+        }
+        if (isset(static::$cache[$path]) === false) {
+            throw new NotFoundException(
+                'Application path \'' . $path . '\' not found'
+            );
+        }
+        return static::$cache[$path];
+    }
+
     protected function executeAction($info) {
         $actionInfo = null;
         if (isset($info['Action'])) {
@@ -30,7 +52,7 @@ class Application {
         $processor = new ActionProcessor;
         $this->actionResult = $processor->run($actionInfo);
     }
- 
+
     protected function renderView($info) {
         if (isset($info['View']) && $this->isViewEnabled) {
             $processor = new ViewProcessor;
