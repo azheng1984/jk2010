5297f5d4dd440e0150e6bd7a86e35761c175f2c6
wangzheng <wangzheng@daoxila.com>
2013-07-18 16:35:29 +0800

    -

--- vendor/hyperframework/tool/lib/web/handler/ActionHandler.php
+++ vendor/hyperframework/tool/lib/web/handler/ActionHandler.php
@@ -1,28 +1,54 @@
 <?php
 class ActionHandler {
-  public function handle($class, $fullPath) {
-    $postfix = 'Action';
-    if (substr($class, -(strlen($postfix))) !== $postfix) {
-      return;
+    public function handle($class, $fullPath) {
+        $postfix = 'Action';
+        if (substr($class, -(strlen($postfix))) !== $postfix) {
+            return;
+        }
+        return $this->getCache($class, $fullPath);
     }
-    return $this->getCache($class, $fullPath);
-  }
 
-  private function getCache($class, $fullPath) {
-    $cache = array('class' => $class, 'method' => array ());
-    $reflectors = $this->getMethodReflectors($class, $fullPath);
-    foreach ($reflectors as $reflector) {
-      $method = $reflector->getName();
-      if (!preg_match('/^[A-Z]+$/', $method)) {
-        throw new Exception("Invalid action method '$method' in '$fullPath'");
-      }
-      $cache['method'][] = $method;
+    private function getCache($class, $fullPath) {
+        $cache = array('class' => $class, 'methods' => array());
+        $httpMethods = array('GET', 'POST', 'PUT', 'DELETE');
+        if ($this->hasPrivateGet($class)) {
+            $cache['GET_not_allowed'] = true;
+        }
+        $reflectors = $this->getMethodReflectors($class, $fullPath);
+        foreach ($reflectors as $reflector) {
+            $method = $reflector->getName();
+            if (strpos($method, '__') === 0) {
+                continue;
+            }
+            if ($method === 'before') {
+                $cache['before_filter'] = true;
+                continue;
+            }
+            if ($method === 'after') {
+                $cache['after_filter'] = true;
+                continue;
+            }
+            if (in_array($method, $httpMethods) === false) {
+                throw new Exception(
+                    "Error: Invalid public method '$method' in '$fullPath'"
+                );
+            }
+            $cache['methods'][$method] = true;
+        }
+        return $cache;
     }
-    return $cache;
-  }
 
-  private function getMethodReflectors($class, $fullPath) {
-    $reflector = new ReflectionClass($class);
-    return $reflector->getMethods(ReflectionMethod::IS_PUBLIC);
-  }
+    private function getMethodReflectors($class, $fullPath) {
+        $reflector = new ReflectionClass($class);
+        return $reflector->getMethods(ReflectionMethod::IS_PUBLIC);
+    }
+
+    private function hasPrivateGet($class) {
+        $reflector = new ReflectionClass($class);
+        if ($reflector->hasMethod('GET') === false) {
+            return false;
+        }
+        $getMethod = $reflector->getMethod('GET');
+        return $getMethod->isPrivate();
+    }
 }
