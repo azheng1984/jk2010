36418755d77baed7ec344879c0a6ec285eb5975e
wangzheng <wangzheng@daoxila.com>
2013-07-24 16:43:26 +0800

    -

---
+++ vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
@@ -0,0 +1,14 @@
+<?php
+class ClassLoaderBuilder {
+  public function build($config) {
+    $cache = new ClassLoaderCache;
+    $directoryReader = new DirectoryReader(
+      new ClassRecognizationHandler($cache)
+    );
+    $configuration = new ClassLoaderConfiguration;
+    foreach ($configuration->extract($config) as $item) {
+      $directoryReader->read($item[0], $item[1]);
+    }
+    return $cache;
+  }
+}
