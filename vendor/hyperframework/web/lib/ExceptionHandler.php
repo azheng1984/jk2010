743e3c9b9b8bdf221603f572c712fa615b9560bb
wangzheng <none@gmail.com>
2013-08-05 16:57:02 +0800

    -

--- vendor/hyperframework/web/lib/ExceptionHandler.php
+++ vendor/hyperframework/web/lib/ExceptionHandler.php
@@ -38,7 +38,7 @@ class ExceptionHandler {
         try {
             $app = new $this->appClass;
             $app->run($config[$statusCode]);
-        } catch (UnsupportedMediaTypeException $recursiveException) {
+        } catch (UnsupportedMediaTypeException $ignoredException) {
         } catch (\Exception $recursiveException) {
             $message = 'Uncaught ' . $this->exception . PHP_EOL .
                 PHP_EOL . 'Next ' . $recursiveException . PHP_EOL;
