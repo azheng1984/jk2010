8d81517b3fdcc8fec969cd37a34c4a14d48efd7a
wangzheng <wangzheng@daoxila.com>
2013-08-06 18:35:45 +0800

    -

--- vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
+++ vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
@@ -1,16 +1,16 @@
 <?php
 class ClassLoaderBuilder {
-  public function build($config) {
-    var_dump($config);
-    return;
-    $cache = new ClassLoaderCache;
-    $directoryReader = new DirectoryReader(
-      new ClassRecognizationHandler($cache)
-    );
-    $configuration = new ClassLoaderConfiguration;
-    foreach ($configuration->extract($config) as $item) {
-      $directoryReader->read($item[0], $item[1]);
+    public function build($config) {
+        var_dump($config);
+        return;
+        $cache = new ClassLoaderCache;
+        $directoryReader = new DirectoryReader(
+            new ClassRecognizationHandler($cache)
+        );
+        $configuration = new ClassLoaderConfiguration;
+        foreach ($configuration->extract($config) as $item) {
+            $directoryReader->read($item[0], $item[1]);
+        }
+        return $cache;
     }
-    return $cache;
-  }
 }
