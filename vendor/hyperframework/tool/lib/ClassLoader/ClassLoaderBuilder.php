ee00b2cd16b64a5a7d66b5005944c5ff3a591157
azheng <azheng@azheng-laptop.(none)>
2013-08-13 23:31:48 +0800

    -

--- vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
+++ vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
@@ -4,7 +4,7 @@ class ClassLoaderBuilder {
 
     public function build() {
         $config = require 'config' . DIRECTORY_SEPARATOR . 'class_loader.config.php';
-        $this->processNamespace('\\', $config);
+        $this->processNamespace('', $config, array());
         exit;
         $cache = new ClassLoaderCache;
         $directoryReader = new DirectoryReader(
@@ -22,13 +22,16 @@ class ClassLoaderBuilder {
             $this->processFolder($namespace, $config, $properties);
             return;
         }
-        $properties = $this->processFolder($config, $properties);
+        $properties = $this->processProperties($config, $properties);
         foreach ($config as $key => $value) {
             if (is_int($key)) {
                 $this->processFolder($namespace, $value, $properties);
             } elseif (strncmp($key, '@', 1) !== 0) {
+                if ($namespace !== '') {
+                    $namespace .= '\\';
+                }
                 $this->processNamespace(
-                    $namespace . '\\' . $key, $value, $properties
+                    $namespace . $key, $value, $properties
                 );
             }
         }
@@ -41,31 +44,56 @@ class ClassLoaderBuilder {
         }
         $properties = $this->processProperties($config, $properties);
         foreach ($config as $key => $value) {
+            if (strncmp($key, '@', 1) === 0) {
+                continue;
+            }
             if (is_int($key)) {
                 if (is_array($value)) {
                     $this->processFolder($namespace, $value, $properties);
+                } else {
+                    $this->addMapping($namespace, $value, $properties);
                 }
-            } else {
-                $this->addMapping($namespace, $value, $properties);
             }
         }
     }
 
     public function addMapping($namespace, $folder, $properties) {
         if (isset($properties['@root'])) {
-            $folder = $properties['@root'] . $folder;
+            if ($folder === '.') {
+                $folder = $properties['@root'];
+            } else {
+                $folder = $properties['@root'] . '/'  . $folder;
+            }
         }
-//        if (isset($properties['folder_mapping']) &&
-//            $properties['folder_mapping'] === false) {
-//
-//            }
+        if (isset($properties['@recursive']) &&
+            $properties['@recursive'] === false) {
+        } else {
+            echo '[recursive]';
+        }
+        if (isset($properties['@folder_mapping']) &&
+            $properties['@folder_mapping'] === false) {
+           //todo 扫描所有子文件，找出类
+        } else {
+            echo '[folder_mapping]';
+        }
+        if (strncmp($folder, '/', 1) !== 0) {
+            $folder = $_SERVER['PWD'] . '/' . $folder;
+        }
+        if (isset($properties['@exclude']) &&
+            $properties['@exclude'] === true) {
+            echo '[exclude]';
+        }
+        //todo
+        // case 1:
+        // case 2:
+        // case 3:
         echo $namespace . ' > ' . $folder . PHP_EOL;
     }
 
     public function processProperties($config, $properties) {
         foreach ($config as $key => $value) {
             if (is_int($key) === false && strncmp($key, '@', 1) === 0) {
-                //@root
+                //@root (可被覆盖或 '相对 root')
                 //@folder_mapping
                 //@recursive
                 //@exclude
