378150de5d2267fe3174b9526804781d4d5a82a2
wangzheng <wangzheng@daoxila.com>
2013-08-14 18:41:02 +0800

    -

--- vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
+++ vendor/hyperframework/tool/lib/ClassLoader/ClassLoaderBuilder.php
@@ -1,20 +1,78 @@
 <?php
 class ClassLoaderBuilder {
-    private $cache;
+    private $classMappings = array();
 
     public function build() {
         $config = require 'config' . DIRECTORY_SEPARATOR . 'class_loader.config.php';
         $this->processNamespace('', $config, array());
-        exit;
-        $cache = new ClassLoaderCache;
-        $directoryReader = new DirectoryReader(
-            new ClassRecognizationHandler($cache)
-        );
-        $configuration = new ClassLoaderConfiguration;
-        foreach ($configuration->extract($config) as $item) {
-            $directoryReader->read($item[0], $item[1]);
+        foreach ($this->classMappings as $namespace => $classMapping) {
+            $tmp = $classMapping[1]->export();
+            $namespaces = explode('\\', $namespace);
+            $target = &$this->output;
+            foreach ($namespaces as $item) {
+                if ($item === '') {
+                    continue;
+                }
+                if (isset($target[$item]) === false) {
+                    if (is_string($target)) {
+                        $target = array($target);
+                    } else {
+                        $target[$item] = array();
+                    }
+                }
+                $target = &$target[$item];
+            }
+            if (is_string($target)) {
+                $target = array($target);
+            }
+            $target['@classes'] = $tmp['class_loader'];
         }
+        $this->checkConflict($this->output);
+        $cache = var_export($this->output, true);
+        echo $cache;
+        exit;
         return $cache;
+    }
+
+    private function checkConflict(&$current) {
+        if (is_string($current)) {
+            return;
+        }
+        $folders = array();
+        $namespaces = array();
+        foreach ($current as $key => $value) {
+            if (is_int($key)) {
+                $folders[] = $value;
+            } else {
+                if (strncmp($key, '@', 1) === 0) {
+                    continue;
+                }
+                $namespaces[] = $key;
+            }
+        }
+        foreach ($folders as $folder) {
+            foreach ($namespaces as $namespace) {
+                if (is_dir($folder . '/' . $namespace)) {
+                    if (is_string($current[$namespace])) {
+                        $current[$namespace] = array(
+                            $current[$namespace], $folder . '/' . $namespace
+                        );
+                    } else {
+                        $current[$namespace][] = $folder . '/' . $namespace;
+                    }
+                }
+            }
+        }
+        if (count($folders) < 2) {
+            foreach ($folders as $folder) {
+                if (is_dir($folder) === false) {
+                    
+                }
+            }
+        }
+        foreach ($namespaces as $namespace) {
+            $this->checkConflict($current[$namespace]);
+        }
     }
 
     public function processNamespace($namespace, $config, $properties) {
@@ -64,34 +122,97 @@ class ClassLoaderBuilder {
             } else {
                 $folder = $properties['@root'] . '/'  . $folder;
             }
+            unset($properties['@root']);
         }
+        $recursive = true;
+        $folderMapping = true;
+        $exclude = false;
         if (isset($properties['@recursive']) &&
             $properties['@recursive'] === false) {
+            $recursive = false;
         } else {
-            echo '[recursive]';
+            unset($properties['@recursive']);
+            //echo '[recursive]';
         }
         if (isset($properties['@folder_mapping']) &&
             $properties['@folder_mapping'] === false) {
-           //todo 扫描所有子文件，找出类
+            $folderMapping = false;
         } else {
-            echo '[folder_mapping]';
+           unset($properties['@folder_mapping']);
+           // echo '[folder_mapping]';
         }
         if (strncmp($folder, '/', 1) !== 0) {
             $folder = $_SERVER['PWD'] . '/' . $folder;
         }
         if (isset($properties['@exclude']) &&
             $properties['@exclude'] === true) {
-            echo '[exclude]';
+            $exclude = true;
+            //echo '[exclude]';
+        } else {
+            unset($properties['@exclude']);
         }
-        //todo
-        // 1: 在 folder_mapping = true 下 exclude，会显式阻止匹配，folder_mapping false 时，扫描器不会进入
-        // 2: 一个命名空间中同时对应两个文件夹，则需要扫描下层文件，如果下层文件还是冲突，扫描下一层，直到所有路径都是明确的
-        // 3: 压缩路径(使用尽量少的配置完成文件定位)：在 folder_mapping 的情况下，在没有歧义时进行，比如 \X\Y\Z.php 对应 /x/Y/Z.php 路径，其实只要 X => x 足够了
-        // 4: recursive = false 停止所有 children 的扫描，如果是 folder_mapping = true 时，在输出 cache 中设置中断匹配标记
-        // 5: 配置允许 “叠加”, 除非是属性配置 比如 recursive = false/true
-        // 6: path 反向匹配命名空间（用于 app build）
-        echo $namespace . ' > ' . $folder . PHP_EOL;
+       // if (count($properties) > 0) {
+       //     $folder = array($folder) +  $properties;
+       // }
+        if (isset($properties['@folder_mapping'])) {
+            if (isset($properties['@exclude'])) {
+                return;
+            }
+            if (isset($this->classMappings[$namespace]) === false) {
+                $cache = new ClassLoaderCache;
+                $directoryReader = new DirectoryReader(
+                    new ClassRecognizationHandler($cache)
+                );
+                $this->classMappings[$namespace] = array(
+                    $directoryReader, $cache
+                );
+            }
+            $this->classMappings[$namespace][0]->read($folder);
+            return;
+        }
+        //todo 自动扩展一个命名空间中的文件夹 & 上层自动匹配的文件夹检查是否有匹配的子 namespace
+
+        $currentNamespace = &$this->output;
+        $count = 0;
+        $namespaces = explode('\\', $namespace);
+        //print_r($namespace);
+        $amount = count($namespaces);
+        //echo $amount;
+        //echo $namespace . ' ' . $folder . PHP_EOL;
+        //var_export($this->output);
+        foreach ($namespaces as $item) {
+            ++$count;
+            //echo $count . ' '. $item;
+            if ($item === '') {
+                continue;
+            }
+            if ($count === $amount) {
+                if (isset($currentNamespace[$item])) {
+                    if (is_string($currentNamespace[$item])) {
+                        $currentNamespace[$item] = array($currentNamespace[$item], $folder);
+                    } else {
+                        $currentNamespace[$item][] = $folder; 
+                    }
+                } else {
+                    if (is_string($currentNamespace)) {
+                        $currentNamespace = array($currentNamespace, $item => $folder);
+                    } else {
+                        $currentNamespace[$item] = $folder;
+                    }
+                }
+            } elseif (isset($currentNamespace[$item]) === false) {
+                if (is_string($currentNamespace)) {
+                    $currentNamespace = array($currentNamespace, $item => array());
+                } else {
+                    $currentNamespace[$item] = array();
+                }
+            }
+            $currentNamespace = &$currentNamespace[$item];
+        }
+        //var_export($this->output);
     }
+
+    private $output = array();
 
     public function processProperties($config, $properties) {
         foreach ($config as $key => $value) {
