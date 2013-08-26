a35535305fc5631343d370673793d95bfe8055bc
wangzheng <none@gmail.com>
2013-08-26 18:14:55 +0800

    -

--- vendor/hyperframework/router/lib/Router.php
+++ vendor/hyperframework/router/lib/Router.php
@@ -1,13 +1,12 @@
 <?php
 namespace Hyperframework;
-use Hyperframework\Web\PathInfo as PathInfo;
 
 class Router {
     private static $parameters = array();
 
     public static function execute() {
         if ($_SERVER['SERVER_NAME'] !== $_SERVER['HTTP_HOST']) {
-            static::redirect($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+            static::redirect($_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
             return;
         }
         $segments = explode('?', $_SERVER['REQUEST_URI'], 2);
@@ -17,41 +16,45 @@ class Router {
         $path = '';
         foreach (explode('/', $segments[0]) as $segment) {
             if ($segment === '') {
+                if ($path !== '') {
+                    $path .= '/';
+                }
                 continue;
             }
             $splittingPosition = strpos($segment, '-');
             if ($splittingPosition === false) {
                 $path .= '/' . $segment;
-                $parameters[] = null;
+                static::$parameters[] = null;
                 continue;
             }
             $items = explode('-', $segment, 2);
             $path .= '/' . $items[0];
-            $parameters[] = $items[1];
+            static::$parameters[] = $items[1];
         }
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
             return $path;
         }
-        if (PathInfo::exists($path)) {
+        if (Web\PathInfo::exists($path)) {
             return $path;
         }
         if (substr($path, -1) === '/') {
             $path = substr($path, 0, strlen($path) - 1);
-            if (PathInfo::exists($path)) {
-                $location = substr($segments[0], 0, strlen($path) - 1);
+            if (Web\PathInfo::exists($path)) {
+                $location = substr($segments[0], 0, strlen($segments[0]) - 1);
                 if (count($segments) === 2) {
-                    $location .= '?' . $segements[1];
+                    $location .= '?' . $segments[1];
                 }
                 static::redirect($location);
+                return;
             } else {
                 throw new Web\NotFoundException;
             }
         }
         $path = $path . '/';
-        if (PathInfo::exists($path)) {
+        if (Web\PathInfo::exists($path)) {
             $location = $segments[0] . '/';
             if (count($segments) === 2) {
-                $location .= '?' . $segements[1];
+                $location .= '?' . $segments[1];
             }
             static::redirect($location);
         }
@@ -67,7 +70,9 @@ class Router {
         if (isset($_SERVER['HTTPS'])) {
             $protocol = 'https';
         }
-        header('Location: ' . $protocol . '://' . $location);
+        $location = 'Location: ' . $protocol .
+            '://' . $_SERVER['SERVER_NAME'] . $location;
+        header($location);
         header('HTTP/1.1 301 Moved Permanently');
     }
 }
