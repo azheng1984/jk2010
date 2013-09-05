14635acaa6eb51f4065374970aaa39c90e114c77
wangzheng <none@gmail.com>
2013-09-05 23:04:37 +0800

    -

---
+++ vendor/hyperframework/routing/lib/HierarchyChecker.php
@@ -0,0 +1,31 @@
+<?php
+namespace Hyperframework\Routing;
+use Hyperframework\Web\PathInfo as PathInfo;
+use Hyperframework\Web\NotFoundException as NotFoundException;
+
+class HierarchyChecker {
+    const REDIRECT_TO_FILE = 0;
+    const REDIRECT_TO_DIRECTORY = 1;
+
+    public static function execute($path = null) {
+        if ($path === null) {
+            $path = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+        }
+        if (PathInfo::exists($path)) {
+           return;
+        }
+        if (substr($path, -1) === '/') {
+            $path = substr($path, 0, strlen($path) - 1);
+            if (PathInfo::exists($path)) {
+                return self::REDIRECT_TO_FILE;
+            } else {
+                throw new NotFoundException;
+            }
+        }
+        $path = $path . '/';
+        if (PathInfo::exists($path)) {
+            return self::REDIRECT_TO_DIRECTORY;
+        }
+        throw new NotFoundException;
+    }
+}
