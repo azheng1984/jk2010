f749f94669af9ce0e58785e7797ca944e79ab114
wangzheng <none@gmail.com>
2013-09-05 11:48:10 +0800

    -

--- vendor/hyperframework/router/lib/LocationMatcher.php
+++ vendor/hyperframework/router/lib/LocationMatcher.php
@@ -1,17 +1,31 @@
 <?php
 namespace Hyperframework\Router;
 
-class LocationMatcher {
-    public static function execute($location) {
-        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+class LinkMatcher {
+    public static function execute($info) {
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET' ||
+            $_SERVER['REQUEST_METHOD'] !== 'HEAD') {
             return;
         }
-        $requestLocation = (isset($_SERVER['HTTPS']) ? 'https' : 'http') .
-            '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        if ($location !== $requestLocation) {
+        $location = static::getCurrent($info);
+        if ($location === null) {
+            return;
+        }
+        if ($location !== static::getRequestLink()) {
             header('HTTP/1.1 301 Moved Permanently');
             header('Location: ' . $location);
             return false;
         }
+    }
+
+    protected static function getCurrentLink($info) {
+        if (isset($info['Link']['can_get_current'])) {
+            return $info['Link']['class']::getCurrent();
+        }
+    }
+
+    protected static function getRequestLink() {
+        return (isset($_SERVER['HTTPS']) ? 'https' : 'http') .
+            '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     }
 }
