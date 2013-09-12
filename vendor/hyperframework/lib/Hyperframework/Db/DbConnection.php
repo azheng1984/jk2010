8b210ddf9e257ec7955640bbb078ce233546a641
wangzheng <none@gmail.com>
2013-09-12 18:50:31 +0800

    -

--- vendor/hyperframework/lib/Hyperframework/Db/DbConnection.php
+++ vendor/hyperframework/lib/Hyperframework/Db/DbConnection.php
@@ -1,69 +1,69 @@
 <?php
 class DbConnection {
-  private static $current = null;
-  private static $pool = array();
-  private static $stack = array();
-  private static $factory;
+    private static $current = null;
+    private static $pool = array();
+    private static $stack = array();
+    private static $factory;
 
-  public static function connect(
-      $name = 'default', $pdo = null, $isReusable = true,
-  ) {
-    if (self::$current !== null) {
-      self::$stack[] = self::$current;
+    public static function connect(
+        $name = 'default', $pdo = null, $isReusable = true,
+    ) {
+        if (self::$current !== null) {
+            self::$stack[] = self::$current;
+        }
+        if ($pdo !== null) {
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);//emulate by default, configurable
+        }
+        if ($pdo === null) {
+            $pdo = self::create($name, $isReusable);
+        }
+        self::$current = $pdo;
     }
-    if ($pdo !== null) {
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);//emulate by default, configurable
-    }
-    if ($pdo === null) {
-      $pdo = self::build($name, $isReusable);
-    }
-    self::$current = $pdo;
-  }
 
-  public static function close() {
-    if (count(self::$stack) > 0) {
-      self::$current = array_pop(self::$stack);
-      return;
+    public static function close() {
+        if (count(self::$stack) > 0) {
+            self::$current = array_pop(self::$stack);
+            return;
+        }
+        self::$current = null;
     }
-    self::$current = null;
-  }
 
-  public static function closeAll() {
-    self::$stack = array();
-    self::$current = null;
-  }
-
-  public static function getCurrent() {
-    if (self::$current === null) {
-      self::connect();
+    public static function closeAll() {
+        self::$stack = array();
+        self::$current = null;
     }
-    return self::$current;
-  }
 
-  public static function reset() {
-    self::$current = null;
-    self::$stack = array();
-    self::$pool = array();
-    self::$factory = null;
-  }
+    public static function getCurrent() {
+        if (self::$current === null) {
+            self::connect();
+        }
+        return self::$current;
+    }
 
-  private static function build($name, $isReusable) {
-    if ($isReusable && isset(self::$pool[$name])) {
-      return self::$pool[$name];
+    public static function reset() {
+        self::$current = null;
+        self::$stack = array();
+        self::$pool = array();
+        self::$factory = null;
     }
-    $pdo = self::getFactory()->get($name);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if ($isReusable) {
-      self::$pool[$name] = $pdo;
-    }
-    return $pdo;
-  }
 
-  private static function getFactory() {
-    if (self::$factory === null) {
-      self::$factory = new DbConnectionFactory;
+    private static function create($name, $isReusable) {
+        if ($isReusable && isset(self::$pool[$name])) {
+            return self::$pool[$name];
+        }
+        $pdo = self::getFactory()->get($name);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if ($isReusable) {
+            self::$pool[$name] = $pdo;
+        }
+        return $pdo;
     }
-    return self::$factory;
-  }
+
+    private static function getFactory() {
+        if (self::$factory === null) {
+            self::$factory = new DbConnectionFactory;
+        }
+        return self::$factory;
+    }
 }
