7e187fc09504544b71ebb4eaaeab712319200d03
azheng <none@gmail.com>
2013-10-07 11:14:38 +0800

    -

--- vendor/yxj/config/env-local.config.php
+++ vendor/yxj/config/env-local.config.php
@@ -1,9 +1,13 @@
 <?php
-require HF_PATH . 'Config.php';
+require HYPERFRAMEWORK_PATH . 'Config.php';
 use Hyperframework\Config;
 Config::set(
-    ['Hyperfraemwork\Web\PathInfo', 'enable_cache', false]
+    ['Hyperframework\Web\PathInfo', 'enable_cache', false],
+    ['Hyperframework\ClassLoader', 'enable_cache', false]
+    ['Hyperframework\View\Asset', 'enable_cache', false]
 );
+/*
 define('ENABLE_HYPERFRAMEWORK_PATH_INFO_CACHE', false);
 define('ENABLE_HYPERFRAMEWORK_CLASS_LOADER_CACHE', false);
 define('ENABLE_HYPERFRAMEWORK_ASSET_CACHE', false);
+*/
