5cd61c4e869948292225ccef1ffce1fe9d7b57a6
azheng <none@gmail.com>
2013-10-07 10:25:15 +0800

    -

--- vendor/yxj/app/Brand/BrandAction.php
+++ vendor/yxj/app/Brand/BrandAction.php
@@ -3,22 +3,16 @@ namespace YouXuanJi\App\Brand;
 use Hyperframework\Db\Client as DbClient;
 
 class BrandAction {
-  /**
-   * list * lsififi * main is the mos
-   */
-  public function get() {
-  }
-
-  public function delete() {
-    DbClient::beginTransaction();
-    try {
-      DbClient::update('brand', array('is_active' => 0), 'id = ?', $id);
-    //delete all category link
-    //delete location link
-    //
-      DbClient::commit();
-    } catch (Exception $exception) {
-      DbClient::rollback();
+    public function delete() {
+        DbClient::beginTransaction();
+        try {
+            DbClient::update('brand', array('is_active' => 0), 'id = ?', $id);
+            //delete all category link
+            //delete location link
+            //
+            DbClient::commit();
+        } catch (Exception $exception) {
+            DbClient::rollback();
+        }
     }
-  }
 }
