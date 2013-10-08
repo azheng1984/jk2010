3d9e1d413bae18709570e1fc7e7f23c950f5f509
azheng <none@google.com>
2013-10-08 16:40:22 +0800

    -

--- vendor/hyperframework/lib/Hyperframework/Config.php
+++ vendor/hyperframework/lib/Hyperframework/Config.php
@@ -2,4 +2,44 @@
 namespace Hyperframework;
 
 class Config {
+    private static $data = array();
+
+    public static function get($key) {
+        if (isset(static::$data[$class][$key])) {
+            return static::$data[$class][$key];
+        }
+    }
+
+    public static function set(/*$mixed, ...*/) {
+        $arguments = func_get_args();
+        if (is_string($arguments[0])) {
+            static::$data[$arguments[0]] = $arguments[1];
+            return;
+        }
+        foreach ($arguments as $key => $item) {
+            if (is_int($key)) {
+                static::$data[$item[0]] = $item[1];
+                return;
+            }
+            static::mergePrefix($key, $item);
+        }
+    }
+
+    public static function mergePrefix($prefix, $data) {
+        if (is_string($data[0])) {
+            static::$data[$prefix . '\\' . $data[0]] = $data[1];
+            return;
+        }
+        foreach ($data as $key => $item) {
+            if (is_int($key)) {
+                static::$data[$prefix . '\\' . $item[0]] = $item[1];
+                continue;
+            }
+            static::mergePrefix($prefix . '\\' . $key, $item);
+        }
+    }
+
+    public static function reset() {
+        static::$data = array();
+    }
 }
