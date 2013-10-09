cc941f13e90ee04656219b502e6415064192020c
azheng <none@google.com>
2013-10-09 14:43:38 +0800

    -

--- vendor/yxj/config/database-development.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-  'default' => array(
-    'dsn' => 'mysql:host=localhost;dbname=youxuanji',
-    'username' => 'root',
-    'password' => 'a841107!',
-  )
-);
--- vendor/yxj/config/env-development.config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('HYPERFRAMEWORK_PATH', '/home/azheng/wj/vendor/hyperframework/');
-$GLOBALS['DOMAIN_PREFIX'] = 'dev.';
-
-Config::set('Connection\ConfigPath', 'database-development.config');
