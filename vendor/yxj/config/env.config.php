b81014c394070c0dcb7e3fcf725ee6659e250b40
azheng <none@google.com>
2013-10-12 13:28:27 +0800

    -

--- vendor/yxj/config/env.config.php
+++ vendor/yxj/config/env.config.php
@@ -1,7 +1,7 @@
 <?php
 namespace Yxj;
 
-const HYPERFRAMEWORK_PATH = '/usr/local/hf/lib/';
+const HYPERFRAMEWORK_PATH = '/usr/local/hyperframework/lib/Hyperframework/';
 require HYPERFRAMEWORK_PATH . 'Config.php';
 Hyperframework\Config::set(array(
     'Hyperframework\Web\PathInfo\EnableCache' => false,
