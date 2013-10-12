760dd5dbc9de96e028ee7370d9480729e600f8d1
azheng <none@google.com>
2013-10-12 13:22:05 +0800

    -

--- vendor/yxj/config/env.config.php
+++ vendor/yxj/config/env.config.php
@@ -2,7 +2,6 @@
 namespace Yxj;
 
 const HYPERFRAMEWORK_PATH = '/usr/local/hf/lib/';
-
 require HYPERFRAMEWORK_PATH . 'Config.php';
 Hyperframework\Config::set(array(
     'Hyperframework\Web\PathInfo\EnableCache' => false,
--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -5,7 +5,7 @@ function initialize() {
     $rootPath = dirname(__DIR__) . DIRECTORY_SEPARATOR;
     require $rootPath . 'config' . DIRECTORY_SEPARATOR . 'env.config.php';
     Hyperframework\Config::setRootPath($rootPath);
-    require Config::getHyperframeworkLibraryPath(). 'ClassLoader.php';
+    require HYPERFRAMEWORK_PATH . 'ClassLoader.php';
     Hyperframework\ClassLoader::run();
     Hyperframework\ExceptionHandler::run();
 }
