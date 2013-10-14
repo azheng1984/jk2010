67f331fa91ad78944ec4dd0bac1755858e8a92d2
azheng <none@gmail.com>
2013-10-15 00:43:32 +0800

    -

---
+++ vendor/yxj/asset/index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Yxj;
+
+function run() {
+    $rootPath = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+    require $rootPath . 'lib' . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+    Bootstrap::run($rootPath);
+    //get asset...
+}
+
+run();
