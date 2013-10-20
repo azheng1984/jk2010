9e36bb3246b1ce8825d2fef558244f3f34957f9a
azheng <none@gmail.com>
2013-10-20 22:12:40 +0800

    -

--- vendor/yxj/app/Category/Brand/Action.php
+++ vendor/yxj/app/Category/Brand/Action.php
@@ -1,13 +1,13 @@
 <?php 
 class CategoryBrandAction {
-  public function PUT() {
-    $categoryId = $GLOBALS['PATH_SECTION_LIST'][1][1];
-    $brandId = $_GET['brand_id'];
-    $pr = $_POST['pr'];
-    Db::update(
-      'brand_category',
-      array('popularity_rank' => $pr),
-      'category_id = ? AND brand_id = ?', $categoryId, $brandId
-    );
-  }
+    public function PUT() {
+        $categoryId = $GLOBALS['PATH_SECTION_LIST'][1][1];
+        $brandId = $_GET['brand_id'];
+        $pr = $_POST['pr'];
+        Db::update(
+            'brand_category',
+            array('popularity_rank' => $pr),
+            'category_id = ? AND brand_id = ?', $categoryId, $brandId
+        );
+    }
 }
