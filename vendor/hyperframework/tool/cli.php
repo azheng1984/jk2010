ffa04178fe26d0d8c5456c5c5cf3525f259daa93
azheng <none@google.com>
2013-10-24 17:15:59 +0800

    -

--- vendor/hyperframework/tool/cli.php
+++ vendor/hyperframework/tool/cli.php
@@ -1,21 +1,13 @@
 #!/usr/bin/env php
 <?php
-throw new Exception;
-namespace Hyperframwork\Tool;
+namespace Hyperframework\Tool;
 
-define('Hyperframework\Tool\ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-define('CACHE_PATH', ROOT_PATH.'cache'.DIRECTORY_SEPARATOR);
-define('CONFIG_PATH', ROOT_PATH.'config'.DIRECTORY_SEPARATOR);
-define('HYPERFRAMEWORK_PATH', dirname(ROOT_PATH).DIRECTORY_SEPARATOR . 'lib'. DIRECTORY_SEPARATOR);
-require HYPERFRAMEWORK_PATH . 'Hyperframework' .
-    DIRECTORY_SEPARATOR . 'ClassLoader2.php';
-$classLoader = new Hyperframework\ClassLoader2;
-$classLoader->run();
-$exceptionHandler = new CommandExceptionHandler;
-$exceptionHandler->run();
-if (!isset($_SERVER['PWD'])) {
-    $_SERVER['PWD'] = getcwd();
+function run() {
+    $rootPath = __DIR__ . DIRECTORY_SEPARATOR;
+    require $rootPath . 'lib' . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+    Bootstrap::run($rootPath);
+    $app = new CommandApplication;
+    $app->run();
 }
-$app = new CommandApplication;
-$app->run();
+
+run();
