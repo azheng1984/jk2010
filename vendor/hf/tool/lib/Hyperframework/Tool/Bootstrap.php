fef3ec2e2f2fcfe98c4538632c89424ed2cf44bc
azheng <none@google.com>
2013-10-29 10:37:30 +0800

    -

--- vendor/hf/tool/lib/Hyperframework/Tool/Bootstrap.php
+++ vendor/hf/tool/lib/Hyperframework/Tool/Bootstrap.php
@@ -1,17 +1,13 @@
 <?php
 namespace Hyperframework\Tool;
-use Hyperframework\Cli;
 
 class Bootstrap {
     public static function run($rootPath) {
-        define(
-            'Hyperframework\Tool\HYPERFRAMEWORK_PATH',
-            dirname($rootPath) . DIRECTORY_SEPARATOR
-        );
-        require HYPERFRAMEWORK_PATH . 'lib' . DIRECTORY_SEPARATOR .
-            'Hyperframework' . DIRECTORY_SEPARATOR . 'ClassLoader.php';
+        require dirname($rootPath) . DIRECTORY_SEPARATOR . 'lib' .
+            DIRECTORY_SEPARATOR . 'Hyperframework' .
+            DIRECTORY_SEPARATOR . 'ClassLoader.php';
         \Hyperframework\ClassLoader::run();
-        Cli\ExceptionHandler::run();
-        Cli\Application::run();
+        \Hyperframework\Cli\ExceptionHandler::run();
+        \Hyperframework\Cli\Application::run();
     }
 }
--- vendor/yxj/lib/Yxj/Bootstrap.php
+++ vendor/yxj/lib/Yxj/Bootstrap.php
@@ -3,7 +3,19 @@ namespace Yxj;
 
 class Bootstrap {
     public static function run($rootPath) {
-        static::initialize();
+        static::initialize($rootPath);
+        static::start();
+    }
+
+    private static function initialize($rootPath) {
+        require $rootPath . 'config' . DIRECTORY_SEPARATOR . 'env.config.php';
+        \Hyperframework\Config::setRootPath($rootPath);
+        require HYPERFRAMEWORK_PATH . 'ClassLoader.php';
+        \Hyperframework\ClassLoader::run();
+        \Hyperframework\ExceptionHandler::run();
+    }
+
+    private static function start() {
         $path = Router::execute();
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['_method'])) {
             $_SERVER['REQUEST_METHOD'] = $_POST['_method'];
@@ -12,13 +24,5 @@ class Bootstrap {
         if ($path !== null) {
             \Hyperframework\Web\Application::run($path);
         }
-    }
-
-    private static function initialize() {
-        require $rootPath . 'config' . DIRECTORY_SEPARATOR . 'env.config.php';
-        \Hyperframework\Config::setRootPath($rootPath);
-        require HYPERFRAMEWORK_PATH . 'ClassLoader.php';
-        \Hyperframework\ClassLoader::run();
-        \Hyperframework\ExceptionHandler::run();
     }
 }
