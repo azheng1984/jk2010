724d3a32b14e187af2b2f6f956aed609e767e731
azheng <none@google.com>
2013-10-30 13:12:15 +0800

    -

--- vendor/hf/tool/cli.php
+++ vendor/hf/tool/cli.php
@@ -5,4 +5,4 @@ namespace Hyperframework\Tool;
 define('Hyperframework\Tool\ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 require ROOT_PATH . 'lib' . DIRECTORY_SEPARATOR . 'Hyperframework' .
     DIRECTORY_SEPARATOR . 'Tool' . DIRECTORY_SEPARATOR . 'Runner.php';
-Runner::run(ROOT_PATH);
+Runner::run();
