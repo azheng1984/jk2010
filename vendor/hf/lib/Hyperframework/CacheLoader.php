a500615737850e855d39dbd17389e06c3ee90246
azheng <none@google.com>
2013-11-05 20:17:31 +0800

    -

--- vendor/hf/lib/Hyperframework/CacheLoader.php
+++ vendor/hf/lib/Hyperframework/CacheLoader.php
@@ -3,11 +3,7 @@ namespace Hyperframework;
 
 class CacheLoader {
     public static function load($pathConfigName, $defaultPath) {
-        return DataLoader::load(
-            'cache',
-            $pathConfigName,
-            $defaultPath,
-            'data' . DIRECTORY_SEPARATOR . 'cache' 
-        );
+        return DataLoader::load('cache', $pathConfigName, $defaultPath,
+            'data' . DIRECTORY_SEPARATOR . 'cache');
     }
 }
