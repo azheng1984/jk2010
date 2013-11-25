eab61bc033a8775bb7e8335a1e5e5aaa1b053269
azheng <none@google.com>
2013-11-25 17:09:15 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/Application.php
+++ vendor/hf/lib/Hyperframework/Web/Application.php
@@ -2,54 +2,33 @@
 namespace Hyperframework\Web;
 
 class Application {
-    private static $instance;
-    private $actionResult;
-    private $isViewEnabled = true;
+    private static $actionResult;
+    private static $isViewEnabled = true;
 
     public static function run($path = null) {
         $pathInfo = PathInfo::get($path);
-        $instance = static::create();
-        $instance->executeAction($pathInfo);
-        $instance->renderView($pathInfo);
-    }
-
-    public static function get() {
-        $result = end(static::$instance);
-        if ($result === false) {
-            throw new \Exception('No application');
-        }
-        return $result;
+        static::executeAction($pathInfo);
+        static::renderView($pathInfo);
     }
 
     public static function reset() {
-        static::$instance = null;
+        static::$actionResult = null;
+        static::$isViewEnabled = true;
     }
 
-    public function enableView() {
-        $this->isViewEnabled = true;
+    public static function enableView() {
+        static::$isViewEnabled = true;
     }
 
-    public function disableView() {
-        $this->isViewEnabled = false;
+    public static function disableView() {
+        static::$isViewEnabled = false;
     }
 
-    public function getActionResult() {
-        return $this->actionResult;
+    public static function getActionResult() {
+        return static::$actionResult;
     }
 
-    protected static function create() {
-        if (static::$instance !== null) {
-            throw new \Exception('Application already exists');
-        }
-        $class = get_called_class();
-        $instance = new $class($name);
-        static::$instances = $instance;
-        return $instance;
-    }
-
-    protected function __construct() {}
-
-    protected function executeAction(
+    protected static function executeAction(
         $pathInfo, $processorClass = 'Hyperframework\Web\ActionProcessor'
     ) {
         $info = null;
@@ -58,10 +37,10 @@ class Application {
             $info['namespace'] = $pathInfo['namespace'];
         }
         $processor = new $processorClass;
-        $this->actionResult = $processor->run($info);
+        static::$actionResult = $processor->run($info);
     }
 
-    protected function renderView(
+    protected static function renderView(
         $pathInfo, $processorClass = 'Hyperframework\Web\ViewProcessor'
     ) {
         if (isset($pathInfo['View']) && $this->isViewEnabled) {
