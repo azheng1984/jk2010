b7c1e2c9e69f7f64bbee8fea88757266741d332e
azheng <none@google.com>
2013-11-25 11:14:50 +0800

    -

--- vendor/hf/lib/Hyperframework/ConfigLoader.php
+++ vendor/hf/lib/Hyperframework/ConfigLoader.php
@@ -7,7 +7,7 @@ class ConfigLoader {
     ) {
         if ($hasEnv) {
             $defaultPath = Config::get(
-                __NAMESPACE__ . '\AppEnv', array('is_nullable' => false)
+                'Hyperframework\AppEnv', array('is_nullable' => false)
             ) . DIRECTORY_SEPARATOR . $defaultPath;
         }
         return DataLoader::load(
