0c324289d8a6afd94510ea6c0fdde123a5ad8808
azheng <none@google.com>
2013-12-18 11:53:29 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/View/Asset/AssetUrl.php
+++ vendor/hf/lib/Hyperframework/Web/View/Asset/AssetUrl.php
@@ -4,15 +4,20 @@ namespace Hyperframework\Web\View\Asset;
 use \Hyperframework\Config;
 
 class AssetUrl {
+    private static $baseUrl;
+
     public static function get($path, $isRelative = false) {
-        $result = $path;
         if ($isRelative === false) {
-           $result = static::appendRoot($path);
+           $path = static::appendRoot($result);
         }
         if (Config::get(__CLASS__ . '\CacheVersionEnabled') === false) {
-            return static::addCacheVersion($path);
+            return static::insertCacheVersion($path, $isRelative);
         }
-        return $result;
+        return $path;
+    }
+
+    public static function setBaseUrl($value) {
+        static::$baseUrl = $value;
     }
 
     protected static function getDefaultRootPath() {
@@ -29,8 +34,8 @@ class AssetUrl {
         return $path;
     }
 
-    private static function addCacheVersion($path) {
-        $cacheVersion = AssetCacheVersion::get($path);
+    private static function insertCacheVersion($path, $isRelative) {
+        $cacheVersion = static::getCacheVersion($path, $isRelative);
         if ($cacheVersion === null) {
             return $path;
         }
@@ -41,5 +46,17 @@ class AssetUrl {
         }
         return dirname($path) . '/' . substr($name, 0, $lastDotPosition)
             . '.' . $cacheVersion . substr($name, $lastDotPosition);
+    }
+
+    private static function getCacheVersion($path, $isRelative) {
+        if ($isRelative === false) {
+            return AssetCacheVersion::get($path);
+        }
+        if (static::$baseUrl === null) {
+            throw new \Exception(
+                'Base url not set when getting relative path cache version'
+            );
+        }
+        return static::getCacheVersion(static::$baseUrl . '/' . $path);
     }
 }
