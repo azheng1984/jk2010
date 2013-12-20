f11a921201e032d1d013e9d7cfc0dc8f361708e6
azheng <none@google.com>
2013-12-20 18:29:46 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/View/AssetPerLoader.php
+++ vendor/hf/lib/Hyperframework/Web/View/AssetPerLoader.php
@@ -5,38 +5,31 @@ class AssetPreloader {
     private static $hasCss;
     private static $hasJs;
 
-    public function hasCss() {
+    public function isCssPreloadEnabled() {
         if (static::$hasCss === null) {
-            static::$hasCss = Config::get(__CLASS__ . '\HasCss') !== false;
+            static::$isCssPreloadEnabled = Config::get(__CLASS__ . '\CssPreloadEnabled') !== false;
         }
         return static::$hasCss;
     }
 
-    public function hasJs() {
+    public function isJsEnabled() {
         if (static::$hasJs === null) {
-            static::$hasJs = Config::get(__CLASS__ . '\HasJs') !== false;
+            static::$is = Config::get(__CLASS__ . '\JsPreloadEnabled') !== false;
         }
         return static::$hasJs;
     }
 
-    public function getCssUrls() {
-        $path = Config::get(
-            __CLASS__ . '\CssPath', array('default' => 'app.css')
-        );
+    public function getCssUrls($path = 'app.css') {
         if (Config::get(__CLASS__ . '\CssManifestEnabled') === true) {
             return CssManifest::getUrls($path);
         }
         return array(CssUrl::get($path));
     }
 
-    public function renderJsLink() {
-        $path = Config::get(
-            __CLASS__ . '\JsPath', array('default' => 'app.js')
-        );
+    public function getJsUrls($path = 'app.js') {
         if (Config::get(__CLASS__ . '\JsManifestEnabled') === true) {
-            JsLink::renderManifest($path);
-            return;
+            return JsManifest::getUrls($path);
         }
-        JsLink::render($path);
+        return array(JsUrl::get($path));
     }
 }
