870f98bda1eb98a187142c455a8530c9a88fa4a1
azheng <none@google.com>
2013-12-26 16:43:03 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/Asset/CssManifest.php
+++ vendor/hf/lib/Hyperframework/Web/Asset/CssManifest.php
@@ -2,18 +2,23 @@
 namespace Hyperframework\Web\Asset;
 
 class CssManifest {
-    //todo 和 asset proxy 统一 asset 定位逻辑
-    public static function getUrls($path, $vendor = null) {
-        $path = DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR
-            . 'css' . DIRECTORY_SEPARATOR . $path;
-        if (Config::get(__CLASS__ . '\CacheEnabled')) {
-            return require static::getCacheFullPath($path, $vendor);
-        }
-        $manifest = require static::getManifestFullPath($path, $vendor);
+    public static function render() {
         //fetch build url may be scan dir like class loader or asset proxy
+        if ($shouldSeparateFiles) {
+        //foreach () {
+        //echo '@import url(/asset/css/)';
+        //}
+        return;
+        }
+        // foreach
+        // require ...;
     }
 
+    //todo 和 asset proxy 统一 asset 定位逻辑
     private static function getFullPath($path, $vendor) {
+        $path = DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR
+            . 'css' . DIRECTORY_SEPARATOR . $path;
+
         $suffix = DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR
             . 'css' . DIRECTORY_SEPARATOR . $path . '.manifest.php';
         $appPath = Config::get(
@@ -26,15 +31,15 @@ class CssManifest {
             . $vendor . DIRECTORY_SEPARATOR . $suffix;
     }
 
-    private static function getCacheFullPath($path, $vendor) {
-        $prefix = Config::get('Hyperframework\AppPath') . DIRECTORY_SEPARATOR
-            . 'data' . DIRECTORY_SEPARATOR . 'cache'
-            . DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR . 'manifest'
-            . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR;
-        if ($vendor !== null) {
-            $prefix .= 'vendor' . DIRECTORY_SEPARATOR . $vendor
-                . DIRECTORY_SEPARATOR;
-        }
-        return $prefix . $path . '.cache.php';
-    }
+//    private static function getCacheFullPath($path, $vendor) {
+//        $prefix = Config::get('Hyperframework\AppPath') . DIRECTORY_SEPARATOR
+//            . 'data' . DIRECTORY_SEPARATOR . 'cache'
+//            . DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR . 'manifest'
+//            . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR;
+//        if ($vendor !== null) {
+//            $prefix .= 'vendor' . DIRECTORY_SEPARATOR . $vendor
+//                . DIRECTORY_SEPARATOR;
+//        }
+//        return $prefix . $path . '.cache.php';
+//    }
 }
