b51092a9eeb228af7d890998e30aa130a12477c8
azheng <none@google.com>
2014-01-08 15:10:22 +0800

    -

--- vendor/yxj/lib/Yxj/Runner.php
+++ vendor/yxj/lib/Yxj/Runner.php
@@ -5,6 +5,10 @@ require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Hyperframework'
     . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Runner.php';
 
 class Runner extends \Hyperframework\Web\Runner {
+    public static function run() {
+        parent::run(HYPERFRAMEWORK_PATH, __NAMESPACE__);
+    }
+
     protected static function getPath() {
         $requestPath = parent::getPath();
         //use custom router
