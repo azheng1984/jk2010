66a6e6663e8b8d442dcfea6b3f59bac15291b29c
azheng <none@google.com>
2014-01-15 14:49:32 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/ErrorApplication.php
+++ vendor/hf/lib/Hyperframework/Web/ErrorApplication.php
@@ -5,10 +5,9 @@ class ErrorApplication {
     private static $previousRequestMethod;
 
     public static function run($statusCode) {
-        static::initialize();
         $path = static::getErrorPath($statusCode);
         if ($path !== null) {
-            static::restartApplication($path);
+            static::runByPath($path);
         }
     }
 
@@ -20,11 +19,6 @@ class ErrorApplication {
         static::$previousRequestMethod = null;
     }
 
-    protected static function initialize() {
-        static::$previousRequestMethod = $_SERVER['REQUEST_METHOD'];
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-    }
-
     protected static function getErrorPath($statusCode) {
         if (strncmp($statusCode, '4', 1) === 0) {
             return '#Error/Client';
@@ -32,7 +26,9 @@ class ErrorApplication {
         return '#Error/Server';
     }
 
-    protected static function restart($path) {
+    protected static function runByPath($path) {
+        static::$previousRequestMethod = $_SERVER['REQUEST_METHOD'];
+        $_SERVER['REQUEST_METHOD'] = 'GET';
         Application::reset();
         try {
             Application::run($path);
