35f5b2a021e76d42421baf50bb3709884e6e442a
azheng <none@google.com>
2014-01-17 17:49:53 +0800

    -

--- vendor/hf/lib/Hyperframework/Logger.php
+++ vendor/hf/lib/Hyperframework/Logger.php
@@ -1,39 +1,51 @@
 <?php
+namespace Hyperframework;
+
 class Logger {
-    const TYPE_TRACE = 0;
-    const TYPE_DEBUG = 1;
-    const TYPE_INFO  = 2;
-    const TYPE_WARN  = 3;
-    const TYPE_ERROR = 4;
+    const TYPE_TRACE = 2;
+    const TYPE_DEBUG = 4;
+    const TYPE_INFO  = 8;
+    const TYPE_WARN  = 16;
+    const TYPE_ERROR = 32;
 
-    public static function trace($message) {
-        static::output(self::TYPE_TRACE, $message);
+    public static function trace($entry) {
+        static::output(self::TYPE_TRACE, $entry);
     }
 
-    public static function debug($message) {
-        static::output(self::TYPE_DEBUG, $message);
+    public static function debug($entry) {
+        static::output(self::TYPE_DEBUG, $entry);
     }
 
-    public static function info($message) {
-        static::output(self::TYPE_INFO, $message);
+    public static function info($entry) {
+        static::output(self::TYPE_INFO, $entry);
     }
 
-    public static function warn($message) {
-        static::output(self::TYPE_WARN, $message);
+    public static function warn($entry) {
+        static::output(self::TYPE_WARN, $entry);
     }
 
-    public static function error($message) {
-        static::output(self::TYPE_ERROR, $message);
+    public static function error($entry) {
+        static::output(self::TYPE_ERROR, $entry);
     }
 
-    protected static function output($type, $message) {
-        $outputLevel = Config::get(
-            __CLASS__, '\OutputLevel', array('default' >= self::TYPE_WARN)
+    protected static function output($type, $entry) {
+        $errorReporting = Config::get(
+            __CLASS__ . '\ErrorReporting',
+            //todo 使用常量会导致 logger 类被提前加载
+            array('default' => self::TYPE_WARN | self::TYPE_ERROR)
         );
-        if ($type >= $outputLevel) {
-            Config::get(
-                __CLASS__, '\Handlers', array('default' >= array('DefaultHandler'))
-            );
+        if ($type & $errorReporting === 0) {
+            return;
         }
+        $delegate = Config::get(__CLASS__, '\Delegate');
+        if ($delegate !== null) {
+            $delegate->output($type, $entry);
+            return;
+        }
+        $path = Config::get(
+            __CLASS__ . '\LogPath',
+            array('default' => APPLICATION_PATH . 'data/log.txt')
+        );
+        file_put_contents($path, $entry, FILE_APPEND | LOCK_EX);
     }
 }
