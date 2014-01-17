e31094305cb6886ce552ba2e7b1cc415bd0ff894
azheng <none@google.com>
2014-01-17 14:31:45 +0800

    -

---
+++ vendor/yxj/config/env/init.php
@@ -0,0 +1,15 @@
+<?php
+//svn ignore
+//ln -s to x/init.php
+
+namespace Yxj;
+
+const Env = 'development';
+const HYPERFRMEWORK_PATH = '/home/azheng/daoxila_www/vendor/hf/lib';
+//require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Hyperframework'
+//    . DIRECTORY_SEPARATOR . 'Config.php';
+//\Hyperframework\Config::set(
+//    ['Hyperframework\ClassLoader\CacheEnabled', false],
+//    ['Hyperframework\Web\PathInfo\CacheEnabled', false],
+//    ['Hyperframework\Web\View\Asset\CacheEnabled', false],
+//);
