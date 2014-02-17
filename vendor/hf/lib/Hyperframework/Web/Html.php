d40781cd51e89c636c03c6c9121f3393af547366
azheng <none@google.com>
2014-02-17 14:59:30 +0800

    -

--- vendor/hf/lib/Hyperframework/Web/Html.php
+++ vendor/hf/lib/Hyperframework/Web/Html.php
@@ -2,21 +2,26 @@
 namespace Hyperframework\Web;
 
 //html helper
-//includes:
+//includes
 //form helper
 //head helper
 //other html tag helper
 
+//Html::beginBindingByInputMapper('article');
+
 class Html {
     private $bindingStack;
 
-    public static function beginBinding($data = array(), $errors = null) {
+    public static function beginBinding($data = null, $errors = null) {
+    }
+
+    public static function beginBindingByInputMapper($name) {
     }
 
     public static function endBinding() {
     }
 
-    public static function beginForm($options) {
+    public static function beginForm($options = null) {
     }
 
     public static function endForm() {
--- vendor/hf/lib/Hyperframework/Web/InputMapper.php
+++ vendor/hf/lib/Hyperframework/Web/InputMapper.php
@@ -1,14 +1,37 @@
 <?php
 namespace Hyperframework\Web;
 
+//new InputMapper(array('#source' => 'GET', '#name' => 'article', '#validation' => ''));
+//$inputMapper = InputMapper::create('basic', array(), 'GET');
+//Html::beginInputMapperBinding('article');
+//$inputMapper = new FormInput(
+//    array('list'),
+//    array('source' => 'GET', 'name' => 'article')
+//);
+
+$someCode = array();
+$articleInputMapper = new InputMapper($config1, $config2);
+$articleInputMapper = InputMapper::getInstance('article');
+InputMapper::configSource();
+Html::beginBindingByInputMapper('article');
+$someCode = array();
+Html::endBinding();
+
 class InputMapper {
-    public function __construct($config) {
-        //config load from config and share with asset/js
-        //config should be shared with client controller - js, not client model
-    }
+    private static $instances = array();
+
+    private function __construct($fieldConfig, $globalConfig = null) {}
 
     public function getResult() {
         //if invalid, throw ValidationException
+    }
+
+    public static function create($fieldConfig, $globalConfig = null) {
+    }
+
+    public static function getInstance($name) {
+        //config load from config and share with asset/js
+        //config should be shared with client controller - js, not client model
     }
 
     public function getInput() {
