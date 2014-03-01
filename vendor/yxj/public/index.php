b3d60c041904c65b3bb55487cf79381c1997d179
azheng <none@gmail.com>
2014-03-01 12:54:19 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -4,6 +4,6 @@ namespace Yxj;
 define(__NAMESPACE__ . '\ROOT_PATH', dirname(__DIR__));
 $config = require ROOT_PATH . DIRECTORY_SEPARATOR . 'config'
     . DIRECTORY_SEPARATOR . 'env' . DIRECTORY_SEPARATOR . 'init.php';
-require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Hyperframework'
-    . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Runner.php';
+require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Web'
+    . DIRECTORY_SEPARATOR . 'Runner.php';
 \Hyperframework\Web\Runner::run(__NAMESPACE__, $config);
