46c25f0adfd79164cbab42a36daca7368c1140de
azheng <none@google.com>
2014-03-03 11:34:24 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -6,4 +6,4 @@ require ROOT_PATH . DIRECTORY_SEPARATOR . 'config'
     . DIRECTORY_SEPARATOR . 'bootstrap.php';
 require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Web'
     . DIRECTORY_SEPARATOR . 'Runner.php';
-\Hyperframework\Web\Runner::run(__NAMESPACE__, $runnerConfig);
+\Hyperframework\Web\Runner::run(__NAMESPACE__, $bootstrapConfig);
