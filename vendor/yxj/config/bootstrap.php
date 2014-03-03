807134d7ba765be8a3d266a8cb8e68a1dd750583
azheng <none@google.com>
2014-03-03 10:51:53 +0800

    -

--- vendor/yxj/config/bootstrap.php
+++ vendor/yxj/config/bootstrap.php
@@ -3,7 +3,7 @@ namespace Yxj;
 
 const Env = 'development';
 const HYPERFRMEWORK_PATH = '/home/azheng/daoxila_www/vendor/hf/lib';
-$envConfig = [
+$runnerConfig = [
     ['Hyperframework\ClassLoader\CacheEnabled', false],
     ['Hyperframework\Web\PathInfo\CacheEnabled', false],
     ['Hyperframework\Web\View\Asset\CacheEnabled', false],
--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -3,7 +3,7 @@ namespace Yxj;
 
 define(__NAMESPACE__ . '\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'config'
-    . DIRECTORY_SEPARATOR . 'env' . DIRECTORY_SEPARATOR . 'init.php';
+    . DIRECTORY_SEPARATOR . 'bootstrap.php';
 require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Web'
     . DIRECTORY_SEPARATOR . 'Runner.php';
-\Hyperframework\Web\Runner::run(__NAMESPACE__, $envConfig);
+\Hyperframework\Web\Runner::run(__NAMESPACE__, $runnerConfig);
