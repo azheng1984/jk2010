8fb4f25147010ceda2f7e414e585a6f9ffc6f0ef
azheng <none@google.com>
2014-03-12 10:27:56 +0800

    -

--- vendor/hf/core/lib/Web/ViewDispatcher.php
+++ vendor/hf/core/lib/Web/ViewDispatcher.php
@@ -2,10 +2,9 @@
 namespace Hyperframework\Web;
 
 class ViewDispatcher {
-    public static function run($mediaType, $pathInfo) {
-        $isMediaTypeExists =
-            in_array($_SERVER['REQUEST_MEDIA_TYPE'], $pathInfo['views']);
-        if ($isMediaTypeExists === false) {
+    public static function run($pathInfo, $mediaType) {
+        $hasView = in_array($_SERVER['REQUEST_MEDIA_TYPE'], $pathInfo['views']);
+        if ($hasView === false) {
             throw new UnsupportedMediaTypeException;
         }
         $class = $pathInfo['namespace'] . '\\' . $mediaType;
