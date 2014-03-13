120fa1f08e92e13370c33e7a60df51637b7bf340
azheng <none@google.com>
2014-03-13 13:46:26 +0800

    -

--- vendor/hf/core/lib/ConfigLoader.php
+++ vendor/hf/core/lib/ConfigLoader.php
@@ -2,24 +2,6 @@
 namespace Hyperframework;
 
 class ConfigLoader extends DataLoader {
-    public static function loadByEnv($defaultPath, $pathConfigName) {
-        return parent::load(
-            static::appendEnvPath($defaultPath), $pathConfigName
-        );
-    }
-
-    protected static function appendEnvPath($defaultPath) {
-        $applicationEnv = Config::get(
-            __NAMESPACE__ . '\ApplicationEnv',
-            array('default' => array('applicaiton_const' => 'ENV'))
-        )
-        if ($applicationEnv === null) {
-            return $defaultPath;
-        }
-        return 'env' . DIRECTORY_SEPARATOR . $applicationEnv
-            . DIRECTORY_SEPARATOR . $defaultPath;
-    }
-
     protected static function getDefaultRootPath() {
         return Config::getApplicationPath() . DIRECTORY_SEPARATOR . 'config';
     }
