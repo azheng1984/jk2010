2aa0cae6e3154a62c7a2fc78912c8bf923bf1ef0
azheng <none@google.com>
2014-03-13 17:40:40 +0800

    -

--- vendor/hf/core/lib/ClassLoader.php
+++ vendor/hf/core/lib/ClassLoader.php
@@ -2,34 +2,42 @@
 namespace Hyperframework;
 
 class ClassLoader {
-    private static $cacheEnabled;
+    private static $isCacheEnabled;
+    private static $config;
+    private static $cache;
 
     public static function run() {
         spl_autoload_register(array(__CLASS__, 'load'));
-        static::$cacheEnabled = Config::get(__NAMESPACE__ . '\CacheEnabled');
-        $classLocator = Config::get(__NAMESPACE__ . '\ClassLocator', function() {
-            return __DIR__ . DIRECTORY_SEPARATOR . 'ClassLocator.php';
-        });
-        if (static::$cacheEnabled === false) {
-            require ;
-        }
-    }
+        self::$isCacheEnabled = Config::get(__NAMESPACE__ . '\CacheEnabled');
+   }
 
     public static function load($name) {
         require static::getPath($name);
     }
 
-    protected static function getConfig() {
-        ConfigLoader::get('class_loader', __CLASS__ . '\CachePath');
+    private static function getPath($name) {
+        if (static::$isCacheEnabled) {
+            return static::getPathByCache($name);
+        }
+        return static::getPathByConfig($name);
     }
 
-    private static function getPath($name) {
-        if (static::$cacheEnabled) {
-            return static::getPathFromCache($name);
-        }
-        return ClassLocator::getPath($name);
+    private static function getPathByCache($name) {
+    }
+
+    private static function getPathByConfig($name) {
+    }
+
+//    protected static function loadClassLocator() {
+//        require __DIR__ . DIRECTORY_SEPARATOR . 'ClassLocator.php';
+//        return __NAMESPACE__ . '\ClassLocator';
+//    }
+
+    protected static function loadConfig() {
+        return ConfigLoader::get('class_loader', __CLASS__ . '\ConfigPath');
     }
 
     protected static function loadCache() {
+        return CacheLoader::get('class_loader', __CLASS__ . '\CachePath');
     }
 }
