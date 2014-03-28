601d85de558e682859cf7272d8f6cd806050443c
azheng <none@google.com>
2014-03-28 18:05:09 +0800

    -

--- vendor/hf/core/lib/ClassLoaderCacheBuilder.php
+++ vendor/hf/core/lib/ClassLoaderCacheBuilder.php
@@ -1,9 +1,39 @@
 <?php
+namespace Hyperframework;
+
 class ClassLoaderCacheBuilder {
-    public static function build($config, $isOneToManyMappingAllowed = true) {
+    public static function build($config) {
         $cache = array();
         foreach ($config as $namespace => $path) {
             $segments = explode('\\', $namespace);
+            $parent =& $cache;
+            $amount = count($segments);
+            $index = 0;
+            foreach ($segments as $segment) {
+                ++$index;
+                if (isset($parent[$segment]) === false) {
+                    if ($index !== $amount) {
+                        $parent[$segment] = array();
+                        $parent =& $parent[$segment];
+                        continue;
+                    }
+                    $parent[$segment] = $path;
+                    break;
+                }
+                if ($index === $amount) {
+                    if (is_string($parent[$segment])) {
+                        $parent[$segment] = array($parent[$segment], $path);
+                        continue;
+                    }
+                    $parent[$segment][] = $path;
+                    break;
+                }
+                if (is_string($parent[$segment])) {
+                    $parent[$segment] = array($parent[$segment]);
+                }
+                $parent =& $parent[$segment]; 
+            }
         }
+        return $cache;
     }
 }
