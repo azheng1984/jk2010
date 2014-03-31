962ada4464c2194f208e2fc42cd5eced6ffd3ede
azheng <none@google.com>
2014-03-31 09:49:34 +0800

    -

--- vendor/yxj/config/init.php
+++ vendor/yxj/config/init.php
@@ -4,7 +4,7 @@ namespace Yxj;
 const HYPERFRMEWORK_PATH = '/home/azheng/daoxila_www/vendor/hf/core/lib';
 
 return array(
-    ['Hyperframework\ClassLoader\CacheEnabled', false],
-    ['Hyperframework\Web\PathInfo\CacheEnabled', false],
-    ['Hyperframework\Web\View\Asset\CacheEnabled', false],
+    'Hyperframework\ClassLoader\CacheEnabled' => false,
+    'Hyperframework\Web\PathInfo\CacheEnabled' => false,
+    'Hyperframework\Web\View\Asset\CacheEnabled' => false,
 );
