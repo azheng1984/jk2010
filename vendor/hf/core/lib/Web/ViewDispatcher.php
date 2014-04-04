83b930d30646847b87baaf75d086eb56a451df87
azheng <none@google.com>
2014-04-04 13:06:24 +0800

    -

--- vendor/hf/core/lib/Web/ViewDispatcher.php
+++ vendor/hf/core/lib/Web/ViewDispatcher.php
@@ -3,20 +3,19 @@ namespace Hyperframework\Web;
 
 class ViewDispatcher {
     public static function run($pathInfo) {
-        static::prepare($pathInfo);
+        static::initialize($pathInfo);
         static::dispatch($pathInfo);
     }
 
-    protected static function prepare($pathInfo) {
+    protected static function initailize($pathInfo) {
         if (isset($pathInfo['views']) === false) {
             throw new NotAcceptableException;
         }
         $views = $pathInfo['views'];
+        if (is_string($views)) {
+            $views = array($views);
+        }
         if ($_SERVER['REQUEST_MEDIA_TYPE'] === null) {
-            if (is_string($views)) {
-                $_SERVER['REQUEST_MEDIA_TYPE'] = $views;
-                return;
-            }
             $_SERVER['REQUEST_MEDIA_TYPE'] = $views[0];
         } elseif (in_array($_SERVER['REQUEST_MEDIA_TYPE'], $views) === false) {
             throw new NotAcceptableException;
