073f4ffd9ad03f9cf33820b30a71f8fd3a672ac7
azheng <none@google.com>
2014-04-22 12:44:35 +0800

    -

--- vendor/yxj/lib/Runner.php
+++ vendor/yxj/lib/Runner.php
@@ -1,8 +1,5 @@
 <?php
 namespace Yxj;
 
-require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Web'
-    . DIRECTORY_SEPARATOR . 'Runner.php';
- 
 class Runner extends \Hyperframework\Web\Runner {
 }
