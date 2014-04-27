819abb92b1e7528499bac0ed1429feaf59f8eadc
azheng <none@gmail.com>
2014-04-27 21:46:28 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -3,7 +3,7 @@ namespace Yxj;
 
 define('Yxj\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'
-    . DIRECTORY_SEPARATOR . 'hyperframework_core'
+    . DIRECTORY_SEPARATOR . 'hyperframework' . DIRECTORY_SEPARATOR . 'core'
     . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Runner.php';
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'lib'
     . DIRECTORY_SEPARATOR . 'Runner.php';
