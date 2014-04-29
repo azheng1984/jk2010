163a2db338e1ffed386af0c853f080f4f41e8249
azheng <none@gmail.com>
2014-04-29 22:58:15 +0800

    -

--- vendor/yxj/config/asset/manifest.php
+++ vendor/yxj/config/asset/manifest.php
@@ -1,3 +1,5 @@
-//ignore_path
-//include_path
-//virtual_path
+<?php
+return array(
+    'application.css' => 12, // /assets/css/application.12.css
+    'logo.png' => 0, // /assets/images/logo.png
+);
