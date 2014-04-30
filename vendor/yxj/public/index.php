22aacc015a703917b4bc25ba207572729adf832a
azheng <none@google.com>
2014-04-30 10:26:34 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -1,7 +1,7 @@
 <?php
-namespace ProjectName;
+namespace ProjectNamespace;
 
-define('ProjectName\ROOT_PATH', dirname(__DIR__));
+define('ProjectNamespace\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'
     . DIRECTORY_SEPARATOR . 'hyperframework' . DIRECTORY_SEPARATOR . 'core'
     . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Initializer.php';
