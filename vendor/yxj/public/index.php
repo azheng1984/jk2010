237df548004b5358b48e2110ceff7f6255c8c25c
azheng <none@google.com>
2014-04-30 10:21:54 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -1,12 +1,9 @@
 <?php
 namespace ProjectName;
 
-use Hyperframework\Web\Initializer;
-use Hyperframework\Web\Runner;
-
 define('ProjectName\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'
     . DIRECTORY_SEPARATOR . 'hyperframework' . DIRECTORY_SEPARATOR . 'core'
     . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Initializer.php';
-Initializer::run(__NAMESPACE__, ROOT_PATH);
-Runner::run();
+\Hyperframework\Web\Initializer::run(__NAMESPACE__, ROOT_PATH);
+\Hyperframework\Web\Runner::run();
