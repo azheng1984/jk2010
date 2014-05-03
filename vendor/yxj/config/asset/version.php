7a50600b9b470e07d031f106b4e08811b70b7cd9
azheng <none@gmail.com>
2014-05-03 20:14:20 +0800

    -

--- vendor/yxj/config/asset/version.php
+++ vendor/yxj/config/asset/version.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'application.css' => 12, // /assets/css/application.12.css
+    'application.css' => 12, // /assets/css/application-12.css
     'logo.png' => 0, // /assets/images/logo.png
 );
--- vendor/yxj/data/user_and_article_amount.php
+++
@@ -1,2 +0,0 @@
-<?php
-return array(0, 0);
