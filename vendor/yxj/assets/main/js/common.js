6b786622f6117f48dc0fe186a0f35ec5af21e650
azheng <none@gmail.com>
2014-05-10 09:16:09 +0800

    -

--- vendor/yxj/assets/main/js/common.js
+++ vendor/yxj/assets/main/js/common.js
@@ -1,5 +1,5 @@
 //var list = list.__inline_file('');
-var data = __inline_file('/cache/home/hi');
+var data = __inline_file('/assets/home/hi');
 var obj = {
     'hi': __cache_url('hello/hi.png'),
     'main': 'hello'
