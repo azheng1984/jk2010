b7027fd041d51a8c6bd94c8362f37e6459598aff
azheng <none@google.com>
2014-05-14 18:28:15 +0800

    -

--- vendor/hf/core/lib/Web/AssetProxy.php
+++ vendor/hf/core/lib/Web/AssetProxy.php
@@ -3,73 +3,77 @@ namespace Hyperframework\Web;
 
 class AssetProxy {
     public static function run() {
-        header('Cache-Control: private, max-age=0, must-revalidate');
         $url = $_SERVER['REQUEST_URI'];
-        $assetCacheVersionEnabled =
-            Config::get(__NAMESPACE__ . '\AssetCacheVersionEnabled');
-        if ($assetCacheVersionEnabled !== false) {
+        $isAssetCacheVersionEnabled = Config::get(
+            'hyperframework.web.enable_asset_cache_version'
+        ) !== false;
+        if ($isAssetCacheVersionEnabled) {
             $segments = explode('.', $url);
-            if (count($segments === 1) {
-                $segments[] = explode('-', $url);
-                array_pop($segments);
-                $url = implode('-', $segments);
-            } else {
-                $extension = array_pop($segments);
-                array_pop($segments);
-                $url = implode('.', $segments) . '.' . $extension;
+            $amount = count($segments);
+            if ($amount < 3) {
+                throw new Exception;
             }
-            //no expire
-        } else {
-            header('Cache-Control: private, max-age=0, must-revalidate');
+            //todo 检查版本，如果不匹配，抛出 not found
+            unset($segments[$amount - 2]);
+            if (self::isValidFilterType($segments[$amount - 1])) {
+                throw new NotFoundException;
+            }
+            $path = implode('.', $segments);
         }
-        $pathPrefix = array_shift($segments);
-        if (static::startsWith($uri, '/assets/js/')) {
-            $assetPath = \Hyperframework\Config::get('Hyperframework\AppPath');
-            //$tmp = explode('/', $filePath);
-            //array_pop($tmp);
-            //$dirName = implode('/', $tmp);
-            $search = $assetPath . $pathPrefix . '.' . $segments[0];
-            $result = glob($search . '*.js');
-            if (count($result) === 0) {
-                throw new Exceptions\NotFoundException;
-                return;
-            }
-            if ($result[0] === $search) {
-                echo file_get_contents(\Hyperframework\Config::get('Hyperframework\AppPath') . $uri);
-                return;
-            }
-            $suffix = substr($result[0], strlen($search));
-            foreach (explode('.', $suffix) as $segment) {
-                if ($segment === 'php') {
-                    ob_start();
-                    require $result[0];
-                    $js = ob_get_contents();
-                    ob_clean();
-                    echo $js;
-                }
-            }
-        } else {
-            static::renderVendor($uri);
-        }
-        //Todo call pipline processor by extension & config
+        $file = self::getFile($path);
+        static::render($file);
     }
 
-    private static function renderVendor($uri) {
-        //abc-12.3.1-1234.js
-        //abc-11.js.gz
-        //abc-12.js
-        //abc-12.js.gz
-        //../tv/css/abc.css.less.php
-        //export => /tv/css/abc.css => merge + import => deploy
-        //=> /tv/css/abc.css
-        if (strpos($uri, '/js/tv/') === 0) {
-            $path = \Hyperframework\Config::get('Hyperframework\AppPath')
-                . '/asset/vendor/tv/public' . $uri;
-            if (file_exists($path)) {
-                require $path;
-                return;
+    private static function searchFile($path) {
+        $segments = explode('/', $path);
+        $fileName = array_pop($segments);
+        $dirName = implode(DIRECTORY_SEPARATOR, $segments);
+        $rootPaths = self::getIncludePath();
+        $ignorePaths = self::getIgnorePath();
+        foreach ($rootPaths as $rootPath) {
+            if (is_dir($rootPath . $dirName)) {
+                $fullPath = $rootPath . $dirName
+                    . DIRECTORY_SEPARATOR . $fileName;
+                $files = glob($fullPath . '*');
+                foreach ($files as $file) {
+                    if (self::isExcluded($file))
+                    $suffix = substr($file, strlen($fullPath) - 1);
+                    if ($suffix === '') {
+                        return $file;
+                    }
+                    $filterTypes = explode('.', $suffix);
+                    foreach ($types as $type) {
+                        if (static::isValidFilterSuffix($type) === false) {
+                            break;
+                        }
+                    }
+                    return $file;
+                }
             }
         }
-        throw new Exceptions\NotFoundException;
+        throw new NotFoundException;
+    }
+
+    private static function render($path) {
+        $segments = explode('.', $path);
+        $output = file_get_contents($path);
+        for (;;) {
+            $filterType = array_pop($path);
+            if (self::isValidFilterType($fileType) === false) {
+                return;
+            }
+            $content = self::filte($content, $fileType);
+        }
+    }
+
+    private static function filte($content, $fileType) {
+        if ($fileType === 'php') {
+            ob_start();
+            eval('?>' . $content);
+            $result = ob_get_contents();
+            ob_end_clean();
+            return $result;
+        }
+        return $content;
     }
 }
