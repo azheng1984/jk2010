110137ffc64971f1dbfa48100d80e0bbdadda751
azheng <none@google.com>
2014-05-16 09:18:59 +0800

    -

---
+++ vendor/hf/core/lib/Web/AssetCachePathPrefix.php
@@ -0,0 +1,19 @@
+<?php
+namespace Hyperframework\Web;
+
+class AssetCachePathPrefix {
+    private static $value;
+
+    public static function get() {
+        if (self::$value === null) {
+            return self::$value;
+        }
+        self::$value = Config::get(
+            'hyperframework.web.asset_cache_path_prefix'
+        );
+        if (self::$value === null) {
+            self::$value = '/cache/';
+        }
+        return self::$value;
+    }
+}
