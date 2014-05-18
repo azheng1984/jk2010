10cddedc59d2192a7fd009c672f8c0a1f17bac02
azheng <none@gmail.com>
2014-05-18 23:01:45 +0800

    -

--- vendor/yxj/lib/App/Articles/Action.php
+++ vendor/yxj/lib/App/Articles/Action.php
@@ -2,6 +2,9 @@
 namespace Yxj\App\Articles\Item;
 
 class Action extends \Yxj\Actions\ArticleAction {
+    public function before() {
+    }
+
     public function delete() {
         //bind id
         DbArticle::delete($articleId);
---
+++ vendor/yxj/lib/App/Articles/Xml.php
@@ -0,0 +1,16 @@
+<?php
+namespace Yxj\App\Article;
+
+class Html {
+    public function render() {
+        //render header
+        //render status || content
+        //render footer
+    }
+
+    public function renderStatus() {
+    }
+
+    public function renderContent() {
+    }
+}
