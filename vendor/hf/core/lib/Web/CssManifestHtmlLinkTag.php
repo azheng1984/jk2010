29bcd68a9fe2dcdf75e1d5409089a468a2162e2e
azheng <none@google.com>
2014-05-20 18:47:07 +0800

    -

--- vendor/hf/core/lib/Web/CssManifestHtmlLinkTag.php
+++ vendor/hf/core/lib/Web/CssManifestHtmlLinkTag.php
@@ -4,18 +4,21 @@ namespace Hyperframework\Web;
 use Hyperframework\Config;
 
 class CssManifestHtmlLinkTag {
+    //main.css.manifest
     public static function render($path, $media = null) {
         if (Config::get('hyperframework.web.concatenate_assets') === true) {
             self::renderItem($path, $media);
             return;
         }
-        $manifest = AssetManifest($path);
-        foreach ($manifest->getPaths($path) as $path) {
-            self::renderItem($path, $media);
+        $manifest = new AssetManifest($path);
+        $manifest->getConcatenatedContent();
+        $manifest->getCacheUrl();
+        foreach ($manifest->getInnerCacheUrls() as $url) {
+            self::renderItem($url, $media);
         }
     }
 
-    private static function renderItem($path, $media) {
+    private static function renderItem($url, $media) {
         echo '<link type="text/css" rel="stylesheet" href="',
             AssetCacheUrl::get($path), '"';
         if ($media !== null) {
