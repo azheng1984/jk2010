73fbd9f689f2c1abb3f0bb7d3db8dfb319add3a5
azheng <none@gmail.com>
2014-05-22 23:31:29 +0800

    -

--- vendor/yxj/assets/main.css.manifest.php
+++ vendor/yxj/assets/main.css.manifest.php
@@ -1,9 +1,9 @@
-main/css/
-dream/up.css.php
-vendor/css/
+main/css/.  #no recursive
+dream/up.css .php
+vendor/css  #recursvie by default
 project//:public/hello_world.css
-home/screen.css.sass
-test/common.css.manifest
+home/screen.css .sass
+test/common.css .manifest
 /vendor/yui/asset/css/
 /vendor/test/all.css
 //c:\windows
