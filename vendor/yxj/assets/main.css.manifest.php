4be9b4e234431757865d2824a45bc12f5baacb80
azheng <none@google.com>
2014-05-23 11:43:47 +0800

    -

--- vendor/yxj/assets/main.css.manifest.php
+++ vendor/yxj/assets/main.css.manifest.php
@@ -1,12 +1,18 @@
-main/css/.  #no recursive
+./main/css/. #no recursive
 dream/up.css.php
 vendor/css  #recursvie by default
 home/screen.css.sass
 test/common.css.manifest.php
 /vendor/yui/asset/css
 /vendor/test/all.css
-//c:\windows
+//c:\windows\.
 //main.css
+vendor/css
+#//require_css dir
+#//require_js dir
+
 <?php
+//css.manifest
 //可以通过 php 生成 manifest 这样就可以支持按照后缀过滤文件了
-Config::get(''); ?>
+//Config::get('');
+echo AssetMinifestFileFilter::execute('js', 'js');
