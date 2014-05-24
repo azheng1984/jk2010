7e423f6a4b657f776fa1f2bbbc7869c0563b2d88
azheng <none@gmail.com>
2014-05-24 15:39:39 +0800

    -

---
+++ vendor/hf/website_skeleton/cli.php
@@ -0,0 +1,8 @@
+#!/usr/bin/env php
+<?php
+namespace Hyperframework\Tool;
+
+define(__NAMESPACE__ . '\ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+require ROOT_PATH . 'lib' . DIRECTORY_SEPARATOR . 'Hyperframework'
+    . DIRECTORY_SEPARATOR . 'Tool' . DIRECTORY_SEPARATOR . 'Runner.php';
+Runner::run();
