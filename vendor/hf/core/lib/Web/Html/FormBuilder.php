17180a74c7ad46ac1e2705c6cbe38c1f7a9666c0
azheng <none@google.com>
2014-06-04 18:24:41 +0800

    -

--- vendor/hf/core/lib/Web/Html/FormBuilder.php
+++ vendor/hf/core/lib/Web/Html/FormBuilder.php
@@ -1,40 +1,7 @@
 <?php
 namespace Hyperframework\Web\Html;
-
-class FormBuilder {
-    public static function render($configs/*, ...*/) {
-    }
-}
-
-FormFilter::render('product');
-//form rendering definition
-array(
-    'category' => array(
-        'id',
-        //rendering
-//        'type' => 'email',
-        'label' => '分类',
-        //validation
-        //'format' => 'email',
-        'default' => '不限'
-    ),
-    'submit' => array(
-        'value' => '提交',
-    ),
-)
-
-//validation
-array(
-    'category' => array(
-        'max_length' => '100',
-//      'min_length' => '20',
-        'required' => true,
-    )
-);
-
-//other input filter
-
-FormBuilder::render(array(
+/*
+ *array(
     'data' => $_GET,
     'name="" action="/product"',
     'method' => 'GET',
@@ -60,85 +27,10 @@ FormBuilder::render(array(
             'value' => '提交'
         ),
     ),
-));
-
-array('id & name' => 'category', 'class="main"');
-
-//$f->render('category');
-
-$form = new FormHelper($data);
-$form = new FormBuilder(array(
-    'data' => $data,
-    'method' => 'GET',
-    'input_filter_configs' => 'product',
-));
-
-try {
-    FormFilter::execute('product', array(
-        'check_patterns' => true,
-        'ignored_patterns' => 'category',
-        'permitted_fields' => array('category', 'content'),
-    ));
-    Validator::execute('', $data);
-    $product = InputFilter::execute(
-    );
-} catch (ValidationException $ex) {
-}
-
-InputFilterCollection::execute(
-    new FormFilter('product'),
-    new InputFilter(array(
-        '' => '',
-        '' => ''
-    ))
 );
-
-class ProductFormBuilder {
-    public function getRenderingConfig() {
-    }
-
-    public function getValidationConfig() {
-    }
-}
-
-$product = InputFilter::execute(
-    $config, ProductForm::getValidationConfig()
-);
-
-InputFilter::execute($config);
-
-//type="email"
-$product = InputFilter::execute(array(
-    'data' => $_GET,
-    'validation_config_name' => 'product',
-    'extra_fields' => array()
-));
-
-//min max require
-
-try {
-    $product = $filter->get('title', 'category');
-}
-
-$form = new Form(array(
-    'validation_configs' => 'product'
-    'data' => $data
-));
-
-new Form($data);
-
-//hyperframework.web.html.form.html5 = true
-//hyperframework.web.html.form.has_id = true
-
+ */
 class FormBuilder {
-    public static function render($options) {
-        if (is_string($options)) {
-            $options = array('config_name' => $options);
-        }
+    public static function render($configs/*, ...*/) {
     }
 }
 
-FormBuilder::render('rendering_configs', 'validation_configs', $data);
-FormFilter::execute('validation_configs', 'inclusion_fields or rendering_configs', 'request_method');
-// rendering_configs validation_configs request_method => form builder configs
-// 1. 是否需要使用 inclusion fields
--- vendor/hf/core/lib/Web/Html/FormFilter.php
+++ vendor/hf/core/lib/Web/Html/FormFilter.php
@@ -3,15 +3,3 @@ class FormFilter {
     public static function execute($configs/*, ...*/) {
     }
 }
-
-try {
-    $data = FormFilter::execute('product', array(
-        'permitted_fields' => array(
-            'category' => array('pattern' => ''), 'content'
-        ),
-//        'validate_patterns' => true,
-//        'patterns' => 'category',
-    ));
-    //equals to InputFilter::execute($configs);
-} catch (ValidationException) {
-}
