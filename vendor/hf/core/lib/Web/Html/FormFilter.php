f6b137d13db36a77b5c5887d74ed86e601ed7354
azheng <none@google.com>
2014-06-04 10:11:05 +0800

    -

--- vendor/hf/core/lib/Web/Html/FormFilter.php
+++ vendor/hf/core/lib/Web/Html/FormFilter.php
@@ -3,12 +3,13 @@ class FormFilter {
     public static function execute($configs/*, ...*/) {
     }
 }
+
 try {
     $data = FormFilter::execute('product', array(
         'permitted_fields' => array(
             'category' => array('pattern' => ''), 'content'
         ),
-        'validate_patterns' => true,
+//        'validate_patterns' => true,
 //        'patterns' => 'category',
     ));
     //equals to InputFilter::execute($configs);
