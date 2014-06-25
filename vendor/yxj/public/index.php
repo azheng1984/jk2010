9c44b1f87264fcc7da8ab6080e8a3a91f7b44e25
azheng <your_email@youremail.com>
2014-06-25 17:40:01 +0800

    -

--- vendor/yxj/public/index.php
+++ vendor/yxj/public/index.php
@@ -1,9 +1,12 @@
 <?php
 namespace ProjectNamespace;
 
+use Hyperframework\Web\EnvironmentBuilder;
+use Hyperframework\Web\Runner;
+
 define('ProjectNamespace\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'
     . DIRECTORY_SEPARATOR . 'hyperframework' . DIRECTORY_SEPARATOR . 'core'
     . DIRECTORY_SEPARATOR . 'Web' . DIRECTORY_SEPARATOR . 'Initializer.php';
-\Hyperframework\Web\EnvironmentBuilder::run(__NAMESPACE__, ROOT_PATH);
-\Hyperframework\Web\Runner::run();
+EnvironmentBuilder::run(__NAMESPACE__, ROOT_PATH);
+Runner::run();
