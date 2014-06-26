14d3b18544c0ff2ac0cc2fab533cafddd4477a27
azheng <your_email@youremail.com>
2014-06-26 12:31:39 +0800

    -

--- vendor/www/config/init_const.php
+++ vendor/www/config/init_const.php
@@ -1,6 +1,5 @@
 <?php
 namespace Hft;
 
-define('HYPERFRAMEWORK_PATH', dirname(ROOT_PATH)
-    . DIRECTORY_SEPARATOR .'hf' . DIRECTORY_SEPARATOR
-    . 'core' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Web');
+define('HYPERFRAMEWORK_PATH', dirname(ROOT_PATH) . DIRECTORY_SEPARATOR .'hf'
+    . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'lib');
--- vendor/www/public/index.php
+++ vendor/www/public/index.php
@@ -6,7 +6,8 @@ use Hyperframework\Web\Runner;
 
 define('Hft\ROOT_PATH', dirname(__DIR__));
 require ROOT_PATH . '/config/init_const.php';
-require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'EnvironmentBuilder.php';
+require HYPERFRAMEWORK_PATH . DIRECTORY_SEPARATOR . 'Web'
+    . DIRECTORY_SEPARATOR . 'EnvironmentBuilder.php';
 EnvironmentBuilder::run(__NAMESPACE__, ROOT_PATH);
 echo 'hi';
 exit;
