1ae39e430adf1539ebc61107cefe8656a6aad47c
azheng <your_email@youremail.com>
2014-07-03 11:25:23 +0800

    -

--- vendor/hf/core/lib/Db/DbModel.php
+++ vendor/hf/core/lib/Db/DbModel.php
@@ -3,21 +3,23 @@ namespace Hyperframework\Db;
 
 class DbModel {
     public static function getRowById($id, $selector = '*') {
-        DbClient::getRowById(static::getTableName(), $id, $selector);
+        return DbClient::getRowById(static::getTableName(), $id, $selector);
     }
 
     public static function save(&$row, $options = null) {
-        DbSaveCommand::save(static::getTableName(), $row, $options);
+        return DbSaveCommand::save(static::getTableName(), $row, $options);
     }
 
     public static function delete($where/*, $mixed, ...*/) {
         $args = func_get_args();
         array_unshift($args, static::getTableName());
-        call_user_func_array('Hyperframework\Db\DbClient::delete', $args);
+        return call_user_func_array(
+            'Hyperframework\Db\DbClient::delete', $args
+        );
     }
 
     public static function deleteById($id) {
-        DbClient::deleteById(static::getTableName(), $id);
+        return DbClient::deleteById(static::getTableName(), $id);
     }
 
     protected static function getTableName() {
