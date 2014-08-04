0f75102f60eb4c13dbd7371485bd52baa8b42b99
Your Name <you@example.com>
2014-08-04 13:26:24 +0800

    -

--- vendor/hf/core/lib/Web/AssetUrl.php
+++ vendor/hf/core/lib/Web/AssetUrl.php
@@ -4,11 +4,22 @@ namespace Hyperframework\Web;
 use Hyperframework\Config;
 
 class AssetUrl {
-    public static function get($path) {
-        $prefix = Config::get('hyperframework.asset_url_prefix');
-        if ($prefix !== null) {
-            return $prefix . $path;
+    public function get($path) {
+        if (Config::get(
+            'hyperframework.asset_cache.enable_versioning') !== false
+        ) {
+            $version = AssetCacheVersion::get($path);
+            $segments = explode('.', $path);
+            if (count($segments) === 1) {
+                $path .= '-' . $version;
+            } else {
+                $lastSegment = array_pop($segments);
+                array_push($segments, $version);
+                array_push($segments, $lastSegment);
+                $result = implode('.', $segments);
+            }
         }
-        return $path;
+        $path = Config::get('hyperframework.asset_url_prefix')
+            . AssetCachePathPrefix::get() . $path;
     }
 }
