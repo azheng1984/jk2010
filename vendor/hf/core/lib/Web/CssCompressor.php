fdade056c03ae315afc255c439a6a1620162a085
Your Name <you@example.com>
2014-08-04 11:51:45 +0800

    -

--- vendor/hf/core/lib/Web/CssCompressor.php
+++ vendor/hf/core/lib/Web/CssCompressor.php
@@ -3,7 +3,24 @@ namespace Hyperframework\Web;
 
 class CssCompressor {
     public static function process($content) {
+        $ds = array(
+            0 => array('pipe', 'r'),
+            1 => array('pipe', 'w'),
+            2 => array('pipe', 'w'),
+        );
+        $rc = proc_open('cleancss', $ds, $p);
+        fwrite($p[0], $content);
+        fclose($p[0]);
+        $result = stream_get_contents($p[1]);
+        fclose($p[1]);
+        $err = stream_get_contents($p[2]);
+        fclose($p[2]);
+        echo $err;
+        //var_dump(proc_close($rc));
+        return $result;
+
+        $content = str_replace('"', '\\"', $content);
+        return exec('echo "' . $content . '" | cleancss');
         //默认使用 nodejs => clean-css
-        return $content;
     }
 }
