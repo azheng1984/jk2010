5a8d16bc8aad5b52dd48934b5f1e79bca4e8931a
Your Name <you@example.com>
2014-08-04 22:52:44 +0800

    -

--- vendor/hf/core/lib/Web/Build/Runner.php
+++ vendor/hf/core/lib/Web/Build/Runner.php
@@ -7,15 +7,67 @@ use Hyperframework\Cli\ExceptionHandler;
 class Runner {
     public static function run($rootNamespace, $rootPath) {
         static::initialize($rootNamespace, $rootPath);
-        //read option
-        //class_loader cache
         //path_info cache
-        //asset cache
-        self::buildPathInfoCache();
+        //preprocess composer class_loader cache
+        //generate asset cache
+        self::buildPathInfoCache('App');
+        self::buildPathInfoCache('ErrorApp');
     }
 
-    private static function buildPathInfoCache() {
-        //$namespace = $namespace;
+    private static function buildPathInfoCache($type) {
+        $root = Hyeprframework\APP_ROOT_PATH
+            . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . $type;
+        $pathInfo = array();
+
+        $scanner = new DirectoryScanner(function($path) use (&$pathInfo){
+            $name = basename($path, $relativePath);
+            if ($name === 'Action') {
+                ActionInfoBuilder::($relativePath, $pathInfo[$path]);
+            } else {
+                ViewInfoBuilder::($relativePath, $pathInfo[$path]);
+            }
+        }, function($path, $relativePath) use (&$pathInfo) {
+            $name = basename($path);
+            if ($name === 'Action') {
+            } else {
+            }
+        });
+        $scanner->run();
+    }
+
+    private static function get($path, $folder, &$pathInfo) {
+        foreach(scandir($folder) as $entry) {
+            if ($entry === '.'
+                || $entry === '..'
+            ) {
+                continue;
+            }
+            if (is_dir($folder . DIRECTORY_SEPARATOR . $entry)) {
+                self::get($folder);
+            }
+            $name = ClassRecognizer::getName($entry);
+            if ($name === null) {
+                continue;
+            }
+            if ($name === 'Action') {
+                ActionInfoBuilder::run($namespace . '\\' . $name, $pathInfo);
+            } else {
+                $viewTypes[] = $name;
+            }
+        }
+        if (count($viewTypes) !== 0) {
+            $viewOrder = null;
+            if (isset($options['view_order']) !== false) {
+                $viewOrder = $options['view_order'];
+            }
+            ViewInfoBuilder::run(
+                $namespace, $viewTypes, $viewOrder, $pathInfo
+            );
+        }
+        $pathInfo['namespace'] = $namespace;
+    }
+
+    private static function convertToPath($namespace) {
     }
 
     protected static function initialize($rootNamespace, $rootPath) {
