89b87eb508a0c379fab004d5017aff906b79280d
Your Name <you@example.com>
2014-08-06 10:29:57 +0800

    -

--- vendor/hf/core/lib/ClassLoaderCacheBuilder.php
+++ vendor/hf/core/lib/ClassLoaderCacheBuilder.php
@@ -2,6 +2,8 @@
 namespace Hyperframework;
 
 class ClassLoaderCacheBuilder {
+    private static $cache = array();
+
     public static function run() {
         $folder = \Hyperframework\APP_ROOT_PATH . DIRECTORY_SEPARATOR
             . 'vendor' . DIRECTORY_SEPARATOR . 'composer';
@@ -13,55 +15,72 @@ class ClassLoaderCacheBuilder {
                 $psr4[$key] = array();
             }
             foreach ($item as $i) {
+                //do: 处理下划线
                 $psr4[$key][] =realpath($i . DIRECTORY_SEPARATOR . substr(
                     str_replace("\\", DIRECTORY_SEPARATOR, $key), 0, strlen($key) -1));
             }
         }
-        $cache = array();
         foreach ($psr4 as $namespace => &$paths) {
             foreach ($paths as $path) {
-                $segments = explode('\\', $namespace);
-                array_pop($segments);
-                $parent =& $cache;
-                $amount = count($segments);
-                $index = 0;
-                foreach ($segments as $segment) {
-                    ++$index;
-                    if (isset($parent[$segment]) === false) {
-                        if ($index !== $amount) {
-                            $parent[$segment] = array();
-                            $parent =& $parent[$segment];
-                            continue;
-                        }
-                        $parent[$segment] = $path;
-                        break;
-                    }
-                    if ($index === $amount) {
-                        if (is_string($parent[$segment])) {
-                            //confilict!
-                            $parent[$segment] = self::merge(
-                                $parent[$segment], $parent[$segment], $path
-                            );
-                            continue;
-                        }
-                        foreach ($parent[$segment] as $key => $value) {
-                            if (is_int($key)) {
-                                //confilict!
-                            } elseif (is_dir($key)) {
-                            }
-                        }
-                        $parent[$segment][] = $path;
-                        break;
-                    }
-                    if (is_string($parent[$segment])) {
-                        $parent[$segment] = array($parent[$segment]);
-                    }
-                    $parent =& $parent[$segment];
-                }
+                self::add($namespace, $path);
             }
         }
     }
 
-    private static function merge(&$node, $path1, $path2) {
+    private static function checkDefaultNode(
+        &$defaultNode, $segments, $defaultNodeindex, $maxIndex, $namespace
+    ) {
+        $file = $defaultNode[0];
+        for ($index = $defaultNodeIndex; $index <= $maxIndex; ++$index) {
+            $file .= DIRECTORY_SEPARATOR . $segments[$index];
+        }
+        if (is_file($file) || is_dir($file)) {
+            self::add($namespace, $defaultNode[0]);
+        }
+        //do: 如果 default node 的所有子路经都被封死，删除 default node 的默认值(default node 失效)
+    }
+
+    private static function add($namespace, $path) {
+        $segments = explode('\\', $namespace);
+        array_pop($segments);
+        $parent =& $cache;
+        $maxIndex = count($segments) - 1;
+        $defaultNode = null;
+        $defaultNodeIndex = null;
+        for ($index = 0; $index <= $maxIndex; ++$index) {
+            if (isset($parent[$segment]) === false) {
+                if ($index !== $maxIndex) {
+                    $parent[$segment] = array();
+                    $parent =& $parent[$segment];
+                    continue;
+                }
+                $parent[$segment] = $path;
+                if ($defaultNode !== null) {
+                    self::checkDefaultNode(
+                        $defaultNode,
+                        $segments,
+                        $defaultNodeIndex,
+                        $maxIndex,
+                        $namespace
+                    );
+                }
+                break;
+            }
+            if ($index === $maxIndex) {
+                if (is_string($parent[$segment]) || isset($parent[$segment[0]])) {
+                    //展开所有后重新插入，因为不可能有两个默认路径
+                    break;
+                }
+                $parent[$segment][0] = $path;
+                //检查所有带路径数据的子节点
+                //如果存在，当前路径显式插入冲突位置（由于重复插入路径，递归需加判断）
+                break;
+            }
+            if (isset($parent[$segment][0])) {
+                $defaultNode =& $parent[$segment];
+                $defaultNodeIndex = $index;
+            }
+            $parent =& $parent[$segment];
+        }
     }
 }
