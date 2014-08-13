5de61392e2c16fedbc3c4395a24712da293d455e
Your Name <you@example.com>
2014-08-13 23:41:22 +0800

    -

--- vendor/blog/vendor/autoload.php
+++ vendor/blog/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit58fc6d59488cf2d28dedc8a8b5fa2a2b::getLoader();
+return ComposerAutoloaderInitea3624f4cf0969780967d6da3530f3f0::getLoader();
