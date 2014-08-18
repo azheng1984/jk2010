703a3b88363f82e11008ac7cf59347472804f249
Your Name <you@example.com>
2014-08-19 00:04:54 +0800

    -

--- vendor/blog/public/index.php
+++ vendor/blog/public/index.php
@@ -1,5 +1,21 @@
 <?php
 namespace Hyperframework\Blog;
+ob_start();
+ob_start('ob_gzhandler');
+echo 'sdf';
+var_dump(headers_list());
+ob_end_flush();
+
+var_dump(headers_list());
+header_remove();
+
+var_dump(headers_list());
+header("Content-Encoding: gzip");
+
+var_dump(headers_list());
+echo 'adfafsff';
+exit;
+//测试低版本
 
 use Hyperframework\Web\Runner;
 define('Hyperframework\Blog\ROOT_PATH', dirname(__DIR__));
