b8c869eadefeefe883a6f07a749f0153ef0d98d5
Your Name <you@example.com>
2014-08-18 23:15:57 +0800

    -

--- vendor/hf/core/lib/Web/ErrorHandler.php
+++ vendor/hf/core/lib/Web/ErrorHandler.php
@@ -9,6 +9,8 @@ class ErrorHandler {
     private static $log;
     private static $isOutputBufferEnabled;
     private static $isDebugEnabled;
+    private static $hasError;
+
     private $levels = array(
         E_DEPRECATED        => 'Deprecated',
         E_USER_DEPRECATED   => 'User Deprecated',
@@ -28,10 +30,11 @@ class ErrorHandler {
     );
 
     final public static function run() {
-        if (Config::get('hyperframework.web.error_handler.debug.enable') === true) {
-            //只有在调试时关闭错误显示，否则按照服务器默认设置
+        if (ini_get('display_errors') === '1') {
             ini_set('display_errors', false);
             self::$isDebugEnabled = true;
+            Config::get('hyperframework.web.error_handler.debug.format');
+            //html for text
             if (Config::get('hyperframework.web.error_handler.debug.enable_output_buffer') !== false) {
                 if (ob_get_level() === 0 || headers_sent() === false) {
                     ob_start();
@@ -40,43 +43,28 @@ class ErrorHandler {
             }
         }
         $class = get_called_class();
-        set_error_handler(array($class, 'handleError'));
+        set_error_handler(array($class, 'handleError'), E_ALL);
         set_exception_handler(array($class, 'handleException'));
-        //register_shutdown_function(array($class, 'handleFatalError'));
+        register_shutdown_function(array($class, 'handleFatalError'));
     }
 
     final public static function handleError($type, $message, $file, $line) {
+        if (self::$hasError) {
+            return;
+        }
         if (error_reporting() & $type) {
             if (self::$isDebugEnabled) {
-                //错误处理不可能被循环调用, 如果在错误处理时不开启 error 展示，应用内 error 无法显示
+                self::$hasError = true;
                 ini_set('display_errors', true);
-            }
-            if (self::$exception !== null) {
-                if (self::$isDebugEnabled) {
-                    echo "\n" . self::$log;
-                }
-                return false;
             }
             self::$exception = new \ErrorException(
                 $message, 0, $type, $file, $line
             );
-            self::$log = self::$exception;
             self::handleException($exception);
         }
-        return false;
-    }
-
-    final public static function handleException($exception) {
-        echo 'hi2';
-        return false;
-        //echo 'hi';
-        //echo xxxxxxx;
     }
 
     final public static function handleFatalError() {
-        throw new adsf;
-        //如果没有关闭错误展示，fatal error 在次之前就已经被输出了
-        //echo xxx;
         $error = error_get_last();
         if ($error === null) {
             return;
@@ -96,6 +84,69 @@ class ErrorHandler {
                 $error['file'],
                 $error['line']
             );
+        }
+    }
+
+    final public static function handleException($exception) {
+        self::$exception = $exception;
+        if ($exception instanceof HttpException === false) {
+            $exception = new InternalServerErrorException;
+        }
+        if (headers_sent()) {
+            if (self::$isDebugEnabled !== true) {
+                self::writeExceptionLog($exception);
+                exit(1);
+            }
+        } else {
+            self::$headers = headers_list();
+            header_remove();
+            $exception->setHeader();
+        }
+        if (self::$isDebugEnabled !== true) {
+            static::cleanOutputBuffer();
+        } elseif (self::$isOutpuBufferEnabled) {
+            self::$output = static::getOutputBuffer();
+        } else {
+            static::flushOutputBuffer();
+        }
+        if (self::$isDebugEnabled) {
+            if ($_SERVER['REQUEST_METHOD'] !== 'HEAD') {
+                static::renderCustomErrorPage(self::$exception);
+            }
+        } else {
+            ErrorPage::renderException(self::$exception);
+        }
+        exit(1);
+    }
+
+    protected static function cleanOutputBuffer() {
+        $obLevel = ob_get_level();
+        while ($obLevel > 0) {
+            ob_end_clean();
+            --$obLevel;
+        }
+    }
+
+    protected static function getOutputBuffer() {
+        $obLevel = ob_get_level();
+        if ($obLevel < 1) {
+            //display output buffer error!!!
+            return;
+        }
+        while ($obLevel > 1) {
+            ob_end_clean();
+            --$obLevel;
+        }
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    protected static function flushOutputBuffer() {
+        $obLevel = ob_get_level();
+        while ($obLevel > 0) {
+            ob_end_flush();
+            --$obLevel;
         }
     }
 
@@ -131,37 +182,6 @@ class ErrorHandler {
 //        error_log($message);
 //    }
 //
-//    final public static function handleException($exception) {
-//        if (self::$exception !== null || self::$error !== null) {
-//            echo self::$log;
-//            return false;
-//        }
-//        if (headers_sent() && self::$isDebugEnabled !== true) {
-//            self::writeErrorLog($exception);
-//            return;
-//        }
-//        self::$exception = $exception;
-//        if ($exception instanceof HttpException === false) {
-//            $exception = new InternalServerErrorException;
-//        }
-//        if (self::$isDebugEnabled === true) {
-//            ErrorPage::renderException(self::$exception);
-//            return;
-//        }
-//        static::resetOutput();
-//        $exception->setHeader();
-//        if ($_SERVER['REQUEST_METHOD'] !== 'HEAD') {
-//            try {
-//                static::renderCustomErrorPage(self::$exception);
-//            } catch (\Exception $e) {
-//                static::triggerError(self::$exception, $e);
-//            }
-//        }
-//        if ($exception instanceof InternalServerErrorException) {
-//            echo 'hi';
-//            static::triggerError(self::$exception);
-//        }
-//    }
 //
 //    protected static function getException() {
 //        return self::$exception;
