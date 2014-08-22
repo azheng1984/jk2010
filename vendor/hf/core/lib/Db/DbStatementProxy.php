8714827c769cafb15c98e4a8e3dbcde0f1900b05
Your Name <you@example.com>
2014-08-22 19:44:03 +0800

    -

--- vendor/hf/core/lib/Db/DbStatementProxy.php
+++ vendor/hf/core/lib/Db/DbStatementProxy.php
@@ -2,15 +2,13 @@
 namespace Hyperframework\Db;
 
 use Hyperframework\Config;
-use Traversable;
 
-class DbStatementProxy { //implements Traversable {
+class DbStatementProxy {
     private $statement;
     private $connection;
     private $isProfilerEnabled;
 
     public function __construct($statement, $connection) {
-        var_dump($statement->next());
         $this->statement = $statement;
         $this->connection = $connection;
         $this->isProfilerEnabled =
@@ -36,77 +34,103 @@ class DbStatementProxy { //implements Traversable {
         return $this->statement->queryString;
     }
 
-    public function bindColumn (
-        $column ,&$param , int $type = null, $maxlen = null, $driverdata = null
+    public function bindColumn(
+        $column,
+        &$param,
+        $type = null,
+        $maxLength = null,
+        $driverData = null
     ) {
-
+        $this->statement->bindColumn(
+            $column, $param, $type, $maxLength, $driverData
+        );
     }
 
-    public function bindParam (
-        $parameter,
+    public function bindParam(
+        $param,
         &$variable,
-        $data_type = PDO::PARAM_STR, $length = null, $driver_options = null
+        $dataType = PDO::PARAM_STR,
+        $length = null,
+        $driverOptions = null
     ) {
+        $this->statement->bindParam(
+            $param, $variable, $dataType, $length, $driverOptions
+        );
     }
 
-    public function bindValue ($parameter ,  $value ,  $data_type = PDO::PARAM_STR ) {
-
+    public function bindValue($param, $value, $dataType = PDO::PARAM_STR) {
+        $this->statement->bindValue($param, $value, $dataType);
     }
 
-    public function closeCursor () {
-
+    public function closeCursor() {
+        $this->statement->closeCursor();
     }
 
-    public function columnCount () {
-        
+    public function columnCount() {
+        $this->statement->columnCount();
     }
 
-    public function debugDumpParams () {
-
+    public function debugDumpParams() {
+        $this->statement->debugDumpParams();
     }
 
-    public function errorCode () {
+    public function errorCode() {
+        $this->statement->errorCode();
     }
 
-    public function errorInfo () {
-
+    public function errorInfo() {
+        $this->statement->errorInfo();
     }
 
-    public function fetch ($fetch_style, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0) { 
-    }
-    public function fetchAll ($fetch_style, $fetch_argument = null, $ctor_args = array()) {
-
-    }
-
-    public function fetchColumn ($column_number = 0) {
-
-    }
-
-    public function fetchObject ($class_name = "stdClass", $ctor_args = null) {
-        
+    public function fetch(
+        $fetchStyle,
+        $cursorOrientation = PDO::FETCH_ORI_NEXT,
+        $cursorOffset = 0
+    ) {
+        $this->statement->fetch(
+            $fetchStyle, $cursorOrientation, $cursorOffset
+        );
     }
 
-    public function getAttribute ($attribute ) {
-
+    public function fetchAll(
+        $fetchStyle, $fetchArgument = null, $constructorArguments = array()
+    ) {
+        $this->statement->fetchAll(
+            $fetchStyle, $fetchArgument, $constructorArguments
+        );
     }
 
-    public function getColumnMeta ($column) {
+    public function fetchColumn($columnNumber = 0) {
+        $this->statement->fetchColumn($columnNumber);
     }
 
-    public function nextRowset () {
-
+    public function fetchObject(
+        $className = "stdClass", $constructArguments = null
+    ) {
+        $this->statement->fetchObject($className, $constructArguments); 
     }
 
-    public function rowCount () {
+    public function getAttribute($attribute) {
+        $this->statement->getAttribute($attribute);
     }
 
-    public function setAttribute ($attribute, $value) {
+    public function getColumnMeta($column) {
+        $this->statement->getColumnMeta($column);
     }
 
-    public function setFetchMode ($mode) {
+    public function nextRowset() {
+        $this->statement->nextRowset();
     }
 
-    public function __call($method, $params) {
-        call_user_func_array(array($this->statement, $method), $params);
+    public function rowCount() {
+        $this->statement->rowCount();
+    }
+
+    public function setAttribute($attribute, $value) {
+        $this->statement->setAttribute($attribute, $value);
+    }
+
+    public function setFetchMode($mode) {
+        $this->statement->setFetchMode($mode);
     }
 }
