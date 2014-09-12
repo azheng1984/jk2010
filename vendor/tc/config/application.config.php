c97f0f9a48d01a98324106abad222abf8c751329
Your Name <you@example.com>
2014-09-12 18:32:34 +0800

    -

--- vendor/tc/config/application.config.php
+++ vendor/tc/config/application.config.php
@@ -3,6 +3,7 @@ return array(
     'description' => 'Hyperframework CLI Tool 0.2.1',
     'commands' => array(
         'hello' => array(
+//            'alias' => 'shit',
             'description' => 'Build application',
             'class' => '\Tc\App\HelloCommand',
             'option' => array(
@@ -10,20 +11,20 @@ return array(
                 'hi2' => array('short' => 2, 'description' => 'hello hi2'),
                 'flatoption',
             ),
+//            'commands' => array(
+//                'dis' => array(
+//                    'description' => 'hi',
+//                    'class' => '\Tc\App\HelloCommand',
+//                    'commands' => array(
+//                        'dis' => array(
+//                            'description' => 'hi',
+//                            'class' => '\Tc\App\HelloCommand',
+//                        )                
+//                    )
+//                ),
+//
+//            )
         ),
     ),
-    'package' => array(
-        'description' => 'sub key',
-        'commands' => array(
-            'hello' => array(
-                'description' => 'Build application',
-                'class' => '\Tc\App\HelloCommand',
-                'option' => array(
-                    'hi' => array('short' => 'h', 'class' => '\Tc\TestOption', 'infinite'),
-                    'hi2' => array('short' => 2, 'description' => 'hello hi2'),
-                    'flatoption',
-                ),
-            ),
-        ),
-    )
+    'option' => array('hi'),
 );
--- vendor/tc/lib/App/HelloCommand.php
+++ vendor/tc/lib/App/HelloCommand.php
@@ -2,6 +2,9 @@
 namespace Tc\App;
 
 class HelloCommand {
+    public function __construct($options) {
+    }
+
     public function execute($hi, $hi2 = null) {
         echo $hi;
     }
