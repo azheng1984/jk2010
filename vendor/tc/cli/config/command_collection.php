cf4e9f59ccb071b971a06f7b58784e0a01fc2c1e
Your Name <you@example.com>
2014-09-25 10:56:14 +0800

    -

--- vendor/tc/cli/config/command_collection.php
+++ vendor/tc/cli/config/command_collection.php
@@ -5,12 +5,25 @@ return array(
     'version' => '1.1.1',
     'description' => 'Hyperframework CLI Tool',
     'arguments' => array('arg1', '[arg2]...'),
-    'mutex_options' => array('xx'),
     'options' => array(
         array(
-            'group name',
+            'title' => 'f1 group',
+            '--f1' => array('repeatable'),
             array(
-                '--name' => ''
+                '--good' => '',
+                '--bad' => '',
+                'mutex',
+                //'required',
+                //'repeatable'
+            )
+        ),
+        array(
+            'title' => 'f2 group',
+            '--f2' => array('repeatable'),
+            array(
+                '--slow' => '',
+                '--fast' => '',
+                'mutex',
             )
         ),
         '-x,--opt[=(ax-d|bd-sf|cds-fadf)]',
@@ -19,9 +32,12 @@ return array(
         '-n,--name-of-option3[=<arg-name>]' => array(
             'description' => 'x',
             'repeatable',
-            'required'
+            'required',
         ),
         '--name-of-option[=<arg-key>]' => 'message',
+        'mutex' => array(
+            '--f1', '--f2', 'required'
+        ),
     ),
 //  'class' => 'CommandCollection', //default null
     'commands' => array( //subcommands = 'folder'
@@ -29,11 +45,10 @@ return array(
         ),
         'hello' => array( //inline. lazy load is supported
 //          'alias' => 'hill',
-            'usage' => '',
             'description' => 'Build application',
 //          'class' => 'HelloCommand', //default
             'options' => array(
-                '-h,--hi' => array('multiple'),
+                '-h,--hi=<arg>' => array('repeatable'),
                 '--hi2' => array('description' => 'hello hi2'),
                 '--flatoption',
             ),
