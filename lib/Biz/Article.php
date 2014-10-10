1aaebd19bd398644079d3fa65fb6fe15cccbc9e9
Your Name <you@example.com>
2014-10-10 10:19:46 +0800

    -

--- lib/Biz/Article.php
+++ lib/Biz/Article.php
@@ -7,10 +7,6 @@ use Hyperframework\Blog\Biz\Comment;
 
 class Article extends DbTable {
     public static function getTopLike() {
-        $client = static::getClient();
-        return $client->getColumn(
-            'SELECT * FROM Article ORDER BY like_count DESC LIMIT 1'
-        );
         DbClient::getColumn(
             'SELECT * FROM Article ORDER BY like_count DESC LIMIT 1'
         );
@@ -18,9 +14,10 @@ class Article extends DbTable {
 
     public static function delete($article) {
         DbTransaction::run(function() use ($article) {
-            parent::delete($row);
-            static::getClient()->deleteByColumns('Comment', ['article_id' => $row['id']]);
-            DbClient::deleteByColumns('Comment', ['article_id' => $row['id']]);
+            parent::delete($article);
+            DbClient::deleteByColumns(
+                'Comment', ['article_id' => $article['id']]
+            );
         });
     }
 
