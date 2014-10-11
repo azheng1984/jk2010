56aac5b1b8efd410f8e4874f507b51d55c93d36f
Your Name <you@example.com>
2014-10-11 12:30:53 +0800

    -

--- vendor/hf_v1/core/lib/Db/DbClientEngine.php
+++ vendor/hf_v1/core/lib/Db/DbClientEngine.php
@@ -7,70 +7,70 @@ use Exception;
 class DbClientEngine {
     public function getById($table, $id, $columnNameOrNames = null) {
         $sql = 'SELECT * FROM '
-            . self::quoteIdentifier($table) . ' WHERE id = ?';
-        return static::getRow($sql, array($id));
+            . $this->quoteIdentifier($table) . ' WHERE id = ?';
+        return $this->getRow($sql, array($id));
     }
 
     public function getColumn($sql, array $params = null) {
-        return static::query($sql, $params)->fetchColumn();
+        return $this->query($sql, $params)->fetchColumn();
     }
 
     public function getColumnByColumns($table, array $columns, $columnName) {
-        $result = self::queryByColumns($table, $columns, $columnName);
+        $result = $this->queryByColumns($table, $columns, $columnName);
         return $result->fetchColumn();
     }
 
     public function getRow($sql, array $params = null) {
-        return static::query($sql, $params)->fetch(PDO::FETCH_ASSOC);
+        return $this->query($sql, $params)->fetch(PDO::FETCH_ASSOC);
     }
 
     public function getRowByColumns($table, array $columns, $selector = '*') {
-        $result = self::queryByColumns($table, $columns, $selector);
+        $result = $this->queryByColumns($table, $columns, $selector);
         return $result->fetch(PDO::FETCH_ASSOC);
     }
 
     public function getAll($sql, array $params = null) {
-        return static::query($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+        return $this->query($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getAllByColumns($table, array $columns, $selector = '*') {
-        $result = self::queryByColumns($table, $columns, $selector);
+        $result = $this->queryByColumns($table, $columns, $selector);
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function count($table) {
-        return self::calculate($table, '*', 'COUNT');
+        return $this->calculate($table, '*', 'COUNT');
     }
 
     public function min($table, $columnName) {
-        return self::calculate($table, $columnName, 'MIN');
+        return $this->calculate($table, $columnName, 'MIN');
     }
 
     public function max($table, $columnName) {
-        return self::calculate($table, $columnName, 'MAX');
+        return $this->calculate($table, $columnName, 'MAX');
     }
 
     public function sum($table, $columnName) {
-        return self::calculate($table, $columnName, 'SUM');
+        return $this->calculate($table, $columnName, 'SUM');
     }
 
     public function average($table, $columnName) {
-        return self::calculate($table, $columnName, 'AVG');
+        return $this->calculate($table, $columnName, 'AVG');
     }
 
     public function insert($table, array $row) {
         $keys = array();
         foreach (array_keys($row) as $key) {
-            $keys[] = self::quoteIdentifier($key);
+            $keys[] = $this->quoteIdentifier($key);
         }
         $columnCount = count($row);
         if ($columnCount === 0) {
             throw new Exception;
         }
         $placeHolders = str_repeat('?, ', $columnCount - 1) . '?';
-        $sql = 'INSERT INTO ' . self::quoteIdentifier($table)
+        $sql = 'INSERT INTO ' . $this->quoteIdentifier($table)
             . '(' . implode($keys, ', ') . ') VALUES(' . $placeHolders . ')';
-        return static::sendSql($sql, array_values($row));
+        return $this->sendSql($sql, array_values($row));
     }
 
     public function update(
@@ -84,19 +84,19 @@ class DbClientEngine {
         }
         $tmp = null;
         foreach (array_keys($columns) as $key) {
-            $tmp .= self::quoteIdentifier($key) . ' = ?';
+            $tmp .= $this->quoteIdentifier($key) . ' = ?';
         }
-        $sql = 'UPDATE ' . self::quoteIdentifier($table)
+        $sql = 'UPDATE ' . $this->quoteIdentifier($table)
             . ' SET ' . $tmp . $where;
-        return static::sendSql($sql, $params);
+        return $this->sendSql($sql, $params);
     }
 
     public function updateByColumns(
         $table, array $replacementColumns, array $filterColumns
     ) {
-        list($where, $params) = self::buildWhereByColumns($filterColumns);
+        list($where, $params) = $this->buildWhereByColumns($filterColumns);
         return call_user_func_array(
-            'static::update',
+            '$this->update',
             array_merge(array($table, $replacementColumns, $where), $params)
         );
     }
@@ -105,61 +105,61 @@ class DbClientEngine {
         if ($where !== null) {
             $where = ' WHERE ' . $where;
         }
-        $sql = 'DELETE FROM ' . self::quoteIdentifier($table) . $where;
-        return static::sendSql($sql, $params);
+        $sql = 'DELETE FROM ' . $this->quoteIdentifier($table) . $where;
+        return $this->sendSql($sql, $params);
     }
 
     public function deleteByColumns($table, array $columns) {
-        list($where, $params) = self::buildWhereByColumns($columns);
-        return static::sendSql(
-            'DELETE FROM ' . self::quoteIdentifier($table)
+        list($where, $params) = $this->buildWhereByColumns($columns);
+        return $this->sendSql(
+            'DELETE FROM ' . $this->quoteIdentifier($table)
                 . ' WHERE ' . $where, $params
         );
     }
 
     public function deleteById($table, $id) {
-        return static::delete($table, 'id = ?', $id);
+        return $this->delete($table, 'id = ?', $id);
     }
 
     public function save($table, array &$row) {
         if (isset($row['id'])) {
             $id = $row['id'];
             unset($row['id']);
-            $result = static::update($table, $row, 'id = ?', $id);
+            $result = $this->update($table, $row, 'id = ?', $id);
             $row['id'] = $id;
             return $result;
         }
-        static::insert($table, $row);
-        $row['id'] = static::getLastInsertId();
+        $this->insert($table, $row);
+        $row['id'] = $this->getLastInsertId();
         return 1;
     }
 
     public function execute($sql, array $params = null) {
-        return static::sendSql($sql, $params);
+        return $this->sendSql($sql, $params);
     }
  
     public function getLastInsertId() {
-        return static::getConnection()->lastInsertId();
+        return $this->getConnection()->lastInsertId();
     }
 
     public function beginTransaction() {
-        return static::getConnection()->beginTransaction();
+        return $this->getConnection()->beginTransaction();
     }
 
     public function commit() {
-        return static::getConnection()->commit();
+        return $this->getConnection()->commit();
     }
 
     public function rollback() {
-        return static::getConnection()->rollBack();
+        return $this->getConnection()->rollBack();
     }
 
     public function inTransaction() {
-        return static::getConnection()->inTransaction();
+        return $this->getConnection()->inTransaction();
     }
 
     public function quoteIdentifier($identifier) {
-        return static::getConnection()->quoteIdentifier($identifier);
+        return $this->getConnection()->quoteIdentifier($identifier);
     }
 
     public function getConnection() {
@@ -170,7 +170,7 @@ class DbClientEngine {
         return $this->getConnection()->prepare($sql, $driverOptions);
     }
 
-    protected function sendSql($sql, array $params, $isQuery = false) {
+    protected function sendSql($sql, array $params = null, $isQuery = false) {
         $connection = $this->getConnection();
         if ($params === null || count($params) === 0) {
             return $isQuery ?
@@ -188,22 +188,22 @@ class DbClientEngine {
     }
 
     private function calculate($table, $columnName, $function) {
-        $table = self::quoteIdentifier($table);
+        $table = $this->quoteIdentifier($table);
         if ($columnName !== '*') {
-            $columnName = self::quoteIdentifier($columnName);
+            $columnName = $this->quoteIdentifier($columnName);
         }
         return $this->getColumn(
             'SELECT ' . $function . '(' . $columnName . ') FROM ' . $table
         );
     }
 
-    private function query($sql, array $params) {
+    private function query($sql, array $params = null) {
         return $this->sendSql($sql, $params, true);
     }
 
     private function queryByColumns($table, array $columns, $selector) {
-        list($where, $params) = self::buildWhereByColumns($columns);
-        $sql = 'SELECT ' . $selector . ' FROM ' . self::quoteIdentifier($table);
+        list($where, $params) = $this->buildWhereByColumns($columns);
+        $sql = 'SELECT ' . $selector . ' FROM ' . $this->quoteIdentifier($table);
         if ($where !== null) {
             $sql .= ' WHERE ' . $where;
         }
@@ -219,7 +219,7 @@ class DbClientEngine {
             if ($where !== null) {
                 $where = ' AND ';
             }
-            $where .= self::quoteIdentifier($key) . ' = ?';
+            $where .= $this->quoteIdentifier($key) . ' = ?';
         }
         if ($where === null) {
             throw new Exception;
