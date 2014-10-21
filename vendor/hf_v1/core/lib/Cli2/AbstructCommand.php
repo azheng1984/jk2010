2aaefcfd33a2ec65012bcdf2e32998a9537b2e0f
Your Name <you@example.com>
2014-10-21 17:37:00 +0800

    -

---
+++ vendor/hf_v1/core/lib/Cli2/AbstructCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+class AbstructCommand {
+    public function __construct($ctx) {
+    }
+
+    public function dispatchByOptions() {
+    }
+
+    public function dispatchAllByOptions() {
+    }
+
+    public function getOptions() {
+    }
+
+    public function getArguments() {
+    }
+
+    public function quit() {
+    }
+}
