11d423b44cc3a14e05268d719366545a6d27c606
Your Name <you@example.com>
2014-10-21 18:57:59 +0800

    -

---
+++ vendor/hf_v1/core/lib/Cli2/ExecutableElement.php
@@ -0,0 +1,32 @@
+<?php
+namespace Hyperframework\Cli;
+
+abstract class ExecutableElement {
+    private $ctx;
+
+    public function __construct($ctx) {
+        $this->ctx = $ctx;
+    }
+
+    abstract public function execute(/*...*/) {}
+
+    protected function getContext() {
+        return $this->ctx;
+    }
+
+    protected function dispatch($config) {
+        OptionCallback::dispatch($this->getOptions(), $config);
+    }
+
+    protected function dispatchAll($config) {
+        OptionCallback::dispatchAll($this->getOptions(), $config);
+    }
+
+    protected function getOptions() {
+        $this->getContext()->getOptions();
+    }
+
+    protected function quit() {
+        $this->getContext()->quit();
+    }
+}
