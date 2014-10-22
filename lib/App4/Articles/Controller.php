dffe2d5483c38830c95befce4a248964edca1b41
Your Name <you@example.com>
2014-10-22 18:13:32 +0800

    -

---
+++ lib/App4/Articles/Controller.php
@@ -0,0 +1,20 @@
+<?php
+namespace Hyperframework\Blog\App\Article;
+
+class Controller extends AbstractController {
+    public function create() {
+    }
+
+    public function get() {
+    }
+
+    public function update() {
+    }
+
+    public function delete() {
+    }
+}
+
+// post /articles => redirect new resource
+// patch /articles/332323
+// delete /articles/21323/comments
