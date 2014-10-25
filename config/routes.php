b473df0f70d1c78fb34bf141a2980cad3c34e537
Your Name <you@example.com>
2014-10-25 17:04:54 +0800

    -

--- config/routes.php
+++ config/routes.php
@@ -5,12 +5,13 @@ return [
         'type' => 'collection', //singular by default if has member set collection automaticly
         'children' => [], //as collection paths when type = collection or has member config
         'additional_children' => ['index' => 'IndexPreview'], //rename children?
+        'set_child_name' => ['IndexPreview' => 'index'],
         'additional_children' => ['new' => ['children' => ['preview']]], //postpone
-        'removed_children' => ['new'], //postpone
+        'removed_children' => ['index_preview'], //postpone
         'root' => 'index', //default, can be configed globally
         'include' => [],
         'item' => [
-            'pattern' => function($segment) {
+            'pattern' => function($segment) { //or id constraints?
                 //number by default
             },
             'children' => ['show', 'edit', 'new', 'preview'], // rewrite
