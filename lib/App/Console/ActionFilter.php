2b1e5b06759f12e3be2f33c1713271fbe40dcc5a
Your Name <you@example.com>
2014-10-25 16:40:27 +0800

    -

---
+++ lib/App/Console/ActionFilter.php
@@ -0,0 +1,9 @@
+<?php
+//support chained filter for security check or use base class
+class ActionFilter {
+    public function onExecuting() {
+    }
+
+    public function onExecuted() {
+    }
+}
