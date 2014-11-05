c54680aabab75328baace608944d81dbe2cc8a19
Your Name <you@example.com>
2014-11-05 23:13:13 +0800

    -

--- lib/Router.php
+++ lib/Router.php
@@ -15,20 +15,20 @@ class Router extends Base {
         $this->match('article/:id(/*comments)', [':id' => '[0-9]+', 'formats' => 'jpg']);
         $this->match('article/:id(/*comments)', [':id' => '[0-9]+']);
 
-        if ($this->match('/')) return 'main/index/show';
-        if ($this->match('article/:id(/*comments)', [':id' => '[0-9]+']))
-            return 'comments/show';
-        if ($this->matchResource('article')) return;
-        if ($this->matchScope('main', function() {
-            echo $this->getPath();
-            $this->matchPost('*path');
-        })) {
-            $this->setModule('main');
-            return;
-        }
-        if ($this->matchGet('(:module(/:controller(/:action)))', [':id' => '[0-9]+'])) return;
-        if ($this->matchPost('article/:id(/*comments)', [':id' => '[0-9]+', 'formats' => 'jpg'])) return;
-        if ($this->matchDelete('article/:id(/*comments)', [':id' => '[0-9]+'])) return;
+//        if ($this->match('/')) return 'main/index/show';
+//        if ($this->match('article/:id(/*comments)', [':id' => '[0-9]+']))
+//            return 'comments/show';
+//        if ($this->matchResource('article')) return;
+//        if ($this->matchScope('main', function() {
+//            echo $this->getPath();
+//            $this->matchPost('*path');
+//        })) {
+//            $this->setModule('main');
+//            return;
+//        }
+//        if ($this->matchGet('(:module(/:controller(/:action)))', [':id' => '[0-9]+'])) return;
+//        if ($this->matchPost('article/:id(/*comments)', [':id' => '[0-9]+', 'formats' => 'jpg'])) return;
+//        if ($this->matchDelete('article/:id(/*comments)', [':id' => '[0-9]+'])) return;
 
     }
 }
