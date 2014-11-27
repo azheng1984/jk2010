89f835edc1eb087aa9d53fd561e42ecac231ea01
Your Name <you@example.com>
2014-11-27 22:53:35 +0800

    -

--- vendor/hyperframework/hyperframework/lib/WebClient/CurlResponseHeaderParser.php
+++ vendor/hyperframework/hyperframework/lib/WebClient/CurlResponseHeaderParser.php
@@ -4,6 +4,17 @@ namespace Hyperframework\WebClient;
 use Exception;
 
 class CurlResponseHeaderParser {
-    public static function parse($raw) {
+    public static function parse($handle, $rawHeaders) {
+        $url = curl_getinfo($handle, CURLINFO_EFFECTIVE_URL);
+        $tmp = explode('://', $url, 2);
+        $protocol = strtolower($tmp[0]);
+        if ($protocol === 'http'
+            || $protocol === 'https'
+            || $protocol === 'file'
+            || $protocol === 'ftp'
+        ) {
+        } else {
+            return [];
+        }
     }
 }
