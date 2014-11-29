1451ea0b581af76806d8788da707764cdf2d1f4d
Your Name <you@example.com>
2014-11-29 17:22:42 +0800

    -

--- vendor/hyperframework/hyperframework/lib/Cli/HelpView.php
+++ vendor/hyperframework/hyperframework/lib/Cli/HelpView.php
@@ -4,11 +4,15 @@ namespace Hyperframework\Cli;
 class HelpView {
     private $app;
 
-    public function __construct($app) {
+    public function __construct($app, $commandParsingException = null) {
         $this->app = $app;
     }
 
     public function render() {
         echo 'Usage: command_name [options] command' . PHP_EOL;
+    }
+
+    public function renderUsage() {
+        //short version for exception
     }
 }
