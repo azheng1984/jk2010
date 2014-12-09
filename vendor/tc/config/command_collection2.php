b564d996d936621934097884d5615bfefaf865ad
Your Name <you@example.com>
2014-12-09 11:26:52 +0800

    -

--- vendor/tc/config/command_collection2.php
+++ vendor/tc/config/command_collection2.php
@@ -4,16 +4,17 @@ return array(
     'namespace' => 'Tc', //default: app_root_namespace
     'version' => '1.1.1',
     'description' => 'Hyperframework CLI Tool', //optional
-    'arguments' => array('<arg1>', '<arg2>...'),
-
+    'arguments' => [
+        'arg1' => 'xxxxxx',
+        '[<arg2>...]' => 'description'
+    ],
     //'pass_options' => false, //commands only, hf.cli.commands.pass_options to set all
     // = false to disable passing collection & commands options
     //default: use command execute function signature
     //collection must not have arguments
     //cancel! get options via $this
-
     'options' => [
-        '--right' => ['repeatable'],
+        '--right' => ['description' => 'wow', 'repeatable'],
         '--left'  => ['repeatable'],
         '--top'   => ['repeatable'],
         '--down'  => ['repeatable'],
@@ -26,21 +27,20 @@ return array(
         '--name-of-option2' => 'description',
         '-n, --name-of-option3 <arg-name>',
         '-n, --name-of-option3=<arg-name>',
-        '-n, --name-of-option3[=<arg-name>]' => array(
+        '-n, --name-of-option3[=<arg-name>]' => [
+            'description' => 'x'
             'repeatable',
             'required',
-            'description' => 'x',
-        ),
+        ],
         '--name-of-option[=<arg-key>]' => 'message',
     ],
     'mutually_exclusive_options' => [
-        '--right | --left | --top | --down' => 'required'
+        ['--right', '--left', '--top', '--down', 'required']
     ],
     //'class' => 'CommandCollection', //default CommandCollection
     //\Xx\CommandCollection //same as php
     //CommandCollection if have options
     //'root_namespace' => '\Hyperframework\Blog\RootPath'
-
     'commands' => array( //or subcommands = 'folder' default to config/commands
         'hello' => array( //inline. lazy load is supported
             'description' => 'Build application',
