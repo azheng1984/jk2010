0ed7387e40cedb5beb22bdde636e89b4116ecaf1
Your Name <you@example.com>
2014-12-16 23:22:55 +0800

    -

--- vendor/hyperframework/hyperframework/lib/Common/Inflector.php
+++ vendor/hyperframework/hyperframework/lib/Common/Inflector.php
@@ -3,97 +3,58 @@ namespace Hyperframework\Common;
 
 class Inflector {
     public static function pluralize($word) {
-        $word = strtolower($word);
-        $result = self::getSpecialResult($word);
-        if ($result !== null) {
-            return $result;
-        }
-        static $rules = [
-            '/(quiz)$/' => '\1zes',
-            '/^(oxen)$/' => '\1',
-            '/^(ox)$/' => '\1en',
-            '/^(m|l)ice$/' => '\1ice',
-            '/^(m|l)ouse$/' => '\1ice',
-            '/(matr|vert|ind)(?:ix|ex)$/' => '\1ices',
-            '/(x|ch|ss|sh)$/' => '\1es',
-            '/([^aeiouy]|qu)y$/' => '\1ies',
-            '/(hive)$/' => '\1s',
-            '/(?:([^f])fe|([lr])f)$/' => '\1\2ves',
-            '/sis$/' => 'ses',
-            '/([ti])a$/' => '\1a',
-            '/([ti])um$/' => '\1a',
-            '/(buffal|tomat)o$/' => '\1oes',
-            '/(bu)s$/' => '\1ses',
-            '/(alias|status)$/' => '\1es',
-            '/(octop|vir)i$/' => '\1i',
-            '/(octop|vir)us$/' => '\1i',
-            '/^(ax|test)is$/' => '\1es',
-            '/s$/' => 's',
-            '/$/', 's',
-        ];
-        return self::convert($word, $rules);
+        return self::convert($word, true);
+    }
+
+    public static function singularize($word) {
+        return self::convert($word, false);
     }
 
     private static function convert($word, $isSingular) {
         $originalWord = $word;
         $word = strtolower($word);
         static $specialWords = [
-            'atlas' => 'atlases',#?
-            'beef' => 'beefs',#?
-            'brother' => 'brothers', #?
-            'cafe' => 'cafes', #?
-    //        'child' => 'children',
-            'cookie' => 'cookies', #?
+            'atlas' => 'atlases',
+            'cafe' => 'cafes',
+            'carp' => 'carp',
+            'chassis' => 'chassis',
+            'child' => 'children',
+            'clippers' => 'clippers',
+            'cod' => 'cod',
+            'cookie' => 'cookies',
             'corpus' => 'corpuses',
-            'cow' => 'cows', #?
-            'curve' => 'curves',#?
-            'foe' => 'foes',#?
-            'ganglion' => 'ganglions',
-            'genie' => 'genies', #?
-            'genus' => 'genera',#?
+            'curve' => 'curves',
+            'equipment' => 'equipment',
+            'fish' => 'fish',
+            'foe' => 'foes',
+            'genie' => 'genies',
+            'genus' => 'genera',
             'graffito' => 'graffiti',
-            'hoof' => 'hoofs', #?
-            'loaf' => 'loaves', #?
-     //       'man' => 'men',
+            'information' => 'information',
+            'jeans' => 'jeans',
+            'loaf' => 'loaves',
+            'man' => 'men',
             'money' => 'money',
-    //        'move' => 'moves',
-            'niche' => 'niches',#?
-            'opus' => 'opuses',#?
-            'ox' => 'oxen', #?
-    //        'sex' => 'sexes', 
-            'testis' => 'testes', #?
-            'turf' => 'turfs', #?
-            'wave' => 'waves', #?
-            'carp' => 'carp', #fish
-            'chassis' => 'chassis',#?
-            'clippers' => 'clippers',#?
-            'cod' => 'cod', #fish
-    //        'equipment' => 'equipment',
-    //        'information' => 'information',
-            'news' => 'news', #
-            'nexus' => 'nexus', #
+            'move' => 'moves',
+            'nexus' => 'nexus',
+            'niche' => 'niches',
+            'opus' => 'opuses',
+            'ox' => 'oxen',
+            'person' => 'people',
             'pincers' => 'pincers',
             'pliers' => 'pliers',
-    //        'rice' => 'rice',
-            'salmon' => 'salmon', #fish
-            'scissors' => 'scissors', #
-    //        'series' => 'series',
-            'shears' => 'shears',
-    //        'species' => 'species',
-    
-            'zombie' => 'zombies',
-            'move' => 'moves',
-            'sex' => 'sexes',
-            'child' => 'children',
-            'man' => 'men',
-            'person' => 'people',
-    
             'police' => 'police',
-            'jeans' => 'jeans',
+            'rice' => 'rice',
+            'salmon' => 'salmon',
+            'scissors' => 'scissors',
+            'shears' => 'shears',
             'sheep' => 'sheep',
-            'fish' => 'fish',
+            'species' => 'species',
+            'turf' => 'turfs',
+            'wave' => 'waves',
+            'zombie' => 'zombies',
         ];
-        $result = null;
+        $result = false;
         if ($isSingular) {
             if (isset($specialWords[$word])) {
                 $result = $specialWords[$word];
@@ -104,12 +65,10 @@ class Inflector {
                 $result = $result;
             }
         }
-        if ($result === null) {
+        if ($result === false) {
             if ($isSingular) {
                 static $pluralRules = [
                     '/(quiz)$/' => '\1zes',
-                    '/^(oxen)$/' => '\1',
-                    '/^(ox)$/' => '\1en',
                     '/^(m|l)ice$/' => '\1ice',
                     '/^(m|l)ouse$/' => '\1ice',
                     '/(matr|vert|ind)(?:ix|ex)$/' => '\1ices',
@@ -127,16 +86,15 @@ class Inflector {
                     '/(octop|vir)us$/' => '\1i',
                     '/^(ax|test)is$/' => '\1es',
                     '/s$/' => 's',
-                    '/$/', 's',
+                    '/$/' => 's',
                 ];
-                $rules = $pluralRules;
+                $rules =& $pluralRules;
             } else {
                 static $singularRules = [
                     '/(database)s$/' => '\1',
                     '/(quiz)zes$/' => '\1',
                     '/(matr)ices$/' => '\1ix',
                     '/(vert|ind)ices$/' => '\1ex',
-                    '/^(ox)en/' => '\1',
                     '/(alias|status)(es)?$/' => '\1',
                     '/(octop|vir)(us|i)$/' => '\1us',
                     '/^(a)x[ie]s$/' => '\1xis',
@@ -154,33 +112,42 @@ class Inflector {
                     '/(hive)s$/' => '\1',
                     '/([^f])ves$/' => '\1fe',
                     '/(^analy)(sis|ses)$/' => '\1sis',
-                    '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$/' => '\1sis',
+                    '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$/'
+                        => '\1sis',
                     '/([ti])a$/' => '\1um',
                     '/(n)ews$/' => '\1ews',
                     '/(ss)$/' => '\1',
-                    '/s$/' => '',
+                    '/s$/' => ''
                 ];
-                $rules = $singularRules;
+                $rules =& $singularRules;
             }
             foreach ($rules as $rule => $replacement) {
                 if (preg_match($rule, $word)) {
+                    if ($word === 'sex' ||$word === 'sexes') {
+                        echo $rule;
+                    } 
                     $result = preg_replace($rule, $replacement, $word);
+                    break;
                 }
             }
         }
-        if ($result === null) {
+        if ($result === false || $result === $originalWord) {
             return $originalWord;
-        }
-    }
-
-    public static function singularize($word) {
-        $result = self::getSpecialResult($word, false);
-        if ($result !== null) {
+        } else {
+            $originalWordLength = strlen($originalWord);
+            $resultLength = strlen($result);
+            $length = $originalWordLength < $resultLength ?
+                $originalWordLength : $resultLength;
+            for ($index = 0; $index < $length; ++$index) {
+                if ($result[$index] !== $originalWord[$index]) {
+                    if ($result[$index] === strtolower($originalWord[$index])) {
+                        $result[$index] = $originalWord[$index];
+                    } else {
+                        return $result;
+                    }
+                }
+            }
             return $result;
-        }
-        $result = self::$convert($word, true, $rules);
-        if ($result === null) {
-            return $word;
         }
     }
 }
