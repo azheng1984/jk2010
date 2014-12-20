a253f3243abf79f2902b38ba45e83bce83b28a0b
Your Name <you@example.com>
2014-12-20 23:14:02 +0800

    -

--- vendor/hyperframework/hyperframework/lib/Common/ErrorException.php
+++ vendor/hyperframework/hyperframework/lib/Common/ErrorException.php
@@ -75,7 +75,7 @@ class ErrorException extends Base {
         if ($this->sourceTrace === null) {
             return 'undefined';
         } else {
-            $result = '';
+            $result = null;
             $index = 0;
             foreach ($this->sourceTrace as $item) {
                 $result .= PHP_EOL . '#' . $index . ' '
@@ -83,7 +83,6 @@ class ErrorException extends Base {
                     . $item['function'];
                 ++$index;
             }
-            return $result;
         }
     }
 
