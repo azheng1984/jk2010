d51a75690c5395c72c473823a0a9ad71c99d3204
Your Name <you@example.com>
2014-12-25 18:39:55 +0800

    -

--- vendor/hyperframework/hyperframework/lib/Common/StackTraceFormatter.php
+++ vendor/hyperframework/hyperframework/lib/Common/StackTraceFormatter.php
@@ -2,59 +2,64 @@
 namespace Hyperframework\Common;
 
 class StackTraceFormatter {
-    public static function format(array $trace, $shouldMergeStackFrame = true) {
+    public static function format(array $trace) {
         $result = [];
         $index = 0;
-        foreach ($trace as $item) {
-            $value = '';
-            if (isset($item['file']) === false) {
-                $value .= '[internal function]: ';
-            } else {
-                $value .= $item['file'] . '(' . $item['line'] . '): ';
-            }
-            if (isset($item['class'])) {
-                $value .= $item['class'] . $item['type'];
-            }
-            $value .= $item['function'];
-            $arguments = [];
-            foreach ($item['args'] as $argument) {
-                if (is_string($argument)) {
-                    if (mb_strlen($argument) > 15) {
-                        $argument = mb_substr($argument, 0, 15) . '...';
-                    }
-                    $argument = str_replace(
-                        ["\\", "'", "\n", "\r", "\t", "\v", "\e", "\f"],
-                        ['\\\\', '\\\'', '\n', '\r', '\t', '\v', '\e', '\f'],
-                        $argument
-                    );
-                    $arguments[] = "'$argument'";
-                } elseif (is_array($argument)) {
-                    $arguments[] = 'Array';
-                } elseif (is_null($argument)) {
-                    $arguments[] = 'NULL';
-                } elseif (is_object($argument)) {
-                    $arguments[] = 'Object(' . get_class($argument) . ')';
-                } else {
-                    $arguments[] = $argument;
-                }
-            }
-            $value .= '(' . implode(', ', $arguments) . ')';
-            $result[] = $value;
+        foreach ($trace as $stackFrame) {
+            $result[] = static::formatStackFrame($stackFrame);
             ++$index;
         }
         $result[] = '{main}';
-        if ($shouldMergeStackFrame) {
-            $message = '';
-            $index = 0;
-            foreach ($result as $item) {
-                if ($index !== 0) {
-                    $message .= PHP_EOL;
-                }
-                $message .= '#' . $index . ' ' . $item;
-                ++$index;
+        $message = '';
+        $index = 0;
+        foreach ($result as $line) {
+            if ($index !== 0) {
+                $message .= PHP_EOL;
             }
-            return $message;
+            $message .= '#' . $index . ' ' . $line;
+            ++$index;
         }
-        return $result;
+        return $message;
+    }
+
+    public static function formatStackFrame($stackFrame) {
+        $result = '';
+        if (isset($stackFrame['file']) === false) {
+            $result .= '[internal function]: ';
+        } else {
+            $result .= $stackFrame['file'] . '(' . $stackFrame['line'] . '): ';
+        }
+        return $result . static::formatInvocation($stackFrame);
+    }
+
+    public static function formatInvocation($stackFrame) {
+        $result = '';
+        if (isset($stackFrame['class'])) {
+            $result .= $stackFrame['class'] . $stackFrame['type'];
+        }
+        $result .= $stackFrame['function'];
+        $arguments = [];
+        foreach ($stackFrame['args'] as $argument) {
+            if (is_string($argument)) {
+                if (mb_strlen($argument) > 15) {
+                    $argument = mb_substr($argument, 0, 15) . '...';
+                }
+                $argument = str_replace(
+                    ["\\", "'", "\n", "\r", "\t", "\v", "\e", "\f"],
+                    ['\\\\', '\\\'', '\n', '\r', '\t', '\v', '\e', '\f'],
+                    $argument
+                );
+                $arguments[] = "'$argument'";
+            } elseif (is_array($argument)) {
+                $arguments[] = 'Array';
+            } elseif (is_null($argument)) {
+                $arguments[] = 'NULL';
+            } elseif (is_object($argument)) {
+                $arguments[] = 'Object(' . get_class($argument) . ')';
+            } else {
+                $arguments[] = $argument;
+            }
+        }
+        return $result . '(' . implode(', ', $arguments) . ')';
     }
 }
