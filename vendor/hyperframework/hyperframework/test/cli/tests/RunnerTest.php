769e0e0cdb03bec714c46b5c8dacb3eff6c11fd5
Your Name <you@example.com>
2015-01-17 11:49:57 +0800

    -

--- vendor/hyperframework/hyperframework/test/cli/tests/RunnerTest.php
+++ vendor/hyperframework/hyperframework/test/cli/tests/RunnerTest.php
@@ -4,7 +4,18 @@ namespace Hyperframework\Cli;
 use Hyperframework\Common\Config;
 
 class RunnerTest extends \PHPUnit_Framework_TestCase {
+    protected function setUp() {
+        Config::set(
+            'hyperframework.app_root_path',
+            '/home/az/quickquick/vendor/hyperframework/hyperframework/test/cli'
+        );
+        Config::set(
+            'hyperframework.app_root_namespace', 'Hyperframework\Cli\Test'
+        );
+    }
+
     public function testRun() {
+        $this->expectOutputString("success");
         Runner::run('/home/az/quickquick/vendor/hyperframework/hyperframework/test/cli');
     }
 
