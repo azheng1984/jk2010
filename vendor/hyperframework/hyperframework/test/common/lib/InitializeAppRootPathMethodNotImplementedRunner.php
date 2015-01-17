42a89628e77e99ff60e37784a6b0d9193c9345b7
Your Name <you@example.com>
2015-01-17 18:32:20 +0800

    -

---
+++ vendor/hyperframework/hyperframework/test/common/lib/InitializeAppRootPathMethodNotImplementedRunner.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hyperframework\Common\Test;
+
+use Hyperframework\Common\Runner as Base;
+
+class InitializeAppRootPathMethodNotImplementedRunner extends Base {
+    public static function run() {
+        parent::initialize();
+    }
+}
