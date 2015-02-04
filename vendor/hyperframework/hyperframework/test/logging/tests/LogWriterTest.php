f00093a45b506ce59cb92915f688bf8227ab3e28
Your Name <you@example.com>
2015-02-04 18:15:16 +0800

    -

--- vendor/hyperframework/hyperframework/test/logging/tests/LogWriterTest.php
+++ vendor/hyperframework/hyperframework/test/logging/tests/LogWriterTest.php
@@ -4,4 +4,7 @@ namespace Hyperframework\Logging;
 use Hyperframework\Test\TestCase as Base;
 
 class LogWriterTest extends Base {
+    public function testX() {
+        $this->assertTrue(true);
+    }
 }
