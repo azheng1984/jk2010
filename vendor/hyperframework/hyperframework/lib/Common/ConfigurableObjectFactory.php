861d9c083f3a4deafe841708b7b6b7c9123dc0d4
Your Name <you@example.com>
2015-02-04 18:16:22 +0800

    -

--- vendor/hyperframework/hyperframework/lib/Common/ConfigurableObjectFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class ConfigurableObjectFactory {
-    public function create($configName, $defaultClass, array $params = null) {
-        $class = new ReflectionClass('ReflectionFunction');
-        return $class->newInstanceArgs(array('substr'));
-    }
-}
