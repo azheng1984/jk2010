997bbd9c22076cb9d7c383a16ce1a514c4bd3b09
Your Name <you@example.com>
2015-02-14 16:28:35 +0800

    -

--- vendor/hyperframework/hyperframework/test/logging/tests/LogRecordTest.php
+++ vendor/hyperframework/hyperframework/test/logging/tests/LogRecordTest.php
@@ -10,7 +10,7 @@ class LogRecordTest extends Base {
         $this->assertTrue($record->getTime() instanceof DateTime);
     }
 
-    public function testIntegerTimestamp() {
+    public function testCustomIntegerTimestamp() {
         $time = time();
         $record = new LogRecord(LogLevel::ERROR, null, $time);
         $this->assertSame(
@@ -19,13 +19,7 @@ class LogRecordTest extends Base {
         );
     }
 
-    public function testDateTime() {
-        $time = new DateTime;
-        $record = new LogRecord(LogLevel::ERROR, null, $time);
-        $this->assertSame($time, $record->getTime());
-    }
-
-    public function testMicrosecond() {
+    public function testCustomFloatTimestamp() {
         $time = microtime(true);
         $record = new LogRecord(LogLevel::ERROR, null, $time);
         $this->assertSame(
@@ -33,15 +27,25 @@ class LogRecordTest extends Base {
         );
     }
 
+    public function testCustomDateTime() {
+        $time = new DateTime;
+        $record = new LogRecord(LogLevel::ERROR, null, $time);
+        $this->assertSame($time, $record->getTime());
+    }
+
     /**
      * @expectedException Hyperframework\Logging\LoggingException
      */
-    public function testInvalidTime() {
+    public function testInvalidCustomTime() {
         $record = new LogRecord(LogLevel::ERROR, null, 'invalid time');
     }
 
+    public function testLevel() {
+        $record = new LogRecord(LogLevel::ERROR, null);
+        $this->assertSame(LogLevel::ERROR, $record->getLevel());
+    }
+
     public function testMessage() {
-        $time = new DateTime;
         $record = new LogRecord(LogLevel::ERROR, 'message');
         $this->assertSame('message', $record->getMessage());
     }
