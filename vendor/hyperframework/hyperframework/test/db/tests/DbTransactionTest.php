c57061a21a71def2ab59f81d06ec61e401c8de5a
Your Name <you@example.com>
2015-02-26 11:22:48 +0800

    -

--- vendor/hyperframework/hyperframework/test/db/tests/DbTransactionTest.php
+++ vendor/hyperframework/hyperframework/test/db/tests/DbTransactionTest.php
@@ -1,12 +1,47 @@
 <?php
 namespace Hyperframework\Db;
 
+use Exception;
 use Hyperframework\Db\Test\TestCase as Base;
 
 class DbTransactionTest extends Base {
     public function testRun() {
         $this->assertFalse(DbClient::inTransaction());
         DbTransaction::run(function() {
+            $this->assertTrue(DbClient::inTransaction());
+        });
+        $this->assertFalse(DbClient::inTransaction());
+    }
+
+    public function testRollbackAutomaticly() {
+        try {
+            DbTransaction::run(function() {
+                throw new Exception;
+            });
+        } catch (Exception $e) {
+            $this->assertFalse(DbClient::inTransaction());
+        }
+    }
+
+    public function testNestedTransactionUsingSameConnection() {
+        DbTransaction::run(function() {
+            DbTransaction::run(function() {
+                $this->assertTrue(DbClient::inTransaction());
+            });
+            $this->assertTrue(DbClient::inTransaction());
+        });
+        $this->assertFalse(DbClient::inTransaction());
+    }
+
+    public function testNestedTransactionUsingDifferentConnections() {
+        DbTransaction::run(function() {
+            $defaultConnection = DbClient::getConnection();
+            DbClient::connect('backup');
+            DbTransaction::run(function() {
+                $this->assertTrue(DbClient::inTransaction());
+            });
+            $this->assertFalse(DbClient::inTransaction());
+            DbClient::setConnection($defaultConnection);
             $this->assertTrue(DbClient::inTransaction());
         });
         $this->assertFalse(DbClient::inTransaction());
