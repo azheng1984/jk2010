28f67702f24b84e04af9eb8850b38d0898924f21
Your Name <you@example.com>
2015-02-28 11:43:38 +0800

    -

--- lib/Models/Category.php
+++ lib/Models/Category.php
@@ -1,38 +1,3 @@
 <?php
-
-class Category extends DbActiveRecord {
-    public function getName() {
-        return $this->getColumn('name');
-    }
-
-    public function setName($value) {
-        if (DbClient::isConnected()) {
-            $conn = DbClient::getConnection();//c2
-        } else {
-            $conn = null;
-        }
-        $shouldConnect = false;
-        $conn = DbClient::getConnection($shouldConnect);//c2
-        //DbClient::connect('c2');
-        try {
-            DbClient::find('xx');
-        } finally {
-            DbClient::setConnection($conn);
-        }
-
-        DbClient::setConnection();
-
-        DbConnectionManager::connect('c2');
-
-        {
-            DbConnectionManager::connect('c2');
-            DbConnectionManager::closeConnection();
-            DbClient::find();
-        }
-
-        DbConnectionManager::closeConnection();
-
-        DbClient::closeConnection('c2');
-        return $this->setColumn('name', $value);
-    }
+class Category {
 }
