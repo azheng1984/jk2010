4f04c5fe6a722ef8441e67dc0c4872656904b7ca
Your Name <you@example.com>
2015-03-15 17:01:46 +0800

    -

--- vendor/hyperframework/hyperframework/test/common/tests/ConfigTest.php
+++ vendor/hyperframework/hyperframework/test/common/tests/ConfigTest.php
@@ -4,4 +4,9 @@ namespace Hyperframework\Common;
 use Hyperframework\Test\TestCase as Base;
 
 class ConfigTest extends Base {
+    public function testGet() {
+    }
+
+    public function testGetString() {
+    }
 }
