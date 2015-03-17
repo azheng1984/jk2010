3df4bc8e5738037983fd8fff4468595328d70106
Your Name <you@example.com>
2015-03-17 12:01:35 +0800

    -

--- vendor/hyperframework/hyperframework/test/cli/tests/OptionConfigParserTest.php
+++ vendor/hyperframework/hyperframework/test/cli/tests/OptionConfigParserTest.php
@@ -4,4 +4,12 @@ namespace Hyperframework\Cli;
 use Hyperframework\Cli\Test\TestCase as Base;
 
 class OptionConfigParserTest extends Base {
+    public function testParse() {
+        $result = OptionConfigParser::parse([
+            '-t' => 'description'
+        ]);
+        $config = $result['t'];
+        $this->assertSame('description', $config->getDescription());
+        $this->assertSame('t', $config->getShortName());
+    }
 }
