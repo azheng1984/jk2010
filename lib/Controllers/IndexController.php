dd827afbc01b1bd73fb94fb05817d453f08f2a11
Your Name <you@example.com>
2015-04-03 22:24:49 +0800

    -

--- lib/Controllers/IndexController.php
+++ lib/Controllers/IndexController.php
@@ -5,7 +5,6 @@ use Hyperframework\Web\Controller;
 
 class IndexController extends Controller {
     public function doShowAction() {
-        sdf;
         return ['title' => 'Hyperframework - 简单、专业的 PHP 框架'];
     }
 }
