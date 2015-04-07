9589c8fb8493b7b23ca9ce696585f863b5a0d806
Your Name <you@example.com>
2015-04-07 22:50:55 +0800

    -

--- views/manual/show.html.php
+++ views/manual/show.html.php
@@ -5,13 +5,13 @@ $this['css'] = ['/highlight-8.4/styles/github.css'];
 $this['js'] = ['/highlight-8.4/highlight.pack.js'];
 $this['js_code'] = '<script>hljs.initHighlightingOnLoad();</script>';
 $this->setBlock('content', function() {?>
+<h2><a href="/cn/manual">Hyperframework 手册</a></h2>
 <?php 
 if (isset($this['doc'])) {
     echo $this['doc'];
     return;
 }
 ?>
-<h2>Hyperframework 手册</h2>
 <h3><a href="/cn/manual/copyright">版权声明</a></h3>
 <h3><a href="/cn/manual/v1/web">Web 应用开发框架</a></h3>
 <div>description</div>
