86e686582e0a6cbb2f40ab2468d2c10fe5b78b57
Your Name <you@example.com>
2015-04-28 11:35:23 +0800

    -

--- vendor/hyperframework/hyperframework/test/db/config/init.sql
+++ vendor/hyperframework/hyperframework/test/db/config/init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `test`;
+CREATE DATABASE IF NOT EXISTS `test`;
 USE `test`;
 DROP TABLE IF EXISTS `Document`;
 CREATE TABLE `Document` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `decimal` decimal(8,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
